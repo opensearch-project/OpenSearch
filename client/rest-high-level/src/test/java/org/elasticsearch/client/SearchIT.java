171ce992e42fce22f10f8a50937805e530b244d0
Rabi Panda <adnapibar@gmail.com>
2021-02-02 15:32:05 -0800

    Fix compilation issues for tests. (#29)
    Some of the tests that use the x-pack code are failing compilation. This PR cleans up the references to fix the issue.
    
    Signed-off-by: Peter Nied <petern@amazon.com>

--- client/rest-high-level/src/test/java/org/elasticsearch/client/SearchIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/SearchIT.java
@@ -85,7 +85,6 @@ import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.search.suggest.Suggest;
 import org.elasticsearch.search.suggest.SuggestBuilder;
 import org.elasticsearch.search.suggest.phrase.PhraseSuggestionBuilder;
-import org.elasticsearch.xpack.core.index.query.PinnedQueryBuilder;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 
@@ -1385,8 +1384,6 @@ public class SearchIT extends ESRestHighLevelClientTestCase {
     public void testSearchWithBasicLicensedQuery() throws IOException {
         SearchRequest searchRequest = new SearchRequest("index");
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        PinnedQueryBuilder pinnedQuery = new PinnedQueryBuilder(new MatchAllQueryBuilder(), "2", "1");
-        searchSourceBuilder.query(pinnedQuery);
         searchRequest.source(searchSourceBuilder);
         SearchResponse searchResponse = execute(searchRequest, highLevelClient()::search, highLevelClient()::searchAsync);
         assertSearchHeader(searchResponse);
