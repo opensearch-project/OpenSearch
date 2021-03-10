2840887b514819c5910b2262b4fd3cbfe3f6398e
Harold Wang <74381974+harold-wang@users.noreply.github.com>
2021-03-09 22:14:48 -0800

    [Rename] Rename files under three server sub folders (#195)
    Refactor below three folders as part of the Elasticsearch to OpenSearch renaming effort.
    . server/src/internalClusterTest
    . server/src/main/java11
    . server/src/main/resources
    . rest-api-spec
    
    
    Signed-off-by: Harold Wang <harowang@amazon.com>

---
+++ server/src/internalClusterTest/java/org/opensearch/indices/mapping/SimpleGetMappingsIT.java
@@ -0,0 +1,150 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.opensearch.indices.mapping;
+
+import org.opensearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.opensearch.action.admin.indices.mapping.get.GetMappingsResponse;
+import org.opensearch.common.Priority;
+import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.plugins.Plugin;
+import org.opensearch.test.OpenSearchIntegTestCase;
+import org.opensearch.test.InternalSettingsPlugin;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.opensearch.cluster.metadata.IndexMetadata.INDEX_METADATA_BLOCK;
+import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_BLOCKS_METADATA;
+import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_BLOCKS_READ;
+import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_BLOCKS_WRITE;
+import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_READ_ONLY;
+import static org.opensearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertBlocked;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class SimpleGetMappingsIT extends OpenSearchIntegTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return Collections.singleton(InternalSettingsPlugin.class);
+    }
+
+    public void testGetMappingsWhereThereAreNone() {
+        createIndex("index");
+        GetMappingsResponse response = client().admin().indices().prepareGetMappings().execute().actionGet();
+        assertThat(response.mappings().containsKey("index"), equalTo(true));
+        assertThat(response.mappings().get("index").size(), equalTo(0));
+    }
+
+    private XContentBuilder getMappingForType(String type) throws IOException {
+        return jsonBuilder().startObject().startObject(type).startObject("properties")
+                .startObject("field1").field("type", "text").endObject()
+                .endObject().endObject().endObject();
+    }
+
+    public void testSimpleGetMappings() throws Exception {
+        client().admin().indices().prepareCreate("indexa")
+                .addMapping("typeA", getMappingForType("typeA"))
+                .execute().actionGet();
+        client().admin().indices().prepareCreate("indexb")
+                .addMapping("typeA", getMappingForType("typeA"))
+                .execute().actionGet();
+
+        ClusterHealthResponse clusterHealth = client().admin().cluster().prepareHealth()
+            .setWaitForEvents(Priority.LANGUID).setWaitForGreenStatus().execute().actionGet();
+        assertThat(clusterHealth.isTimedOut(), equalTo(false));
+
+        // Get all mappings
+        GetMappingsResponse response = client().admin().indices().prepareGetMappings().execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(2));
+        assertThat(response.mappings().get("indexa").size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").get("typeA"), notNullValue());
+        assertThat(response.mappings().get("indexb").size(), equalTo(1));
+        assertThat(response.mappings().get("indexb").get("typeA"), notNullValue());
+
+        // Get all mappings, via wildcard support
+        response = client().admin().indices().prepareGetMappings("*").setTypes("*").execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(2));
+        assertThat(response.mappings().get("indexa").size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").get("typeA"), notNullValue());
+        assertThat(response.mappings().get("indexb").size(), equalTo(1));
+        assertThat(response.mappings().get("indexb").get("typeA"), notNullValue());
+
+        // Get all typeA mappings in all indices
+        response = client().admin().indices().prepareGetMappings("*").setTypes("typeA").execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(2));
+        assertThat(response.mappings().get("indexa").size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").get("typeA"), notNullValue());
+        assertThat(response.mappings().get("indexb").size(), equalTo(1));
+        assertThat(response.mappings().get("indexb").get("typeA"), notNullValue());
+
+        // Get all mappings in indexa
+        response = client().admin().indices().prepareGetMappings("indexa").execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").get("typeA"), notNullValue());
+
+        // Get all mappings beginning with A* in indexa
+        response = client().admin().indices().prepareGetMappings("indexa").setTypes("*A").execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").get("typeA"), notNullValue());
+
+        // Get all mappings beginning with B* in all indices
+        response = client().admin().indices().prepareGetMappings().setTypes("B*").execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(0));
+
+        // Get all mappings beginning with B* and A* in all indices
+        response = client().admin().indices().prepareGetMappings().setTypes("B*", "*A").execute().actionGet();
+        assertThat(response.mappings().size(), equalTo(2));
+        assertThat(response.mappings().get("indexa").size(), equalTo(1));
+        assertThat(response.mappings().get("indexa").get("typeA"), notNullValue());
+        assertThat(response.mappings().get("indexb").size(), equalTo(1));
+        assertThat(response.mappings().get("indexb").get("typeA"), notNullValue());
+    }
+
+    public void testGetMappingsWithBlocks() throws IOException {
+        client().admin().indices().prepareCreate("test")
+                .addMapping("_doc", getMappingForType("_doc"))
+                .execute().actionGet();
+        ensureGreen();
+
+        for (String block : Arrays.asList(SETTING_BLOCKS_READ, SETTING_BLOCKS_WRITE, SETTING_READ_ONLY)) {
+            try {
+                enableIndexBlock("test", block);
+                GetMappingsResponse response = client().admin().indices().prepareGetMappings().execute().actionGet();
+                assertThat(response.mappings().size(), equalTo(1));
+                assertThat(response.mappings().get("test").size(), equalTo(1));
+            } finally {
+                disableIndexBlock("test", block);
+            }
+        }
+
+        try {
+            enableIndexBlock("test", SETTING_BLOCKS_METADATA);
+            assertBlocked(client().admin().indices().prepareGetMappings(), INDEX_METADATA_BLOCK);
+        } finally {
+            disableIndexBlock("test", SETTING_BLOCKS_METADATA);
+        }
+    }
+}
---
+++ server/src/internalClusterTest/java/org/opensearch/search/StressSearchServiceReaperIT.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.opensearch.search;
+
+import org.apache.lucene.util.English;
+import org.opensearch.action.index.IndexRequestBuilder;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.unit.TimeValue;
+import org.opensearch.test.OpenSearchIntegTestCase;
+import org.opensearch.test.OpenSearchIntegTestCase.ClusterScope;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.opensearch.index.query.QueryBuilders.matchAllQuery;
+import static org.opensearch.test.OpenSearchIntegTestCase.Scope.SUITE;
+import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertHitCount;
+import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertNoFailures;
+
+@ClusterScope(scope = SUITE)
+public class StressSearchServiceReaperIT extends OpenSearchIntegTestCase {
+
+    @Override
+    protected Settings nodeSettings(int nodeOrdinal) {
+        // very frequent checks
+        return Settings.builder().put(super.nodeSettings(nodeOrdinal))
+                .put(SearchService.KEEPALIVE_INTERVAL_SETTING.getKey(), TimeValue.timeValueMillis(1)).build();
+    }
+
+    // see issue #5165 - this test fails each time without the fix in pull #5170
+    public void testStressReaper() throws ExecutionException, InterruptedException {
+        int num = randomIntBetween(100, 150);
+        IndexRequestBuilder[] builders = new IndexRequestBuilder[num];
+        for (int i = 0; i < builders.length; i++) {
+            builders[i] = client().prepareIndex("test", "type", "" + i).setSource("f", English.intToEnglish(i));
+        }
+        createIndex("test");
+        indexRandom(true, builders);
+        final int iterations = scaledRandomIntBetween(500, 1000);
+        for (int i = 0; i < iterations; i++) {
+            SearchResponse searchResponse = client().prepareSearch("test").setQuery(matchAllQuery()).setSize(num).get();
+            assertNoFailures(searchResponse);
+            assertHitCount(searchResponse, num);
+        }
+    }
+}
