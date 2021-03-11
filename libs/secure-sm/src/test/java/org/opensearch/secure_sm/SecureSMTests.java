77a5919428a3d8f49aba9b58d86009ab27953805
Rabi Panda <adnapibar@gmail.com>
2021-03-11 12:11:53 -0800

    [Rename] refactor libs/plugin-cli and libs/secure-sm. (#261)
    Refactor the libs/plugin-cli and libs/secure-sm modules to rename the package names
    
    - `org.elasticsearch.plugins` to `org.opensearch.plugins`
    - `org.elasticsearch.secure_sm` to `org.opensearch.secure_sm`
    
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- libs/secure-sm/src/test/java/org/opensearch/secure_sm/SecureSMTests.java
+++ libs/secure-sm/src/test/java/org/opensearch/secure_sm/SecureSMTests.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.elasticsearch.secure_sm;
+package org.opensearch.secure_sm;
 
 import junit.framework.TestCase;
 
@@ -60,8 +60,8 @@ public class SecureSMTests extends TestCase {
         assertTrue(SecureSM.classCanExit("com.carrotsearch.ant.tasks.junit4.slave.JvmExit", SecureSM.TEST_RUNNER_PACKAGES));
         assertTrue(SecureSM.classCanExit("org.eclipse.jdt.internal.junit.runner.RemoteTestRunner", SecureSM.TEST_RUNNER_PACKAGES));
         assertTrue(SecureSM.classCanExit("com.intellij.rt.execution.junit.JUnitStarter", SecureSM.TEST_RUNNER_PACKAGES));
-        assertTrue(SecureSM.classCanExit("org.elasticsearch.Foo", new String[]{"org.elasticsearch.Foo"}));
-        assertFalse(SecureSM.classCanExit("org.elasticsearch.Foo", new String[]{"org.elasticsearch.Bar"}));
+        assertTrue(SecureSM.classCanExit("org.opensearch.Foo", new String[]{"org.opensearch.Foo"}));
+        assertFalse(SecureSM.classCanExit("org.opensearch.Foo", new String[]{"org.opensearch.Bar"}));
     }
 
     public void testCreateThread() throws Exception {
