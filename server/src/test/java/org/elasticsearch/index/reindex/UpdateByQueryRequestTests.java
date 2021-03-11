f27f5acd0b39a3fde7cb446cf35e3a9ab76db77c
Harold Wang <74381974+harold-wang@users.noreply.github.com>
2021-03-10 21:06:21 -0800

    [Rename] plugins (#193)
    * [Rename] plugins (#193)
    
    This PR refactors files under "plugins" folders part of the Elasticsearch to OpenSearch renaming effort.
    
    Signed-off-by: Harold Wang <harowang@amazon.com>

--- server/src/test/java/org/elasticsearch/index/reindex/UpdateByQueryRequestTests.java
+++ server/src/test/java/org/elasticsearch/index/reindex/UpdateByQueryRequestTests.java
@@ -81,7 +81,7 @@ public class UpdateByQueryRequestTests extends AbstractBulkByScrollRequestTestCa
         assertEquals(original.getPipeline(), forSliced.getPipeline());
     }
 
-    // TODO: Implement standard to/from x-content parsing tests
+    plugins/transport-nio/src/internalClusterTest/java/org/opensearch/NioIntegTestCase.java
 
     @Override
     protected UpdateByQueryRequest createTestInstance() {
