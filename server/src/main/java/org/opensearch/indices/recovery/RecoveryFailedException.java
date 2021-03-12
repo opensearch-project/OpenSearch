e022ede91b0732dfb0b1f6fb0e4c35550e4f03bd
Harold Wang <harowang@amazon.com>
2021-03-11 20:14:01 -0800

    Rename http and indices folder under server module
    Signed-off-by: Harold Wang <harowang@amazon.com>

---
+++ server/src/main/java/org/opensearch/indices/recovery/RecoveryFailedException.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.opensearch.indices.recovery;
+
+import org.opensearch.OpenSearchException;
+import org.opensearch.cluster.node.DiscoveryNode;
+import org.opensearch.common.Nullable;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.index.shard.ShardId;
+
+import java.io.IOException;
+
+public class RecoveryFailedException extends OpenSearchException {
+
+    public RecoveryFailedException(StartRecoveryRequest request, Throwable cause) {
+        this(request, null, cause);
+    }
+
+    public RecoveryFailedException(StartRecoveryRequest request, @Nullable String extraInfo, Throwable cause) {
+        this(request.shardId(), request.sourceNode(), request.targetNode(), extraInfo, cause);
+    }
+
+    public RecoveryFailedException(RecoveryState state, @Nullable String extraInfo, Throwable cause) {
+        this(state.getShardId(), state.getSourceNode(), state.getTargetNode(), extraInfo, cause);
+    }
+
+    public RecoveryFailedException(ShardId shardId,
+                                   DiscoveryNode sourceNode,
+                                   DiscoveryNode targetNode,
+                                   @Nullable String extraInfo,
+                                   Throwable cause) {
+        super(shardId + ": Recovery failed " + (sourceNode != null ? "from " + sourceNode + " into " : "on ") +
+                  targetNode + (extraInfo == null ? "" : " (" + extraInfo + ")"), cause);
+    }
+
+    public RecoveryFailedException(StreamInput in) throws IOException {
+        super(in);
+    }
+}
