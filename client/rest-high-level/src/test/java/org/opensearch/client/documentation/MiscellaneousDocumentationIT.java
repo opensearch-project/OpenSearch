71f4b8110034bfdb210d8786ba7bf4006279d96d
Rabi Panda <adnapibar@gmail.com>
2021-03-15 17:37:05 -0700

    [Rename] refactor client/rest-high-level module. (#312)
    rename `org.elasticsearch.client` to `org.opensearch.client` in package names and references.
    
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

---
+++ client/rest-high-level/src/test/java/org/opensearch/client/documentation/MiscellaneousDocumentationIT.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.opensearch.client.documentation;
+
+import org.apache.http.HttpHost;
+import org.opensearch.client.OpenSearchRestHighLevelClientTestCase;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.core.MainResponse;
+
+import java.io.IOException;
+
+/**
+ * Documentation for miscellaneous APIs in the high level java client.
+ * Code wrapped in {@code tag} and {@code end} tags is included in the docs.
+ */
+public class MiscellaneousDocumentationIT extends OpenSearchRestHighLevelClientTestCase {
+
+    public void testMain() throws IOException {
+        RestHighLevelClient client = highLevelClient();
+        {
+            //tag::main-execute
+            MainResponse response = client.info(RequestOptions.DEFAULT);
+            //end::main-execute
+            //tag::main-response
+            String clusterName = response.getClusterName();
+            String clusterUuid = response.getClusterUuid();
+            String nodeName = response.getNodeName();
+            MainResponse.Version version = response.getVersion();
+            String buildDate = version.getBuildDate();
+            String buildHash = version.getBuildHash();
+            String buildType = version.getBuildType();
+            String luceneVersion = version.getLuceneVersion();
+            String minimumIndexCompatibilityVersion= version.getMinimumIndexCompatibilityVersion();
+            String minimumWireCompatibilityVersion = version.getMinimumWireCompatibilityVersion();
+            String number = version.getNumber();
+            //end::main-response
+            assertNotNull(clusterName);
+            assertNotNull(clusterUuid);
+            assertNotNull(nodeName);
+            assertNotNull(version);
+            assertNotNull(buildDate);
+            assertNotNull(buildHash);
+            assertNotNull(buildType);
+            assertNotNull(luceneVersion);
+            assertNotNull(minimumIndexCompatibilityVersion);
+            assertNotNull(minimumWireCompatibilityVersion);
+            assertNotNull(number);
+        }
+    }
+
+    public void testPing() throws IOException {
+        RestHighLevelClient client = highLevelClient();
+        //tag::ping-execute
+        boolean response = client.ping(RequestOptions.DEFAULT);
+        //end::ping-execute
+        assertTrue(response);
+    }
+
+    public void testInitializationFromClientBuilder() throws IOException {
+        //tag::rest-high-level-client-init
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+        //end::rest-high-level-client-init
+
+        //tag::rest-high-level-client-close
+        client.close();
+        //end::rest-high-level-client-close
+    }
+}
