50d4b585ba901e6fd119ad0538ce004d75addbdf
Rabi Panda <adnapibar@gmail.com>
2021-03-16 01:53:48 -0700

    [Rename] refactor the distribution module. (#316)
    Refactor the `distribution` module to rename elasticsearch to opensearch. The commit doesn't rename the files in the `src/bin` directory as git doesn't detect `git mv`, so that change will be done in a subsequent commit.
    
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- distribution/src/bin/elasticsearch-keystore.bat
+++ distribution/src/bin/elasticsearch-keystore.bat
@@ -3,9 +3,9 @@
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
-set ES_MAIN_CLASS=org.elasticsearch.common.settings.KeyStoreCli
-set ES_ADDITIONAL_CLASSPATH_DIRECTORIES=lib/tools/keystore-cli
-call "%~dp0elasticsearch-cli.bat" ^
+set OPENSEARCH_MAIN_CLASS=org.opensearch.common.settings.KeyStoreCli
+set OPENSEARCH_ADDITIONAL_CLASSPATH_DIRECTORIES=lib/tools/keystore-cli
+call "%~dp0opensearch-cli.bat" ^
   %%* ^
   || goto exit
 
