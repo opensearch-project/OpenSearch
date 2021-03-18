b4d4774eee776e9847242bf1b7b6fbaba0986b47
Rabi Panda <adnapibar@gmail.com>
2021-03-18 06:35:27 -0700

    [Rename] Fix imports in the distribution module. (#381)
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- distribution/tools/keystore-cli/src/test/java/org/opensearch/common/settings/UpgradeKeyStoreCommandTests.java
+++ distribution/tools/keystore-cli/src/test/java/org/opensearch/common/settings/UpgradeKeyStoreCommandTests.java
@@ -21,7 +21,7 @@ package org.opensearch.common.settings;
 
 import org.opensearch.cli.Command;
 import org.opensearch.cli.UserException;
-import org.elasticsearch.env.Environment;
+import org.opensearch.env.Environment;
 
 import java.io.InputStream;
 import java.io.OutputStream;
