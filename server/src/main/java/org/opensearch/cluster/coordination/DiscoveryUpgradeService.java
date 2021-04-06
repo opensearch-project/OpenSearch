ab957549f285ac0354b58afe2377e155d0f84876
Rabi Panda <adnapibar@gmail.com>
2021-04-06 14:41:00 -0700

    Remove unused import.
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- server/src/main/java/org/opensearch/cluster/coordination/DiscoveryUpgradeService.java
+++ server/src/main/java/org/opensearch/cluster/coordination/DiscoveryUpgradeService.java
@@ -21,7 +21,6 @@ package org.opensearch.cluster.coordination;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
-import org.opensearch.LegacyESVersion;
 import org.opensearch.Version;
 import org.opensearch.action.ActionListener;
 import org.opensearch.cluster.ClusterName;
