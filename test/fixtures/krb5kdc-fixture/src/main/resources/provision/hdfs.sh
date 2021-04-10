2a3ce0bb7550e37079029e43f1a92e1f17f0d147
Rabi Panda <adnapibar@gmail.com>
2021-04-09 17:51:27 -0700

    Fix rename issues and failing repository-hdfs tests. (#518)
    This commit fixes some partial rename issues and as a result fixes the failing secure repository-hdfs tests.
    
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- test/fixtures/krb5kdc-fixture/src/main/resources/provision/hdfs.sh
+++ test/fixtures/krb5kdc-fixture/src/main/resources/provision/hdfs.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+#
+# Modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+#
 
 set -e
 
 addprinc.sh "opensearch"
 #TODO(OpenSearch): fix username
-addprinc.sh "hdfs/hdfs.build.opensearch.co"
+addprinc.sh "hdfs/hdfs.build.opensearch.org"
 
 # Use this as a signal that setup is complete
 python3 -m http.server 4444 &
