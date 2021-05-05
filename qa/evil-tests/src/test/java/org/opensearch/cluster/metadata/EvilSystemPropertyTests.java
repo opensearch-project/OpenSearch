41e0ff9d6d030e554407c8cde64d4267e631c746
arunabh23 <singharunabh18@gmail.com>
2021-05-06 00:53:49 +0530

    Make default number of shards configurable (#625)
    * Make default number of shards configurable
    
    The default number of primary shards for a new index, when the number of shards are not provided in the request, can be configured for the cluster.
    
    Signed-off-by: Arunabh Singh <arunabs@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Arunabh Singh <arunabs@amazon.com>
    
    Co-authored-by: Arunabh Singh <arunabs@amazon.com>

--- qa/evil-tests/src/test/java/org/opensearch/cluster/metadata/EvilSystemPropertyTests.java
+++ qa/evil-tests/src/test/java/org/opensearch/cluster/metadata/EvilSystemPropertyTests.java
@@ -35,26 +35,47 @@ import org.opensearch.common.SuppressForbidden;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.test.OpenSearchTestCase;
 
+import static org.opensearch.cluster.metadata.IndexMetadata.DEFAULT_NUMBER_OF_SHARDS;
+import static org.opensearch.cluster.metadata.IndexMetadata.MAX_NUMBER_OF_SHARDS;
+import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_NUMBER_OF_SHARDS;
+
 public class EvilSystemPropertyTests extends OpenSearchTestCase {
 
     @SuppressForbidden(reason = "manipulates system properties for testing")
-    public void testMaxNumShards() {
+    public void testNumShards() {
         IllegalArgumentException exception = expectThrows(IllegalArgumentException.class, () ->
             IndexMetadata.buildNumberOfShardsSetting()
-                .get(Settings.builder().put("index.number_of_shards", 1025).build()));
-        assertEquals("Failed to parse value [1025] for setting [index.number_of_shards] must be <= 1024", exception.getMessage());
+                .get(Settings.builder().put(SETTING_NUMBER_OF_SHARDS, 1025).build()));
+        assertEquals("Failed to parse value [1025] for setting [" + SETTING_NUMBER_OF_SHARDS + "] must be <= 1024", exception.getMessage());
 
-        Integer numShards = IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING.get(Settings.builder().put("index.number_of_shards", 100).build());
+        Integer numShards = IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING.get(Settings.builder().put(SETTING_NUMBER_OF_SHARDS, 100).build());
         assertEquals(100, numShards.intValue());
         int limit = randomIntBetween(1, 10);
-        System.setProperty("opensearch.index.max_number_of_shards", Integer.toString(limit));
+        System.setProperty(MAX_NUMBER_OF_SHARDS, Integer.toString(limit));
         try {
             IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () ->
                 IndexMetadata.buildNumberOfShardsSetting()
                     .get(Settings.builder().put("index.number_of_shards", 11).build()));
             assertEquals("Failed to parse value [11] for setting [index.number_of_shards] must be <= " + limit, e.getMessage());
+            System.clearProperty(MAX_NUMBER_OF_SHARDS);
+
+            Integer defaultFromSetting = IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING.getDefault(Settings.EMPTY);
+            assertEquals(1, defaultFromSetting.intValue());
+
+            int randomDefault = randomIntBetween(1, 10);
+            System.setProperty(DEFAULT_NUMBER_OF_SHARDS, Integer.toString(randomDefault));
+            defaultFromSetting = IndexMetadata.buildNumberOfShardsSetting().getDefault(Settings.EMPTY);
+            assertEquals(randomDefault, defaultFromSetting.intValue());
+
+            randomDefault = randomIntBetween(1, 10);
+            System.setProperty(MAX_NUMBER_OF_SHARDS, Integer.toString(randomDefault));
+            System.setProperty(DEFAULT_NUMBER_OF_SHARDS, Integer.toString(randomDefault + 1));
+            e = expectThrows(IllegalArgumentException.class, IndexMetadata::buildNumberOfShardsSetting);
+            assertEquals(DEFAULT_NUMBER_OF_SHARDS + " value [" + (randomDefault + 1) + "] must between " +
+                "1 and " + MAX_NUMBER_OF_SHARDS + " [" + randomDefault + "]", e.getMessage());
         } finally {
-            System.clearProperty("opensearch.index.max_number_of_shards");
+            System.clearProperty(MAX_NUMBER_OF_SHARDS);
+            System.clearProperty(DEFAULT_NUMBER_OF_SHARDS);
         }
     }
 }
