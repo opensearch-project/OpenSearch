15bf7ae2286bfeb6c6b8706abbcdd96d44f887b7
cliu123 <lc12251109@gmail.com>
2021-07-13 09:23:45 -0700

    Remove unused import
    Signed-off-by: cliu123 <lc12251109@gmail.com>

--- server/src/test/java/org/opensearch/rest/BaseRestHandlerTests.java
+++ server/src/test/java/org/opensearch/rest/BaseRestHandlerTests.java
@@ -49,7 +49,6 @@ import org.opensearch.threadpool.ThreadPool;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
