07ee481bc42eec6f33e9884cdd58f4fcc89b7cf4
Vacha <vachshah@amazon.com>
2021-07-27 09:01:10 -0700

    Validation for official plugins for upgrade tool (#973)
    Add validation to check for official plugins during the plugins installation task for the upgrade tool.
    
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- distribution/tools/upgrade-cli/src/main/java/org/opensearch/upgrade/InstallPluginsTask.java
+++ distribution/tools/upgrade-cli/src/main/java/org/opensearch/upgrade/InstallPluginsTask.java
@@ -11,14 +11,42 @@ package org.opensearch.upgrade;
 import org.opensearch.cli.Terminal;
 import org.opensearch.common.collect.Tuple;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 /**
  * Installs the list of plugins using the opensearch-plugin command.
- */
+*/
 class InstallPluginsTask implements UpgradeTask {
     private static final String ERROR_MSG = "Error installing plugin %s. Please install it manually.";
+
+    /** The list of official plugins that can be installed by the upgrade tool. */
+    static final Set<String> OFFICIAL_PLUGINS;
+    static {
+        try (
+            InputStream stream = InstallPluginsTask.class.getResourceAsStream("/plugins.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))
+        ) {
+            Set<String> plugins = new HashSet<>();
+            String line = reader.readLine();
+            while (line != null) {
+                plugins.add(line.trim());
+                line = reader.readLine();
+            }
+            OFFICIAL_PLUGINS = Collections.unmodifiableSet(plugins);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void accept(final Tuple<TaskInput, Terminal> input) {
@@ -28,25 +56,44 @@ class InstallPluginsTask implements UpgradeTask {
             return;
         }
         terminal.println("Installing core plugins ...");
-        final ProcessBuilder processBuilder = new ProcessBuilder();
+        List<String> manualPlugins = new ArrayList<>();
+
         for (String plugin : taskInput.getPlugins()) {
-            // TODO - validate if plugin is an official plugin for OpenSearch.
-            final String command = taskInput.getOpenSearchBin().resolve("opensearch-plugin") + " install " + plugin;
-            if (OS.WINDOWS == OS.current()) {
-                processBuilder.command("cmd.exe", "/c", command);
+            if (OFFICIAL_PLUGINS.contains(plugin)) {
+                executeInstallPluginCommand(plugin, taskInput, terminal);
             } else {
-                processBuilder.command("sh", "-c", command);
-            }
-            try {
-                final Process process = processBuilder.inheritIO().start();
-                if (process.waitFor() != 0) {
-                    terminal.errorPrint(Terminal.Verbosity.NORMAL, String.format(Locale.getDefault(), ERROR_MSG, plugin));
-                }
-            } catch (IOException | InterruptedException e) {
-                terminal.errorPrint(Terminal.Verbosity.NORMAL, String.format(Locale.getDefault(), ERROR_MSG, plugin) + e.getMessage());
+                manualPlugins.add(plugin);
             }
         }
+        if (!manualPlugins.isEmpty()) {
+            terminal.println("Please install the following custom plugins manually: " + manualPlugins);
+        }
         terminal.println("Success!" + System.lineSeparator());
+    }
+
+    // package private for unit testing
+    void executeInstallPluginCommand(String plugin, TaskInput taskInput, Terminal terminal) {
+        ProcessBuilder processBuilder = getProcessBuilderBasedOnOS(plugin, taskInput);
+        try {
+            final Process process = processBuilder.inheritIO().start();
+            if (process.waitFor() != 0) {
+                terminal.errorPrint(Terminal.Verbosity.NORMAL, String.format(Locale.getDefault(), ERROR_MSG, plugin));
+            }
+        } catch (IOException | InterruptedException e) {
+            terminal.errorPrint(Terminal.Verbosity.NORMAL, String.format(Locale.getDefault(), ERROR_MSG, plugin) + e.getMessage());
+        }
+    }
+
+    // package private for unit testing
+    ProcessBuilder getProcessBuilderBasedOnOS(String plugin, TaskInput taskInput) {
+        final String command = taskInput.getOpenSearchBin().resolve("opensearch-plugin") + " install " + plugin;
+        final ProcessBuilder processBuilder = new ProcessBuilder();
+        if (OS.WINDOWS == OS.current()) {
+            processBuilder.command("cmd.exe", "/c", command);
+        } else {
+            processBuilder.command("sh", "-c", command);
+        }
+        return processBuilder;
     }
 
     private enum OS {
