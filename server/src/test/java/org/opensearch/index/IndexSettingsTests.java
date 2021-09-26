8b94a193f62f5d8e904f228892a86074e4a35d79
Nick Knize <nknize@apache.org>
2021-09-25 23:04:26 -0500

    [Revert] Translog Pruning Setting Deprecation Removal (#1294)
    INDEX_PLUGINS_REPLICATION_TRANSLOG_RETENTION_LEASE_PRUNING_ENABLED_SETTING was
    deprecated, then deprecation was removed. This adds deprecation back so that the
    setting can be moved to the plugin in the next minor release.
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/test/java/org/opensearch/index/IndexSettingsTests.java
+++ server/src/test/java/org/opensearch/index/IndexSettingsTests.java
@@ -673,6 +673,9 @@ public class IndexSettingsTests extends OpenSearchTestCase {
             assertFalse(indexSettings.shouldPruneTranslogByRetentionLease());
             assertThat(indexSettings.getTranslogRetentionSize().getBytes(), equalTo(-1L));
         }
+        assertWarnings("[index.plugins.replication.translog.retention_lease.pruning.enabled] setting " +
+            "was deprecated in OpenSearch and will be removed in a future release! See the breaking changes documentation " +
+            "for the next major version.");
     }
 
     public void testTranslogPruningSettingsWithSoftDeletesDisabled() {
@@ -687,5 +690,8 @@ public class IndexSettingsTests extends OpenSearchTestCase {
         IndexSettings indexSettings = new IndexSettings(metadata, Settings.EMPTY);
         assertFalse(indexSettings.shouldPruneTranslogByRetentionLease());
         assertThat(indexSettings.getTranslogRetentionSize().getBytes(), equalTo(retentionSize.getBytes()));
+        assertWarnings("[index.plugins.replication.translog.retention_lease.pruning.enabled] setting " +
+            "was deprecated in OpenSearch and will be removed in a future release! See the breaking changes documentation " +
+            "for the next major version.");
     }
 }
