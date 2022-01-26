447b20c457308072d48862774988a85090221562
Saurabh Singh <getsaurabh02@gmail.com>
2022-01-26 13:06:01 -0800

    Delay the request size calculation until required by the indexing pressure framework (#1592)
    Logically delay the request size calculation until it's absolutely required by the 
    indexing pressure framework.
    
    Signed-off-by: Saurabh Singh <sisurab@amazon.com>

--- server/src/main/java/org/opensearch/index/IndexingPressureService.java
+++ server/src/main/java/org/opensearch/index/IndexingPressureService.java
@@ -13,6 +13,8 @@ import org.opensearch.index.shard.ShardId;
 import org.opensearch.index.stats.IndexingPressureStats;
 import org.opensearch.index.stats.ShardIndexingPressureStats;
 
+import java.util.function.LongSupplier;
+
 /**
  * Sets up classes for node/shard level indexing pressure.
  * Provides abstraction and orchestration for indexing pressure interfaces when called from Transport Actions or for Stats.
@@ -25,22 +27,48 @@ public class IndexingPressureService {
         shardIndexingPressure = new ShardIndexingPressure(settings, clusterService);
     }
 
-    public Releasable markCoordinatingOperationStarted(long bytes, boolean forceExecution) {
+    /**
+     * Marks the beginning of coordinating operation for an indexing request on the node. Rejects the operation if node's
+     * memory limit is breached.
+     * Performs the node level accounting only if shard indexing pressure is disabled. Else empty releasable is returned.
+     * @param bytes memory bytes to be tracked for the current operation
+     * @param forceExecution permits operation even if the node level memory limit is breached
+     * @return Releasable to mark the completion of operation and release the accounted bytes
+     */
+    public Releasable markCoordinatingOperationStarted(LongSupplier bytes, boolean forceExecution) {
         if (isShardIndexingPressureEnabled() == false) {
-            return shardIndexingPressure.markCoordinatingOperationStarted(bytes, forceExecution);
+            return shardIndexingPressure.markCoordinatingOperationStarted(bytes.getAsLong(), forceExecution);
         } else {
             return () -> {};
         }
     }
 
-    public Releasable markCoordinatingOperationStarted(ShardId shardId, long bytes, boolean forceExecution) {
+    /**
+     * Marks the beginning of coordinating operation for an indexing request on the Shard. Rejects the operation if shard's
+     * memory limit is breached.
+     * Performs the shard level accounting only if shard indexing pressure is enabled. Else empty releasable is returned.
+     * @param shardId Shard ID for which the current indexing operation is targeted for
+     * @param bytes memory bytes to be tracked for the current operation
+     * @param forceExecution permits operation even if the node level memory limit is breached
+     * @return Releasable to mark the completion of operation and release the accounted bytes
+     */
+    public Releasable markCoordinatingOperationStarted(ShardId shardId, LongSupplier bytes, boolean forceExecution) {
         if (isShardIndexingPressureEnabled()) {
-            return shardIndexingPressure.markCoordinatingOperationStarted(shardId, bytes, forceExecution);
+            return shardIndexingPressure.markCoordinatingOperationStarted(shardId, bytes.getAsLong(), forceExecution);
         } else {
             return () -> {};
         }
     }
 
+    /**
+     * Marks the beginning of primary operation for an indexing request. Rejects the operation if memory limit is breached.
+     * Performs the node level accounting only if shard indexing pressure is not disabled. Else shard level accounting
+     * is performed.
+     * @param shardId Shard ID for which the current indexing operation is targeted for
+     * @param bytes memory bytes to be tracked for the current operation
+     * @param forceExecution permits operation even if the memory limit is breached
+     * @return Releasable to mark the completion of operation and release the accounted bytes
+     */
     public Releasable markPrimaryOperationStarted(ShardId shardId, long bytes, boolean forceExecution) {
         if (isShardIndexingPressureEnabled()) {
             return shardIndexingPressure.markPrimaryOperationStarted(shardId, bytes, forceExecution);
@@ -49,6 +77,15 @@ public class IndexingPressureService {
         }
     }
 
+    /**
+     * Marks the beginning of primary operation for an indexing request, when primary shard is local to the coordinator node.
+     * Rejects the operation if memory limit is breached.
+     * Performs the node level accounting only if shard indexing pressure is not disabled. Else shard level accounting
+     * is performed.
+     * @param shardId Shard ID for which the current indexing operation is targeted for
+     * @param bytes memory bytes to be tracked for the current operation
+     * @return Releasable to mark the completion of operation and release the accounted bytes
+     */
     public Releasable markPrimaryOperationLocalToCoordinatingNodeStarted(ShardId shardId, long bytes) {
         if (isShardIndexingPressureEnabled()) {
             return shardIndexingPressure.markPrimaryOperationLocalToCoordinatingNodeStarted(shardId, bytes);
@@ -57,6 +94,15 @@ public class IndexingPressureService {
         }
     }
 
+    /**
+     * Marks the beginning of replication operation for an indexing request. Rejects the operation if memory limit is breached.
+     * Performs the node level accounting only if shard indexing pressure is not disabled. Else shard level accounting
+     * is performed.
+     * @param shardId Shard ID for which the current indexing operation is targeted for
+     * @param bytes memory bytes to be tracked for the current operation
+     * @param forceExecution permits operation even if the memory limit is breached
+     * @return Releasable to mark the completion of operation and release the accounted bytes
+     */
     public Releasable markReplicaOperationStarted(ShardId shardId, long bytes, boolean forceExecution) {
         if (isShardIndexingPressureEnabled()) {
             return shardIndexingPressure.markReplicaOperationStarted(shardId, bytes, forceExecution);
