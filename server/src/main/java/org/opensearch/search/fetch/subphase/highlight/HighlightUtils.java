5408ed363e84fb4168c366bb705921df057c3de6
Nicholas Walter Knize <nknize@apache.org>
2022-02-01 14:12:22 -0600

    spotlessApply
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/main/java/org/opensearch/search/fetch/subphase/highlight/HighlightUtils.java
+++ server/src/main/java/org/opensearch/search/fetch/subphase/highlight/HighlightUtils.java
@@ -43,7 +43,6 @@ import org.opensearch.search.fetch.FetchSubPhase;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static java.util.Collections.singleton;
 
