ee9e5ba57964e017783356e3928370ad9f279437
Nicholas Walter Knize <nknize@apache.org>
2022-02-04 18:21:51 -0600

    Add Version.V_1_2_5 constant
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/main/java/org/opensearch/Version.java
+++ server/src/main/java/org/opensearch/Version.java
@@ -77,6 +77,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_1_2_2 = new Version(1020299, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_2_3 = new Version(1020399, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_2_4 = new Version(1020499, org.apache.lucene.util.Version.LUCENE_8_10_1);
+    public static final Version V_1_2_5 = new Version(1020599, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_3_0 = new Version(1030099, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_2_0_0 = new Version(2000099, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version CURRENT = V_2_0_0;
