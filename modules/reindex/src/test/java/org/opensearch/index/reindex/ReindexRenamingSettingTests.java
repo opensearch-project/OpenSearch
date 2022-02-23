43ce60b4937d2432d24dae62206f7033983e10f8
Tianli Feng <ftianli@amazon.com>
2022-02-22 17:22:44 -0800

    Adjust format by spotlessApply task
    Signed-off-by: Tianli Feng <ftianli@amazon.com>

--- modules/reindex/src/test/java/org/opensearch/index/reindex/ReindexRenamingSettingTests.java
+++ modules/reindex/src/test/java/org/opensearch/index/reindex/ReindexRenamingSettingTests.java
@@ -17,37 +17,35 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * A unit test to validate the former name of the setting 'reindex.remote.allowlist' still take effect, 
+ * A unit test to validate the former name of the setting 'reindex.remote.allowlist' still take effect,
  * after it is deprecated, so that the backwards compatibility is maintained.
  * The test can be removed along with removing support of the deprecated setting.
  */
 public class ReindexRenamingSettingTests extends OpenSearchTestCase {
     ReindexPlugin plugin;
-    
+
     @Before
     public void setup() {
         this.plugin = new ReindexPlugin();
     }
-    
+
     public void testReindexSettingsExist() {
         List<Setting<?>> settings = plugin.getSettings();
-        assertTrue("Both 'reindex.remote.allowlist' and its predecessor should exist in settings of Reindex plugin",
-                settings.containsAll(
-                        Arrays.asList(
-                                TransportReindexAction.REMOTE_CLUSTER_WHITELIST,
-                                TransportReindexAction.REMOTE_CLUSTER_ALLOWLIST
-                        )
-                )
+        assertTrue(
+            "Both 'reindex.remote.allowlist' and its predecessor should exist in settings of Reindex plugin",
+            settings.containsAll(
+                Arrays.asList(TransportReindexAction.REMOTE_CLUSTER_WHITELIST, TransportReindexAction.REMOTE_CLUSTER_ALLOWLIST)
+            )
         );
     }
 
     public void testSettingFallback() {
         assertEquals(
-                TransportReindexAction.REMOTE_CLUSTER_ALLOWLIST.get(Settings.EMPTY),
-                TransportReindexAction.REMOTE_CLUSTER_WHITELIST.get(Settings.EMPTY)
+            TransportReindexAction.REMOTE_CLUSTER_ALLOWLIST.get(Settings.EMPTY),
+            TransportReindexAction.REMOTE_CLUSTER_WHITELIST.get(Settings.EMPTY)
         );
     }
-    
+
     public void testSettingGetValue() {
         Settings settings = Settings.builder().put("reindex.remote.allowlist", "127.0.0.1:*").build();
         assertEquals(TransportReindexAction.REMOTE_CLUSTER_ALLOWLIST.get(settings), Arrays.asList("127.0.0.1:*"));
@@ -57,6 +55,6 @@ public class ReindexRenamingSettingTests extends OpenSearchTestCase {
     public void testSettingGetValueWithFallback() {
         Settings settings = Settings.builder().put("reindex.remote.whitelist", "127.0.0.1:*").build();
         assertEquals(TransportReindexAction.REMOTE_CLUSTER_ALLOWLIST.get(settings), Arrays.asList("127.0.0.1:*"));
-        assertSettingDeprecationsAndWarnings(new Setting<?>[]{TransportReindexAction.REMOTE_CLUSTER_WHITELIST});
+        assertSettingDeprecationsAndWarnings(new Setting<?>[] { TransportReindexAction.REMOTE_CLUSTER_WHITELIST });
     }
 }
