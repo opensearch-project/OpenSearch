cfa81f996ec85a6eefb0a1efef62e500cf3a4c9b
Vacha Shah <vachshah@amazon.com>
2022-03-04 07:06:11 -0800

    Fixing soft deletes deprecation warning (#2339)
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- server/src/main/java/org/opensearch/cluster/metadata/MetadataCreateIndexService.java
+++ server/src/main/java/org/opensearch/cluster/metadata/MetadataCreateIndexService.java
@@ -931,7 +931,7 @@ public class MetadataCreateIndexService {
         shardLimitValidator.validateShardLimit(indexSettings, currentState);
         if (indexSettings.getAsBoolean(IndexSettings.INDEX_SOFT_DELETES_SETTING.getKey(), true) == false) {
             DEPRECATION_LOGGER.deprecate(
-                "soft_deletes_disabled",
+                request.index() + "soft_deletes_disabled",
                 "Creating indices with soft-deletes disabled is deprecated and will be removed in future OpenSearch versions. "
                     + "Please do not specify value for setting [index.soft_deletes.enabled] of index ["
                     + request.index()
