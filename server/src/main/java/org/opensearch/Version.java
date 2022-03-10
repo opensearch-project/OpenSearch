294ed3a05b36ac400aa2be661aff7e73071e3163
Suraj Singh <surajrider@gmail.com>
2022-03-09 16:24:01 -0800

    [Version] Increment 1.x to 1.4
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/main/java/org/opensearch/Version.java
+++ server/src/main/java/org/opensearch/Version.java
@@ -81,7 +81,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_1_2_4 = new Version(1020499, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_2_5 = new Version(1020599, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_3_0 = new Version(1030099, org.apache.lucene.util.Version.LUCENE_8_10_1);
-    public static final Version CURRENT = V_1_3_0;
+    public static final Version V_1_4_0 = new Version(1040099, org.apache.lucene.util.Version.LUCENE_8_10_1);
+    public static final Version CURRENT = V_1_4_0;
 
     public static Version readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());
