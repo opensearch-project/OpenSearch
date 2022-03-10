085f07fa5a7bb8f8dac09c9c09e24f11de2f03b5
Tianli Feng <ftianli@amazon.com>
2022-03-09 17:20:38 -0800

    add hack in setAdditionalRoles() to allow CLUSTER_MANAGER_ROLE having same abbr name with MASTER_ROLE
    Signed-off-by: Tianli Feng <ftianli@amazon.com>

--- server/src/main/java/org/opensearch/cluster/node/DiscoveryNode.java
+++ server/src/main/java/org/opensearch/cluster/node/DiscoveryNode.java
@@ -577,9 +577,13 @@ public class DiscoveryNode implements Writeable, ToXContentFragment {
 
     public static void setAdditionalRoles(final Set<DiscoveryNodeRole> additionalRoles) {
         assert additionalRoles.stream().allMatch(r -> r.legacySetting() == null || r.legacySetting().isDeprecated()) : additionalRoles;
-        final Map<String, DiscoveryNodeRole> roleNameToPossibleRoles = rolesToMap(
-            Stream.concat(DiscoveryNodeRole.BUILT_IN_ROLES.stream(), additionalRoles.stream())
+        // TODO: Remove 'new HashMap<>()' and only keep 'rolesToMap(...)' after removing the MASTER_ROLE.
+        //  It's used to allow CLUSTER_MANAGER_ROLE that introduced in 2.0, having the same abbreviation name with MASTER_ROLE.
+        final Map<String, DiscoveryNodeRole> roleNameToPossibleRoles = new HashMap<>(rolesToMap(
+            Stream.concat(DiscoveryNodeRole.BUILT_IN_ROLES.stream(), additionalRoles.stream()))
         );
+        // TODO: Remove this line after removing the MASTER_ROLE.
+        roleNameToPossibleRoles.remove(DiscoveryNodeRole.MASTER_ROLE.roleName());
         // collect the abbreviation names into a map to ensure that there are not any duplicate abbreviations
         final Map<String, DiscoveryNodeRole> roleNameAbbreviationToPossibleRoles = Collections.unmodifiableMap(
             roleNameToPossibleRoles.values()
@@ -591,7 +595,11 @@ public class DiscoveryNode implements Writeable, ToXContentFragment {
             + "], roles by name abbreviation ["
             + roleNameAbbreviationToPossibleRoles
             + "]";
-        roleMap = roleNameToPossibleRoles;
+        // TODO: Remove this line after removing the MASTER_ROLE.
+        roleNameToPossibleRoles.put(DiscoveryNodeRole.MASTER_ROLE.roleName(), DiscoveryNodeRole.MASTER_ROLE);
+        // TODO: Remove 'Collections.unmodifiableMap()', and only keep 'roleNameToPossibleRoles', after removing the MASTER_ROLE, 
+        //  because the map roleNameToPossibleRoles is supposed to be immutable.
+        roleMap = Collections.unmodifiableMap(roleNameToPossibleRoles);
     }
 
     public static Set<String> getPossibleRoleNames() {
@@ -618,5 +626,4 @@ public class DiscoveryNode implements Writeable, ToXContentFragment {
         }
 
     }
-
 }
