09a8b744df1592fb22c55e81008ac76c4b7ff9f8
Nicholas Walter Knize <nknize@apache.org>
2022-03-14 23:12:27 -0500

    [Remove] type from TaskResults index and IndexMetadata.getMappings
    Removes types from the TaskResults internal index along with the getMappings
    method from IndexMetadata. This is needed to further remove types from
    CreateIndexRequest.
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/main/java/org/opensearch/cluster/metadata/IndexMetadata.java
+++ server/src/main/java/org/opensearch/cluster/metadata/IndexMetadata.java
@@ -661,17 +661,6 @@ public class IndexMetadata implements Diffable<IndexMetadata>, ToXContentFragmen
     }
 
     /**
-     * Return an object that maps each type to the associated mappings.
-     * The return value is never {@code null} but may be empty if the index
-     * has no mappings.
-     * @deprecated Use {@link #mapping()} instead now that indices have a single type
-     */
-    @Deprecated
-    public ImmutableOpenMap<String, MappingMetadata> getMappings() {
-        return mappings;
-    }
-
-    /**
      * Return the concrete mapping for this index or {@code null} if this index has no mappings at all.
      */
     @Nullable
@@ -1175,7 +1164,10 @@ public class IndexMetadata implements Diffable<IndexMetadata>, ToXContentFragmen
         }
 
         public Builder putMapping(MappingMetadata mappingMd) {
-            mappings.put(mappingMd.type(), mappingMd);
+            mappings.clear();
+            if (mappingMd != null) {
+                mappings.put(mappingMd.type(), mappingMd);
+            }
             return this;
         }
 
@@ -1464,23 +1456,25 @@ public class IndexMetadata implements Diffable<IndexMetadata>, ToXContentFragmen
 
             if (context != Metadata.XContentContext.API) {
                 builder.startArray(KEY_MAPPINGS);
-                for (ObjectObjectCursor<String, MappingMetadata> cursor : indexMetadata.getMappings()) {
+                MappingMetadata mmd = indexMetadata.mapping();
+                if (mmd != null) {
                     if (binary) {
-                        builder.value(cursor.value.source().compressed());
+                        builder.value(mmd.source().compressed());
                     } else {
-                        builder.map(XContentHelper.convertToMap(cursor.value.source().uncompressed(), true).v2());
+                        builder.map(XContentHelper.convertToMap(mmd.source().uncompressed(), true).v2());
                     }
                 }
                 builder.endArray();
             } else {
                 builder.startObject(KEY_MAPPINGS);
-                for (ObjectObjectCursor<String, MappingMetadata> cursor : indexMetadata.getMappings()) {
-                    Map<String, Object> mapping = XContentHelper.convertToMap(cursor.value.source().uncompressed(), false).v2();
-                    if (mapping.size() == 1 && mapping.containsKey(cursor.key)) {
+                MappingMetadata mmd = indexMetadata.mapping();
+                if (mmd != null) {
+                    Map<String, Object> mapping = XContentHelper.convertToMap(mmd.source().uncompressed(), false).v2();
+                    if (mapping.size() == 1 && mapping.containsKey(mmd.type())) {
                         // the type name is the root value, reduce it
-                        mapping = (Map<String, Object>) mapping.get(cursor.key);
+                        mapping = (Map<String, Object>) mapping.get(mmd.type());
                     }
-                    builder.field(cursor.key);
+                    builder.field(mmd.type());
                     builder.map(mapping);
                 }
                 builder.endObject();
