451a40915e78b68c6e7c2aff3768c3173e69fa7a
Nicholas Walter Knize <nknize@apache.org>
2022-03-15 14:39:15 -0500

    add spdx header to lucene copied files
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/main/java/org/apache/lucene/util/packed/XPacked64.java
+++ server/src/main/java/org/apache/lucene/util/packed/XPacked64.java
@@ -1,4 +1,12 @@
 /*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -13,6 +21,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
  */
 package org.apache.lucene.util.packed;
 
