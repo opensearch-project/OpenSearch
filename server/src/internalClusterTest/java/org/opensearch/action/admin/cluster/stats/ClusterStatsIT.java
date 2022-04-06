c57b6950104588fdd3e6acd2fd52c005e43660a9
Tianli Feng <ftianli@amazon.com>
2022-04-06 16:23:42 -0700

    Adjust format by spotlessApply task
    Signed-off-by: Tianli Feng <ftianli@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/action/admin/cluster/stats/ClusterStatsIT.java
+++ server/src/internalClusterTest/java/org/opensearch/action/admin/cluster/stats/ClusterStatsIT.java
@@ -145,25 +145,27 @@ public class ClusterStatsIT extends OpenSearchIntegTestCase {
         }
     }
 
-    // 
-    public void testNodeCountsWithDeprecatedMasterRole(){
+    // Validate assigning value "master" to setting "node.roles" can get correct count in Node Stats response after MASTER_ROLE deprecated.
+    public void testNodeCountsWithDeprecatedMasterRole() {
         int total = 1;
         Settings settings = Settings.builder()
-                .putList(
-                        NodeRoleSettings.NODE_ROLES_SETTING.getKey(), Collections.singletonList(DiscoveryNodeRole.MASTER_ROLE.roleName())
-                )
-                .build();
+            .putList(NodeRoleSettings.NODE_ROLES_SETTING.getKey(), Collections.singletonList(DiscoveryNodeRole.MASTER_ROLE.roleName()))
+            .build();
         internalCluster().startNode(settings);
         waitForNodes(total);
-        
+
         Map<String, Integer> expectedCounts = new HashMap<>();
+        expectedCounts.put(DiscoveryNodeRole.DATA_ROLE.roleName(), 0);
         expectedCounts.put(DiscoveryNodeRole.MASTER_ROLE.roleName(), 1);
         expectedCounts.put(DiscoveryNodeRole.CLUSTER_MANAGER_ROLE.roleName(), 1);
+        expectedCounts.put(DiscoveryNodeRole.INGEST_ROLE.roleName(), 0);
+        expectedCounts.put(DiscoveryNodeRole.REMOTE_CLUSTER_CLIENT_ROLE.roleName(), 0);
+        expectedCounts.put(ClusterStatsNodes.Counts.COORDINATING_ONLY, 0);
 
         ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
         assertCounts(response.getNodesStats().getCounts(), total, expectedCounts);
     }
-    
+
     private static void incrementCountForRole(String role, Map<String, Integer> counts) {
         Integer count = counts.get(role);
         if (count == null) {
