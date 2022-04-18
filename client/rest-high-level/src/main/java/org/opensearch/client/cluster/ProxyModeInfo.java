b2f2658f8d342340ce6047ccd3027aba04a88f07
Kunal Kotwani <kkotwani@amazon.com>
2022-04-18 15:50:21 -0700

    Override toString for usable logs (#2895)
    Signed-off-by: Kunal Kotwani <kkotwani@amazon.com>

--- client/rest-high-level/src/main/java/org/opensearch/client/cluster/ProxyModeInfo.java
+++ client/rest-high-level/src/main/java/org/opensearch/client/cluster/ProxyModeInfo.java
@@ -94,4 +94,20 @@ public class ProxyModeInfo implements RemoteConnectionInfo.ModeInfo {
     public int hashCode() {
         return Objects.hash(address, serverName, maxSocketConnections, numSocketsConnected);
     }
+
+    @Override
+    public String toString() {
+        return "ProxyModeInfo{"
+            + "address='"
+            + address
+            + '\''
+            + ", serverName='"
+            + serverName
+            + '\''
+            + ", maxSocketConnections="
+            + maxSocketConnections
+            + ", numSocketsConnected="
+            + numSocketsConnected
+            + '}';
+    }
 }
