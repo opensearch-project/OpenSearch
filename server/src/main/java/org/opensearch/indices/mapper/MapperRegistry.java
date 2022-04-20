b0d6ec85c5838bfea619385eaa2d4726ec08fc52
Nicholas Walter Knize <nknize@apache.org>
2022-04-20 14:14:24 -0500

    [Remove] remaining AllFieldMapper references
    AllFieldMapper was deprecated in legacy 6x. The remaining references are
    removed, along with the field mapper and corresponding tests.
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/main/java/org/opensearch/indices/mapper/MapperRegistry.java
+++ server/src/main/java/org/opensearch/indices/mapper/MapperRegistry.java
@@ -32,9 +32,7 @@
 
 package org.opensearch.indices.mapper;
 
-import org.opensearch.LegacyESVersion;
 import org.opensearch.Version;
-import org.opensearch.index.mapper.AllFieldMapper;
 import org.opensearch.index.mapper.Mapper;
 import org.opensearch.index.mapper.MetadataFieldMapper;
 import org.opensearch.plugins.MapperPlugin;
@@ -52,7 +50,6 @@ public final class MapperRegistry {
 
     private final Map<String, Mapper.TypeParser> mapperParsers;
     private final Map<String, MetadataFieldMapper.TypeParser> metadataMapperParsers;
-    private final Map<String, MetadataFieldMapper.TypeParser> metadataMapperParsers6x;
     private final Function<String, Predicate<String>> fieldFilter;
 
     public MapperRegistry(
@@ -62,11 +59,6 @@ public final class MapperRegistry {
     ) {
         this.mapperParsers = Collections.unmodifiableMap(new LinkedHashMap<>(mapperParsers));
         this.metadataMapperParsers = Collections.unmodifiableMap(new LinkedHashMap<>(metadataMapperParsers));
-        // add the _all field mapper for indices created in 6x
-        Map<String, MetadataFieldMapper.TypeParser> metadata6x = new LinkedHashMap<>();
-        metadata6x.put(AllFieldMapper.NAME, AllFieldMapper.PARSER);
-        metadata6x.putAll(metadataMapperParsers);
-        this.metadataMapperParsers6x = Collections.unmodifiableMap(metadata6x);
         this.fieldFilter = fieldFilter;
     }
 
@@ -83,7 +75,7 @@ public final class MapperRegistry {
      * returned map uses the name of the field as a key.
      */
     public Map<String, MetadataFieldMapper.TypeParser> getMetadataMapperParsers(Version indexCreatedVersion) {
-        return indexCreatedVersion.onOrAfter(LegacyESVersion.V_7_0_0) ? metadataMapperParsers : metadataMapperParsers6x;
+        return metadataMapperParsers;
     }
 
     /**
