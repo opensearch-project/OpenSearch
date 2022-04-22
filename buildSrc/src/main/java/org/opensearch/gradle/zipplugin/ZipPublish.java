9ccbf811a46e26148e157b1501d6429659995308
pgodithi <pgodithi@amazon.com>
2022-04-22 15:31:57 -0700

    ran spotlessApply
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- buildSrc/src/main/java/org/opensearch/gradle/zipplugin/ZipPublish.java
+++ buildSrc/src/main/java/org/opensearch/gradle/zipplugin/ZipPublish.java
@@ -60,7 +60,7 @@ public class ZipPublish implements Plugin<Project> {
         this.project = project;
         project.getExtensions().create(EXTENSION_NAME, ZipPublishExtension.class);
         project.afterEvaluate(evaluatedProject -> { configMaven(project); });
-        Task compileJava = project.getTasks().findByName("compileJava");
+        /*Task compileJava = project.getTasks().findByName("compileJava");
         if (compileJava != null) {
             compileJava.setEnabled(false);
         }
@@ -71,7 +71,7 @@ public class ZipPublish implements Plugin<Project> {
         Task javaDocJarTask = project.getTasks().findByName("javadocJar");
         if (javaDocJarTask != null) {
             javaDocJarTask.setEnabled(false);
-        }
+        }*/
         project.getGradle().getTaskGraph().whenReady(graph -> {
             if (graph.hasTask(LOCALMAVEN)) {
                 project.getTasks().getByName(MAVEN_ZIP_PUBLISH_POM_TASK).setEnabled(false);
