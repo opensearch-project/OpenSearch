f44aa2a6ab189158d00423c5acd3191eed75010a
pgodithi <pgodithi@amazon.com>
2022-04-23 08:42:54 -0700

    Code cleanup
    Signed-off-by: pgodithi <pgodithi@amazon.com>

---
+++ buildSrc/src/test/java/org/opensearch/gradle/zipplugin/ZipPublishTests.java
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.gradle.zipplugin;
+
+import org.gradle.testfixtures.ProjectBuilder;
+import org.gradle.api.Project;
+import org.opensearch.gradle.test.GradleUnitTestCase;
+import org.junit.Test;
+import java.io.IOException;
+import org.gradle.api.publish.maven.tasks.PublishToMavenRepository;
+
+public class ZipPublishTests extends GradleUnitTestCase {
+
+    @Test
+    public void testZipPublish() throws IOException {
+        Project project = ProjectBuilder.builder().build();
+        project.getPluginManager().apply("opensearch.zippublish");
+        assertTrue(project.getPluginManager().hasPlugin("opensearch.zippublish"));
+        assertNotNull("plugin extension created with the right name", project.getExtensions().findByName(ZipPublish.EXTENSION_NAME));
+        assertNotNull("plugin extensions has the right type", project.getExtensions().findByType(ZipPublishExtension.class));
+        assertNotNull(project.getTasks().withType(PublishToMavenRepository.class));
+    }
+
+}
