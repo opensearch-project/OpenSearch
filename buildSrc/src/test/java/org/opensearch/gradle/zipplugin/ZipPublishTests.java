3071a55e45670755c0e2d974a1c88e41d6352a1b
pgodithi <pgodithi@amazon.com>
2022-04-23 08:38:33 -0700

    fix lint issues
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- buildSrc/src/test/java/org/opensearch/gradle/zipplugin/ZipPublishTests.java
+++ buildSrc/src/test/java/org/opensearch/gradle/zipplugin/ZipPublishTests.java
@@ -25,7 +25,6 @@ public class ZipPublishTests extends GradleUnitTestCase {
         assertNotNull("plugin extension created with the right name", project.getExtensions().findByName(ZipPublish.EXTENSION_NAME));
         assertNotNull("plugin extensions has the right type", project.getExtensions().findByType(ZipPublishExtension.class));
         assertNotNull(project.getTasks().withType(PublishToMavenRepository.class));
-        assertTrue(project.getTasks().findByName("publishMavenzipPublicationToZipstagingRepository") instanceof ZipPublish);
     }
 
 }
