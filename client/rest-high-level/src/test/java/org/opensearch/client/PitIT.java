11c5195857c64020b893f2757c11767e9073f103
Bharathwaj G <bharath78910@gmail.com>
2022-05-16 11:11:53 +0530

    addressing comments
    Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

--- client/rest-high-level/src/test/java/org/opensearch/client/PitIT.java
+++ client/rest-high-level/src/test/java/org/opensearch/client/PitIT.java
@@ -25,24 +25,21 @@ public class PitIT extends OpenSearchRestHighLevelClientTestCase {
 
     @Before
     public void indexDocuments() throws IOException {
-        {
-            Request doc1 = new Request(HttpPut.METHOD_NAME, "/index/_doc/1");
-            doc1.setJsonEntity("{\"type\":\"type1\", \"id\":1, \"num\":10, \"num2\":50}");
-            client().performRequest(doc1);
-            Request doc2 = new Request(HttpPut.METHOD_NAME, "/index/_doc/2");
-            doc2.setJsonEntity("{\"type\":\"type1\", \"id\":2, \"num\":20, \"num2\":40}");
-            client().performRequest(doc2);
-            Request doc3 = new Request(HttpPut.METHOD_NAME, "/index/_doc/3");
-            doc3.setJsonEntity("{\"type\":\"type1\", \"id\":3, \"num\":50, \"num2\":35}");
-            client().performRequest(doc3);
-            Request doc4 = new Request(HttpPut.METHOD_NAME, "/index/_doc/4");
-            doc4.setJsonEntity("{\"type\":\"type2\", \"id\":4, \"num\":100, \"num2\":10}");
-            client().performRequest(doc4);
-            Request doc5 = new Request(HttpPut.METHOD_NAME, "/index/_doc/5");
-            doc5.setJsonEntity("{\"type\":\"type2\", \"id\":5, \"num\":100, \"num2\":10}");
-            client().performRequest(doc5);
-        }
-
+        Request doc1 = new Request(HttpPut.METHOD_NAME, "/index/_doc/1");
+        doc1.setJsonEntity("{\"type\":\"type1\", \"id\":1, \"num\":10, \"num2\":50}");
+        client().performRequest(doc1);
+        Request doc2 = new Request(HttpPut.METHOD_NAME, "/index/_doc/2");
+        doc2.setJsonEntity("{\"type\":\"type1\", \"id\":2, \"num\":20, \"num2\":40}");
+        client().performRequest(doc2);
+        Request doc3 = new Request(HttpPut.METHOD_NAME, "/index/_doc/3");
+        doc3.setJsonEntity("{\"type\":\"type1\", \"id\":3, \"num\":50, \"num2\":35}");
+        client().performRequest(doc3);
+        Request doc4 = new Request(HttpPut.METHOD_NAME, "/index/_doc/4");
+        doc4.setJsonEntity("{\"type\":\"type2\", \"id\":4, \"num\":100, \"num2\":10}");
+        client().performRequest(doc4);
+        Request doc5 = new Request(HttpPut.METHOD_NAME, "/index/_doc/5");
+        doc5.setJsonEntity("{\"type\":\"type2\", \"id\":5, \"num\":100, \"num2\":10}");
+        client().performRequest(doc5);
         client().performRequest(new Request(HttpPost.METHOD_NAME, "/_refresh"));
     }
 
