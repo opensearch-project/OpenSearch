ffc7ec3c4b91dac089691ea45176f225106e9647
Owais Kazi <owaiskazi19@gmail.com>
2022-05-18 18:02:01 +0000

    Added missing package-info.java file
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

---
+++ server/src/main/java/org/opensearch/extensions/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/** 
+ * Main OpenSearch extensions package. OpenSearch extensions provide extensibility to OpenSearch.
+ * <p>
+ * The primary extension points are: 
+ *  <<enumerate the extension points and ideally give an example so users no how to extend OpenSearch>>>
+ */
+package org.opensearch.extensions;
