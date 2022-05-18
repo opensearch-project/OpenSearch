5a5273443dcad948468c08cbd0b0e3072ea45bd7
Ryan Bogan <rbogan@amazon.com>
2022-05-18 20:37:26 +0000

    Added more asserts to tests and removed unnecessary dependencies
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- server/src/test/java/org/opensearch/extensions/ExtensionsOrchestratorTests.java
+++ server/src/test/java/org/opensearch/extensions/ExtensionsOrchestratorTests.java
@@ -18,7 +18,17 @@ public class ExtensionsOrchestratorTests extends OpenSearchTestCase {
 
         assertNotNull(extensions);
         assertEquals("firstExtension", extensions.getExtensions().get(0).getName());
+        assertEquals("uniqueid1", extensions.getExtensions().get(0).getEphemeralId());
+        assertEquals("myIndependentPluginHost1", extensions.getExtensions().get(0).getHostName());
+        assertEquals("127.0.0.0", extensions.getExtensions().get(0).getHostAddress());
+        assertEquals("9301", extensions.getExtensions().get(0).getPort());
+        assertEquals("2.0.0", extensions.getExtensions().get(0).getVersion());
+        assertEquals("secondExtension", extensions.getExtensions().get(1).getName());
+        assertEquals("uniqueid2", extensions.getExtensions().get(1).getEphemeralId());
+        assertEquals("myIndependentPluginHost2", extensions.getExtensions().get(1).getHostName());
+        assertEquals("127.0.0.0", extensions.getExtensions().get(1).getHostAddress());
         assertEquals("9301", extensions.getExtensions().get(1).getPort());
+        assertEquals("2.0.0", extensions.getExtensions().get(1).getVersion());
 
     }
 
