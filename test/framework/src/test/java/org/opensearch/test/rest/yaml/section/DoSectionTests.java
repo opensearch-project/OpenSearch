b8450bdc41e2df23c90acfcc7e6db771ff5f8ab4
Suraj Singh <surajrider@gmail.com>
2022-05-19 10:13:20 -0700

    Fix assertion failures
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- test/framework/src/test/java/org/opensearch/test/rest/yaml/section/DoSectionTests.java
+++ test/framework/src/test/java/org/opensearch/test/rest/yaml/section/DoSectionTests.java
@@ -180,7 +180,7 @@ public class DoSectionTests extends AbstractClientYamlTestFragmentParserTestCase
 
         assertThat(apiCallSection, notNullValue());
         assertThat(apiCallSection.getApi(), equalTo("get"));
-        assertThat(apiCallSection.getParams().size(), equalTo(3));
+        assertThat(apiCallSection.getParams().size(), equalTo(2));
         assertThat(apiCallSection.getParams().get("index"), equalTo("test_index"));
         assertThat(apiCallSection.getParams().get("id"), equalTo("1"));
         assertThat(apiCallSection.hasBody(), equalTo(false));
@@ -207,7 +207,7 @@ public class DoSectionTests extends AbstractClientYamlTestFragmentParserTestCase
 
         assertThat(apiCallSection, notNullValue());
         assertThat(apiCallSection.getApi(), equalTo("index"));
-        assertThat(apiCallSection.getParams().size(), equalTo(3));
+        assertThat(apiCallSection.getParams().size(), equalTo(2));
         assertThat(apiCallSection.getParams().get("index"), equalTo("test_1"));
         assertThat(apiCallSection.getParams().get("id"), equalTo("1"));
         assertThat(apiCallSection.hasBody(), equalTo(true));
@@ -436,7 +436,7 @@ public class DoSectionTests extends AbstractClientYamlTestFragmentParserTestCase
         assertThat(doSection.getCatch(), nullValue());
         assertThat(doSection.getApiCallSection(), notNullValue());
         assertThat(doSection.getApiCallSection().getApi(), equalTo("indices.get_field_mapping"));
-        assertThat(doSection.getApiCallSection().getParams().size(), equalTo(3));
+        assertThat(doSection.getApiCallSection().getParams().size(), equalTo(2));
         assertThat(doSection.getApiCallSection().getParams().get("index"), equalTo("test_index"));
         assertThat(doSection.getApiCallSection().getParams().get("field"), equalTo("text,text1"));
         assertThat(doSection.getApiCallSection().hasBody(), equalTo(false));
