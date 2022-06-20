48e8ade500e84a8324cea352cff130abb086fc44
Marc Handalian <handalm@amazon.com>
2022-06-20 09:18:52 -0700

    Resolving import conflict in Node.java and mergining PR https://github.com/opensearch-project/OpenSearch/pull/3525.
    Signed-off-by: Rishikesh1159 <rishireddy1159@gmail.com>

---
+++ server/src/main/java/org/opensearch/indices/replication/RemoteSegmentFileChunkWriter.java
@@ -0,0 +1,125 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.indices.replication;
+
+import org.apache.lucene.store.RateLimiter;
+import org.opensearch.OpenSearchException;
+import org.opensearch.action.ActionListener;
+import org.opensearch.common.bytes.BytesReference;
+import org.opensearch.common.io.stream.Writeable;
+import org.opensearch.index.shard.ShardId;
+import org.opensearch.index.store.StoreFileMetadata;
+import org.opensearch.indices.recovery.FileChunkRequest;
+import org.opensearch.indices.recovery.RecoverySettings;
+import org.opensearch.indices.recovery.RetryableTransportClient;
+import org.opensearch.indices.recovery.FileChunkWriter;
+import org.opensearch.transport.TransportRequestOptions;
+import org.opensearch.transport.TransportResponse;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+
+/**
+ * This class handles sending file chunks over the transport layer to a target shard.
+ *
+ * @opensearch.internal
+ */
+public final class RemoteSegmentFileChunkWriter implements FileChunkWriter {
+
+    private final AtomicLong requestSeqNoGenerator;
+    private final RetryableTransportClient retryableTransportClient;
+    private final ShardId shardId;
+    private final RecoverySettings recoverySettings;
+    private final long replicationId;
+    private final AtomicLong bytesSinceLastPause = new AtomicLong();
+    private final TransportRequestOptions fileChunkRequestOptions;
+    private final Consumer<Long> onSourceThrottle;
+    private final String action;
+
+    public RemoteSegmentFileChunkWriter(
+        long replicationId,
+        RecoverySettings recoverySettings,
+        RetryableTransportClient retryableTransportClient,
+        ShardId shardId,
+        String action,
+        AtomicLong requestSeqNoGenerator,
+        Consumer<Long> onSourceThrottle
+    ) {
+        this.replicationId = replicationId;
+        this.recoverySettings = recoverySettings;
+        this.retryableTransportClient = retryableTransportClient;
+        this.shardId = shardId;
+        this.requestSeqNoGenerator = requestSeqNoGenerator;
+        this.onSourceThrottle = onSourceThrottle;
+        this.fileChunkRequestOptions = TransportRequestOptions.builder()
+            .withType(TransportRequestOptions.Type.RECOVERY)
+            .withTimeout(recoverySettings.internalActionTimeout())
+            .build();
+
+        this.action = action;
+    }
+
+    @Override
+    public void writeFileChunk(
+        StoreFileMetadata fileMetadata,
+        long position,
+        BytesReference content,
+        boolean lastChunk,
+        int totalTranslogOps,
+        ActionListener<Void> listener
+    ) {
+        // Pause using the rate limiter, if desired, to throttle the recovery
+        final long throttleTimeInNanos;
+        // always fetch the ratelimiter - it might be updated in real-time on the recovery settings
+        final RateLimiter rl = recoverySettings.rateLimiter();
+        if (rl != null) {
+            long bytes = bytesSinceLastPause.addAndGet(content.length());
+            if (bytes > rl.getMinPauseCheckBytes()) {
+                // Time to pause
+                bytesSinceLastPause.addAndGet(-bytes);
+                try {
+                    throttleTimeInNanos = rl.pause(bytes);
+                    onSourceThrottle.accept(throttleTimeInNanos);
+                } catch (IOException e) {
+                    throw new OpenSearchException("failed to pause recovery", e);
+                }
+            } else {
+                throttleTimeInNanos = 0;
+            }
+        } else {
+            throttleTimeInNanos = 0;
+        }
+
+        final long requestSeqNo = requestSeqNoGenerator.getAndIncrement();
+        /* we send estimateTotalOperations with every request since we collect stats on the target and that way we can
+         * see how many translog ops we accumulate while copying files across the network. A future optimization
+         * would be in to restart file copy again (new deltas) if we have too many translog ops are piling up.
+         */
+        final FileChunkRequest request = new FileChunkRequest(
+            replicationId,
+            requestSeqNo,
+            shardId,
+            fileMetadata,
+            position,
+            content,
+            lastChunk,
+            totalTranslogOps,
+            throttleTimeInNanos
+        );
+        final Writeable.Reader<TransportResponse.Empty> reader = in -> TransportResponse.Empty.INSTANCE;
+        retryableTransportClient.executeRetryableAction(
+            action,
+            request,
+            fileChunkRequestOptions,
+            ActionListener.map(listener, r -> null),
+            reader
+        );
+    }
+}
--- server/src/main/java/org/opensearch/node/Node.java
+++ server/src/main/java/org/opensearch/node/Node.java
@@ -40,6 +40,8 @@ import org.opensearch.cluster.routing.allocation.AwarenessReplicaBalance;
 import org.opensearch.index.IndexingPressureService;
 import org.opensearch.tasks.TaskResourceTrackingService;
 import org.opensearch.threadpool.RunnableTaskExecutionListener;
+import org.opensearch.common.util.FeatureFlags;
+import org.opensearch.indices.replication.SegmentReplicationSourceService;
 import org.opensearch.watcher.ResourceWatcherService;
 import org.opensearch.Assertions;
 import org.opensearch.Build;
@@ -223,6 +225,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
+import static org.opensearch.common.util.FeatureFlags.REPLICATION_TYPE;
 import static org.opensearch.index.ShardIndexingPressureSettings.SHARD_INDEXING_PRESSURE_ENABLED_ATTRIBUTE_KEY;
 
 /**
@@ -944,6 +947,10 @@ public class Node implements Closeable {
                         .toInstance(new PeerRecoverySourceService(transportService, indicesService, recoverySettings));
                     b.bind(PeerRecoveryTargetService.class)
                         .toInstance(new PeerRecoveryTargetService(threadPool, transportService, recoverySettings, clusterService));
+                    if (FeatureFlags.isEnabled(REPLICATION_TYPE)) {
+                        b.bind(SegmentReplicationSourceService.class)
+                            .toInstance(new SegmentReplicationSourceService(indicesService, transportService, recoverySettings));
+                    }
                 }
                 b.bind(HttpServerTransport.class).toInstance(httpServerTransport);
                 pluginComponents.stream().forEach(p -> b.bind((Class) p.getClass()).toInstance(p));
