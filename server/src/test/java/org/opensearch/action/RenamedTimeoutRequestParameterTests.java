d89e96d8eefd2974b8cb7301d89c67f8cc6801e4
Tianli Feng <ftianli@amazon.com>
2022-07-14 16:39:15 -0700

    Deprecate public class names with master terminology (#3871)
    - Add back the usage of old names for some classes for backwards compatibility. Those public classes were renamed from "master" to "cluster manager" In PR https://github.com/opensearch-project/OpenSearch/pull/3619 / commit https://github.com/opensearch-project/OpenSearch/commit/a7e113a67a9d8c4dd7298956174ff2f666a2c2b7.
     - Add a unit test to validate the backwards compatibility of interface `LocalNodeMasterListener` remains.
     - Revert the renaming of class `MasterService`, `FakeThreadPoolMasterService`, `BlockMasterServiceOnMaster` and `BusyMasterServiceDisruption`, as well as instance variable names of class `MasterService`.
     Because I couldn't solve the compatibility problem of a public method `public ClusterManagerService getMasterService()` (https://github.com/opensearch-project/OpenSearch/pull/3871#discussion_r920623073)
    
    **List of classes that renamed in previous PR:**
    In this PR, the usages of the old names should all be restored.
    `sever` directory:
    interface LocalNodeMasterListener -> LocalNodeClusterManagerListener
    final class MasterNodeChangePredicate -> ClusterManagerNodeChangePredicate
    NotMasterException -> NotClusterManagerException
    NoMasterBlockService -> NoClusterManagerBlockService
    UnsafeBootstrapMasterCommand - UnsafeBootstrapClusterManagerCommand
    MasterNotDiscoveredException -> ClusterManagerNotDiscoveredException
    RestMasterAction -> RestClusterManagerAction
    
    **List of classes that not renamed:**
    `sever` directory:
    MasterService
    `test/framework` directory:
    FakeThreadPoolMasterService
    BlockMasterServiceOnMaster
    BusyMasterServiceDisruption
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>

--- server/src/test/java/org/opensearch/action/RenamedTimeoutRequestParameterTests.java
+++ server/src/test/java/org/opensearch/action/RenamedTimeoutRequestParameterTests.java
@@ -66,6 +66,7 @@ import org.opensearch.rest.action.admin.cluster.RestDeleteStoredScriptAction;
 import org.opensearch.rest.action.admin.cluster.RestGetStoredScriptAction;
 import org.opensearch.rest.action.admin.cluster.RestPutStoredScriptAction;
 import org.opensearch.rest.action.cat.RestAllocationAction;
+import org.opensearch.rest.action.cat.RestMasterAction;
 import org.opensearch.rest.action.cat.RestRepositoriesAction;
 import org.opensearch.rest.action.cat.RestThreadPoolAction;
 import org.opensearch.rest.action.cat.RestClusterManagerAction;
@@ -135,6 +136,12 @@ public class RenamedTimeoutRequestParameterTests extends OpenSearchTestCase {
 
     public void testCatClusterManager() {
         RestClusterManagerAction action = new RestClusterManagerAction();
+        Exception e = assertThrows(OpenSearchParseException.class, () -> action.doCatRequest(getRestRequestWithBothParams(), client));
+        assertThat(e.getMessage(), containsString(DUPLICATE_PARAMETER_ERROR_MESSAGE));
+    }
+
+    public void testCatMaster() {
+        RestMasterAction action = new RestMasterAction();
         Exception e = assertThrows(OpenSearchParseException.class, () -> action.doCatRequest(getRestRequestWithBothParams(), client));
         assertThat(e.getMessage(), containsString(DUPLICATE_PARAMETER_ERROR_MESSAGE));
     }
