1c787e8e28e04ca7f07ffd47b91fb6ff088d9648
piyush <pdaftary@amazon.com>
2022-07-22 13:04:55 -0700

    Parallelize stale blobs deletion during snapshot delete (#3796)
    * Parallelize stale blobs deletion during snapshot delete
    
    Signed-off-by: Piyush Daftary <pdaftary@amazon.com>
    
    * Adding test which throws exception
    
    Signed-off-by: Piyush Daftary <pdaftary@amazon.com>
    
    * Adusting identation for spotlessJavaCheck
    
    Signed-off-by: Piyush Daftary <pdaftary@amazon.com>
    
    * Adding more description to MAX_SHARD_BLOB_DELETE_BATCH_SIZE
    
    Signed-off-by: Piyush Daftary <pdaftary@amazon.com>
    
    * Renaming max_shard_blob_delete_batch_size to max_snapshot_shard_blob_delete_batch_size
    
    Signed-off-by: Piyush Daftary <pdaftary@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/snapshots/RepositoriesIT.java
+++ server/src/internalClusterTest/java/org/opensearch/snapshots/RepositoriesIT.java
@@ -35,6 +35,8 @@ package org.opensearch.snapshots;
 import org.opensearch.action.admin.cluster.repositories.get.GetRepositoriesResponse;
 import org.opensearch.action.admin.cluster.repositories.verify.VerifyRepositoryResponse;
 import org.opensearch.action.admin.cluster.state.ClusterStateResponse;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.support.master.AcknowledgedResponse;
 import org.opensearch.client.Client;
 import org.opensearch.cluster.metadata.Metadata;
@@ -46,6 +48,7 @@ import org.opensearch.common.unit.ByteSizeUnit;
 import org.opensearch.repositories.RepositoriesService;
 import org.opensearch.repositories.RepositoryException;
 import org.opensearch.repositories.RepositoryVerificationException;
+import org.opensearch.repositories.blobstore.BlobStoreRepository;
 import org.opensearch.snapshots.mockstore.MockRepository;
 import org.opensearch.test.OpenSearchIntegTestCase;
 import org.opensearch.threadpool.ThreadPool;
@@ -327,5 +330,126 @@ public class RepositoriesIT extends AbstractSnapshotIntegTestCase {
         } catch (RepositoryVerificationException ex) {
             assertThat(ex.getMessage(), containsString("is not shared"));
         }
+    }
+
+    public void testSnapshotShardBlobDelete() throws Exception {
+        Client client = client();
+        Path repositoryPath = randomRepoPath();
+        final String repositoryName = "test-repo";
+        final String firstSnapshot = "first-snapshot";
+        final String secondSnapshot = "second-snapshot";
+        final String indexName = "test-idx";
+
+        logger.info("-->  creating repository at {}", repositoryPath.toAbsolutePath());
+        int maxShardBlobDeleteBatchSize = randomIntBetween(1, 1000);
+        createRepository(
+            "test-repo",
+            "mock",
+            Settings.builder()
+                .put("location", repositoryPath)
+                .put(BlobStoreRepository.MAX_SNAPSHOT_SHARD_BLOB_DELETE_BATCH_SIZE.getKey(), maxShardBlobDeleteBatchSize)
+        );
+
+        logger.info("--> creating index-0 and ingest data");
+        createIndex(indexName);
+        ensureGreen();
+        for (int j = 0; j < randomIntBetween(1, 1000); j++) {
+            index(indexName, "_doc", Integer.toString(j), "foo", "bar" + j);
+        }
+        refresh();
+
+        logger.info("--> creating first snapshot");
+        createFullSnapshot(repositoryName, firstSnapshot);
+
+        int numberOfFiles = numberOfFiles(repositoryPath);
+
+        logger.info("--> adding some more documents to test index");
+        for (int j = 0; j < randomIntBetween(100, 10000); ++j) {
+            final BulkRequest bulkRequest = new BulkRequest();
+            for (int i = 0; i < randomIntBetween(100, 1000); ++i) {
+                bulkRequest.add(new IndexRequest(indexName).source("foo" + j, "bar" + i));
+            }
+            client().bulk(bulkRequest).get();
+        }
+        refresh();
+
+        logger.info("--> creating second snapshot");
+        createFullSnapshot(repositoryName, secondSnapshot);
+
+        // Delete second snapshot
+        logger.info("--> delete second snapshot");
+        client.admin().cluster().prepareDeleteSnapshot(repositoryName, secondSnapshot).get();
+
+        logger.info("--> make sure that number of files is back to what it was when the first snapshot was made");
+        assertFileCount(repositoryPath, numberOfFiles);
+
+        logger.info("--> done");
+    }
+
+    public void testSnapshotShardBlobDeletionRepositoryThrowingError() throws Exception {
+        Client client = client();
+        Path repositoryPath = randomRepoPath();
+        final String repositoryName = "test-repo";
+        final String firstSnapshot = "first-snapshot";
+        final String secondSnapshot = "second-snapshot";
+        final String indexName = "test-idx";
+
+        logger.info("-->  creating repository at {}", repositoryPath.toAbsolutePath());
+        int maxShardBlobDeleteBatchSize = randomIntBetween(1, 1000);
+        createRepository(
+            "test-repo",
+            "mock",
+            Settings.builder()
+                .put("location", repositoryPath)
+                .put(BlobStoreRepository.MAX_SNAPSHOT_SHARD_BLOB_DELETE_BATCH_SIZE.getKey(), maxShardBlobDeleteBatchSize)
+        );
+
+        logger.info("--> creating index-0 and ingest data");
+        createIndex(indexName);
+        ensureGreen();
+        for (int j = 0; j < randomIntBetween(1, 1000); j++) {
+            index(indexName, "_doc", Integer.toString(j), "foo", "bar" + j);
+        }
+        refresh();
+
+        logger.info("--> creating first snapshot");
+        createFullSnapshot(repositoryName, firstSnapshot);
+
+        logger.info("--> adding some more documents to test index");
+        for (int j = 0; j < randomIntBetween(100, 1000); ++j) {
+            final BulkRequest bulkRequest = new BulkRequest();
+            for (int i = 0; i < randomIntBetween(100, 1000); ++i) {
+                bulkRequest.add(new IndexRequest(indexName).source("foo" + j, "bar" + i));
+            }
+            client().bulk(bulkRequest).get();
+        }
+        refresh();
+
+        logger.info("--> creating second snapshot");
+        createFullSnapshot(repositoryName, secondSnapshot);
+
+        // Make repository to throw exception when trying to delete stale snapshot shard blobs
+        String clusterManagerNode = internalCluster().getMasterName();
+        ((MockRepository) internalCluster().getInstance(RepositoriesService.class, clusterManagerNode).repository("test-repo"))
+            .setThrowExceptionWhileDelete(true);
+
+        // Delete second snapshot
+        logger.info("--> delete second snapshot");
+        client.admin().cluster().prepareDeleteSnapshot(repositoryName, secondSnapshot).get();
+
+        // Make repository to work normally
+        ((MockRepository) internalCluster().getInstance(RepositoriesService.class, clusterManagerNode).repository("test-repo"))
+            .setThrowExceptionWhileDelete(false);
+
+        // This snapshot should delete last snapshot's residual stale shard blobs as well
+        logger.info("--> delete first snapshot");
+        client.admin().cluster().prepareDeleteSnapshot(repositoryName, firstSnapshot).get();
+
+        // Expect two files to remain in the repository:
+        // (1) index-(N+1)
+        // (2) index-latest
+        assertFileCount(repositoryPath, 2);
+
+        logger.info("--> done");
     }
 }
