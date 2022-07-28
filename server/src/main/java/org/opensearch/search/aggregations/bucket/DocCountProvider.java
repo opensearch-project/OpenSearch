5907dbc037c2f756b0a46b2ac9d285f2ba7bf8cb
Petar Dzepina <petar.dzepina@gmail.com>
2022-07-28 19:49:57 +0200

    license missing
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- server/src/main/java/org/opensearch/search/aggregations/bucket/DocCountProvider.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/DocCountProvider.java
@@ -1,4 +1,12 @@
 /*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
  * Licensed to Elasticsearch under one or more contributor
  * license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright
@@ -7,7 +15,7 @@
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -15,6 +23,11 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
  */
 
 package org.opensearch.search.aggregations.bucket;
