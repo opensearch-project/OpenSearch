360bcd11ba739302311207a04bc2c5deba00b227
Rishikesh1159 <rishireddy1159@gmail.com>
2022-08-01 19:20:39 +0000

    Removing Wildcard imports
    Signed-off-by: Rishikesh1159 <rishireddy1159@gmail.com>

--- server/src/test/java/org/opensearch/indices/replication/SegmentReplicationTargetServiceTests.java
+++ server/src/test/java/org/opensearch/indices/replication/SegmentReplicationTargetServiceTests.java
@@ -23,7 +23,10 @@ import org.opensearch.transport.TransportService;
 import java.io.IOException;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
 
 public class SegmentReplicationTargetServiceTests extends IndexShardTestCase {
 
