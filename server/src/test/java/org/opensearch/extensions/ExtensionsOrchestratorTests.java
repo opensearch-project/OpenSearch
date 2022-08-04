54498eab0b4d703b46e55bec91d9ad1d3e4112e5
Daniel Widdis <widdis@gmail.com>
2022-08-04 15:10:54 -0700

    Fix failing test as we now register four handlers
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- server/src/test/java/org/opensearch/extensions/ExtensionsOrchestratorTests.java
+++ server/src/test/java/org/opensearch/extensions/ExtensionsOrchestratorTests.java
@@ -386,7 +386,7 @@ public class ExtensionsOrchestratorTests extends OpenSearchTestCase {
         );
 
         extensionsOrchestrator.setTransportService(mockTransportService);
-        verify(mockTransportService, times(3)).registerRequestHandler(anyString(), anyString(), anyBoolean(), anyBoolean(), any(), any());
+        verify(mockTransportService, times(4)).registerRequestHandler(anyString(), anyString(), anyBoolean(), anyBoolean(), any(), any());
 
     }
 
