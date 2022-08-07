d8e7d87daa84db605360970d210b923c3f4b3117
Bukhtawar Khan <bukhtawa@amazon.com>
2022-08-07 14:48:00 +0530

    integrate remotefstranslog
    Signed-off-by: Bukhtawar Khan <bukhtawa@amazon.com>

--- server/src/main/java/org/opensearch/index/translog/RemoteFsTranslog.java
+++ server/src/main/java/org/opensearch/index/translog/RemoteFsTranslog.java
@@ -28,25 +28,6 @@ import java.util.function.LongSupplier;
 
 public class RemoteFsTranslog extends Translog {
 
-    /**
-     * Creates a new Translog instance. This method will create a new transaction log unless the given {@link TranslogGeneration} is
-     * {@code null}. If the generation is {@code null} this method is destructive and will delete all files in the translog path given. If
-     * the generation is not {@code null}, this method tries to open the given translog generation. The generation is treated as the last
-     * generation referenced from already committed data. This means all operations that have not yet been committed should be in the
-     * translog file referenced by this generation. The translog creation will fail if this generation can't be opened.
-     *
-     * @param config                          the configuration of this translog
-     * @param translogUUID                    the translog uuid to open, null for a new translog
-     * @param deletionPolicy                  an instance of {@link TranslogDeletionPolicy} that controls when a translog file can be safely
-     *                                        deleted
-     * @param globalCheckpointSupplier        a supplier for the global checkpoint
-     * @param primaryTermSupplier             a supplier for the latest value of primary term of the owning index shard. The latest term value is
-     *                                        examined and stored in the header whenever a new generation is rolled. It's guaranteed from outside
-     *                                        that a new generation is rolled when the term is increased. This guarantee allows to us to validate
-     *                                        and reject operation whose term is higher than the primary term stored in the translog header.
-     * @param persistedSequenceNumberConsumer a callback that's called whenever an operation with a given sequence number is successfully
-     */
-
     private final BlobStore blobStore;
     private final TranslogTransferManager translogTransferManager;
 
@@ -76,33 +57,102 @@ public class RemoteFsTranslog extends Translog {
 
     @Override
     boolean ensureSynced(Location location) throws IOException {
-        return false;
+        try (ReleasableLock lock = readLock.acquire()) {
+            assert location.generation <= current.getGeneration();
+            if (location.generation == current.getGeneration()) {
+                ensureOpen();
+                prepareUpload(location.generation);
+            }
+            return upload();
+        } catch (final Exception ex) {
+            closeOnTragicEvent(ex);
+            throw ex;
+        }
     }
 
     @Override
     public void rollGeneration() throws IOException {
-        super.rollGeneration();
-        try (ReleasableLock ignored = readLock.acquire()) {
-            ensureOpen();
-            TransferSnapshotProvider transferSnapshotProvider = new TransferSnapshotProvider(this.location, readers);
-            Releasable transferReleasable = Releasables.wrap(deletionPolicy.acquireTranslogGen(getMinFileGeneration()));
-            translogTransferManager.uploadTranslog(transferSnapshotProvider.get(), new TranslogTransferListener() {
-                @Override
-                public void onUploadComplete(TransferSnapshot transferSnapshot) throws IOException {
-                    transferReleasable.close();
-                    closeFilesIfNoPendingRetentionLocks();
-                }
+        syncBeforeRollGeneration();
+        if (current.totalOperations() == 0 && primaryTermSupplier.getAsLong() == current.getPrimaryTerm()) {
+            return;
+        }
+        prepareUpload(null);
+        upload();
+    }
 
-                @Override
-                public void onUploadFailed(TransferSnapshot transferSnapshot, Exception ex) throws IOException {
-                    transferReleasable.close();
-                    closeFilesIfNoPendingRetentionLocks();
+    private void prepareUpload(Long generation) throws IOException {
+        try (Releasable ignored = writeLock.acquire()) {
+            if (generation == null || generation == current.getGeneration()) {
+                try {
+                    final TranslogReader reader = current.closeIntoReader();
+                    readers.add(reader);
+                    if (closed.get() == false) {
+                        // create a new translog file; this will sync it and update the checkpoint data;
+                        logger.trace("Creating new writer for gen: [{}]", current.getGeneration() + 1);
+                        current = createWriter(current.getGeneration() + 1);
+                        logger.trace("current translog set to [{}]", current.getGeneration());
+                    }
+                } catch (final Exception e) {
+                    tragedy.setTragicException(e);
+                    closeOnTragicEvent(e);
+                    throw e;
                 }
-            });
+            }
+        }
+    }
+
+    private boolean upload() throws IOException {
+        TransferSnapshotProvider transferSnapshotProvider = new TransferSnapshotProvider(this.location, readers);
+        Releasable transferReleasable = Releasables.wrap(deletionPolicy.acquireTranslogGen(getMinFileGeneration()));
+        return translogTransferManager.uploadTranslog(transferSnapshotProvider.get(), new TranslogTransferListener() {
+            @Override
+            public void onUploadComplete(TransferSnapshot transferSnapshot) throws IOException {
+                transferReleasable.close();
+                closeFilesIfNoPendingRetentionLocks();
+            }
+
+            @Override
+            public void onUploadFailed(TransferSnapshot transferSnapshot, Exception ex) throws IOException {
+                transferReleasable.close();
+                closeFilesIfNoPendingRetentionLocks();
+            }
+        });
+    }
+
+    private boolean syncToDisk() throws IOException {
+        try (ReleasableLock lock = readLock.acquire()) {
+            return current.sync();
+        } catch (final Exception ex) {
+            closeOnTragicEvent(ex);
+            throw ex;
+        }
+    }
+
+    @Override
+    public void sync() throws IOException {
+
+        try {
+            if(syncToDisk()) {
+                upload();
+            }
         } catch (final Exception e) {
             tragedy.setTragicException(e);
             closeOnTragicEvent(e);
             throw e;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        assert Translog.calledFromOutsideOrViaTragedyClose()
+            : "Translog.close method is called from inside Translog, but not via closeOnTragicEvent method";
+        if (closed.compareAndSet(false, true)) {
+            try (ReleasableLock lock = writeLock.acquire()) {
+                prepareUpload(null);
+                upload();
+            } finally {
+                logger.debug("translog closed");
+            }
         }
     }
 }
