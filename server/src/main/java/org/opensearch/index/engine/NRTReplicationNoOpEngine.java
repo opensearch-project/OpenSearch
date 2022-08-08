3012063b8c17bc96cab33489ca1adf7bdc6ee720
Ashish Singh <ssashish@amazon.com>
2022-08-08 12:34:18 +0530

    Adds rationale behind new Engine as comments
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- server/src/main/java/org/opensearch/index/engine/NRTReplicationNoOpEngine.java
+++ server/src/main/java/org/opensearch/index/engine/NRTReplicationNoOpEngine.java
@@ -9,6 +9,9 @@
 package org.opensearch.index.engine;
 
 import org.apache.lucene.util.SetOnce;
+import org.opensearch.action.ActionListener;
+import org.opensearch.action.bulk.BulkShardRequest;
+import org.opensearch.index.shard.IndexShard;
 import org.opensearch.index.translog.NoOpTranslogManager;
 import org.opensearch.index.translog.Translog;
 import org.opensearch.index.translog.TranslogManager;
@@ -18,7 +21,12 @@ import java.io.IOException;
 
 /**
  * This is an {@link Engine} implementation intended for replica shards when Segment Replication and remote store
- * are enabled together.
+ * are enabled together. The new engine has been extended from {@link AbstractNRTReplicationEngine} to allow NRT
+ * Replication to work as is. Also, we want the recovery to work for which this engine would act as a dummy engine which
+ * acks the primary with the expected value as the status quo. There is a plan to remove this or refactor this basis the
+ * primary-primary recovery with SegRep and Remote store for translog. If we were to remove this engine, we can programme
+ * the {@link org.opensearch.action.bulk.TransportShardBulkAction#dispatchedShardOperationOnReplica(BulkShardRequest,
+ * IndexShard, ActionListener)} method to complete the listener on a dummy response.
  *
  * @opensearch.internal
  */
@@ -58,13 +66,51 @@ public class NRTReplicationNoOpEngine extends AbstractNRTReplicationEngine {
         return 0;
     }
 
-    @Override
-    public long getPersistedLocalCheckpoint() {
-        return getLocalCheckpointTracker().getMaxSeqNo();
-    }
-
+    /**
+     * This method is essentially no-op but does however updates in-memory local checkpoint tracker.
+     *
+     * @param index request for index
+     * @return index result.
+     */
     @Override
     public IndexResult index(Index index) throws IOException {
         return super.index(index);
+    }
+
+    /**
+     * This method is essentially no-op but does however updates in-memory local checkpoint tracker.
+     *
+     * @param delete request for delete.
+     * @return delete result.
+     */
+    @Override
+    public DeleteResult delete(Delete delete) throws IOException {
+        return super.delete(delete);
+    }
+
+    /**
+     * This method is essentially no-op but does however updates in-memory local checkpoint tracker.
+     *
+     * @param noOp request for no-op.
+     * @return no-op result.
+     */
+    @Override
+    public NoOpResult noOp(NoOp noOp) throws IOException {
+        return super.noOp(noOp);
+    }
+
+    /**
+     * This method tracks the maximum sequence number of the request that has been given for indexing to this replica.
+     * Currently, the recovery process involves replaying translog operation on the replica by the primary. For recovery
+     * step to finish, and finalize step to kick in, this method should return expected value. Hence it has been overridden.
+     * There is plan to make changes in recovery code to skip the translog replay. Post which this engine can be evaluated
+     * for its existence basis the primary-primary recovery usecase.
+     *
+     * @return the max sequence number that has been sent to {@link #index(Index)}, {@link #delete(Delete)}, and {@link
+     * #noOp(NoOp)} methods.
+     */
+    @Override
+    public long getPersistedLocalCheckpoint() {
+        return getLocalCheckpointTracker().getMaxSeqNo();
     }
 }
