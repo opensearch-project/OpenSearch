0c123e24b872afb416751e4781294999557856fa
Rishab Nahata <rnnahata@amazon.com>
2022-08-25 17:14:49 +0530

    Move observer logic to helper
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- server/src/main/java/org/opensearch/cluster/decommission/DecommissionHelper.java
+++ server/src/main/java/org/opensearch/cluster/decommission/DecommissionHelper.java
@@ -10,17 +10,24 @@ package org.opensearch.cluster.decommission;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.opensearch.action.ActionListener;
+import org.opensearch.cluster.ClusterState;
+import org.opensearch.cluster.ClusterStateObserver;
 import org.opensearch.cluster.ClusterStateTaskConfig;
 import org.opensearch.cluster.ClusterStateTaskListener;
+import org.opensearch.cluster.ack.ClusterStateUpdateResponse;
 import org.opensearch.cluster.coordination.NodeRemovalClusterStateTaskExecutor;
 import org.opensearch.cluster.node.DiscoveryNode;
 import org.opensearch.cluster.routing.allocation.AllocationService;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.Priority;
+import org.opensearch.common.unit.TimeValue;
+import org.opensearch.threadpool.ThreadPool;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 /**
  * Helper executor class to remove list of nodes from the cluster
@@ -34,24 +41,61 @@ public class DecommissionHelper {
 
     private final NodeRemovalClusterStateTaskExecutor nodeRemovalExecutor;
     private final ClusterService clusterService;
+    private final ThreadPool threadPool;
 
-    DecommissionHelper(ClusterService clusterService, AllocationService allocationService) {
+    DecommissionHelper(
+        ClusterService clusterService,
+        AllocationService allocationService,
+        ThreadPool threadPool
+    ) {
         this.clusterService = clusterService;
         this.nodeRemovalExecutor = new NodeRemovalClusterStateTaskExecutor(allocationService, logger);
+        this.threadPool = threadPool;
     }
 
-    public void handleNodesDecommissionRequest(List<DiscoveryNode> nodesToBeDecommissioned, String reason) {
+    public void handleNodesDecommissionRequest(
+        List<DiscoveryNode> nodesToBeDecommissioned,
+        String reason,
+        TimeValue timeout,
+        Predicate<ClusterState> allDecommissionedNodesRemoved,
+        ActionListener<ClusterStateUpdateResponse> nodesRemovedListener
+    ) {
         final Map<NodeRemovalClusterStateTaskExecutor.Task, ClusterStateTaskListener> nodesDecommissionTasks = new LinkedHashMap<>();
         nodesToBeDecommissioned.forEach(discoveryNode -> {
             final NodeRemovalClusterStateTaskExecutor.Task task = new NodeRemovalClusterStateTaskExecutor.Task(discoveryNode, reason);
             nodesDecommissionTasks.put(task, nodeRemovalExecutor);
         });
-        final String source = "node-decommissioned";
         clusterService.submitStateUpdateTasks(
-            source,
+            "node-decommissioned",
             nodesDecommissionTasks,
             ClusterStateTaskConfig.build(Priority.IMMEDIATE),
             nodeRemovalExecutor
         );
+
+        final ClusterStateObserver observer = new ClusterStateObserver(
+            clusterService,
+            timeout,
+            logger,
+            threadPool.getThreadContext()
+        );
+
+        observer.waitForNextChange(new ClusterStateObserver.Listener() {
+            @Override
+            public void onNewClusterState(ClusterState state) {
+                logger.info("successfully removed all decommissioned nodes [{}] from the cluster", nodesToBeDecommissioned.toString());
+                nodesRemovedListener.onResponse(new ClusterStateUpdateResponse(true));
+            }
+
+            @Override
+            public void onClusterServiceClose() {
+                logger.debug("cluster service closed while waiting for removal of decommissioned nodes.");
+            }
+
+            @Override
+            public void onTimeout(TimeValue timeout) {
+                logger.info("timed out while waiting for removal of decommissioned nodes");
+                nodesRemovedListener.onResponse(new ClusterStateUpdateResponse(false));
+            }
+        }, allDecommissionedNodesRemoved);
     }
 }
