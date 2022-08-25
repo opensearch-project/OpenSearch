65880a1f4d49ded21452198187c7ab582ec9e928
Darshit Chanpura <dchanp@amazon.com>
2022-08-25 01:30:17 -0400

    Adds comments for readability
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- server/src/main/java/org/opensearch/identity/Identity.java
+++ server/src/main/java/org/opensearch/identity/Identity.java
@@ -11,17 +11,26 @@
 
 package org.opensearch.identity;
 
-import org.opensearch.common.UUIDs;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-import java.util.*;
+/**
+ * A serializable implementation of principal for requests to extensions
+ * Should be an authenticated user in the system
+ */
+public class Identity implements Principal {
 
-public class Identity implements Principal{
+    // For requests from unknown identity
+    public static final AnonymousIdentity ANONYMOUS_IDENTITY = new AnonymousIdentity();
+
     private UUID id;
     private String userName;
     private List<String> schemas;
     private Map<String, String> metaData;
 
-    public Identity(UUID id, String userName, List<String> schemas, Map<String, String> metaData){
+    public Identity(UUID id, String userName, List<String> schemas, Map<String, String> metaData) {
         this.id = id;
         this.userName = userName;
         this.schemas = schemas;
@@ -48,13 +57,14 @@ public class Identity implements Principal{
         return metaData;
     }
 
-    protected final static class Anonymous extends Identity{
+    protected final static class AnonymousIdentity extends Identity {
+        // TODO: Determine risk of collision when generating random UUID
         private final static UUID ID = new UUID(0L, 0L);
         private final static String userName = "Anonymous Panda";
         private final static List<String> schemas = Collections.emptyList();
         private final static Map<String, String> metaData = Collections.emptyMap();
 
-        protected Anonymous() {
+        protected AnonymousIdentity() {
             super(ID, userName, schemas, metaData);
         }
     }
--- server/src/main/java/org/opensearch/identity/Principal.java
+++ server/src/main/java/org/opensearch/identity/Principal.java
@@ -15,13 +15,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * A principal identifier (user or a group) for extensions requests
+ * The implementation should be serializable/deserializable
+*/
+
+// TODO: Determine how this principal can be shared between threads
 public interface Principal {
+    // TODO: Determine how to handle blank or null users/groups (if there is such possibility)
+    /* A unique identifier for each entity */
     UUID getId();
 
+    /* Contains principal type definition */
     List<String> getSchemas();
 
+    /* A non-unique human readable identifier */
     String getUserName();
 
+    /* Contains metadata relevant to entity*/
     Map<String, String> getMetaData();
 
 }
