09d64176fb396bbee16abb5e352e412c7bf3fab1
Rishab Nahata <rnnahata@amazon.com>
2022-08-25 16:17:26 +0530

    Test files
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

---
+++ server/src/test/java/org/opensearch/cluster/decommission/DecommissionServiceTests.java
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.cluster.decommission;
+
+import org.opensearch.test.OpenSearchTestCase;
+
+public class DecommissionServiceTests extends OpenSearchTestCase {
+}
