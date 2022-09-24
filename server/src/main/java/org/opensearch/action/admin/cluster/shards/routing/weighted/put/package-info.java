e7946d84d7389defbed1b570d411a1e20e0c05bc
pranikum <109206473+pranikum@users.noreply.github.com>
2022-09-24 08:29:04 +0530

    Add package info
    Signed-off-by: pranikum <109206473+pranikum@users.noreply.github.com>

---
+++ server/src/main/java/org/opensearch/action/admin/cluster/shards/routing/weighted/put/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/**
+ * WRR PUT API request/response.
+ */
+package org.opensearch.action.admin.cluster.shards.routing.weighted.put;
