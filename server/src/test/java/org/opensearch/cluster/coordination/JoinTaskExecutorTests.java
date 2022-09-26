a206a08afe0b8d3534260a820493aaf376bb2334
Rishab Nahata <rnnahata@amazon.com>
2022-09-26 12:55:09 +0530

    Update join executor tests
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- server/src/test/java/org/opensearch/cluster/coordination/JoinTaskExecutorTests.java
+++ server/src/test/java/org/opensearch/cluster/coordination/JoinTaskExecutorTests.java
@@ -236,7 +236,6 @@ public class JoinTaskExecutorTests extends OpenSearchTestCase {
         Settings.builder().build();
         DecommissionAttribute decommissionAttribute = new DecommissionAttribute("zone", "zone-1");
         DecommissionStatus decommissionStatus = randomFrom(
-            DecommissionStatus.INIT,
             DecommissionStatus.IN_PROGRESS,
             DecommissionStatus.SUCCESSFUL
         );
@@ -252,7 +251,7 @@ public class JoinTaskExecutorTests extends OpenSearchTestCase {
     public void testJoinClusterWithDifferentDecommission() {
         Settings.builder().build();
         DecommissionAttribute decommissionAttribute = new DecommissionAttribute("zone", "zone-1");
-        DecommissionStatus decommissionStatus = randomFrom(DecommissionStatus.values());
+        DecommissionStatus decommissionStatus = randomFrom(DecommissionStatus.INIT, DecommissionStatus.IN_PROGRESS, DecommissionStatus.SUCCESSFUL);
         DecommissionAttributeMetadata decommissionAttributeMetadata = new DecommissionAttributeMetadata(
             decommissionAttribute,
             decommissionStatus
