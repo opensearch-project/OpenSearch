d872065cae5012800697b4ce9b68670fe9ec9b62
Rishab Nahata <rnnahata@amazon.com>
2022-09-28 15:21:35 +0530

    Fix spotless check
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- server/src/main/java/org/opensearch/discovery/PeerFinder.java
+++ server/src/main/java/org/opensearch/discovery/PeerFinder.java
@@ -144,7 +144,8 @@ public abstract class PeerFinder {
         findPeersInterval = localNodeCommissioned
             ? DISCOVERY_FIND_PEERS_INTERVAL_SETTING.get(settings)
             : DISCOVERY_FIND_PEERS_INTERVAL_DURING_DECOMMISSION_SETTING.get(settings);
-        logger.info("setting findPeersInterval to [{}] as node commission status = [{}] for local node [{}]",
+        logger.info(
+            "setting findPeersInterval to [{}] as node commission status = [{}] for local node [{}]",
             findPeersInterval,
             localNodeCommissioned,
             transportService.getLocalNode()
