3ee67fcd3571fa6cf708f92eab231d2d20adf62e
pranikum <109206473+pranikum@users.noreply.github.com>
2022-10-03 12:59:41 +0530

    Merge conflict with latest
    Signed-off-by: pranikum <109206473+pranikum@users.noreply.github.com>

--- client/rest-high-level/src/test/java/org/opensearch/client/RestHighLevelClientTests.java
+++ client/rest-high-level/src/test/java/org/opensearch/client/RestHighLevelClientTests.java
@@ -891,7 +891,8 @@ public class RestHighLevelClientTests extends OpenSearchTestCase {
             "remote_store.restore",
             "cluster.put_weighted_routing",
             "cluster.put_decommission_awareness",
-            "cluster.get_decommission_awareness", };
+            "cluster.get_decommission_awareness",
+            "cluster.delete_decommission_awareness", };
         List<String> booleanReturnMethods = Arrays.asList("security.enable_user", "security.disable_user", "security.change_password");
         Set<String> deprecatedMethods = new HashSet<>();
         deprecatedMethods.add("indices.force_merge");
