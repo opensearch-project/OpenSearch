opensearch-project/OpenSearch

commit 2c0b4e7bd86f6c1689ff25bc0d49e29b050e8b63

[Backport 2.x] [Point in time] Backport point in time changes (#4616)
* Adding create pit service layer changes

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Adding delete pit service layer changes

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Add service layer changes for list all PITs

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Add Point In Time Node Stats API ServiceLayer Changes

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Add changes to Point in time segments API service layer

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Add changes for Create PIT and Delete PIT rest layer and rest high level client

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Added restlayer changes for pit stats

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* 2.4 specific fixes

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Modified cat shards test for pit stats

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

* Added rest layer changes for List all PITs and PIT segments

Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

server/src/main/java/org/opensearch/index/search/stats/SearchStats.java
@@ -32,6 +32,7 @@
 
 package org.opensearch.index.search.stats;
 
+import org.opensearch.Version;
 import org.opensearch.common.Nullable;
 import org.opensearch.common.Strings;
 import org.opensearch.common.io.stream.StreamInput;
@@ -77,6 +78,10 @@ public class SearchStats implements Writeable, ToXContentFragment {
         private long suggestTimeInMillis;
         private long suggestCurrent;
 
+        private long pitCount;
+        private long pitTimeInMillis;
+        private long pitCurrent;
+
         private Stats() {
             // for internal use, initializes all counts to 0
         }
@@ -91,6 +96,9 @@ public class SearchStats implements Writeable, ToXContentFragment {
             long scrollCount,
             long scrollTimeInMillis,
             long scrollCurrent,
+            long pitCount,
+            long pitTimeInMillis,
+            long pitCurrent,
             long suggestCount,
             long suggestTimeInMillis,
             long suggestCurrent
@@ -110,6 +118,10 @@ public class SearchStats implements Writeable, ToXContentFragment {
             this.suggestCount = suggestCount;
             this.suggestTimeInMillis = suggestTimeInMillis;
             this.suggestCurrent = suggestCurrent;
+
+            this.pitCount = pitCount;
+            this.pitTimeInMillis = pitTimeInMillis;
+            this.pitCurrent = pitCurrent;
         }
 
         private Stats(StreamInput in) throws IOException {
@@ -128,6 +140,12 @@ public class SearchStats implements Writeable, ToXContentFragment {
             suggestCount = in.readVLong();
             suggestTimeInMillis = in.readVLong();
             suggestCurrent = in.readVLong();
+
+            if (in.getVersion().onOrAfter(Version.V_2_4_0)) {
+                pitCount = in.readVLong();
+                pitTimeInMillis = in.readVLong();
+                pitCurrent = in.readVLong();
+            }
         }
 
         public void add(Stats stats) {
@@ -146,6 +164,10 @@ public class SearchStats implements Writeable, ToXContentFragment {
             suggestCount += stats.suggestCount;
             suggestTimeInMillis += stats.suggestTimeInMillis;
             suggestCurrent += stats.suggestCurrent;
+
+            pitCount += stats.pitCount;
+            pitTimeInMillis += stats.pitTimeInMillis;
+            pitCurrent += stats.pitCurrent;
         }
 
         public void addForClosingShard(Stats stats) {
@@ -162,6 +184,10 @@ public class SearchStats implements Writeable, ToXContentFragment {
 
             suggestCount += stats.suggestCount;
             suggestTimeInMillis += stats.suggestTimeInMillis;
+
+            pitCount += stats.pitCount;
+            pitTimeInMillis += stats.pitTimeInMillis;
+            pitCurrent += stats.pitCurrent;
         }
 
         public long getQueryCount() {
@@ -212,6 +238,22 @@ public class SearchStats implements Writeable, ToXContentFragment {
             return scrollCurrent;
         }
 
+        public long getPitCount() {
+            return pitCount;
+        }
+
+        public TimeValue getPitTime() {
+            return new TimeValue(pitTimeInMillis);
+        }
+
+        public long getPitTimeInMillis() {
+            return pitTimeInMillis;
+        }
+
+        public long getPitCurrent() {
+            return pitCurrent;
+        }
+
         public long getSuggestCount() {
             return suggestCount;
         }
@@ -249,6 +291,12 @@ public class SearchStats implements Writeable, ToXContentFragment {
             out.writeVLong(suggestCount);
             out.writeVLong(suggestTimeInMillis);
             out.writeVLong(suggestCurrent);
+
+            if (out.getVersion().onOrAfter(Version.V_2_4_0)) {
+                out.writeVLong(pitCount);
+                out.writeVLong(pitTimeInMillis);
+                out.writeVLong(pitCurrent);
+            }
         }
 
         @Override
@@ -264,6 +312,10 @@ public class SearchStats implements Writeable, ToXContentFragment {
             builder.field(Fields.SCROLL_TOTAL, scrollCount);
             builder.humanReadableField(Fields.SCROLL_TIME_IN_MILLIS, Fields.SCROLL_TIME, getScrollTime());
             builder.field(Fields.SCROLL_CURRENT, scrollCurrent);
+
+            builder.field(Fields.PIT_TOTAL, pitCount);
+            builder.humanReadableField(Fields.PIT_TIME_IN_MILLIS, Fields.PIT_TIME, getPitTime());
+            builder.field(Fields.PIT_CURRENT, pitCurrent);
 
             builder.field(Fields.SUGGEST_TOTAL, suggestCount);
             builder.humanReadableField(Fields.SUGGEST_TIME_IN_MILLIS, Fields.SUGGEST_TIME, getSuggestTime());
@@ -385,6 +437,10 @@ public class SearchStats implements Writeable, ToXContentFragment {
         static final String SCROLL_TIME = "scroll_time";
         static final String SCROLL_TIME_IN_MILLIS = "scroll_time_in_millis";
         static final String SCROLL_CURRENT = "scroll_current";
+        static final String PIT_TOTAL = "point_in_time_total";
+        static final String PIT_TIME = "point_in_time_time";
+        static final String PIT_TIME_IN_MILLIS = "point_in_time_time_in_millis";
+        static final String PIT_CURRENT = "point_in_time_current";
         static final String SUGGEST_TOTAL = "suggest_total";
         static final String SUGGEST_TIME = "suggest_time";
         static final String SUGGEST_TIME_IN_MILLIS = "suggest_time_in_millis";

rest-api-spec/src/main/resources/rest-api-spec/test/cat.shards/10_basic.yml
@@ -1,11 +1,14 @@
 ---
 "Help":
   - skip:
-      version: " - 7.99.99"
-      reason:  shard path stats were added in 8.0.0
+      version: " - 2.3.99"
+      reason:  point in time stats were added in 2.4.0
+      features: node_selector
   - do:
       cat.shards:
         help: true
+      node_selector:
+        version: "2.4.0 - "
 
   - match:
       $body: |
@@ -67,6 +70,9 @@
                     search.scroll_current            .+   \n
                     search.scroll_time               .+   \n
                     search.scroll_total              .+   \n
+                    search.point_in_time_current     .+   \n
+                    search.point_in_time_time        .+   \n
+                    search.point_in_time_total       .+   \n
                     segments.count                   .+   \n
                     segments.memory                  .+   \n
                     segments.index_writer_memory     .+   \n
@@ -81,6 +87,92 @@
                     path.data                        .+   \n
                     path.state                       .+   \n
                $/
+---
+"Help before - 2.4.0":
+  - skip:
+      version: "2.4.0 - "
+      reason:  point in time stats were added in 2.4.0
+      features: node_selector
+  - do:
+      cat.shards:
+        help: true
+      node_selector:
+        version: " - 2.3.99"
+
+  - match:
+      $body: |
+        /^   index                            .+   \n
+             shard                            .+   \n
+             prirep                           .+   \n
+             state                            .+   \n
+             docs                             .+   \n
+             store                            .+   \n
+             ip                               .+   \n
+             id                               .+   \n
+             node                             .+   \n
+             sync_id                          .+   \n
+             unassigned.reason                .+   \n
+             unassigned.at                    .+   \n
+             unassigned.for                   .+   \n
+             unassigned.details               .+   \n
+             recoverysource.type              .+   \n
+             completion.size                  .+   \n
+             fielddata.memory_size            .+   \n
+             fielddata.evictions              .+   \n
+             query_cache.memory_size          .+   \n
+             query_cache.evictions            .+   \n
+             flush.total                      .+   \n
+             flush.total_time                 .+   \n
+             get.current                      .+   \n
+             get.time                         .+   \n
+             get.total                        .+   \n
+             get.exists_time                  .+   \n
+             get.exists_total                 .+   \n
+             get.missing_time                 .+   \n
+             get.missing_total                .+   \n
+             indexing.delete_current          .+   \n
+             indexing.delete_time             .+   \n
+             indexing.delete_total            .+   \n
+             indexing.index_current           .+   \n
+             indexing.index_time              .+   \n
+             indexing.index_total             .+   \n
+             indexing.index_failed            .+   \n
+             merges.current                   .+   \n
+             merges.current_docs              .+   \n
+             merges.current_size              .+   \n
+             merges.total                     .+   \n
+             merges.total_docs                .+   \n
+             merges.total_size                .+   \n
+             merges.total_time                .+   \n
+             refresh.total                    .+   \n
+             refresh.time                     .+   \n
+             refresh.external_total           .+   \n
+             refresh.external_time            .+   \n
+             refresh.listeners                .+   \n
+             search.fetch_current             .+   \n
+             search.fetch_time                .+   \n
+             search.fetch_total               .+   \n
+             search.open_contexts             .+   \n
+             search.query_current             .+   \n
+             search.query_time                .+   \n
+             search.query_total               .+   \n
+             search.scroll_current            .+   \n
+             search.scroll_time               .+   \n
+             search.scroll_total              .+   \n
+             segments.count                   .+   \n
+             segments.memory                  .+   \n
+             segments.index_writer_memory     .+   \n
+             segments.version_map_memory      .+   \n
+             segments.fixed_bitset_memory     .+   \n
+             seq_no.max                       .+   \n
+             seq_no.local_checkpoint          .+   \n
+             seq_no.global_checkpoint         .+   \n
+             warmer.current                   .+   \n
+             warmer.total                     .+   \n
+             warmer.total_time                .+   \n
+             path.data                        .+   \n
+             path.state                       .+   \n
+        $/
 ---
 "Test cat shards output":
 

rest-api-spec/src/main/resources/rest-api-spec/test/pit/10_basic.yml
@@ -1,0 +1,142 @@
+"Create PIT, Search with PIT ID and Delete":
+  - skip:
+      version: " - 2.3.99"
+      reason: "mode to be introduced later than 2.4"
+  - do:
+      indices.create:
+        index: test_pit
+  - do:
+      index:
+        index:  test_pit
+        id:     42
+        body:   { foo: 1 }
+
+  - do:
+      index:
+        index:  test_pit
+        id:     43
+        body:   { foo: 2 }
+
+  - do:
+      indices.refresh: {}
+
+  - do:
+      create_pit:
+        allow_partial_pit_creation: true
+        index: test_pit
+        keep_alive: 23h
+
+  - set: {pit_id: pit_id}
+  - match:   { _shards.failed: 0}
+  - do:
+      search:
+        rest_total_hits_as_int: true
+        size: 1
+        sort: foo
+        body:
+          query:
+            match_all: {}
+          pit: {"id": "$pit_id"}
+
+  - match: {hits.total:      2    }
+  - length: {hits.hits:      1    }
+  - match: {hits.hits.0._id: "42" }
+
+  - do:
+      index:
+        index:  test_pit
+        id:     44
+        body:   { foo: 3 }
+
+  - do:
+      indices.refresh: {}
+
+  - do:
+      search:
+        rest_total_hits_as_int: true
+        size: 1
+        sort: foo
+        body:
+          query:
+            match_all: {}
+          pit: {"id": "$pit_id", "keep_alive":"10m"}
+
+  - match: {hits.total:      2    }
+  - length: {hits.hits:      1    }
+  - match: {hits.hits.0._id: "42" }
+
+
+  - do:
+      search:
+        rest_total_hits_as_int: true
+        index: test_pit
+        size: 1
+        sort: foo
+        body:
+          query:
+            match_all: {}
+
+  - match: {hits.total:      3    }
+  - length: {hits.hits:      1    }
+
+  - do:
+      get_all_pits: {}
+
+  - match: {pits.0.pit_id: $pit_id}
+  - match: {pits.0.keep_alive: 82800000 }
+
+  - do:
+      delete_pit:
+        body:
+          "pit_id": [$pit_id]
+
+  - match: {pits.0.pit_id: $pit_id}
+  - match: {pits.0.successful: true }
+
+---
+"Delete all":
+  - skip:
+      version: " - 2.3.99"
+      reason: "mode to be introduced later than 2.4"
+  - do:
+      indices.create:
+        index: test_pit
+  - do:
+      index:
+        index:  test_pit
+        id:     42
+        body:   { foo: 1 }
+
+  - do:
+      index:
+        index:  test_pit
+        id:     43
+        body:   { foo: 2 }
+
+  - do:
+      indices.refresh: {}
+
+  - do:
+      create_pit:
+        allow_partial_pit_creation: true
+        index: test_pit
+        keep_alive: 23h
+
+  - set: {pit_id: pit_id}
+  - match:   { _shards.failed: 0}
+
+  - do:
+      get_all_pits: {}
+
+  - match: {pits.0.pit_id: $pit_id}
+  - match: {pits.0.keep_alive: 82800000 }
+
+  - do:
+      delete_all_pits: {}
+
+  - match: {pits.0.pit_id: $pit_id}
+  - match: {pits.0.successful: true }
+
+  - do:
+      catch: missing
+      delete_all_pits: { }

commit 53a206b063a15cb0531eeefa3f65918197b4d134

Fixing SearchStats (de)serialization (caused by https://github.com/opensearch-project/OpenSearch/pull/4616)
Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

server/src/main/java/org/opensearch/index/search/stats/SearchStats.java
@@ -141,7 +141,7 @@ public class SearchStats implements Writeable, ToXContentFragment {
             suggestTimeInMillis = in.readVLong();
             suggestCurrent = in.readVLong();
 
-            if (in.getVersion().onOrAfter(Version.V_3_0_0)) {
+            if (in.getVersion().onOrAfter(Version.V_2_4_0)) {
                 pitCount = in.readVLong();
                 pitTimeInMillis = in.readVLong();
                 pitCurrent = in.readVLong();
@@ -292,7 +292,7 @@ public class SearchStats implements Writeable, ToXContentFragment {
             out.writeVLong(suggestTimeInMillis);
             out.writeVLong(suggestCurrent);
 
-            if (out.getVersion().onOrAfter(Version.V_3_0_0)) {
+            if (out.getVersion().onOrAfter(Version.V_2_4_0)) {
                 out.writeVLong(pitCount);
                 out.writeVLong(pitTimeInMillis);
                 out.writeVLong(pitCurrent);

rest-api-spec/src/main/resources/rest-api-spec/test/cat.shards/10_basic.yml
@@ -1,14 +1,14 @@
 ---
 "Help":
   - skip:
-      version: " - 2.9.99"
-      reason:  point in time stats were added in 3.0.0
+      version: " - 2.3.99"
+      reason:  point in time stats were added in 2.4.0
       features: node_selector
   - do:
       cat.shards:
         help: true
       node_selector:
-        version: "3.0.0 - "
+        version: "2.4.0 - "
 
   - match:
       $body: |
@@ -88,16 +88,16 @@
                     path.state                       .+   \n
                $/
 ---
-"Help before - 3.0.0":
+"Help before - 2.4.0":
   - skip:
-      version: "3.0.0 - "
-      reason:  point in time stats were added in 3.0.0
+      version: "2.4.0 - "
+      reason:  point in time stats were added in 2.4.0
       features: node_selector
   - do:
       cat.shards:
         help: true
       node_selector:
-        version: " - 2.9.99"
+        version: " - 2.3.99"
 
   - match:
       $body: |

rest-api-spec/src/main/resources/rest-api-spec/test/pit/10_basic.yml
@@ -1,7 +1,7 @@
 "Create PIT, Search with PIT ID and Delete":
   - skip:
-      version: " - 2.9.99"
-      reason: "mode to be introduced later than 3.0"
+      version: " - 2.3.99"
+      reason: "mode to be introduced later than 2.4.0"
   - do:
       indices.create:
         index: test_pit