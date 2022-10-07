474ce8c9e3e000783f93636e16af5a61932d048f
Nicholas Walter Knize <nknize@apache.org>
2022-10-07 09:12:20 -0500

    spotless
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- server/src/main/java/org/opensearch/cluster/metadata/Metadata.java
+++ server/src/main/java/org/opensearch/cluster/metadata/Metadata.java
@@ -39,7 +39,6 @@ import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.lucene.util.CollectionUtil;
-import org.opensearch.LegacyESVersion;
 import org.opensearch.action.AliasesRequest;
 import org.opensearch.cluster.ClusterState;
 import org.opensearch.cluster.ClusterState.FeatureAware;
