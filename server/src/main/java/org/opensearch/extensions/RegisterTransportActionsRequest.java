58f2c6ecdc99383d1f63b9dddd73bedc3ba63479
Frank Lou <107960841+mloufra@users.noreply.github.com>
2022-10-21 12:16:49 -0700

    [Feature/extensions]Enforce type safety for RegisterTransportActionsRequest (#4796)
    * fix compiler error for test file
    
    Signed-off-by: mloufra <mloufra@amazon.com>
    
    * fix test class RegisterTransportActionsRequestTests
    
    Signed-off-by: mloufra <mloufra@amazon.com>
    
    Signed-off-by: mloufra <mloufra@amazon.com>

--- server/src/main/java/org/opensearch/extensions/RegisterTransportActionsRequest.java
+++ server/src/main/java/org/opensearch/extensions/RegisterTransportActionsRequest.java
@@ -8,6 +8,9 @@
 
 package org.opensearch.extensions;
 
+import org.opensearch.action.ActionRequest;
+import org.opensearch.action.ActionResponse;
+import org.opensearch.action.support.TransportAction;
 import org.opensearch.common.io.stream.StreamInput;
 import org.opensearch.common.io.stream.StreamOutput;
 import org.opensearch.transport.TransportRequest;
@@ -16,6 +19,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Map.Entry;
 
 /**
  * Request to register extension Transport actions
@@ -24,9 +28,12 @@ import java.util.Objects;
  */
 public class RegisterTransportActionsRequest extends TransportRequest {
     private String uniqueId;
-    private Map<String, Class> transportActions;
+    private Map<String, Class<? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>>> transportActions;
 
-    public RegisterTransportActionsRequest(String uniqueId, Map<String, Class> transportActions) {
+    public RegisterTransportActionsRequest(
+        String uniqueId,
+        Map<String, Class<? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>>> transportActions
+    ) {
         this.uniqueId = uniqueId;
         this.transportActions = new HashMap<>(transportActions);
     }
@@ -34,12 +41,14 @@ public class RegisterTransportActionsRequest extends TransportRequest {
     public RegisterTransportActionsRequest(StreamInput in) throws IOException {
         super(in);
         this.uniqueId = in.readString();
-        Map<String, Class> actions = new HashMap<>();
+        Map<String, Class<? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>>> actions = new HashMap<>();
         int actionCount = in.readVInt();
         for (int i = 0; i < actionCount; i++) {
             try {
                 String actionName = in.readString();
-                Class transportAction = Class.forName(in.readString());
+                @SuppressWarnings("unchecked")
+                Class<? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>> transportAction = (Class<
+                    ? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>>) Class.forName(in.readString());
                 actions.put(actionName, transportAction);
             } catch (ClassNotFoundException e) {
                 throw new IllegalArgumentException("Could not read transport action");
@@ -52,7 +61,9 @@ public class RegisterTransportActionsRequest extends TransportRequest {
         return uniqueId;
     }
 
-    public Map<String, Class> getTransportActions() {
+    /// comments
+
+    public Map<String, Class<? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>>> getTransportActions() {
         return transportActions;
     }
 
@@ -61,7 +72,8 @@ public class RegisterTransportActionsRequest extends TransportRequest {
         super.writeTo(out);
         out.writeString(uniqueId);
         out.writeVInt(this.transportActions.size());
-        for (Map.Entry<String, Class> action : transportActions.entrySet()) {
+        for (Entry<String, Class<? extends TransportAction<? extends ActionRequest, ? extends ActionResponse>>> action : transportActions
+            .entrySet()) {
             out.writeString(action.getKey());
             out.writeString(action.getValue().getName());
         }
