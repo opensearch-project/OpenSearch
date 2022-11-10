4fc5c8d3fe17a87d2dc6e14a4d1c153b2e06e3e0
Rabi Panda <adnapibar@gmail.com>
2022-11-10 13:48:41 -0800

    Check for JDK version while setting java.security.manager option (#5205)
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- buildSrc/src/main/java/org/opensearch/gradle/OpenSearchTestBasePlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/OpenSearchTestBasePlugin.java
@@ -109,7 +109,10 @@ public class OpenSearchTestBasePlugin implements Plugin<Project> {
                         test.systemProperty("java.locale.providers", "SPI,JRE");
                     } else {
                         test.systemProperty("java.locale.providers", "SPI,COMPAT");
-                        test.jvmArgs("--illegal-access=warn", "-Djava.security.manager=allow");
+                        test.jvmArgs("--illegal-access=warn");
+                    }
+                    if (test.getJavaVersion().compareTo(JavaVersion.VERSION_17) > 0) {
+                        test.jvmArgs("-Djava.security.manager=allow");
                     }
                 }
             });
