e510c1c430c7eb7c05859f144c3d36edb5f04331
Joshua Palis <jpalis@amazon.com>
2022-11-10 20:29:21 +0000

    Relocating job scheduler sample extension to plugins/examples/job-scheduler-sample-extension without the test directory as integration/bwd tests are not currently supported for native plugins
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- plugins/examples/job-scheduler-sample-extension/src/main/java/org/opensearch/example/jobschedulersampleextension/SampleExtensionRestHandler.java
+++ plugins/examples/job-scheduler-sample-extension/src/main/java/org/opensearch/example/jobschedulersampleextension/SampleExtensionRestHandler.java
@@ -1,9 +1,12 @@
 /*
- * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
  */
 
-package org.opensearch.jobscheduler.sampleextension;
+package org.opensearch.example.jobschedulersampleextension;
 
 import org.opensearch.action.support.WriteRequest;
 import org.opensearch.jobscheduler.spi.schedule.IntervalSchedule;
