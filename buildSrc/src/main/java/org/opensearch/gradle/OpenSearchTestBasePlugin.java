059dfa9dd3af46afd05b15440320be53e1d4a82e
Rabi Panda <adnapibar@gmail.com>
2022-11-10 20:58:53 +0000

    Use test.getJavaVersion instead of BuildParams
    Signed-off-by: Rabi Panda <adnapibar@gmail.com>

--- buildSrc/src/main/java/org/opensearch/gradle/OpenSearchTestBasePlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/OpenSearchTestBasePlugin.java
@@ -111,8 +111,7 @@ public class OpenSearchTestBasePlugin implements Plugin<Project> {
                         test.systemProperty("java.locale.providers", "SPI,COMPAT");
                         test.jvmArgs("--illegal-access=warn");
                     }
-                    if (BuildParams.getIsRuntimeJavaHomeSet() == false
-                        || BuildParams.getRuntimeJavaVersion().compareTo(JavaVersion.VERSION_17) > 0) {
+                    if (test.getJavaVersion().compareTo(JavaVersion.VERSION_17) > 0) {
                         test.jvmArgs("-Djava.security.manager=allow");
                     }
                 }
