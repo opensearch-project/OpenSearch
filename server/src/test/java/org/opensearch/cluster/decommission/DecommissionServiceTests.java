eb35a3d1e7b3cf3526ed6250dc082ad1a1e20a8a
Rishab Nahata <rnnahata@amazon.com>
2022-11-18 15:36:08 +0530

    Fix spotless check
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- server/src/test/java/org/opensearch/cluster/decommission/DecommissionServiceTests.java
+++ server/src/test/java/org/opensearch/cluster/decommission/DecommissionServiceTests.java
@@ -34,7 +34,6 @@ import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.ClusterSettings;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.unit.TimeValue;
-import org.opensearch.rest.RestStatus;
 import org.opensearch.test.ClusterServiceUtils;
 import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.test.transport.MockTransport;
