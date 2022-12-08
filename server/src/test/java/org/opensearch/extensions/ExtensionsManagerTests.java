b93d41f1f037247ee11d3cc432721f5a9846ce75
Ryan Bogan <rbogan@amazon.com>
2022-12-08 19:47:24 +0000

    Fix ExtensionsManagerTests
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- server/src/test/java/org/opensearch/extensions/ExtensionsManagerTests.java
+++ server/src/test/java/org/opensearch/extensions/ExtensionsManagerTests.java
@@ -405,7 +405,7 @@ public class ExtensionsManagerTests extends OpenSearchTestCase {
                 new MockLogAppender.SeenEventExpectation(
                     "IndicesModuleRequest Failure",
                     "org.opensearch.extensions.ExtensionsManager",
-                    Level.INFO,
+                    Level.ERROR,
                     "IndicesModuleRequest failed"
                 )
             );
