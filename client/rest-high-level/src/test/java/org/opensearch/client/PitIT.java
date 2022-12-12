e22dc9d2f2296db721613dd4cc4a0a213579c8f0
Craig Perkins <cwperx@amazon.com>
2022-12-12 16:38:05 -0500

    Use AwaitsFix instead of Ignore
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- client/rest-high-level/src/test/java/org/opensearch/client/PitIT.java
+++ client/rest-high-level/src/test/java/org/opensearch/client/PitIT.java
@@ -74,7 +74,7 @@ public class PitIT extends OpenSearchRestHighLevelClientTestCase {
     }
 
     // TODO Figure out why this test is failing with identity module
-    @Ignore
+    @AwaitsFix(bugUrl = "")
     public void testDeleteAllAndListAllPits() throws IOException, InterruptedException {
         CreatePitRequest pitRequest = new CreatePitRequest(new TimeValue(1, TimeUnit.DAYS), true, "index");
         CreatePitResponse pitResponse = execute(pitRequest, highLevelClient()::createPit, highLevelClient()::createPitAsync);
