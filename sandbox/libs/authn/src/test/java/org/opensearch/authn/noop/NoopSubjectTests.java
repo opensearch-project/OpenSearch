54d1b48c08fdaf2e689741ec1a89acd0d355c066
Craig Perkins <cwperx@amazon.com>
2022-12-12 11:37:31 -0500

    Move noop tests to opensearch-authn
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- sandbox/libs/authn/src/test/java/org/opensearch/authn/noop/NoopSubjectTests.java
+++ sandbox/libs/authn/src/test/java/org/opensearch/authn/noop/NoopSubjectTests.java
@@ -1,9 +1,12 @@
 /*
- * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
  */
 
-package org.opensearch.identity.noop;
+package org.opensearch.authn.noop;
 
 import org.opensearch.authn.noop.NoopSubject;
 import org.opensearch.test.OpenSearchTestCase;
