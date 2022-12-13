369fedfd7d37dc5e3608ebdcd6e2279594ed21b3
Stephen Crawford <steecraw@amazon.com>
2022-12-13 10:05:14 -0500

    Bearer Auth Tests
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- sandbox/libs/authn/src/main/java/org/opensearch/authn/jwt/JwtVendor.java
+++ sandbox/libs/authn/src/main/java/org/opensearch/authn/jwt/JwtVendor.java
@@ -42,6 +42,16 @@ public class JwtVendor {
         return jwk;
     }
 
+    static JsonWebKey getESJsonWebKey() {
+        JsonWebKey jwk = new JsonWebKey();
+        jwk.setKeyType(KeyType.OCTET);
+        jwk.setAlgorithm("ES256");
+        jwk.setPublicKeyUse(PublicKeyUse.SIGN);
+        String b64SigningKey = Base64.getEncoder().encodeToString("exchangeKey".getBytes(StandardCharsets.UTF_8));
+        jwk.setProperty("k", b64SigningKey);
+        return jwk;
+    }
+
     public static String createJwt(Map<String, String> claims) {
         JoseJwtProducer jwtProducer = new JoseJwtProducer();
         jwtProducer.setSignatureProvider(JwsUtils.getSignatureProvider(getDefaultJsonWebKey()));
@@ -126,6 +136,44 @@ public class JwtVendor {
             jwtClaims.setProperty("sub", claims.get("sub"));
         } else {
             jwtClaims.setProperty("sub", "example_subject");
+        }
+
+        String encodedJwt = jwtProducer.processJwt(jwt);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                "Created JWT: "
+                    + encodedJwt
+                    + "\n"
+                    + jsonMapReaderWriter.toJson(jwt.getJwsHeaders())
+                    + "\n"
+                    + JwtUtils.claimsToJson(jwt.getClaims())
+            );
+        }
+
+        return encodedJwt;
+    }
+
+    public static String createInvalidJwt(Map<String, String> claims) {
+        JoseJwtProducer jwtProducer = new JoseJwtProducer();
+        jwtProducer.setSignatureProvider(JwsUtils.getSignatureProvider(getESJsonWebKey()));
+        JwtClaims jwtClaims = new JwtClaims();
+        JwtToken jwt = new JwtToken(jwtClaims);
+
+        jwtClaims.setNotBefore(System.currentTimeMillis() / 1000);
+        long expiryTime = System.currentTimeMillis() / 1000 + (60 * 60);
+        jwtClaims.setExpiryTime(expiryTime);
+
+        if (claims.containsKey("sub")) {
+            jwtClaims.setProperty("sub", claims.get("sub"));
+        } else {
+            jwtClaims.setProperty("sub", "example_subject");
+        }
+
+        if (claims.containsKey("iat")) {
+            jwtClaims.setProperty("iat", claims.get("iat"));
+        } else {
+            jwtClaims.setProperty("iat", Instant.now().toString());
         }
 
         String encodedJwt = jwtProducer.processJwt(jwt);
--- sandbox/modules/identity/src/test/java/org/opensearch/identity/BearerAuthTests.java
+++ sandbox/modules/identity/src/test/java/org/opensearch/identity/BearerAuthTests.java
@@ -31,8 +31,8 @@ import static org.opensearch.authn.AuthenticationTokenHandler.extractShiroAuthTo
 public class BearerAuthTests extends OpenSearchTestCase {
 
     public void testExpiredValidJwt() {
-        Map<String, String> jwtClaims = new HashMap<>();
 
+        Map<String, String> jwtClaims = new HashMap<>();
         jwtClaims.put("sub", "testSubject");
         String encodedToken = JwtVendor.createExpiredJwt(jwtClaims);
         String headerBody = "Bearer " + encodedToken;
@@ -47,8 +47,8 @@ public class BearerAuthTests extends OpenSearchTestCase {
     }
 
     public void testEarlyValidJwt() {
-        Map<String, String> jwtClaims = new HashMap<>();
 
+        Map<String, String> jwtClaims = new HashMap<>();
         jwtClaims.put("sub", "testSubject");
         String encodedToken = JwtVendor.createEarlyJwt(jwtClaims);
         String headerBody = "Bearer " + encodedToken;
@@ -63,14 +63,14 @@ public class BearerAuthTests extends OpenSearchTestCase {
     }
 
     public void testValidJwt() {
-        Map<String, String> jwtClaims = new HashMap<>();
 
+        Map<String, String> jwtClaims = new HashMap<>();
         jwtClaims.put("sub", "testSubject");
         String encodedToken = JwtVendor.createJwt(jwtClaims);
         String headerBody = "Bearer " + encodedToken;
         BearerAuthToken bearerAuthToken = new BearerAuthToken(headerBody);
-
         AuthenticationToken extractedShiroToken;
+
         try {
             extractedShiroToken = extractShiroAuthToken(bearerAuthToken); // This should verify and then extract the shiro token for login
         } catch (RuntimeException ex) {
@@ -82,16 +82,16 @@ public class BearerAuthTests extends OpenSearchTestCase {
     }
 
     public void testInvalidJwt() {
-        // TODO: Token should fail because of attempt to verify incorrect signature -- need to make it try to decode with "HSA512"
+
         Map<String, String> jwtClaims = new HashMap<>();
         jwtClaims.put("sub", "testSubject");
 
-        String encodedToken = JwtVendor.createJwt(jwtClaims);
-
+        String encodedToken = JwtVendor.createInvalidJwt(jwtClaims);
         try {
             JwtToken token = JwtVerifier.getVerifiedJwtToken(encodedToken);
-        } catch (RuntimeException e) {
-            fail("Unexpected RuntimeException thrown");
+        } catch (RuntimeException ex) {
+            assertFalse(ex.getMessage().isEmpty());
+            assertEquals("Invalid JWT signature", ex.getMessage());
         }
         throw new Error("Function should have failed when trying to verify JWT with incorrect signature.");
     }
@@ -106,9 +106,7 @@ public class BearerAuthTests extends OpenSearchTestCase {
         RequestOptions options = RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", headerBody).build();
         request.setOptions(options);
         Response response = OpenSearchRestTestCase.client().performRequest(request);
-
         OpenSearchRestTestCase.assertOK(response);
-
         // Standard cluster health response
         MatcherAssert.assertThat(OpenSearchRestTestCase.entityAsMap(response).size(), equalTo(17));
         MatcherAssert.assertThat(OpenSearchRestTestCase.entityAsMap(response).get("status"), equalTo("green"));
@@ -124,41 +122,33 @@ public class BearerAuthTests extends OpenSearchTestCase {
 
         Request request = new Request("GET", "/_cluster/health");
         RequestOptions options = RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", headerBody).build();
-
         request.setOptions(options);
-        Response response = OpenSearchRestTestCase.client().performRequest(request);
-
-        // Should not fail, because current implementation allows a unauthorized request to pass
-        // TODO: Update this to test for UNAUTHORIZED once that flow is implemented
-        OpenSearchRestTestCase.assertOK(response);
-
-        // Standard cluster health response
-        MatcherAssert.assertThat(OpenSearchRestTestCase.entityAsMap(response).size(), equalTo(17));
-        MatcherAssert.assertThat(OpenSearchRestTestCase.entityAsMap(response).get("status"), equalTo("green"));
+        // Should be unauthorized since JWT is expired
+        try {
+            OpenSearchRestTestCase.client().performRequest(request);
+        } catch (ResponseException e) {
+            MatcherAssert.assertThat(e.getResponse().getStatusLine().getStatusCode(), equalTo(403));
+        }
     }
 
-    public void testClusterHealthWithInvalidBearerAuthenticationHeader() throws IOException { // Should have this use the createInvalidJwt
-        // method once that is created.
+    public void testClusterHealthWithInvalidBearerAuthenticationHeader() throws IOException {
 
         Map<String, String> jwtClaims = new HashMap<>();
         jwtClaims.put("sub", "testSubject");
-
-        String encodedToken = JwtVendor.createExpiredJwt(jwtClaims);
+        String encodedToken = JwtVendor.createInvalidJwt(jwtClaims);
         String headerBody = "Bearer " + encodedToken;
+
         Request request = new Request("GET", "/_cluster/health");
         RequestOptions options = RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", headerBody).build();
-
         request.setOptions(options);
         Response response = OpenSearchRestTestCase.client().performRequest(request);
-
-        // Should not fail, because current implementation allows a unauthorized request to pass
-        // TODO: Update this to test for UNAUTHORIZED once that flow is implemented
-        OpenSearchRestTestCase.assertOK(response);
-
-        // Standard cluster health response
-        MatcherAssert.assertThat(OpenSearchRestTestCase.entityAsMap(response).size(), equalTo(17));
-        MatcherAssert.assertThat(OpenSearchRestTestCase.entityAsMap(response).get("status"), equalTo("green"));
-
+        // Should be unauthorized because created with a different signing algorithm
+        //  Current implementation allows a unauthorized request to pass
+        try {
+            OpenSearchRestTestCase.client().performRequest(request);
+        } catch (ResponseException e) {
+            MatcherAssert.assertThat(e.getResponse().getStatusLine().getStatusCode(), equalTo(403));
+        }
     }
 
     public void testClusterHealthWithCorruptBearerAuthenticationHeader() throws IOException {
@@ -167,12 +157,11 @@ public class BearerAuthTests extends OpenSearchTestCase {
 
         Request request = new Request("GET", "/_cluster/health");
         RequestOptions options = RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", headerBody).build();
-
         request.setOptions(options);
         try {
             OpenSearchRestTestCase.client().performRequest(request);
         } catch (ResponseException e) {
-            MatcherAssert.assertThat(e.getResponse().getStatusLine().getStatusCode(), equalTo(405));
+            MatcherAssert.assertThat(e.getResponse().getStatusLine().getStatusCode(), equalTo(403));
         }
     }
 }
