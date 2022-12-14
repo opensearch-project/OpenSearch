1c655059c464c3d2d0fb032b24aedf912ff28a35
Louis Chu <lingzhichu.clz@gmail.com>
2022-12-14 14:39:58 -0500

    Refactor fuzziness interface on query builders (#5433)
    * Refactor Object to Fuzziness type for all query builders
    
    Signed-off-by: noCharger <lingzhichu.clz@gmail.com>
    
    * Revise on bwc
    
    Signed-off-by: noCharger <lingzhichu.clz@gmail.com>
    
    * Update change log
    
    Signed-off-by: noCharger <lingzhichu.clz@gmail.com>
    
    Signed-off-by: noCharger <lingzhichu.clz@gmail.com>
    Co-authored-by: Daniel (dB.) Doubrovkine <dblock@amazon.com>
    (cherry picked from commit d3f6dfab89720df155ee9cb4789aa642c2238057)
    Signed-off-by: Louis Chu <clingzhi@amazon.com>

--- server/src/main/java/org/opensearch/index/query/MultiMatchQueryBuilder.java
+++ server/src/main/java/org/opensearch/index/query/MultiMatchQueryBuilder.java
@@ -404,6 +404,7 @@ public class MultiMatchQueryBuilder extends AbstractQueryBuilder<MultiMatchQuery
         return slop;
     }
 
+    @Deprecated
     /**
      * Sets the fuzziness used when evaluated to a fuzzy query type. Defaults to "AUTO".
      */
@@ -411,6 +412,14 @@ public class MultiMatchQueryBuilder extends AbstractQueryBuilder<MultiMatchQuery
         if (fuzziness != null) {
             this.fuzziness = Fuzziness.build(fuzziness);
         }
+        return this;
+    }
+
+    /**
+     * Sets the fuzziness used when evaluated to a fuzzy query type. Defaults to "AUTO".
+     */
+    public MultiMatchQueryBuilder fuzziness(Fuzziness fuzziness) {
+        this.fuzziness = fuzziness;
         return this;
     }
 
