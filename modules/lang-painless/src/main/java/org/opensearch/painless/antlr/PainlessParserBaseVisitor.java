306b199b5e85124b953c080fe474e3c34ad0f5bc
Andriy Redko <andriy.redko@aiven.io>
2022-12-19 18:46:56 -0500

    Bump antlr4 from 4.9.3 to 4.11.1 (#4546)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- modules/lang-painless/src/main/java/org/opensearch/painless/antlr/PainlessParserBaseVisitor.java
+++ modules/lang-painless/src/main/java/org/opensearch/painless/antlr/PainlessParserBaseVisitor.java
@@ -43,6 +43,7 @@ import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
  * @param <T> The return type of the visit operation. Use {@link Void} for
  * operations with no return type.
  */
+@SuppressWarnings("CheckReturnValue")
 class PainlessParserBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements PainlessParserVisitor<T> {
     /**
      * {@inheritDoc}
