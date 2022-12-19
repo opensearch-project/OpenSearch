882ff3be3ecf1d132595190eb3be08183e7114a6
Priya Seth <sethp@us.ibm.com>
2022-12-19 10:52:41 +0530

    add support for ppc64le architecture (#5459)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- buildSrc/src/main/java/org/opensearch/gradle/Jdk.java
+++ buildSrc/src/main/java/org/opensearch/gradle/Jdk.java
@@ -48,7 +48,7 @@ import java.util.regex.Pattern;
 
 public class Jdk implements Buildable, Iterable<File> {
 
-    private static final List<String> ALLOWED_ARCHITECTURES = Collections.unmodifiableList(Arrays.asList("aarch64", "x64"));
+    private static final List<String> ALLOWED_ARCHITECTURES = Collections.unmodifiableList(Arrays.asList("aarch64", "x64", "ppc64le"));
     private static final List<String> ALLOWED_VENDORS = Collections.unmodifiableList(Arrays.asList("adoptium", "adoptopenjdk", "openjdk"));
     private static final List<String> ALLOWED_PLATFORMS = Collections.unmodifiableList(
         Arrays.asList("darwin", "freebsd", "linux", "mac", "windows")
