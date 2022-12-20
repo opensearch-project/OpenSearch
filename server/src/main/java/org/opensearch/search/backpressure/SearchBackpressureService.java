72c5d8700e04ad292afcbe32855c2bb598366ac9
PritLadani <pritkladani@gmail.com>
2022-12-20 14:05:39 +0530

    java code formatting
    Signed-off-by: PritLadani <pritkladani@gmail.com>

--- server/src/main/java/org/opensearch/search/backpressure/SearchBackpressureService.java
+++ server/src/main/java/org/opensearch/search/backpressure/SearchBackpressureService.java
@@ -379,9 +379,7 @@ public class SearchBackpressureService extends AbstractLifecycleComponent
             searchBackpressureStates.get(SearchTask.class).getLimitReachedCount(),
             taskResourceUsageTrackers.stream()
                 .collect(
-                    Collectors.toUnmodifiableMap(
-                        t -> TaskResourceUsageTrackerType.fromName(t.name()),
-                        t -> t.searchTaskStats(searchTasks))
+                    Collectors.toUnmodifiableMap(t -> TaskResourceUsageTrackerType.fromName(t.name()), t -> t.searchTaskStats(searchTasks))
                 )
         );
 
