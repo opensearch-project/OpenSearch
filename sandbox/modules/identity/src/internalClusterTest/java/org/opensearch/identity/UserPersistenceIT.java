954f87a4c28ebaee892e00e29d675177df7be1a0
Craig Perkins <cwperx@amazon.com>
2022-12-21 13:17:35 -0500

    Add integration test to ensure identity module is initialized with .identity_config index
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- sandbox/modules/identity/src/internalClusterTest/java/org/opensearch/identity/UserPersistenceIT.java
+++ sandbox/modules/identity/src/internalClusterTest/java/org/opensearch/identity/UserPersistenceIT.java
@@ -8,7 +8,11 @@
 
 package org.opensearch.identity;
 
+import org.opensearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.opensearch.action.admin.cluster.node.info.NodeInfo;
 import org.opensearch.action.admin.cluster.state.ClusterStateResponse;
+import org.opensearch.cluster.health.ClusterHealthStatus;
+import org.opensearch.cluster.health.ClusterIndexHealth;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.test.OpenSearchIntegTestCase;
 
@@ -16,6 +20,8 @@ import java.io.File;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertNoTimeout;
 
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.TEST, numDataNodes = 0)
 public class UserPersistenceIT extends HttpSmokeTestCaseWithIdentity {
@@ -44,6 +50,33 @@ public class UserPersistenceIT extends HttpSmokeTestCaseWithIdentity {
             .get();
         assertNotNull(clusterStateResponse.getState().nodes().getClusterManagerNodeId());
 
-        ensureGreen(ConfigConstants.IDENTITY_CONFIG_INDEX_NAME);
+        // start another node
+        final String dataNode = internalCluster().startDataOnlyNode();
+        clusterStateResponse = client(dataNode).admin()
+            .cluster()
+            .prepareState()
+            .setClusterManagerNodeTimeout("1s")
+            .clear()
+            .setNodes(true)
+            .setLocal(true)
+            .get();
+        assertNotNull(clusterStateResponse.getState().nodes().getClusterManagerNodeId());
+        // wait for the cluster to form
+        assertNoTimeout(client().admin().cluster().prepareHealth().setWaitForNodes(Integer.toString(2)).get());
+        List<NodeInfo> nodeInfos = client().admin().cluster().prepareNodesInfo().get().getNodes();
+        assertEquals(2, nodeInfos.size());
+
+        Thread.sleep(3000);
+
+        ClusterHealthResponse clusterHealthResponse = client().admin()
+            .cluster()
+            .prepareHealth()
+            .setClusterManagerNodeTimeout("1s")
+            .get();
+
+        assertTrue(ConfigConstants.IDENTITY_DEFAULT_CONFIG_INDEX + " index exists", clusterHealthResponse.getIndices().containsKey(ConfigConstants.IDENTITY_DEFAULT_CONFIG_INDEX));
+
+        ClusterIndexHealth identityIndexHealth = clusterHealthResponse.getIndices().get(ConfigConstants.IDENTITY_DEFAULT_CONFIG_INDEX);
+        assertEquals(ClusterHealthStatus.GREEN, identityIndexHealth.getStatus());
     }
 }
