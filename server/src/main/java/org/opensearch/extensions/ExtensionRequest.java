3ba2c633e1fcf790020f6c68a8c0e8f7a8202079
Ryan Bogan <10944539+ryanbogan@users.noreply.github.com>
2022-12-23 13:00:17 -0600

    Added getSettings() support, ActionListener onFailure(), and initial createComponents support for extensions (#5621)
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>
    
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- server/src/main/java/org/opensearch/extensions/ExtensionRequest.java
+++ server/src/main/java/org/opensearch/extensions/ExtensionRequest.java
@@ -51,7 +51,6 @@ public class ExtensionRequest extends TransportRequest {
 
     @Override
     public boolean equals(Object o) {
-
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ExtensionRequest that = (ExtensionRequest) o;
@@ -62,5 +61,4 @@ public class ExtensionRequest extends TransportRequest {
     public int hashCode() {
         return Objects.hash(requestType);
     }
-
 }
