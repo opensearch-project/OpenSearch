bc21aba4198113ad661156b0cfb5a50c525f5452
Suraj Singh <surajrider@gmail.com>
2023-01-04 20:52:25 -0800

    Spotless fix
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/internalClusterTest/java/org/opensearch/indices/replication/SegmentReplicationRelocationIT.java
+++ server/src/internalClusterTest/java/org/opensearch/indices/replication/SegmentReplicationRelocationIT.java
@@ -31,7 +31,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertHitCount;
 
 /**
