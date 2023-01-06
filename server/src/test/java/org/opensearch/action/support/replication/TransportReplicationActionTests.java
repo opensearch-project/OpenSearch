ee33c9e01483611c62ecede115d695bc82a67178
Arpit-Bandejiya <109738717+Arpit-Bandejiya@users.noreply.github.com>
2023-01-06 22:17:06 +0530

    Add support of default replica count change (#5610) (#5732)
    *[Backport 2.x] Add default replica count config settings
    
    Signed-off-by: Arpit Bandejiya <abandeji@amazon.com>

--- server/src/test/java/org/opensearch/action/support/replication/TransportReplicationActionTests.java
+++ server/src/test/java/org/opensearch/action/support/replication/TransportReplicationActionTests.java
@@ -201,6 +201,11 @@ public class TransportReplicationActionTests extends OpenSearchTestCase {
         transportService.acceptIncomingRequests();
         shardStateAction = new ShardStateAction(clusterService, transportService, null, null, threadPool);
         action = new TestAction(Settings.EMPTY, "internal:testAction", transportService, clusterService, shardStateAction, threadPool);
+        Metadata metadata = Metadata.builder().build();
+        ClusterState clusterState = ClusterState.builder(org.opensearch.cluster.ClusterName.CLUSTER_NAME_SETTING.getDefault(Settings.EMPTY))
+            .metadata(metadata)
+            .build();
+        setState(clusterService, clusterState);
     }
 
     @Override
