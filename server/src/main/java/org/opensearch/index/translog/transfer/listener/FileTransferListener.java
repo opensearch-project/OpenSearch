d54b370fb6c8f3e76d015d12ffb2f7b2358632a4
Gaurav Bafna <85113518+gbbafna@users.noreply.github.com>
2023-01-09 12:16:50 +0530

    [Remote Translog] Introduce remote translog transfer support (#4480) (#5693)
    * [Backport 2.x] Introduce remote translog transfer support
    
    Signed-off-by: Bukhtawar Khan <bukhtawa@amazon.com>

---
+++ server/src/main/java/org/opensearch/index/translog/transfer/listener/FileTransferListener.java
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.index.translog.transfer.listener;
+
+import org.opensearch.index.translog.transfer.FileSnapshot.TransferFileSnapshot;
+
+/**
+ * The listener to be invoked on the completion or failure of a {@link TransferFileSnapshot}
+ *
+ * @opensearch.internal
+ */
+public interface FileTransferListener {
+
+    /**
+     * Invoked when the transfer of a single {@link TransferFileSnapshot} succeeds
+     * @param fileSnapshot the corresponding file snapshot
+     */
+    void onSuccess(TransferFileSnapshot fileSnapshot);
+
+    /**
+     * Invoked when the transfer of a single {@link TransferFileSnapshot} fails
+     * @param fileSnapshot the corresponding file snapshot
+     * @param e the exception while processing the {@link TransferFileSnapshot}
+     */
+    void onFailure(TransferFileSnapshot fileSnapshot, Exception e);
+}
