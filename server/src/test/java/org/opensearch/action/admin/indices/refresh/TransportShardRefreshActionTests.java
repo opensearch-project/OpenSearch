dec40a94f60a6cf3aa621158cd9839a5b5c70a9b
Sachin Kale <sachinpkale@gmail.com>
2023-01-10 10:32:55 +0530

    Support no-op replication for remote-backed indexes (#5731) (#5774)
    * Remove PRRL creation/deletion in peer recovery of remote store enabled replica (#4954)
    
    * Add RecoverySourceHandlerFactory for extensibility
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * recoverToTarget made extensible to allow multiple implementations
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Remove PRRL after SendFileStep in Peer Recovery
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Incorporate PR review feedback
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Empty-Commit
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Incorporate PR review feedback
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Empty-Commit
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Empty-Commit
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Remove CHANGELOG entry as this is incremental PR
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Incorporate PR review feedback
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Enhance CheckpointState to support no-op replication (#5282)
    
    * CheckpointState enhanced to support no-op replication
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    Co-authored-by: Bukhtawar Khan<bukhtawa@amazon.com>
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    * Add transport action for primary term validation for remote-backed indices (#5616)
    
    Add transport action for primary term validation for remote-backed indices
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    Co-authored-by: Ashish <ssashish@amazon.com>

---
+++ server/src/test/java/org/opensearch/action/admin/indices/refresh/TransportShardRefreshActionTests.java
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.action.admin.indices.refresh;
+
+import org.opensearch.action.support.ActionFilters;
+import org.opensearch.action.support.replication.ReplicationMode;
+import org.opensearch.cluster.action.shard.ShardStateAction;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.index.shard.IndexShard;
+import org.opensearch.indices.IndicesService;
+import org.opensearch.test.OpenSearchTestCase;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.transport.TransportService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TransportShardRefreshActionTests extends OpenSearchTestCase {
+
+    public void testGetReplicationModeWithRemoteTranslog() {
+        TransportShardRefreshAction action = createAction();
+        final IndexShard indexShard = mock(IndexShard.class);
+        when(indexShard.isRemoteTranslogEnabled()).thenReturn(true);
+        assertEquals(ReplicationMode.NO_REPLICATION, action.getReplicationMode(indexShard));
+    }
+
+    public void testGetReplicationModeWithLocalTranslog() {
+        TransportShardRefreshAction action = createAction();
+        final IndexShard indexShard = mock(IndexShard.class);
+        when(indexShard.isRemoteTranslogEnabled()).thenReturn(false);
+        assertEquals(ReplicationMode.FULL_REPLICATION, action.getReplicationMode(indexShard));
+    }
+
+    private TransportShardRefreshAction createAction() {
+        ClusterService clusterService = mock(ClusterService.class);
+        ClusterSettings clusterSettings = new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS);
+        when(clusterService.getClusterSettings()).thenReturn(clusterSettings);
+        return new TransportShardRefreshAction(
+            Settings.EMPTY,
+            mock(TransportService.class),
+            clusterService,
+            mock(IndicesService.class),
+            mock(ThreadPool.class),
+            mock(ShardStateAction.class),
+            mock(ActionFilters.class)
+        );
+    }
+}
