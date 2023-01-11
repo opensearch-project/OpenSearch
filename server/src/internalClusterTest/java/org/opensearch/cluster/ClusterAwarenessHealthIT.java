9028dc58ef142fa07138b598a241db8b8406937a
Nishchay Malhotra <nishcha@amazon.com>
2023-01-11 11:37:51 +0530

    Refactored ClusterAwarenessHealthIT class to add liscence information
    Signed-off-by: Nishchay Malhotra <nishcha@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/cluster/ClusterAwarenessHealthIT.java
+++ server/src/internalClusterTest/java/org/opensearch/cluster/ClusterAwarenessHealthIT.java
@@ -6,6 +6,29 @@
  * compatible open source license.
  */
 
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.cluster;
 
 import org.opensearch.action.admin.cluster.health.ClusterHealthResponse;
@@ -21,7 +44,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.opensearch.test.NodeRoles.onlyRole;
 
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.TEST, numDataNodes = 0)
-public class clusterAwarenessHealthIT extends OpenSearchIntegTestCase {
+public class ClusterAwarenessHealthIT extends OpenSearchIntegTestCase {
 
     public void testAwarenessAttributeHealthSucceeded() {
         createIndex("test");
