5ca4758ceb45bd95e52cb143fe260179cd91eed2
Frank Lou <mloufra@amazon.com>
2023-01-20 00:38:06 +0000

    address comment
    Signed-off-by: Frank Lou <mloufra@amazon.com>

--- server/src/main/java/org/opensearch/common/settings/DoubleParser.java
+++ server/src/main/java/org/opensearch/common/settings/DoubleParser.java
@@ -12,57 +12,32 @@ import org.opensearch.common.settings.Setting.Property;
 import org.opensearch.common.settings.Setting.Validator;
 
 public class DoubleParser<T> implements Function<String, T>, Writeable {
-    private Setting<Double> setting;
+    private Double defaultValue;
+    private String key;
     private Double minValue;
     private Double maxValue;
-    private Double fallback;
-
-    public DoubleParser(Setting<Double> setting) {
-        this.setting = setting;
-    }
+    private EnumSet<Property> propSet;
+    private Property[] properties;
 
     public DoubleParser(StreamInput in) throws IOException {
-        super();
         // Read the key
-        String key = in.readString();
+        this.key = in.readString();
         // Read the default value
-        Double defaultValue = in.readDouble();
+        this.defaultValue = in.readDouble();
         // Read the min value
         this.minValue = in.readDouble();
         // Read the max value
         this.maxValue = in.readDouble();
-        // Read the fallback
-        boolean hasFallback = in.readBoolean();
-        if (hasFallback) {
-            this.fallback = in.readDouble();
-        }
         // Read properties
-        EnumSet<Property> propSet = in.readEnumSet(Property.class);
-        this.setting = doubleSetting(key, defaultValue, minValue, maxValue, propSet.toArray(Property[]::new));
-
-    }
-
-    public static Setting<Double> doubleSetting(
-        String key,
-        double defaultValue,
-        double minValue,
-        double maxValue,
-        Property[] properties
-    ) {
-        return new Setting<Double>(
-            key,
-            Double.toString(defaultValue),
-            (s) -> doubleParser(s, minValue, maxValue, key, isFiltered(properties)),
-            v -> {},
-            properties
-        );
+        this.propSet = in.readEnumSet(Property.class);
+        this.properties = propSet.toArray(Property[]::new);
     }
 
     private static boolean isFiltered(Property[] properties) {
         return properties != null && Arrays.asList(properties).contains(Property.Filtered);
     }
 
-    private static double doubleParser(String s, double minValue, double maxValue, String key, boolean isFiltered) {
+    private static Double perser(String s, double minValue, double maxValue, String key, boolean isFiltered) {
         double value = Double.parseDouble(s);
         if(value < minValue) {
             String err = "Failed to parse value" + (isFiltered ? "" : " [" + s + "]") + " for setting [" + key + "] must be >= " + minValue;
@@ -77,23 +52,17 @@ public class DoubleParser<T> implements Function<String, T>, Writeable {
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
-        out.writeString(setting.getKey());
-        out.writeDouble(setting.getDefault(Settings.EMPTY));
+        out.writeString(key);
+        out.writeDouble(defaultValue);
         out.writeDouble(minValue);
         out.writeDouble(maxValue);
-        boolean hasFallback = setting.fallbackSetting != null;
-        out.writeBoolean(hasFallback);
-        if (hasFallback) {
-            out.writeDouble(fallback);
-        }
-        out.writeEnumSet(setting.getProperties());
+        out.writeEnumSet(propSet);
 
     }
 
     @Override
     public T apply(String s) {
-        // TODO Auto-generated method stub
-        return new doubleParser(key, defaultValue, minValue, maxValue, properties);
+        return parser(s, minValue, maxValue, key, isFiltered(properties));
     }
 
 }
