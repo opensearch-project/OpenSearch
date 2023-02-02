cef96709bd88fd1435ce9078c1137baffcae7c5d
Stephen Crawford <steecraw@amazon.com>
2023-02-02 15:36:58 -0500

    Start on tests
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

---
+++ sandbox/modules/identity/src/test/java/org/opensearch/identity/permissions/PermissionParsingTests.java
@@ -0,0 +1,59 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.identity.permissions;
+
+import org.opensearch.identity.User;
+import org.opensearch.identity.realm.InternalUsersStore;
+import org.opensearch.test.OpenSearchTestCase;
+
+import java.util.Map;
+
+public class PermissionParsingTests extends OpenSearchTestCase{
+
+    public void testParsePermissionTest() {
+        String principalString = "test_user";
+        String permissionString = "cluster:admin/read";
+        Map<String, User> internalUserMap = InternalUsersStore.readUsersAsMap(internalSubjectsYaml);
+        assertTrue(internalUserMap.containsKey("new-user"));
+        assertFalse(internalUserMap.containsKey("new-user2"));
+    }
+
+    public void testParseMalformedPermissionTest() {
+        String internalSubjectsYaml = "internal_users_test.yml";
+        Map<String, User> internalUserMap = InternalUsersStore.readUsersAsMap(internalSubjectsYaml);
+        assertTrue(internalUserMap.containsKey("new-user"));
+        assertFalse(internalUserMap.containsKey("new-user2"));
+    }
+
+    public void testParseWildcardPermissionTest() {
+        String internalSubjectsYaml = "internal_users_test.yml";
+        Map<String, User> internalUserMap = InternalUsersStore.readUsersAsMap(internalSubjectsYaml);
+        assertTrue(internalUserMap.containsKey("new-user"));
+        assertFalse(internalUserMap.containsKey("new-user2"));
+    }
+
+    public void testParseNegatedPermissionTest() {
+        String internalSubjectsYaml = "config/does_not_exist.yml";
+        try {
+            Map<String, User> internalUserMap = InternalUsersStore.readUsersAsMap(internalSubjectsYaml);
+            fail("Expected to throw FileNotFoundException");
+        } catch (RuntimeException e) {
+            // expected
+        }
+    }
+
+    public void testParseWildcardAndNegatedPermissionTest() {
+        String internalSubjectsYaml = "internal_users_test.yml";
+        Map<String, User> internalUserMap = InternalUsersStore.readUsersAsMap(internalSubjectsYaml);
+        assertTrue(internalUserMap.containsKey("new-user"));
+        assertFalse(internalUserMap.containsKey("new-user2"));
+    }
+}
+
+
