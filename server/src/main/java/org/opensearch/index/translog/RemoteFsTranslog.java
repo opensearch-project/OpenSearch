7dd90b948f3d24920d22d21362b66d26e352b9c4
Ashish Singh <ssashish@amazon.com>
2023-02-02 19:15:39 +0530

    Incorporate PR feedback - Make list primary terms inline
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- server/src/main/java/org/opensearch/index/translog/RemoteFsTranslog.java
+++ server/src/main/java/org/opensearch/index/translog/RemoteFsTranslog.java
@@ -379,7 +379,15 @@ public class RemoteFsTranslog extends Translog {
         if (olderPrimaryCleaned.trySet(Boolean.TRUE)) {
             logger.info("Cleaning up translog uploaded by previous primaries");
             long minPrimaryTermInMetadata = current.getPrimaryTerm();
-            Set<Long> primaryTermsInRemote = getPrimaryTermsInRemote();
+            Set<Long> primaryTermsInRemote;
+            try {
+                primaryTermsInRemote = translogTransferManager.listPrimaryTerms();
+            } catch (IOException e) {
+                logger.error("Exception occurred while getting primary terms from remote store", e);
+                // If there are exceptions encountered, then we try to delete all older primary terms lesser than the
+                // minimum referenced primary term in remote translog metadata.
+                primaryTermsInRemote = LongStream.range(0, current.getPrimaryTerm()).boxed().collect(Collectors.toSet());
+            }
             // Delete all primary terms that are no more referenced by the metadata file and exists in the
             Set<Long> primaryTermsToDelete = primaryTermsInRemote.stream()
                 .filter(term -> term < minPrimaryTermInMetadata)
@@ -399,14 +407,5 @@ public class RemoteFsTranslog extends Translog {
                 }
             }));
         }
-    }
-
-    private Set<Long> getPrimaryTermsInRemote() {
-        try {
-            return translogTransferManager.listPrimaryTerms();
-        } catch (IOException e) {
-            logger.error("Exception occurred while getting primary terms from remote store", e);
-        }
-        return LongStream.range(0, current.getPrimaryTerm()).boxed().collect(Collectors.toSet());
     }
 }
