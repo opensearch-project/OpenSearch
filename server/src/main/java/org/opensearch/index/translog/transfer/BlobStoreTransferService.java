49d9a90b52d5451e0eda95ce03dfb94dd505a905
Ashish <ssashish@amazon.com>
2023-02-14 17:23:08 +0530

    Purge stale translog metadata files for remote-backed indexes (#6238)
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- server/src/main/java/org/opensearch/index/translog/transfer/BlobStoreTransferService.java
+++ server/src/main/java/org/opensearch/index/translog/transfer/BlobStoreTransferService.java
@@ -115,6 +115,17 @@ public class BlobStoreTransferService implements TransferService {
     }
 
     @Override
+    public void listAllAsync(String threadpoolName, Iterable<String> path, ActionListener<Set<String>> listener) {
+        threadPool.executor(threadpoolName).execute(() -> {
+            try {
+                listener.onResponse(listAll(path));
+            } catch (IOException e) {
+                listener.onFailure(e);
+            }
+        });
+    }
+
+    @Override
     public Set<String> listFolders(Iterable<String> path) throws IOException {
         return blobStore.blobContainer((BlobPath) path).children().keySet();
     }
