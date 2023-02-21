6a9273ec5b111a835e8310f35cb813aad3912258
gashutos <gashutos@amazon.com>
2023-02-22 00:27:18 +0530

    Adding unit tests for sort optimization enabledment

--- server/src/main/java/org/opensearch/index/fielddata/fieldcomparator/IntValuesComparatorSource.java
+++ server/src/main/java/org/opensearch/index/fielddata/fieldcomparator/IntValuesComparatorSource.java
@@ -32,36 +32,17 @@
 
 package org.opensearch.index.fielddata.fieldcomparator;
 
-import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.NumericDocValues;
-import org.apache.lucene.index.SortedNumericDocValues;
-import org.apache.lucene.search.DocIdSetIterator;
-import org.apache.lucene.search.FieldComparator;
-import org.apache.lucene.search.LeafFieldComparator;
-import org.apache.lucene.search.Scorable;
 import org.apache.lucene.search.SortField;
-import org.apache.lucene.search.comparators.IntComparator;
-import org.apache.lucene.util.BitSet;
 import org.opensearch.common.Nullable;
-import org.opensearch.common.util.BigArrays;
-import org.opensearch.index.fielddata.FieldData;
-import org.opensearch.index.fielddata.IndexFieldData;
 import org.opensearch.index.fielddata.IndexNumericFieldData;
-import org.opensearch.search.DocValueFormat;
 import org.opensearch.search.MultiValueMode;
-import org.opensearch.search.sort.BucketedSort;
-import org.opensearch.search.sort.SortOrder;
-
-import java.io.IOException;
 
 /**
  * Comparator source for int values.
  *
  * @opensearch.internal
  */
-public class IntValuesComparatorSource extends IndexFieldData.XFieldComparatorSource {
-
-    private final IndexNumericFieldData indexFieldData;
+public class IntValuesComparatorSource extends LongValuesComparatorSource {
 
     public IntValuesComparatorSource(
         IndexNumericFieldData indexFieldData,
@@ -69,90 +50,11 @@ public class IntValuesComparatorSource extends IndexFieldData.XFieldComparatorSo
         MultiValueMode sortMode,
         Nested nested
     ) {
-        super(missingValue, sortMode, nested);
-        this.indexFieldData = indexFieldData;
+        super(indexFieldData, missingValue, sortMode, nested);
     }
 
     @Override
     public SortField.Type reducedType() {
         return SortField.Type.INT;
-    }
-
-    protected SortedNumericDocValues getValues(LeafReaderContext context) throws IOException {
-        return indexFieldData.load(context).getLongValues();
-    }
-
-    private NumericDocValues getNumericDocValues(LeafReaderContext context, int missingValue) throws IOException {
-        final SortedNumericDocValues values = getValues(context);
-        if (nested == null) {
-            return FieldData.replaceMissing(sortMode.select(values), missingValue);
-        } else {
-            final BitSet rootDocs = nested.rootDocs(context);
-            final DocIdSetIterator innerDocs = nested.innerDocs(context);
-            final int maxChildren = nested.getNestedSort() != null ? nested.getNestedSort().getMaxChildren() : Integer.MAX_VALUE;
-            return sortMode.select(values, missingValue, rootDocs, innerDocs, context.reader().maxDoc(), maxChildren);
-        }
-    }
-
-    protected void setScorer(Scorable scorer) {}
-
-    @Override
-    public FieldComparator<?> newComparator(String fieldname, int numHits, boolean enableSkipping, boolean reversed) {
-        assert indexFieldData == null || fieldname.equals(indexFieldData.getFieldName());
-
-        final int dMissingValue = (Integer) missingObject(missingValue, reversed);
-        // NOTE: it's important to pass null as a missing value in the constructor so that
-        // the comparator doesn't check docsWithField since we replace missing values in select()
-        return new IntComparator(numHits, null, null, reversed, false) {
-            @Override
-            public LeafFieldComparator getLeafComparator(LeafReaderContext context) throws IOException {
-                return new IntLeafComparator(context) {
-                    @Override
-                    protected NumericDocValues getNumericDocValues(LeafReaderContext context, String field) throws IOException {
-                        return IntValuesComparatorSource.this.getNumericDocValues(context, dMissingValue);
-                    }
-
-                    @Override
-                    public void setScorer(Scorable scorer) {
-                        IntValuesComparatorSource.this.setScorer(scorer);
-                    }
-                };
-            }
-        };
-    }
-
-    @Override
-    public BucketedSort newBucketedSort(
-        BigArrays bigArrays,
-        SortOrder sortOrder,
-        DocValueFormat format,
-        int bucketSize,
-        BucketedSort.ExtraData extra
-    ) {
-        return new BucketedSort.ForInt(bigArrays, sortOrder, format, bucketSize, extra) {
-            private final int dMissingValue = (Integer) missingObject(missingValue, sortOrder == SortOrder.DESC);
-
-            @Override
-            public Leaf forLeaf(LeafReaderContext ctx) throws IOException {
-                return new Leaf(ctx) {
-                    private final NumericDocValues docValues = getNumericDocValues(ctx, dMissingValue);
-                    private int docValue;
-
-                    @Override
-                    protected boolean advanceExact(int doc) throws IOException {
-                        if (docValues.advanceExact(doc)) {
-                            docValue = (int) docValues.longValue();
-                            return true;
-                        }
-                        return false;
-                    }
-
-                    @Override
-                    protected int docValue() {
-                        return docValue;
-                    }
-                };
-            }
-        };
     }
 }
