47a3aba548ffb21b425814e2e05cce055a7afed7
Kartik Ganesh <gkart@amazon.com>
2023-02-21 17:41:09 -0800

    Bumping 2.x to 2.7.0 (#6428)
    Signed-off-by: Kartik Ganesh <gkart@amazon.com>

--- server/src/main/java/org/opensearch/Version.java
+++ server/src/main/java/org/opensearch/Version.java
@@ -109,7 +109,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_2_5_0 = new Version(2050099, org.apache.lucene.util.Version.LUCENE_9_4_2);
     public static final Version V_2_5_1 = new Version(2050199, org.apache.lucene.util.Version.LUCENE_9_4_2);
     public static final Version V_2_6_0 = new Version(2060099, org.apache.lucene.util.Version.LUCENE_9_5_0);
-    public static final Version CURRENT = V_2_6_0;
+    public static final Version V_2_7_0 = new Version(2070099, org.apache.lucene.util.Version.LUCENE_9_5_0);
+    public static final Version CURRENT = V_2_7_0;
 
     public static Version readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());
