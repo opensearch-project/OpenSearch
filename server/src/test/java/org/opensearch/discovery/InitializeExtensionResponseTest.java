e1c05d8c89d4ccee4fe821a8127b0bede96a60d9
Varun Hegde <44541344+varuntumbe@users.noreply.github.com>
2023-03-01 21:25:47 +0530

    Update server/src/test/java/org/opensearch/discovery/InitializeExtensionResponseTest.java
    changing name InitializeExtensionResponseTest  to InitializeExtensionResponseTests.
    Signed-off-by: varuntumbe <varuntumbe1@gmail.com>
    
    Co-authored-by: Owais Kazi <owaiskazi19@gmail.com>

--- server/src/test/java/org/opensearch/discovery/InitializeExtensionResponseTest.java
+++ server/src/test/java/org/opensearch/discovery/InitializeExtensionResponseTest.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class InitializeExtensionResponseTest extends OpenSearchTestCase {
+public class InitializeExtensionResponseTests extends OpenSearchTestCase {
     public void testInitialzeExtensionResponse() throws Exception {
         String expectedName = "testsample-sdk";
         List<String> expectedImplementedInterfaces = new ArrayList<>(Arrays.asList("Action", "Search"));
