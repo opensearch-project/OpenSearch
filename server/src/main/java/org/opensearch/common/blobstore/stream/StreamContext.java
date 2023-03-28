003fe53046a0534c0bf7194f857ab646e302de62
Raghuvansh Raj <raghraaj@amazon.com>
2023-03-28 18:54:17 +0530

    fixed javadocs

--- server/src/main/java/org/opensearch/common/blobstore/stream/StreamContext.java
+++ server/src/main/java/org/opensearch/common/blobstore/stream/StreamContext.java
@@ -11,9 +11,7 @@ package org.opensearch.common.blobstore.stream;
 import org.opensearch.common.StreamIterable;
 
 /**
- * ABCDE
- *
- * @opensearch.internal
+ * StreamContext encapsulates all the data required for uploading multiple streams
  */
 public class StreamContext {
 
@@ -22,10 +20,10 @@ public class StreamContext {
     private final int numberOfParts;
 
     /**
-     * ABCDE
+     * Construct a new StreamContext object
      *
-     * @param streamIterable
-     * @param totalContentLength
+     * @param streamIterable A stream iterable to iterate over streams for upload
+     * @param totalContentLength The total content length for the upload
      */
     public StreamContext(StreamIterable streamIterable, long totalContentLength, int numberOfParts) {
         this.streamIterable = streamIterable;
@@ -34,23 +32,22 @@ public class StreamContext {
     }
 
     /**
-     * ABCDE
-     *
-     * @return
+     * @return The stream iterable for the current upload
      */
     public StreamIterable getStreamIterable() {
         return streamIterable;
     }
 
     /**
-     * ABCDE
-     *
-     * @return
+     * @return The total content length for the upload
      */
     public long getTotalContentLength() {
         return totalContentLength;
     }
 
+    /**
+     * @return The number of parts in current upload
+     */
     public int getNumberOfParts() {
         return numberOfParts;
     }
