7ee9ca174667148d4230e4addd538611b4555978
Andriy Redko <andriy.redko@aiven.io>
2023-03-29 17:51:42 -0400

    fix for updating version numbers for deprecation messages (#4719) (#6876)
    (cherry picked from commit 16797d658125a8c28ddf49413222f30675352dd6)
    
    Signed-off-by: Amit Chakraborti <amit.chakraborti09@gmail.com>
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    Co-authored-by: Amit Chakraborti <62286727+amitchakraborti9@users.noreply.github.com>

--- server/src/test/java/org/opensearch/index/mapper/TextFieldMapperTests.java
+++ server/src/test/java/org/opensearch/index/mapper/TextFieldMapperTests.java
@@ -102,7 +102,7 @@ public class TextFieldMapperTests extends MapperTestCase {
 
     @Override
     protected void assertParseMaximalWarnings() {
-        assertWarnings("Parameter [boost] on field [field] is deprecated and will be removed in 8.0");
+        assertWarnings("Parameter [boost] on field [field] is deprecated and will be removed in 3.0");
     }
 
     public final void testExistsQueryIndexDisabled() throws IOException {
