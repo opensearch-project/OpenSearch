ba4a65319e3479cafd2ee032908717ad144028d5
Suraj Singh <surajrider@gmail.com>
2023-04-05 09:30:19 -0700

    [Segment Replication] Add PIT/Scroll compatibility with Segment Replication #6644 (#6765)
    * Segment Replication - PIT/Scroll compatibility.
    
    This change makes updates to make PIT/Scroll queries compatibile with Segment Replication.
    It does this by refcounting files when a new reader is created, and discarding those files after
    a reader is closed.
    
    Signed-off-by: Marc Handalian <handalm@amazon.com>
    
    * Fix broken test.
    
    Signed-off-by: Marc Handalian <handalm@amazon.com>
    
    * Fix test bug with PIT where snapshotted segments are queried instead of current store state.
    
    Signed-off-by: Marc Handalian <handalm@amazon.com>
    
    * Address review comments and prevent temp file deletion during reader close
    
    Signed-off-by: Suraj Singh <surajrider@gmail.com>
    
    * Fix precommit failure
    
    Signed-off-by: Suraj Singh <surajrider@gmail.com>
    
    * Use last committed segment infos reference from replication engine
    
    Signed-off-by: Suraj Singh <surajrider@gmail.com>
    
    * Clean up and prevent incref on segment info file copied from primary
    
    Signed-off-by: Suraj Singh <surajrider@gmail.com>
    
    * Fix failing test
    
    Signed-off-by: Suraj Singh <surajrider@gmail.com>
    
    ---------
    
    Signed-off-by: Marc Handalian <handalm@amazon.com>
    Signed-off-by: Suraj Singh <surajrider@gmail.com>
    Co-authored-by: Marc Handalian <handalm@amazon.com>

--- server/src/main/java/org/opensearch/index/engine/NRTReplicationEngine.java
+++ server/src/main/java/org/opensearch/index/engine/NRTReplicationEngine.java
@@ -70,7 +70,7 @@ public class NRTReplicationEngine extends Engine implements LifecycleAware {
         WriteOnlyTranslogManager translogManagerRef = null;
         try {
             lastCommittedSegmentInfos = store.readLastCommittedSegmentsInfo();
-            readerManager = new NRTReplicationReaderManager(OpenSearchDirectoryReader.wrap(getDirectoryReader(), shardId));
+            readerManager = buildReaderManager();
             final SequenceNumbers.CommitInfo commitInfo = SequenceNumbers.loadSeqNoInfoFromLuceneCommit(
                 this.lastCommittedSegmentInfos.getUserData().entrySet()
             );
@@ -121,6 +121,29 @@ public class NRTReplicationEngine extends Engine implements LifecycleAware {
         }
     }
 
+    private NRTReplicationReaderManager buildReaderManager() throws IOException {
+        return new NRTReplicationReaderManager(
+            OpenSearchDirectoryReader.wrap(getDirectoryReader(), shardId),
+            store::incRefFileDeleter,
+            (files) -> {
+                store.decRefFileDeleter(files);
+                try {
+                    store.cleanupAndPreserveLatestCommitPoint(
+                        "On reader closed",
+                        getLatestSegmentInfos(),
+                        getLastCommittedSegmentInfos(),
+                        false
+                    );
+                } catch (IOException e) {
+                    // Log but do not rethrow - we can try cleaning up again after next replication cycle.
+                    // If that were to fail, the shard will as well.
+                    logger.error("Unable to clean store after reader closed", e);
+                }
+            }
+        );
+    }
+
+    @Override
     public TranslogManager translogManager() {
         return translogManager;
     }
