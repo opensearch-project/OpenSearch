b6a28b2bf57070d7fb2c7c7cfcf44455e7f652ca
Ashish Singh <ssashish@amazon.com>
2023-04-10 14:33:46 +0530

    Save changes
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- server/src/main/java/org/opensearch/index/RemoteSegmentUploadShardStatsTracker.java
+++ server/src/main/java/org/opensearch/index/RemoteSegmentUploadShardStatsTracker.java
@@ -24,21 +24,21 @@ public class RemoteSegmentUploadShardStatsTracker {
         latestUploadFileNameLengthMap = new HashMap<>();
     }
 
-    private long refreshSeqNo = UNASSIGNED;
+    private volatile long refreshSeqNo = UNASSIGNED;
 
-    private long refreshTime = UNASSIGNED;
+    private volatile long refreshTime = UNASSIGNED;
 
-    private long uploadBytesStarted = UNASSIGNED;
+    private volatile long uploadBytesStarted = UNASSIGNED;
 
-    private long uploadBytesFailed = UNASSIGNED;
+    private volatile long uploadBytesFailed = UNASSIGNED;
 
-    private long uploadBytesSucceeded = UNASSIGNED;
+    private volatile long uploadBytesSucceeded = UNASSIGNED;
 
-    private long totalUploadsStarted = UNASSIGNED;
+    private volatile long totalUploadsStarted = UNASSIGNED;
 
-    private long totalUploadsFailed = UNASSIGNED;
+    private volatile long totalUploadsFailed = UNASSIGNED;
 
-    private long totalUploadsSucceeded = UNASSIGNED;
+    private volatile long totalUploadsSucceeded = UNASSIGNED;
 
     /**
      * Keeps map of filename to bytes length of the most recent segments upload as part of refresh.
