b5640c69f79a0853979a4d2e2c9a8e12089ba157
Andrew Ross <andrross@amazon.com>
2023-04-10 12:36:49 -0700

    Use correct import for IOUtils
    Signed-off-by: Andrew Ross <andrross@amazon.com>

--- server/src/main/java/org/opensearch/index/store/remote/filecache/FileCacheCleaner.java
+++ server/src/main/java/org/opensearch/index/store/remote/filecache/FileCacheCleaner.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.common.util.io.IOUtils;
+import org.opensearch.core.internal.io.IOUtils;
 import org.opensearch.env.NodeEnvironment;
 import org.opensearch.index.Index;
 import org.opensearch.index.IndexModule;
