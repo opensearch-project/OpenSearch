a15475addbc3ced41c1cfe265eeb509cf856632b
Andriy Redko <andriy.redko@aiven.io>
2023-04-11 17:05:49 -0400

    Fix HTTP/2 Upgrade flow for requests with content (post/put/...) (#7105)
    * Fix HTTP/2 Upgrade flow for requests with content (post/put/...)
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    * Address code review comments
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    * Address code review comments
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    * Address code review comments
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    ---------
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- modules/transport-netty4/src/internalClusterTest/java/org/opensearch/http/netty4/Netty4Http2IT.java
+++ modules/transport-netty4/src/internalClusterTest/java/org/opensearch/http/netty4/Netty4Http2IT.java
@@ -9,17 +9,21 @@
 package org.opensearch.http.netty4;
 
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.util.ReferenceCounted;
 import org.opensearch.OpenSearchNetty4IntegTestCase;
+import org.opensearch.common.collect.Tuple;
 import org.opensearch.common.transport.TransportAddress;
 import org.opensearch.http.HttpServerTransport;
 import org.opensearch.test.OpenSearchIntegTestCase.ClusterScope;
 import org.opensearch.test.OpenSearchIntegTestCase.Scope;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.util.stream.IntStream;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
 
@@ -31,7 +35,7 @@ public class Netty4Http2IT extends OpenSearchNetty4IntegTestCase {
         return false; // enable http
     }
 
-    public void testThatNettyHttpServerSupportsHttp2() throws Exception {
+    public void testThatNettyHttpServerSupportsHttp2GetUpgrades() throws Exception {
         String[] requests = new String[] { "/", "/_nodes/stats", "/", "/_cluster/state", "/" };
 
         HttpServerTransport httpServerTransport = internalCluster().getInstance(HttpServerTransport.class);
@@ -44,19 +48,42 @@ public class Netty4Http2IT extends OpenSearchNetty4IntegTestCase {
                 assertThat(responses, hasSize(5));
 
                 Collection<String> opaqueIds = Netty4HttpClient.returnOpaqueIds(responses);
-                assertOpaqueIdsInAnyOrder(opaqueIds);
+                assertOpaqueIdsInAnyOrder(5, opaqueIds);
             } finally {
                 responses.forEach(ReferenceCounted::release);
             }
         }
     }
 
-    private void assertOpaqueIdsInAnyOrder(Collection<String> opaqueIds) {
+    public void testThatNettyHttpServerSupportsHttp2PostUpgrades() throws Exception {
+        final List<Tuple<String, CharSequence>> requests = List.of(Tuple.tuple("/_search", "{\"query\":{ \"match_all\":{}}}"));
+
+        HttpServerTransport httpServerTransport = internalCluster().getInstance(HttpServerTransport.class);
+        TransportAddress[] boundAddresses = httpServerTransport.boundAddress().boundAddresses();
+        TransportAddress transportAddress = randomFrom(boundAddresses);
+
+        try (Netty4HttpClient nettyHttpClient = Netty4HttpClient.http2()) {
+            Collection<FullHttpResponse> responses = nettyHttpClient.post(transportAddress.address(), requests);
+            try {
+                assertThat(responses, hasSize(1));
+
+                for (FullHttpResponse response : responses) {
+                    assertThat(response.getStatus(), equalTo(HttpResponseStatus.OK));
+                }
+
+                Collection<String> opaqueIds = Netty4HttpClient.returnOpaqueIds(responses);
+                String msg = String.format(Locale.ROOT, "Expected opaque id [0], got [%s]", opaqueIds);
+                assertOpaqueIdsInAnyOrder(1, opaqueIds);
+            } finally {
+                responses.forEach(ReferenceCounted::release);
+            }
+        }
+    }
+
+    private void assertOpaqueIdsInAnyOrder(int expected, Collection<String> opaqueIds) {
         // check if opaque ids are present in any order, since for HTTP/2 we use streaming (no head of line blocking)
         // and responses may come back at any order
-        int i = 0;
-        String msg = String.format(Locale.ROOT, "Expected list of opaque ids to be in any order, got [%s]", opaqueIds);
-        assertThat(msg, opaqueIds, containsInAnyOrder(IntStream.range(0, 5).mapToObj(Integer::toString).toArray()));
+        assertThat(opaqueIds, containsInAnyOrder(IntStream.range(0, expected).mapToObj(Integer::toString).toArray()));
     }
 
 }
