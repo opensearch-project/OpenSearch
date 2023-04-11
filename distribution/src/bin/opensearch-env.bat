opensearch-project/OpenSearch

commit 0202de5849dcdcef61e1a2ad3b6a270574fbf2ed

feat: allow overwrite opensearch home (#6956)
Signed-off-by: Soner Sayakci <s.sayakci@shopware.com>

distribution/src/bin/opensearch-env.bat
@@ -3,15 +3,17 @@ set SCRIPT=%0
 rem determine OpenSearch home; to do this, we strip from the path until we
 rem find bin, and then strip bin (there is an assumption here that there is no
 rem nested directory under bin also named bin)
-for %%I in (%SCRIPT%) do set OPENSEARCH_HOME=%%~dpI
+if not defined OPENSEARCH_HOME (
+  for %%I in (%SCRIPT%) do set OPENSEARCH_HOME=%%~dpI
 
-:opensearch_home_loop
-for %%I in ("%OPENSEARCH_HOME:~1,-1%") do set DIRNAME=%%~nxI
-if not "%DIRNAME%" == "bin" (
+  :opensearch_home_loop
+  for %%I in ("%OPENSEARCH_HOME:~1,-1%") do set DIRNAME=%%~nxI
+  if not "%DIRNAME%" == "bin" (
+    for %%I in ("%OPENSEARCH_HOME%..") do set OPENSEARCH_HOME=%%~dpfI
+    goto opensearch_home_loop
+  )
   for %%I in ("%OPENSEARCH_HOME%..") do set OPENSEARCH_HOME=%%~dpfI
-  goto opensearch_home_loop
 )
-for %%I in ("%OPENSEARCH_HOME%..") do set OPENSEARCH_HOME=%%~dpfI
 
 rem now set the classpath
 set OPENSEARCH_CLASSPATH=!OPENSEARCH_HOME!\lib\*

distribution/src/bin/opensearch-env
@@ -28,18 +28,20 @@ while [ -h "$SCRIPT" ] ; do
   fi
 done
 
-# determine OpenSearch home; to do this, we strip from the path until we find
-# bin, and then strip bin (there is an assumption here that there is no nested
-# directory under bin also named bin)
-OPENSEARCH_HOME=`dirname "$SCRIPT"`
+if [[ -z "$OPENSEARCH_HOME" ]]; then
+  # determine OpenSearch home; to do this, we strip from the path until we find
+  # bin, and then strip bin (there is an assumption here that there is no nested
+  # directory under bin also named bin)
+  OPENSEARCH_HOME=`dirname "$SCRIPT"`
 
-# now make OPENSEARCH_HOME absolute
-OPENSEARCH_HOME=`cd "$OPENSEARCH_HOME"; pwd`
+  # now make OPENSEARCH_HOME absolute
+  OPENSEARCH_HOME=`cd "$OPENSEARCH_HOME"; pwd`
 
-while [ "`basename "$OPENSEARCH_HOME"`" != "bin" ]; do
+  while [ "`basename "$OPENSEARCH_HOME"`" != "bin" ]; do
+    OPENSEARCH_HOME=`dirname "$OPENSEARCH_HOME"`
+  done
   OPENSEARCH_HOME=`dirname "$OPENSEARCH_HOME"`
-done
-OPENSEARCH_HOME=`dirname "$OPENSEARCH_HOME"`
+fi
 
 # now set the classpath
 OPENSEARCH_CLASSPATH="$OPENSEARCH_HOME/lib/*"

commit 810b98555ea612cbae8310bf670cd515eb8cab34

Resolve windows opensearch-env.bat having variable delayed expansion issues on OPENSEARCH_HOME assignment introduced in #6956 (#7080)
* Resolve windows opensearch-env.bat having variable delayed expansion issues on OPENSEARCH_HOME assignment

Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

* Remove changelog entry

Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

---------

Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

distribution/src/bin/opensearch-env.bat
@@ -3,18 +3,21 @@ set SCRIPT=%0
 rem determine OpenSearch home; to do this, we strip from the path until we
 rem find bin, and then strip bin (there is an assumption here that there is no
 rem nested directory under bin also named bin)
-if not defined OPENSEARCH_HOME (
-  for %%I in (%SCRIPT%) do set OPENSEARCH_HOME=%%~dpI
+if not defined OPENSEARCH_HOME goto opensearch_home_start_setup
+goto opensearch_home_done_setup
 
-  :opensearch_home_loop
-  for %%I in ("%OPENSEARCH_HOME:~1,-1%") do set DIRNAME=%%~nxI
-  if not "%DIRNAME%" == "bin" (
-    for %%I in ("%OPENSEARCH_HOME%..") do set OPENSEARCH_HOME=%%~dpfI
-    goto opensearch_home_loop
-  )
+:opensearch_home_start_setup
+for %%I in (%SCRIPT%) do set OPENSEARCH_HOME=%%~dpI
+
+:opensearch_home_loop
+for %%I in ("%OPENSEARCH_HOME:~1,-1%") do set DIRNAME=%%~nxI
+if not "%DIRNAME%" == "bin" (
   for %%I in ("%OPENSEARCH_HOME%..") do set OPENSEARCH_HOME=%%~dpfI
+  goto opensearch_home_loop
 )
+for %%I in ("%OPENSEARCH_HOME%..") do set OPENSEARCH_HOME=%%~dpfI
 
+:opensearch_home_done_setup
 rem now set the classpath
 set OPENSEARCH_CLASSPATH=!OPENSEARCH_HOME!\lib\*
 