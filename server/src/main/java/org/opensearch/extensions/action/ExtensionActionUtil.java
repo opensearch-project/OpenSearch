b4958606884cbe1d0007ae7627cff702346c5f49
Kuanysh <kuanysh4646@gmail.com>
2023-04-14 22:55:04 +0600

    add ExtensionActionUtilTest class
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

--- server/src/main/java/org/opensearch/extensions/action/ExtensionActionUtil.java
+++ server/src/main/java/org/opensearch/extensions/action/ExtensionActionUtil.java
@@ -11,9 +11,9 @@ package org.opensearch.extensions.action;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.action.ActionRequest;
+import org.opensearch.common.bytes.BytesReference;
+import org.opensearch.common.io.stream.BytesStreamOutput;
 import org.opensearch.common.io.stream.StreamInput;
-
-import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Constructor;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -48,54 +48,70 @@ public class ExtensionActionUtil {
     }
 
     /**
+     * @param bytes the byte array containing the serialized data for the ExtensionActionRequest object.
+     * @return an ExtensionActionRequest object representing the deserialized data,
+     * or {@code null} if an error occurred during the deserialization process.
+     */
+    public static ExtensionActionRequest createExtensionActionRequestFromBytes(byte[] bytes) {
+        int delimPos = delimPos(bytes);
+        try {
+            StreamInput requestByteStream = StreamInput.wrap(Arrays.copyOfRange(bytes, delimPos + 1, bytes.length));
+            Class<?> clazz = ExtensionActionRequest.class;
+            Constructor<?> constructor = clazz.getConstructor(StreamInput.class);
+            return (ExtensionActionRequest) constructor.newInstance(requestByteStream);
+        } catch (Exception e) {
+            logger.error("An error occurred while creating ExtensionActionRequest: " + e.getMessage(), e);
+        }
+        return null;
+    }
+
+    /**
      * @param  requestBytes is a byte array containing the request data, used by the "createActionRequest"
      * method to create an "ActionRequest" object, which represents the request model to be processed on the server.
-     * @return an "Action Request" object, which represents the request model for processing on the server,
-     * and is created using the request data stored in the byte array provided in the "requestBytes" parameter.
+     * @return an "Action Request" object representing the request model for processing on the server,
+     * or {@code null} if the request data is invalid or null.
      */
     public static ActionRequest createActionRequest(ExtensionActionRequest requestBytes) {
-        int nullPos = delimPos(requestBytes.getRequestBytes());
-        String requestClassName = new String(Arrays.copyOfRange(requestBytes.getRequestBytes(), 0, nullPos + 1), StandardCharsets.UTF_8)
+        int delimPos = delimPos(requestBytes.getRequestBytes());
+        String requestClassName = new String(Arrays.copyOfRange(requestBytes.getRequestBytes(), 0, delimPos + 1), StandardCharsets.UTF_8)
             .stripTrailing();
         ActionRequest actionRequest = null;
         try {
             Class<?> clazz = Class.forName(requestClassName);
             Constructor<?> constructor = clazz.getConstructor(StreamInput.class);
             StreamInput requestByteStream = StreamInput.wrap(
-                Arrays.copyOfRange(requestBytes.getRequestBytes(), nullPos + 1, requestBytes.getRequestBytes().length)
+                Arrays.copyOfRange(requestBytes.getRequestBytes(), delimPos + 1, requestBytes.getRequestBytes().length)
             );
             actionRequest = (ActionRequest) constructor.newInstance(requestByteStream);
         } catch (Exception e) {
-            logger.debug("No request class [" + requestClassName + "] is available: " + e.getMessage());
+            logger.error("An error occurred while creating ActionRequest: " + e.getMessage(), e);
         }
         return actionRequest;
     }
 
     /**
      * @param actionRequest  is an instance of the Action Request class that is used to create an ExtensionActionRequest object.
-     * @return an Extension ActionRequest object based on an Action Request input.
+     * @return an Extension ActionRequest object, or {@code null} if an error occurred during the creation process.
      */
     public static ExtensionActionRequest createExtensionActionRequest(ActionRequest actionRequest) {
         byte[] requestBytes;
-        ExtensionActionRequest extensionActionRequest = null;
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            requestBytes = outputStream.toByteArray();
-            String requestClassName = new String(Arrays.copyOfRange(requestBytes, 0, delimPos(requestBytes) + 1), StandardCharsets.UTF_8)
-                .stripTrailing();
-            Class<?> clazz = Class.forName(requestClassName);
+
+        try (BytesStreamOutput out = new BytesStreamOutput()) {
+            actionRequest.writeTo(out);
+            requestBytes = BytesReference.toBytes(out.bytes());
+            Class<?> clazz = ExtensionActionRequest.class;
             Constructor<?> constructor = clazz.getConstructor(StreamInput.class);
             StreamInput requestByteStream = StreamInput.wrap(
                 Arrays.copyOfRange(requestBytes, delimPos(requestBytes) + 1, requestBytes.length)
             );
-            extensionActionRequest = (ExtensionActionRequest) constructor.newInstance(requestByteStream);
+            return (ExtensionActionRequest) constructor.newInstance(requestByteStream);
         } catch (Exception e) {
-            logger.debug("Failed to create request bytes for [" + actionRequest + "]: " + e.getMessage());
+            logger.error("An error occurred while creating ExtensionActionRequest: " + e.getMessage(), e);
         }
-        return extensionActionRequest;
+        return null;
     }
 
-    private static int delimPos(byte[] bytes) {
+    public static int delimPos(byte[] bytes) {
         for (int offset = 0; offset < bytes.length; ++offset) {
             if (bytes[offset] == ExtensionActionUtil.UNIT_SEPARATOR) {
                 return offset;
