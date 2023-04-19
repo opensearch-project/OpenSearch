5c9d353ac155334645e8988e0de90e902df3564d
Ashish Singh <ssashish@amazon.com>
2023-04-19 16:53:35 +0530

    Add more remote refresh segment pressure service tests
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- server/src/test/java/org/opensearch/index/RemoteRefreshSegmentPressureServiceTests.java
+++ server/src/test/java/org/opensearch/index/RemoteRefreshSegmentPressureServiceTests.java
@@ -12,12 +12,18 @@ import org.opensearch.cluster.metadata.IndexMetadata;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.ClusterSettings;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.core.concurrency.OpenSearchRejectedExecutionException;
 import org.opensearch.index.shard.IndexShard;
 import org.opensearch.index.shard.ShardId;
 import org.opensearch.test.IndexSettingsModule;
 import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.threadpool.TestThreadPool;
 import org.opensearch.threadpool.ThreadPool;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -99,6 +105,63 @@ public class RemoteRefreshSegmentPressureServiceTests extends OpenSearchTestCase
 
         pressureService.beforeIndexShardClosed(testShardId, indexShard, settings);
         assertNull(pressureService.getPressureTracker(testShardId));
+    }
+
+    public void testValidateSegmentUploadLag() {
+        // Create the pressure tracker
+        Settings settings = Settings.builder().put(IndexMetadata.SETTING_REMOTE_STORE_ENABLED, "true").build();
+        IndexSettings indexSettings = IndexSettingsModule.newIndexSettings("test_index", settings);
+        ShardId testShardId = new ShardId("index", "uuid", 0);
+        IndexShard indexShard = mock(IndexShard.class);
+        when(indexShard.indexSettings()).thenReturn(indexSettings);
+        when(indexShard.shardId()).thenReturn(testShardId);
+        pressureService = new RemoteRefreshSegmentPressureService(clusterService, Settings.EMPTY);
+        pressureService.afterIndexShardCreated(indexShard);
+
+        // 1. Seq no - add data points to the pressure tracker
+        RemoteRefreshSegmentPressureTracker pressureTracker = pressureService.getPressureTracker(testShardId);
+        pressureTracker.updateLocalRefreshSeqNo(6);
+        assertThrows(OpenSearchRejectedExecutionException.class, () -> pressureService.validateSegmentsUploadLag(testShardId));
+
+        // 2. time lag more than dynamic threshold
+        pressureTracker.updateRemoteRefreshSeqNo(3);
+        AtomicLong sum = new AtomicLong();
+        IntStream.range(0, 20).forEach(i -> {
+            pressureTracker.addUploadTimeMs(i);
+            sum.addAndGet(i);
+        });
+        double avg = (double) sum.get() / 20;
+        long currentMs = System.nanoTime() / 1_000_000;
+        pressureTracker.updateLocalRefreshTimeMs((long) (currentMs + 4 * avg));
+        pressureTracker.updateRemoteRefreshTimeMs(currentMs);
+        assertThrows(OpenSearchRejectedExecutionException.class, () -> pressureService.validateSegmentsUploadLag(testShardId));
+
+        pressureTracker.updateRemoteRefreshTimeMs((long) (currentMs + 2 * avg));
+        pressureService.validateSegmentsUploadLag(testShardId);
+
+        // 3. bytes lag more than dynamic threshold
+        sum.set(0);
+        IntStream.range(0, 20).forEach(i -> {
+            pressureTracker.addUploadBytes(i);
+            sum.addAndGet(i);
+        });
+        avg = (double) sum.get() / 20;
+        Map<String, Long> nameSizeMap = new HashMap<>();
+        nameSizeMap.put("a", (long) (4 * avg));
+        pressureTracker.setLatestLocalFileNameLengthMap(nameSizeMap);
+        assertThrows(OpenSearchRejectedExecutionException.class, () -> pressureService.validateSegmentsUploadLag(testShardId));
+
+        nameSizeMap.put("a", (long) (2 * avg));
+        pressureTracker.setLatestLocalFileNameLengthMap(nameSizeMap);
+        pressureService.validateSegmentsUploadLag(testShardId);
+
+        // 4. Consecutive failures more than the limit
+        IntStream.range(0, 10).forEach(ignore -> pressureTracker.incrementTotalUploadsFailed());
+        pressureService.validateSegmentsUploadLag(testShardId);
+        pressureTracker.incrementTotalUploadsFailed();
+        assertThrows(OpenSearchRejectedExecutionException.class, () -> pressureService.validateSegmentsUploadLag(testShardId));
+        pressureTracker.incrementTotalUploadSucceeded();
+        pressureService.validateSegmentsUploadLag(testShardId);
     }
 
 }
