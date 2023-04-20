9a0ad21241faa91cbd8b0688d78efd263fb6903f
Stephen Crawford <steecraw@amazon.com>
2023-04-20 10:55:22 -0400

    Rollback
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- server/src/main/java/org/opensearch/identity/IdentityService.java
+++ server/src/main/java/org/opensearch/identity/IdentityService.java
@@ -8,7 +8,6 @@ package org.opensearch.identity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.OpenSearchException;
-import org.opensearch.common.NotNull;
 import org.opensearch.identity.noop.NoopIdentityPlugin;
 import java.util.List;
 import org.opensearch.common.settings.Settings;
@@ -47,7 +46,6 @@ public class IdentityService {
     /**
      * Gets the current subject
      */
-    @NotNull
     public Subject getSubject() {
         return identityPlugin.getSubject();
     }
