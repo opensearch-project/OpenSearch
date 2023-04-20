f6e560fd81f3748df0baf7eb9a1dcc7d840730aa
mulugetam <mulugeta.mammo@intel.com>
2023-04-20 11:49:25 -0700

    Use lowercase letters for zstd, zstdnodict compression codecs. (#7231)
    Signed-off-by: Mulugeta Mammo <mulugeta.mammo@intel.com>

--- sandbox/plugins/custom-codecs/src/main/java/org/opensearch/index/codec/customcodecs/Lucene95CustomCodec.java
+++ sandbox/plugins/custom-codecs/src/main/java/org/opensearch/index/codec/customcodecs/Lucene95CustomCodec.java
@@ -12,6 +12,8 @@ import org.apache.lucene.codecs.StoredFieldsFormat;
 import org.apache.lucene.codecs.FilterCodec;
 import org.apache.lucene.codecs.lucene95.Lucene95Codec;
 
+import java.util.Locale;
+
 abstract class Lucene95CustomCodec extends FilterCodec {
     public static final int DEFAULT_COMPRESSION_LEVEL = 6;
 
@@ -23,13 +25,25 @@ abstract class Lucene95CustomCodec extends FilterCodec {
 
     private final StoredFieldsFormat storedFieldsFormat;
 
-    /** new codec for a given compression algorithm and default compression level */
+    /**
+     * Creates a new compression codec with the default compression level.
+     *
+     * @param mode The compression codec (ZSTD or ZSTDNODICT).
+     */
     public Lucene95CustomCodec(Mode mode) {
         this(mode, DEFAULT_COMPRESSION_LEVEL);
     }
 
+    /**
+     * Creates a new compression codec with the given compression level. We use
+     * lowercase letters when registering the codec so that we remain consistent with
+     * the other compression codecs: default, lucene_default, and best_compression.
+     *
+     * @param mode The compression codec (ZSTD or ZSTDNODICT).
+     * @parama compressionLevel The compression level.
+     */
     public Lucene95CustomCodec(Mode mode, int compressionLevel) {
-        super(mode.name(), new Lucene95Codec());
+        super(mode.name().toLowerCase(Locale.ROOT), new Lucene95Codec());
         this.storedFieldsFormat = new Lucene95CustomStoredFieldsFormat(mode, compressionLevel);
     }
 
