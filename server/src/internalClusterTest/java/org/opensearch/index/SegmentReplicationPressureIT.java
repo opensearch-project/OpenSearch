86bbee4757c95989f85b64c444608788c6a9449f
Rishikesh1159 <rishireddy1159@gmail.com>
2023-04-24 15:30:45 +0000

    add testWithDocumentReplicationEnabledIndex().
    Signed-off-by: Rishikesh1159 <rishireddy1159@gmail.com>

--- server/src/internalClusterTest/java/org/opensearch/index/SegmentReplicationPressureIT.java
+++ server/src/internalClusterTest/java/org/opensearch/index/SegmentReplicationPressureIT.java
@@ -40,6 +40,7 @@ import static org.opensearch.index.SegmentReplicationPressureService.MAX_INDEXIN
 import static org.opensearch.index.SegmentReplicationPressureService.MAX_REPLICATION_TIME_SETTING;
 import static org.opensearch.index.SegmentReplicationPressureService.SEGMENT_REPLICATION_INDEXING_PRESSURE_ENABLED;
 import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertAcked;
+import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertHitCount;
 
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.TEST, numDataNodes = 0)
 public class SegmentReplicationPressureIT extends SegmentReplicationBaseIT {
@@ -254,6 +255,30 @@ public class SegmentReplicationPressureIT extends SegmentReplicationBaseIT {
 
         // Verify that new replica shard after failure is different from old replica shard.
         assertNotEquals(replicaAfterFailure.routingEntry().allocationId().getId(), replicaShard.routingEntry().allocationId().getId());
+    }
+
+    public void testWithDocumentReplicationEnabledIndex() throws Exception {
+        Settings settings = Settings.builder().put(MAX_REPLICATION_TIME_SETTING.getKey(), TimeValue.timeValueMillis(500)).build();
+        // Starts a primary and replica node.
+        final String primaryNode = internalCluster().startNode(settings);
+        createIndex(
+            INDEX_NAME,
+            Settings.builder().put(indexSettings()).put(IndexMetadata.SETTING_REPLICATION_TYPE, ReplicationType.DOCUMENT).build()
+        );
+        ensureYellowAndNoInitializingShards(INDEX_NAME);
+        final String replicaNode = internalCluster().startNode(settings);
+        ensureGreen(INDEX_NAME);
+        final AtomicInteger totalDocs = new AtomicInteger(0);
+        // Index docs until replica stale limit is reached.
+        totalDocs.getAndSet(indexUntilCheckpointCount());
+        // index again after stale limit.
+        indexDoc();
+        refresh(INDEX_NAME);
+        totalDocs.incrementAndGet();
+        // verify total doc count is same and docs are not rejected.
+        assertHitCount(client(primaryNode).prepareSearch(INDEX_NAME).setSize(0).setPreference("_only_local").get(), totalDocs.get());
+        assertHitCount(client(replicaNode).prepareSearch(INDEX_NAME).setSize(0).setPreference("_only_local").get(), totalDocs.get());
+
     }
 
     public void testBulkWritesRejected() throws Exception {
