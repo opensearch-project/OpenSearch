4c98b3d38064b94a87f6d7a1359e623849459bac
Chaitanya Gohel <104654647+gashutos@users.noreply.github.com>
2023-04-26 14:52:45 +0000

    Time series based workload desc order optimization through reverse segment read (#7244)
    This commit changes the EngineConfig for timeseries indexes only (e.g., indexes that use 
    the @timestamp metadata field) so that a descending LeafSorter comparator is used to 
    visit segments in order of most newest to oldest. For the more infrequent case that a user 
    chooses to sort query results by ASC time, this would cause a search regression so the 
    ContextIndexSearcher is updated to inspect the sort order from the search request and 
    reverse the comparator so segments are visited in ascending order. LeafSorter behavior 
    for non-timeseries indexes is left the same. 
    
    Signed-off-by: gashutos <gashutos@amazon.com>
    Signed-off-by: Chaitanya Gohel <104654647+gashutos@users.noreply.github.com>

--- server/src/main/java/org/opensearch/index/IndexSettings.java
+++ server/src/main/java/org/opensearch/index/IndexSettings.java
@@ -653,6 +653,7 @@ public final class IndexSettings {
     private volatile long mappingTotalFieldsLimit;
     private volatile long mappingDepthLimit;
     private volatile long mappingFieldNameLengthLimit;
+    private volatile boolean searchSegmentOrderReversed;
 
     /**
      * The maximum number of refresh listeners allows on this shard.
@@ -897,6 +898,10 @@ public final class IndexSettings {
         scopedSettings.addSettingsUpdateConsumer(INDEX_MERGE_ON_FLUSH_POLICY, this::setMergeOnFlushPolicy);
     }
 
+    private void setSearchSegmentOrderReversed(boolean reversed) {
+        this.searchSegmentOrderReversed = reversed;
+    }
+
     private void setSearchIdleAfter(TimeValue searchIdleAfter) {
         this.searchIdleAfter = searchIdleAfter;
     }
@@ -1066,6 +1071,13 @@ public final class IndexSettings {
      */
     public Settings getNodeSettings() {
         return nodeSettings;
+    }
+
+    /**
+     * Returns true if index level setting for leaf reverse order search optimization is enabled
+     */
+    public boolean getSearchSegmentOrderReversed() {
+        return this.searchSegmentOrderReversed;
     }
 
     /**
