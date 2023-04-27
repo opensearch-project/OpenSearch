408d39b0956da82c8d88a0c471d54a6c8b09f353
Kuanysh <kuanysh4646@gmail.com>
2023-04-27 23:15:46 +0600

    refactoring
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

--- server/src/main/java/org/opensearch/extensions/action/ExtensionActionUtil.java
+++ server/src/main/java/org/opensearch/extensions/action/ExtensionActionUtil.java
@@ -31,26 +31,35 @@ public class ExtensionActionUtil {
     public static final byte UNIT_SEPARATOR = (byte) '\u001F';
 
     /**
-     * @param request r is an object of the "Remote Extension Action Request" class, containing information about the
+     * @param request an instance of a request extending {@link ActionRequest}, containing information about the
      * request being sent to the remote server. It is used to create a byte array containing the request data,
      * which will be sent to the remote server.
-     * @return a byte array containing all the necessary information about the request to be sent to the remote server.
-     * This byte array is constructed using the class name of the request, a unit separator, and the request data itself.
+     * @return An Extension ActionRequest object that represents the deserialized data.
+     * If an error occurred during the deserialization process, the method will return {@code null}.
      */
-    public static byte[] createProxyRequestBytes(ExtensionActionRequest request) {
-        String requestClass = request.getClass().getName();
-        byte[] requestClassBytes = requestClass.getBytes(StandardCharsets.UTF_8);
-        return ByteBuffer.allocate(requestClassBytes.length + 1 + request.getRequestBytes().length)
-            .put(requestClassBytes)
-            .put(UNIT_SEPARATOR)
-            .put(request.getRequestBytes())
-            .array();
+    public static byte[] createProxyRequestBytes(ActionRequest request) {
+        byte[] requestClassBytes = request.getClass().getName().getBytes(StandardCharsets.UTF_8);
+        byte[] requestBytes;
+
+        try (BytesStreamOutput out = new BytesStreamOutput()) {
+            request.writeTo(out);
+            requestBytes = BytesReference.toBytes(out.bytes());
+            out.flush();
+
+            return ByteBuffer.allocate(requestClassBytes.length + 1 + requestBytes.length)
+                .put(requestClassBytes)
+                .put(UNIT_SEPARATOR)
+                .put(requestBytes)
+                .array();
+        } catch (Exception e) {
+            logger.error("An error occurred while a creating byte array: " + e.getMessage(), e);
+        }
+        return null;
     }
 
     /**
      * @param bytes the byte array containing the serialized data for the ExtensionActionRequest object.
-     * @return an ExtensionActionRequest object representing the deserialized data,
-     * or {@code null} if an error occurred during the deserialization process.
+     * @return An Extension ActionRequest object that represents the deserialized data. If an error occurred during the deserialization process, the method will return {@code null}.
      */
     public static ExtensionActionRequest createExtensionActionRequestFromBytes(byte[] bytes) {
         int delimPos = delimPos(bytes);
@@ -71,22 +80,18 @@ public class ExtensionActionUtil {
      * @return an "Action Request" object representing the request model for processing on the server,
      * or {@code null} if the request data is invalid or null.
      */
-    public static ActionRequest createActionRequest(ExtensionActionRequest requestBytes) {
-        int delimPos = delimPos(requestBytes.getRequestBytes());
-        String requestClassName = new String(Arrays.copyOfRange(requestBytes.getRequestBytes(), 0, delimPos + 1), StandardCharsets.UTF_8)
-            .stripTrailing();
-        ActionRequest actionRequest = null;
+    public static ActionRequest createActionRequest(byte[] requestBytes) {
+        int delimPos = delimPos(requestBytes);
+        String requestClassName = new String(Arrays.copyOfRange(requestBytes, 0, delimPos + 1), StandardCharsets.UTF_8).stripTrailing();
         try {
             Class<?> clazz = Class.forName(requestClassName);
             Constructor<?> constructor = clazz.getConstructor(StreamInput.class);
-            StreamInput requestByteStream = StreamInput.wrap(
-                Arrays.copyOfRange(requestBytes.getRequestBytes(), delimPos + 1, requestBytes.getRequestBytes().length)
-            );
-            actionRequest = (ActionRequest) constructor.newInstance(requestByteStream);
+            StreamInput requestByteStream = StreamInput.wrap(Arrays.copyOfRange(requestBytes, delimPos + 1, requestBytes.length));
+            return (ActionRequest) constructor.newInstance(requestByteStream);
         } catch (Exception e) {
             logger.error("An error occurred while creating ActionRequest: " + e.getMessage(), e);
         }
-        return actionRequest;
+        return null;
     }
 
     /**
@@ -111,6 +116,12 @@ public class ExtensionActionUtil {
         return null;
     }
 
+    /**
+     * Searches for the position of the unit separator byte in the given byte array.
+     *
+     * @param bytes the byte array to search for the unit separator byte.
+     * @return the index of the unit separator byte in the byte array, or -1 if it was not found.
+     */
     public static int delimPos(byte[] bytes) {
         for (int offset = 0; offset < bytes.length; ++offset) {
             if (bytes[offset] == ExtensionActionUtil.UNIT_SEPARATOR) {
