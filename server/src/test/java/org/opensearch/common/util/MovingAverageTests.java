aa7733e8fcc47b7431bbe8e88e1141fda0b8bc0b
Ashish <ssashish@amazon.com>
2023-05-02 17:06:37 +0530

    Add remote refresh segment pressure service, settings and tracker (#7227)
    Signed-off-by: Ashish Singh <ssashish@amazon.com>
    (cherry picked from commit 18390f9e314f79aa53a251db788217c01732cf1c)

--- server/src/test/java/org/opensearch/common/util/MovingAverageTests.java
+++ server/src/test/java/org/opensearch/common/util/MovingAverageTests.java
@@ -46,4 +46,44 @@ public class MovingAverageTests extends OpenSearchTestCase {
 
         fail("exception should have been thrown");
     }
+
+    public void testUpdateMovingAverageWindowSize() {
+        MovingAverage ma = new MovingAverage(5);
+        ma.record(1);
+        ma.record(2);
+        ma.record(3);
+        double avg = ma.getAverage();
+
+        // Test case 1 - Not ready and increasing size
+        MovingAverage newMa = ma.copyWithSize(10);
+        assertEquals(avg, newMa.getAverage(), 0.0d);
+
+        // Test case 2 - Not ready and decreasing size
+        newMa = ma.copyWithSize(2);
+        assertEquals(2.5, newMa.getAverage(), 0.0d);
+
+        // Test case 3 - Ready and increasing size
+        ma.record(4);
+        ma.record(5);
+        ma.record(6);
+        assertEquals(4.0, ma.getAverage(), 0.0d);
+
+        newMa = ma.copyWithSize(10);
+        assertEquals(ma.getAverage(), newMa.getAverage(), 0.0d);
+
+        // Test case 4 - Ready and decreasing size
+        newMa = ma.copyWithSize(3);
+        assertEquals(5.0, newMa.getAverage(), 0.0d);
+
+        // Test case 5 - Ready, array overwritten and increasing size
+        for (int i = 7; i < 20; i++) {
+            ma.record(i);
+        }
+        newMa = ma.copyWithSize(6);
+        assertEquals(ma.getAverage(), newMa.getAverage(), 0.0d);
+
+        // Test case 6 - Ready, array overwritten and decreasing size
+        newMa = ma.copyWithSize(3);
+        assertEquals(18.0, newMa.getAverage(), 0.0d);
+    }
 }
