e8ba840835a0e14fde1cf6856362e4bfb18bb3b3
Stephen Crawford <steecraw@amazon.com>
2023-05-04 15:53:19 -0400

    Add javadocs
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- server/src/main/java/org/opensearch/identity/RestServiceAccountManager.java
+++ server/src/main/java/org/opensearch/identity/RestServiceAccountManager.java
@@ -27,25 +27,77 @@ import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 import static java.net.http.HttpRequest.BodyPublishers.ofString;
 import static org.opensearch.rest.RestStatus.NOT_FOUND;
 
+/**
+ * A Service Account Manager that operates over the REST layer.
+ *
+ */
 public class RestServiceAccountManager implements ServiceAccountManager {
 
     private Settings settings;
     private final String url;
     public HttpClient httpClient;
+
+    /**
+     * Construct a RestServiceAccountManager only by specifying settings
+     * @param settings The settings used to construct the manager
+     */
+    public RestServiceAccountManager(Settings settings) {
+        this.settings = settings;
+        this.url = buildUrl(settings);
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    /**
+     * Construct a RestServiceAccountManager from settings and an HttpClient
+     * @param settings Settings used to configure the manager
+     * @param httpClient The HttpClient used in the manager
+     */
+    public RestServiceAccountManager(Settings settings, HttpClient httpClient) {
+        this.settings = settings;
+        this.url = buildUrl(settings);
+        this.httpClient = httpClient;
+    }
+
+    /**
+     * Resets a service account token by resetting the password and creating a new basic auth header which is turned into a new Auth Token
+     * @param serviceName The name associated with the service
+     * @return A new auth token
+     */
     @Override
-    public AuthToken resetServiceAccountToken(String principal) {
+    public AuthToken resetServiceAccountToken(String serviceName) {
 
         String responseBody = null;
         try {
-            responseBody = getOrCreateServiceAccount(new NamedPrincipal(principal).getName(),"Basic YWRtaW46YWRtaW4=");
+            HttpRequest request = HttpRequest.newBuilder(URI.create(url + serviceName))
+                .GET()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Basic " + "YWRtaW46YWRtaW4=")
+                .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == NOT_FOUND.getStatus()) {
+                request = HttpRequest.newBuilder(URI.create(url + serviceName + "/authtoken"))
+                    .POST(ofString(createServiceAccountRequest(serviceName)))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Basic " + "YWRtaW46YWRtaW4=")
+                    .build();
+                response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            }
+            responseBody = response.body();
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
 
         BasicAuthToken authToken = new BasicAuthToken(responseBody);
+
         return authToken;
     }
 
+    /**
+     * Checks whether a given AuthToken is valid by executing a get request on the principal associated with the token and using the token as the authentication for the request
+     * @param token The AuthToken to check
+     * @return A boolean stating whether the token is valid
+     */
     @Override
     public Boolean isValidToken(AuthToken token) {
         if (!(token instanceof BasicAuthToken)) {
@@ -73,25 +125,25 @@ public class RestServiceAccountManager implements ServiceAccountManager {
         return false;
     }
 
+    /**
+     * Update a service account by passing in a objectNode with new information
+     *
+     * @param contentAsNode An ObjectNode with the new account details
+     */
     @Override
-    public void updateServiceAccount(ObjectNode contentAsNode) {
+    public void updateServiceAccount(ObjectNode contentAsNode) {}
 
-    }
 
-    public RestServiceAccountManager(Settings settings) {
-        this.settings = settings;
-        this.url = buildUrl(settings);
-        this.httpClient = HttpClient.newHttpClient();
-    }
-
-    public RestServiceAccountManager(Settings settings, HttpClient httpClient) {
-        this.settings = settings;
-        this.url = buildUrl(settings);
-        this.httpClient = httpClient;
-    }
-
-    public String getOrCreateServiceAccount(String extensionId, String authenticationString) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder(URI.create(url + extensionId))
+    /**
+     * Get or create a new service account
+     * @param serviceName The name used for referencing the service
+     * @param authenticationString An authentication string used to authorize the REST request
+     * @return A string of the response body--this will be a new auth header for the account
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public String getOrCreateServiceAccount(String serviceName, String authenticationString) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url + serviceName))
             .GET()
             .header("Content-Type", "application/json")
             .header("Authorization", "Basic " + authenticationString)
@@ -99,8 +151,8 @@ public class RestServiceAccountManager implements ServiceAccountManager {
 
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() == NOT_FOUND.getStatus()) {
-            request = HttpRequest.newBuilder(URI.create(url + extensionId))
-                .PUT(ofString(createServiceAccountRequest(extensionId)))
+            request = HttpRequest.newBuilder(URI.create(url + serviceName))
+                .PUT(ofString(createServiceAccountRequest(serviceName)))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Basic " + authenticationString)
                 .build();
@@ -110,8 +162,18 @@ public class RestServiceAccountManager implements ServiceAccountManager {
         return response.body();
     }
 
-    public int getRequestStatus(String extensionId, String authenticationString) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder(URI.create(url + extensionId))
+    /**
+     *
+     * Calls a Get Request and returns the response code
+     *
+     * @param serviceName The name associated with the service in question
+     * @param authenticationString
+     * @return An int status code from the request
+     * @throws IOException If connection fails
+     * @throws InterruptedException If connection interrupted
+     */
+    public int getRequestStatus(String serviceName, String authenticationString) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url + serviceName))
             .GET()
             .header("Content-Type", "application/json")
             .header("Authorization", "Basic " + authenticationString)
@@ -119,8 +181,8 @@ public class RestServiceAccountManager implements ServiceAccountManager {
 
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() == NOT_FOUND.getStatus()) {
-            request = HttpRequest.newBuilder(URI.create(url + extensionId))
-                .PUT(ofString(createServiceAccountRequest(extensionId)))
+            request = HttpRequest.newBuilder(URI.create(url + serviceName))
+                .PUT(ofString(createServiceAccountRequest(serviceName)))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Basic " + authenticationString)
                 .build();
@@ -130,10 +192,16 @@ public class RestServiceAccountManager implements ServiceAccountManager {
         return response.statusCode();
     }
 
-    private String createServiceAccountRequest(String extensionId) {
+    /**
+     *
+     * Creates a template for a basic service account request
+     * @param serviceName A name associated with the service
+     * @return A string representing the base request body
+     */
+    private String createServiceAccountRequest(String serviceName) {
         try (XContentBuilder xContentBuilder = XContentFactory.contentBuilder(XContentType.JSON)) {
             xContentBuilder.startObject();
-            xContentBuilder.field("name", extensionId);
+            xContentBuilder.field("name", serviceName);
             xContentBuilder.field("attributes");
             xContentBuilder.startObject();
             xContentBuilder.field("isService", "true");
@@ -146,6 +214,11 @@ public class RestServiceAccountManager implements ServiceAccountManager {
         }
     }
 
+    /**
+     * Creates a URL with the route required to interact with the default security plugin implementation
+     * @param settings Settings be configured
+     * @return A string representing the route to the account APIs
+     */
     private static String buildUrl(Settings settings) {
         int port = settings.getAsInt("http.port", 9200);
         String host = settings.get("http.host", "localhost");
--- server/src/main/java/org/opensearch/identity/noop/NoopServiceAccountManager.java
+++ server/src/main/java/org/opensearch/identity/noop/NoopServiceAccountManager.java
@@ -12,8 +12,16 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.opensearch.identity.ServiceAccountManager;
 import org.opensearch.identity.tokens.AuthToken;
 
+/**
+ * A Noop implementation of a service account manager
+ */
 public class NoopServiceAccountManager implements ServiceAccountManager {
 
+    /**
+     * Returns an empty auth token
+     * @param principal A principal an auth token should be created for
+     * @return An empty auth token object
+     */
     @Override
     public AuthToken resetServiceAccountToken(String principal) {
         return new AuthToken() {
@@ -24,11 +32,20 @@ public class NoopServiceAccountManager implements ServiceAccountManager {
         };
     }
 
+    /**
+     * Pass through that always views a token as valid
+     * @param token The token to be verified
+     * @return True stating the token is valid
+     */
     @Override
     public Boolean isValidToken(AuthToken token) {
         return true;
     }
 
+    /**
+     * A method to update a service account with the provided content
+     * @param contentAsNode The content the account should be updated with
+     */
     @Override
     public void updateServiceAccount(ObjectNode contentAsNode) {}
 }
