f523c8a06d6f498b0222e37ac9e0894d4f2d89df
Michael Froh <froh@amazon.com>
2023-05-09 19:25:23 +0000

    Rename default search pipeline setting and add getter/setter
    Also change version check for ad hoc pipeline to 2.8
    
    Signed-off-by: Michael Froh <froh@amazon.com>

--- server/src/main/java/org/opensearch/index/IndexSettings.java
+++ server/src/main/java/org/opensearch/index/IndexSettings.java
@@ -42,6 +42,7 @@ import org.opensearch.common.settings.IndexScopedSettings;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Setting.Property;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.settings.SettingsException;
 import org.opensearch.common.unit.ByteSizeUnit;
 import org.opensearch.common.unit.ByteSizeValue;
 import org.opensearch.common.unit.TimeValue;
@@ -63,6 +64,7 @@ import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 import static org.opensearch.common.util.FeatureFlags.SEARCHABLE_SNAPSHOT_EXTENDED_COMPATIBILITY;
+import static org.opensearch.common.util.FeatureFlags.SEARCH_PIPELINE;
 import static org.opensearch.index.mapper.MapperService.INDEX_MAPPING_DEPTH_LIMIT_SETTING;
 import static org.opensearch.index.mapper.MapperService.INDEX_MAPPING_FIELD_NAME_LENGTH_LIMIT_SETTING;
 import static org.opensearch.index.mapper.MapperService.INDEX_MAPPING_NESTED_DOCS_LIMIT_SETTING;
@@ -580,7 +582,7 @@ public final class IndexSettings {
     );
 
     public static final Setting<String> DEFAULT_SEARCH_PIPELINE = new Setting<>(
-        "index.default_search_pipeline",
+        "index.search.default_pipeline",
         SearchPipelineService.NOOP_PIPELINE_ID,
         Function.identity(),
         Property.Dynamic,
@@ -627,6 +629,8 @@ public final class IndexSettings {
     private volatile long softDeleteRetentionOperations;
 
     private volatile long retentionLeaseMillis;
+
+    private volatile String defaultSearchPipeline;
 
     /**
      * The maximum age of a retention lease before it is considered expired.
@@ -832,6 +836,7 @@ public final class IndexSettings {
         maxFullFlushMergeWaitTime = scopedSettings.get(INDEX_MERGE_ON_FLUSH_MAX_FULL_FLUSH_MERGE_WAIT_TIME);
         mergeOnFlushEnabled = scopedSettings.get(INDEX_MERGE_ON_FLUSH_ENABLED);
         setMergeOnFlushPolicy(scopedSettings.get(INDEX_MERGE_ON_FLUSH_POLICY));
+        defaultSearchPipeline = scopedSettings.get(DEFAULT_SEARCH_PIPELINE);
 
         scopedSettings.addSettingsUpdateConsumer(MergePolicyConfig.INDEX_COMPOUND_FORMAT_SETTING, mergePolicyConfig::setNoCFSRatio);
         scopedSettings.addSettingsUpdateConsumer(
@@ -905,6 +910,7 @@ public final class IndexSettings {
         scopedSettings.addSettingsUpdateConsumer(INDEX_MERGE_ON_FLUSH_MAX_FULL_FLUSH_MERGE_WAIT_TIME, this::setMaxFullFlushMergeWaitTime);
         scopedSettings.addSettingsUpdateConsumer(INDEX_MERGE_ON_FLUSH_ENABLED, this::setMergeOnFlushEnabled);
         scopedSettings.addSettingsUpdateConsumer(INDEX_MERGE_ON_FLUSH_POLICY, this::setMergeOnFlushPolicy);
+        scopedSettings.addSettingsUpdateConsumer(DEFAULT_SEARCH_PIPELINE, this::setDefaultSearchPipeline);
     }
 
     private void setSearchSegmentOrderReversed(boolean reversed) {
@@ -1585,5 +1591,17 @@ public final class IndexSettings {
 
     public Optional<UnaryOperator<MergePolicy>> getMergeOnFlushPolicy() {
         return Optional.ofNullable(mergeOnFlushPolicy);
+    }
+
+    public String getDefaultSearchPipeline() {
+        return defaultSearchPipeline;
+    }
+
+    public void setDefaultSearchPipeline(String defaultSearchPipeline) {
+        if (FeatureFlags.isEnabled(SEARCH_PIPELINE)) {
+            this.defaultSearchPipeline = defaultSearchPipeline;
+        } else {
+            throw new SettingsException("Unsupported setting: " + DEFAULT_SEARCH_PIPELINE.getKey());
+        }
     }
 }
