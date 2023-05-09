ec422585d6e0bbdea3edae7b7ae9cdf0d0c97776
Craig Perkins <cwperx@amazon.com>
2023-05-09 17:06:49 -0400

    Add import Optional
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- server/src/main/java/org/opensearch/extensions/ExtensionsManager.java
+++ server/src/main/java/org/opensearch/extensions/ExtensionsManager.java
@@ -19,6 +19,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
