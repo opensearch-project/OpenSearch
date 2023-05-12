484002da563d3cb3495f9662c83a311e9b470315
Andriy Redko <andriy.redko@aiven.io>
2023-05-12 15:20:51 -0400

    Provide mechanism to configure XContent parsing constraints (after update to Jackson 2.15.0 and above)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- libs/x-content/src/main/java/org/opensearch/common/xcontent/cbor/CborXContent.java
+++ libs/x-content/src/main/java/org/opensearch/common/xcontent/cbor/CborXContent.java
@@ -35,6 +35,7 @@ package org.opensearch.common.xcontent.cbor;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.StreamReadConstraints;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import org.opensearch.core.xcontent.DeprecationHandler;
 import org.opensearch.core.xcontent.MediaType;
@@ -56,6 +57,9 @@ import java.util.Set;
  * A CBOR based content implementation using Jackson.
  */
 public class CborXContent implements XContent {
+    public static final int DEFAULT_MAX_STRING_LEN = Integer.parseInt(
+        System.getProperty("opensearch.xcontent.string.length.max", "50000000" /* ~50 Mb */)
+    );
 
     public static XContentBuilder contentBuilder() throws IOException {
         return XContentBuilder.builder(cborXContent);
@@ -70,6 +74,7 @@ public class CborXContent implements XContent {
         // Do not automatically close unclosed objects/arrays in com.fasterxml.jackson.dataformat.cbor.CBORGenerator#close() method
         cborFactory.configure(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT, false);
         cborFactory.configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, true);
+        cborFactory.setStreamReadConstraints(StreamReadConstraints.builder().maxStringLength(DEFAULT_MAX_STRING_LEN).build());
         cborXContent = new CborXContent();
     }
 
