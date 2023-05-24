2db5960c8f13e8c53d673366e2c94e51fdf3a905
Raghuvansh Raj <raghraaj@amazon.com>
2023-05-24 20:24:05 +0530

    Integrating S3 async upload with S3BlobContainer
    Signed-off-by: Raghuvansh Raj <raghraaj@amazon.com>

--- plugins/repository-s3/src/main/java/org/opensearch/repositories/s3/S3RepositoryPlugin.java
+++ plugins/repository-s3/src/main/java/org/opensearch/repositories/s3/S3RepositoryPlugin.java
@@ -32,44 +32,108 @@
 
 package org.opensearch.repositories.s3;
 
+import org.opensearch.client.Client;
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.metadata.RepositoryMetadata;
 import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.io.stream.NamedWriteableRegistry;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.env.Environment;
+import org.opensearch.env.NodeEnvironment;
 import org.opensearch.indices.recovery.RecoverySettings;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.plugins.ReloadablePlugin;
 import org.opensearch.plugins.RepositoryPlugin;
+import org.opensearch.repositories.RepositoriesService;
 import org.opensearch.repositories.Repository;
+import org.opensearch.repositories.s3.async.AsyncExecutorBuilder;
+import org.opensearch.repositories.s3.async.AsyncUploadUtils;
+import org.opensearch.repositories.s3.async.TransferNIOGroup;
+import org.opensearch.script.ScriptService;
+import org.opensearch.threadpool.ExecutorBuilder;
+import org.opensearch.threadpool.FixedExecutorBuilder;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.watcher.ResourceWatcherService;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 /**
  * A plugin to add a repository type that writes to and from the AWS S3.
  */
 public class S3RepositoryPlugin extends Plugin implements RepositoryPlugin, ReloadablePlugin {
+    private static final String PRIORITY_FUTURE_COMPLETION = "priority_future_completion";
+    private static final String PRIORITY_STREAM_READER = "priority_stream_reader";
+    private static final String FUTURE_COMPLETION = "future_completion";
+    private static final String STREAM_READER = "stream_reader";
 
     protected final S3Service service;
+    private final S3AsyncService s3AsyncService;
+
     private final Path configPath;
 
+    private AsyncExecutorBuilder priorityExecutorBuilder;
+    private AsyncExecutorBuilder normalExecutorBuilder;
+
     public S3RepositoryPlugin(final Settings settings, final Path configPath) {
-        this(settings, configPath, new S3Service(configPath));
+        this(settings, configPath, new S3Service(configPath), new S3AsyncService(configPath));
     }
 
-    S3RepositoryPlugin(final Settings settings, final Path configPath, final S3Service service) {
+    @Override
+    public List<ExecutorBuilder<?>> getExecutorBuilders(Settings settings) {
+        List<ExecutorBuilder<?>> executorBuilders = new ArrayList<>();
+        executorBuilders.add(new FixedExecutorBuilder(settings, PRIORITY_FUTURE_COMPLETION, 4, 10_000, PRIORITY_FUTURE_COMPLETION));
+        executorBuilders.add(new FixedExecutorBuilder(settings, PRIORITY_STREAM_READER, 4, 10_000, PRIORITY_STREAM_READER));
+        executorBuilders.add(new FixedExecutorBuilder(settings, FUTURE_COMPLETION, 1, 10_000, FUTURE_COMPLETION));
+        executorBuilders.add(new FixedExecutorBuilder(settings, STREAM_READER, 1, 10_000, STREAM_READER));
+        return executorBuilders;
+    }
+
+    S3RepositoryPlugin(final Settings settings, final Path configPath, final S3Service service, final S3AsyncService s3AsyncService) {
         this.service = Objects.requireNonNull(service, "S3 service must not be null");
         this.configPath = configPath;
         // eagerly load client settings so that secure settings are read
-        final Map<String, S3ClientSettings> clientsSettings = S3ClientSettings.load(settings, configPath);
+        Map<String, S3ClientSettings> clientsSettings = S3ClientSettings.load(settings, configPath);
+        this.s3AsyncService = Objects.requireNonNull(s3AsyncService, "S3AsyncService must not be null");
         this.service.refreshAndClearCache(clientsSettings);
+        this.s3AsyncService.refreshAndClearCache(clientsSettings);
+    }
+
+    @Override
+    public Collection<Object> createComponents(
+        final Client client,
+        final ClusterService clusterService,
+        final ThreadPool threadPool,
+        final ResourceWatcherService resourceWatcherService,
+        final ScriptService scriptService,
+        final NamedXContentRegistry xContentRegistry,
+        final Environment environment,
+        final NodeEnvironment nodeEnvironment,
+        final NamedWriteableRegistry namedWriteableRegistry,
+        final IndexNameExpressionResolver expressionResolver,
+        final Supplier<RepositoriesService> repositoriesServiceSupplier
+    ) {
+        this.priorityExecutorBuilder = new AsyncExecutorBuilder(
+            threadPool.executor(PRIORITY_FUTURE_COMPLETION),
+            threadPool.executor(PRIORITY_STREAM_READER),
+            new TransferNIOGroup(S3Repository.PRIORITY_UPLOAD_EVENT_LOOP_THREAD_COUNT_SETTING.get(clusterService.getSettings()))
+        );
+        this.normalExecutorBuilder = new AsyncExecutorBuilder(
+            threadPool.executor(FUTURE_COMPLETION),
+            threadPool.executor(STREAM_READER),
+            new TransferNIOGroup(S3Repository.NORMAL_UPLOAD_EVENT_LOOP_THREAD_COUNT_SETTING.get(clusterService.getSettings()))
+        );
+        return Collections.emptyList();
     }
 
     // proxy method for testing
@@ -79,7 +143,23 @@ public class S3RepositoryPlugin extends Plugin implements RepositoryPlugin, Relo
         final ClusterService clusterService,
         final RecoverySettings recoverySettings
     ) {
-        return new S3Repository(metadata, registry, service, clusterService, recoverySettings);
+        AsyncUploadUtils asyncUploadUtils = new AsyncUploadUtils(
+            S3Repository.PARALLEL_MULTIPART_UPLOAD_MINIMUM_PART_SIZE_SETTING.get(clusterService.getSettings()).getBytes(),
+            normalExecutorBuilder.getStreamReader(),
+            priorityExecutorBuilder.getStreamReader()
+        );
+        return new S3Repository(
+            metadata,
+            registry,
+            service,
+            clusterService,
+            recoverySettings,
+            asyncUploadUtils,
+            priorityExecutorBuilder,
+            normalExecutorBuilder,
+            s3AsyncService,
+            S3Repository.PARALLEL_MULTIPART_UPLOAD_ENABLED_SETTING.get(clusterService.getSettings())
+        );
     }
 
     @Override
@@ -119,7 +199,9 @@ public class S3RepositoryPlugin extends Plugin implements RepositoryPlugin, Relo
             S3ClientSettings.REGION,
             S3ClientSettings.ROLE_ARN_SETTING,
             S3ClientSettings.IDENTITY_TOKEN_FILE_SETTING,
-            S3ClientSettings.ROLE_SESSION_NAME_SETTING
+            S3ClientSettings.ROLE_SESSION_NAME_SETTING,
+            S3Repository.PARALLEL_MULTIPART_UPLOAD_MINIMUM_PART_SIZE_SETTING,
+            S3Repository.PARALLEL_MULTIPART_UPLOAD_ENABLED_SETTING
         );
     }
 
@@ -128,10 +210,12 @@ public class S3RepositoryPlugin extends Plugin implements RepositoryPlugin, Relo
         // secure settings should be readable
         final Map<String, S3ClientSettings> clientsSettings = S3ClientSettings.load(settings, configPath);
         service.refreshAndClearCache(clientsSettings);
+        s3AsyncService.refreshAndClearCache(clientsSettings);
     }
 
     @Override
     public void close() throws IOException {
         service.close();
+        s3AsyncService.close();
     }
 }
