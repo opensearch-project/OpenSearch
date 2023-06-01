963befd332a87677be88dc0751709b74abda6476
Shinsuke Sugaya <shinsuke@apache.org>
2023-06-02 07:58:16 +0900

    add import
    Signed-off-by: Shinsuke Sugaya <shinsuke@apache.org>

--- server/src/main/java/org/opensearch/index/mapper/FlatObjectFieldMapper.java
+++ server/src/main/java/org/opensearch/index/mapper/FlatObjectFieldMapper.java
@@ -48,6 +48,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
