daa10bec0f680e78864320df71bf288f9808a7fa
Mohit Godwani <mgodwan@amazon.com>
2023-06-05 16:36:54 +0530

    Enable Fast Double Parser in Jackson
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>

--- libs/x-content/src/main/java/org/opensearch/common/xcontent/json/JsonXContent.java
+++ libs/x-content/src/main/java/org/opensearch/common/xcontent/json/JsonXContent.java
@@ -37,7 +37,7 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.StreamReadConstraints;
-
+import com.fasterxml.jackson.core.StreamWriteFeature;
 import org.opensearch.core.xcontent.DeprecationHandler;
 import org.opensearch.core.xcontent.MediaType;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
@@ -78,6 +78,8 @@ public class JsonXContent implements XContent {
         jsonFactory.configure(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT, false);
         jsonFactory.configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, true);
         jsonFactory.setStreamReadConstraints(StreamReadConstraints.builder().maxStringLength(DEFAULT_MAX_STRING_LEN).build());
+        jsonFactory.configure(JsonParser.Feature.USE_FAST_DOUBLE_PARSER, true);
+        jsonFactory.configure(StreamWriteFeature.USE_FAST_DOUBLE_WRITER.mappedFeature(), true);
         jsonXContent = new JsonXContent();
     }
 
