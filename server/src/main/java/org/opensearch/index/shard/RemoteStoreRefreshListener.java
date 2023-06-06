350a92984918565b952c9679f8825857416bbcbd
Ashish Singh <ssashish@amazon.com>
2023-06-06 11:44:12 +0530

    [Remote Store] Optimize segments metadata upload
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
+++ server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
@@ -169,12 +169,11 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
 
         if (didRefresh) {
             updateLocalRefreshTimeAndSeqNo();
-        }
-
-        try {
-            indexShard.getThreadPool().executor(ThreadPool.Names.REMOTE_REFRESH).submit(() -> syncSegments(false)).get();
-        } catch (InterruptedException | ExecutionException e) {
-            logger.info("Exception occurred while scheduling syncSegments", e);
+            try {
+                indexShard.getThreadPool().executor(ThreadPool.Names.REMOTE_REFRESH).submit(() -> syncSegments(false)).get();
+            } catch (InterruptedException | ExecutionException e) {
+                logger.info("Exception occurred while scheduling syncSegments", e);
+            }
         }
     }
 
@@ -244,9 +243,7 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
                                 segmentInfos.getGeneration()
                             );
                             clearStaleFilesFromLocalSegmentChecksumMap(localSegmentsPostRefresh);
-                            onSuccessfulSegmentsSync(refreshTimeMs, refreshSeqNo);
-                            indexShard.getEngine().translogManager().setMinSeqNoToKeep(lastRefreshedCheckpoint + 1);
-                            checkpointPublisher.publish(indexShard, checkpoint);
+                            onSuccessfulSegmentsSync(refreshTimeMs, refreshSeqNo, lastRefreshedCheckpoint, checkpoint);
                             // At this point since we have uploaded new segments, segment infos and segment metadata file,
                             // along with marking minSeqNoToKeep, upload has succeeded completely.
                             shouldRetry = false;
@@ -298,7 +295,12 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
         segmentTracker.incrementTotalUploadsStarted();
     }
 
-    private void onSuccessfulSegmentsSync(long refreshTimeMs, long refreshSeqNo) {
+    private void onSuccessfulSegmentsSync(
+        long refreshTimeMs,
+        long refreshSeqNo,
+        long lastRefreshedCheckpoint,
+        ReplicationCheckpoint checkpoint
+    ) {
         // Update latest uploaded segment files name in segment tracker
         segmentTracker.setLatestUploadedFiles(latestFileNameSizeOnLocalMap.keySet());
         // Update the remote refresh time and refresh seq no
@@ -307,6 +309,10 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
         resetBackOffDelayIterator();
         // Cancel the scheduled cancellable retry if possible and set it to null
         cancelAndResetScheduledCancellableRetry();
+        // Set the minimum sequence number for keeping translog
+        indexShard.getEngine().translogManager().setMinSeqNoToKeep(lastRefreshedCheckpoint + 1);
+        // Publishing the new checkpoint which is used for remote store + segrep indexes
+        checkpointPublisher.publish(indexShard, checkpoint);
     }
 
     /**
