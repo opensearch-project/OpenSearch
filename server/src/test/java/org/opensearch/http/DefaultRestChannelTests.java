a0fee3693e72009efffb326da5d65b8c8ee39547
Sam Hobbs <shobbs@scottlogic.com>
2023-06-20 13:29:35 +0100

    Ran spotless
    Signed-off-by: Sam Hobbs <shobbs@scottlogic.com>

--- server/src/test/java/org/opensearch/http/DefaultRestChannelTests.java
+++ server/src/test/java/org/opensearch/http/DefaultRestChannelTests.java
@@ -190,7 +190,10 @@ public class DefaultRestChannelTests extends OpenSearchTestCase {
         assertEquals("abc", headers.get(Task.X_OPAQUE_ID).get(0));
         assertEquals(Integer.toString(resp.content().length()), headers.get(DefaultRestChannel.CONTENT_LENGTH).get(0));
         assertEquals(resp.contentType(), headers.get(DefaultRestChannel.CONTENT_TYPE).get(0));
-        assertEquals("OpenSearch/" + Build.CURRENT.getQualifiedVersion() + " (" + Build.CURRENT.getDistribution() + ")",headers.get("serverHeader").get(0));
+        assertEquals(
+            "OpenSearch/" + Build.CURRENT.getQualifiedVersion() + " (" + Build.CURRENT.getDistribution() + ")",
+            headers.get("serverHeader").get(0)
+        );
     }
 
     public void testCookiesSet() {
