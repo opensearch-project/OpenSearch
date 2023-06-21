779a43e89723f036ec5f44e31697451482477945
Stephen Crawford <steecraw@amazon.com>
2023-06-21 11:51:17 -0400

    remove debug
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- server/src/main/java/org/opensearch/identity/ApplicationAwareSubject.java
+++ server/src/main/java/org/opensearch/identity/ApplicationAwareSubject.java
@@ -65,11 +65,9 @@ public class ApplicationAwareSubject implements Subject {
      * @return true if allowed, false if none of the scopes are allowed.
      */
     public boolean isAllowed(final List<Scope> scopes) {
-        System.out.println("At top of is allowed with scopes: " + scopes);
         final Optional<Principal> optionalPrincipal = wrapped.getApplication();
 
         if (optionalPrincipal.isEmpty()) {
-            System.out.println("Returning true because no application");
             // If there is no application, actions are allowed by default
             return true;
         }
@@ -77,7 +75,6 @@ public class ApplicationAwareSubject implements Subject {
         final Principal appPrincipal = optionalPrincipal.get();
 
         if (!this.checkApplicationExists(appPrincipal)) {
-            System.out.println("Returning false because application does not exist");
             return false;
         }
 
@@ -87,18 +84,16 @@ public class ApplicationAwareSubject implements Subject {
             .map(Scope::parseScopeFromString)
             .anyMatch(parsedScope -> parsedScope.equals(ApplicationScope.SuperUserAccess))) {
             // Applications that are fully trusted automatically pass all checks
-            System.out.println("Returning true because superuser");
             return true;
         }
 
         if (scopesOfApplication.stream()
             .map(Scope::parseScopeFromString)
             .anyMatch(parsedScope -> scopes.stream().anyMatch(parsedScope::equals))) { // Find any matches between scope list and subject's
-            System.out.println("Returning true because matching scope");                                            // scopes
+            // scopes
             return true;
         }
 
-        System.out.println("Returning false because no matching scopes");
         return false;
     }
 }
