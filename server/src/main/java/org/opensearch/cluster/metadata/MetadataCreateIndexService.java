7401c12a5de686fa2820aa704c29cdcdef355082
Rishikesh1159 <rishireddy1159@gmail.com>
2023-06-23 12:28:43 +0000

    Add check of feature flag enabled.
    Signed-off-by: Rishikesh1159 <rishireddy1159@gmail.com>

--- server/src/main/java/org/opensearch/cluster/metadata/MetadataCreateIndexService.java
+++ server/src/main/java/org/opensearch/cluster/metadata/MetadataCreateIndexService.java
@@ -72,6 +72,7 @@ import org.opensearch.common.logging.DeprecationLogger;
 import org.opensearch.common.settings.IndexScopedSettings;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.util.FeatureFlags;
 import org.opensearch.common.xcontent.XContentHelper;
 import org.opensearch.core.common.Strings;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
@@ -136,6 +137,7 @@ import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_REMOTE_TRANS
 import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_REMOTE_TRANSLOG_STORE_REPOSITORY;
 import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_REPLICATION_TYPE;
 import static org.opensearch.cluster.metadata.Metadata.DEFAULT_REPLICA_COUNT_SETTING;
+import static org.opensearch.common.util.FeatureFlags.REMOTE_STORE;
 import static org.opensearch.indices.IndicesService.CLUSTER_REMOTE_STORE_REPOSITORY_SETTING;
 import static org.opensearch.indices.IndicesService.CLUSTER_REMOTE_TRANSLOG_REPOSITORY_SETTING;
 import static org.opensearch.indices.IndicesService.CLUSTER_REMOTE_STORE_ENABLED_SETTING;
@@ -900,7 +902,9 @@ public class MetadataCreateIndexService {
 
         if (isSystemIndex || IndexMetadata.INDEX_HIDDEN_SETTING.get(request.settings())) {
             indexSettingsBuilder.put(SETTING_REPLICATION_TYPE, ReplicationType.DOCUMENT);
-            indexSettingsBuilder.put(SETTING_REMOTE_STORE_ENABLED, false);
+            if (FeatureFlags.isEnabled(REMOTE_STORE)) {
+                indexSettingsBuilder.put(SETTING_REMOTE_STORE_ENABLED, false);
+            }
         } else {
             updateReplicationStrategy(indexSettingsBuilder, request.settings(), settings);
             updateRemoteStoreSettings(indexSettingsBuilder, request.settings(), settings);
