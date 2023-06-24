55650278005f530be5234dd0b7e53452316193a7
suranjay <surajkumar.tu@gmail.com>
2023-06-24 17:35:58 +0530

    Fix javadocs error
    Signed-off-by: suranjay <surajkumar.tu@gmail.com>

--- libs/telemetry/src/main/java/org/opensearch/telemetry/tracing/SpanReference.java
+++ libs/telemetry/src/main/java/org/opensearch/telemetry/tracing/SpanReference.java
@@ -15,14 +15,26 @@ public class SpanReference {
 
     private Span span;
 
+    /**
+     * Creates the wrapper with given span
+     * @param span the span object to wrap
+     */
     public SpanReference(Span span) {
         this.span = span;
     }
 
+    /**
+     * Returns the span object
+     * @return underlying span
+     */
     public Span getSpan() {
         return span;
     }
 
+    /**
+     * Updates the underlying span
+     * @param span underlying span
+     */
     public void setSpan(Span span) {
         this.span = span;
     }
