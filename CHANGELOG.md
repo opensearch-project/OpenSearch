0dbafea3c991422ba5c82eae9a8f6953ab54f42a
Heemin Kim <heemin@amazon.com>
2023-06-30 08:14:41 -0700

    [Backport 2.x] GeoTile, and GeoGrid aggregation (#8295)
    * Add GeoTile and GeoHash Grid aggregations on GeoShapes. (#5589)
    
    Src files for GeoTile and GeoHash Aggregations on GeoShape with integration
    tests.
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>
    
    * [opensearch-project/geospatial#212] Fixing the IT for GeoTilesAggrega… (#6120)
    
    Fixing the IT for GeoTilesAggregation.
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>
    
    * [#6187, #6222] Fixing the GeoShapes GeoHash and GeoTile Aggregations Integration tests. (#6242)
    
    Changes done:
    * Fixed the ArrayIndexOutOfBoundsException.
    * Reduced the precision for GeoShapes Aggregation IT testing.
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>
    
    * [#7101] Fixing the GeoTileIT#testMultivaluedGeoPointsAggregation test case. (#7166)
    
    The issue was happening because we encode the GeoPoint as long and error comes in the precision due to that encoding. The error was not taken care while generating the exepected tiles count for execpected output.
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>
    
    ---------
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>
    Signed-off-by: Heemin Kim <heemin@amazon.com>
    Co-authored-by: Navneet Verma <navneev@amazon.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
   - Addition of GeoShape ValueSource level code interfaces for accessing the DocValues.
   - Addition of Missing Value feature in the GeoShape Aggregations.
 - Enable Point based optimization for custom comparators ([#8168](https://github.com/opensearch-project/OpenSearch/pull/8168))
+- Add GeoTile and GeoHash Grid aggregations on GeoShapes. ([#5589](https://github.com/opensearch-project/OpenSearch/pull/5589))
 
 ### Dependencies
 - Bump `com.azure:azure-storage-common` from 12.21.0 to 12.21.1 (#7566, #7814)
--- modules/geo/src/main/java/org/opensearch/geo/search/aggregations/bucket/geogrid/cells/UnboundedCellValues.java
+++ modules/geo/src/main/java/org/opensearch/geo/search/aggregations/bucket/geogrid/cells/UnboundedCellValues.java
@@ -29,39 +29,26 @@
  * GitHub history for details.
  */
 
-package org.opensearch.geo.search.aggregations.bucket.geogrid;
+package org.opensearch.geo.search.aggregations.bucket.geogrid.cells;
 
 import org.opensearch.common.geo.GeoBoundingBox;
 import org.opensearch.index.fielddata.MultiGeoPointValues;
 
 /**
- * Class representing {@link CellValues} whose values are filtered
- * according to whether they are within the specified {@link GeoBoundingBox}.
- *
- * The specified bounding box is assumed to be bounded.
+ * Class representing {@link CellValues} that are unbounded by any
+ * {@link GeoBoundingBox}.
  *
  * @opensearch.internal
  */
-class BoundedCellValues extends CellValues {
+class UnboundedCellValues extends CellValues {
 
-    private final GeoBoundingBox geoBoundingBox;
-
-    protected BoundedCellValues(
-        MultiGeoPointValues geoValues,
-        int precision,
-        CellIdSource.GeoPointLongEncoder encoder,
-        GeoBoundingBox geoBoundingBox
-    ) {
+    UnboundedCellValues(MultiGeoPointValues geoValues, int precision, CellIdSource.GeoPointLongEncoder encoder) {
         super(geoValues, precision, encoder);
-        this.geoBoundingBox = geoBoundingBox;
     }
 
     @Override
     int advanceValue(org.opensearch.common.geo.GeoPoint target, int valuesIdx) {
-        if (geoBoundingBox.pointInBounds(target.getLon(), target.getLat())) {
-            values[valuesIdx] = encoder.encode(target.getLon(), target.getLat(), precision);
-            return valuesIdx + 1;
-        }
-        return valuesIdx;
+        values[valuesIdx] = encoder.encode(target.getLon(), target.getLat(), precision);
+        return valuesIdx + 1;
     }
 }
