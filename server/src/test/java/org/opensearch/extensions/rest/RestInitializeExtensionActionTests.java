ee2218e7ad074adfe1b2cd9b32fa0d0d99990c0f
Craig Perkins <cwperx@amazon.com>
2023-07-03 14:58:46 -0400

    Add tests
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- server/src/test/java/org/opensearch/extensions/rest/RestInitializeExtensionActionTests.java
+++ server/src/test/java/org/opensearch/extensions/rest/RestInitializeExtensionActionTests.java
@@ -9,24 +9,31 @@
 package org.opensearch.extensions.rest;
 
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import org.junit.After;
 import org.junit.Before;
+import org.mockito.Mockito;
 import org.opensearch.Version;
 import org.opensearch.cluster.node.DiscoveryNode;
 import org.opensearch.common.bytes.BytesArray;
 import org.opensearch.common.io.stream.NamedWriteableRegistry;
 import org.opensearch.common.network.NetworkService;
+import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.PageCacheRecycler;
 import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.extensions.ExtensionsManager;
+import org.opensearch.extensions.ExtensionsSettings;
 import org.opensearch.indices.breaker.NoneCircuitBreakerService;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.rest.RestStatus;
@@ -119,6 +126,62 @@ public class RestInitializeExtensionActionTests extends OpenSearchTestCase {
         assertTrue(
             channel.capturedResponse().content().utf8ToString().contains("Required field [extension uniqueId] is missing in the request")
         );
+    }
+
+    public void testRestInitializeExtensionActionResponseWithAdditionalSettings() throws Exception {
+        Setting boolSetting = Setting.boolSetting("boolSetting", false, Setting.Property.ExtensionScope);
+        ExtensionsManager extensionsManager = new ExtensionsManager(Set.of(boolSetting));
+        ExtensionsManager spy = spy(extensionsManager);
+
+        // optionally, you can stub out some methods:
+        when(spy.getAdditionalSettings()).thenCallRealMethod();
+        Mockito.doCallRealMethod().when(spy).loadExtension(any(ExtensionsSettings.Extension.class));
+        Mockito.doNothing().when(spy).initialize();
+        RestInitializeExtensionAction restInitializeExtensionAction = new RestInitializeExtensionAction(spy);
+        final String content = "{\"name\":\"ad-extension\",\"uniqueId\":\"ad-extension\",\"hostAddress\":\"127.0.0.1\","
+            + "\"port\":\"4532\",\"version\":\"1.0\",\"opensearchVersion\":\"3.0.0\","
+            + "\"minimumCompatibleVersion\":\"3.0.0\",\"boolSetting\":true}";
+        RestRequest request = new FakeRestRequest.Builder(xContentRegistry()).withContent(new BytesArray(content), XContentType.JSON)
+            .withMethod(RestRequest.Method.POST)
+            .build();
+
+        FakeRestChannel channel = new FakeRestChannel(request, false, 0);
+        restInitializeExtensionAction.handleRequest(request, channel, null);
+
+        assertEquals(channel.capturedResponse().status(), RestStatus.ACCEPTED);
+        assertTrue(channel.capturedResponse().content().utf8ToString().contains("A request to initialize an extension has been sent."));
+
+        Optional<ExtensionsSettings.Extension> extension = spy.lookupExtensionSettingsById("ad-extension");
+        assertTrue(extension.isPresent());
+        assertEquals(true, extension.get().getAdditionalSettings().get(boolSetting));
+    }
+
+    public void testRestInitializeExtensionActionResponseWithAdditionalSettingsUsingDefault() throws Exception {
+        Setting boolSetting = Setting.boolSetting("boolSetting", false, Setting.Property.ExtensionScope);
+        ExtensionsManager extensionsManager = new ExtensionsManager(Set.of(boolSetting));
+        ExtensionsManager spy = spy(extensionsManager);
+
+        // optionally, you can stub out some methods:
+        when(spy.getAdditionalSettings()).thenCallRealMethod();
+        Mockito.doCallRealMethod().when(spy).loadExtension(any(ExtensionsSettings.Extension.class));
+        Mockito.doNothing().when(spy).initialize();
+        RestInitializeExtensionAction restInitializeExtensionAction = new RestInitializeExtensionAction(spy);
+        final String content = "{\"name\":\"ad-extension\",\"uniqueId\":\"ad-extension\",\"hostAddress\":\"127.0.0.1\","
+            + "\"port\":\"4532\",\"version\":\"1.0\",\"opensearchVersion\":\"3.0.0\","
+            + "\"minimumCompatibleVersion\":\"3.0.0\"}";
+        RestRequest request = new FakeRestRequest.Builder(xContentRegistry()).withContent(new BytesArray(content), XContentType.JSON)
+            .withMethod(RestRequest.Method.POST)
+            .build();
+
+        FakeRestChannel channel = new FakeRestChannel(request, false, 0);
+        restInitializeExtensionAction.handleRequest(request, channel, null);
+
+        assertEquals(channel.capturedResponse().status(), RestStatus.ACCEPTED);
+        assertTrue(channel.capturedResponse().content().utf8ToString().contains("A request to initialize an extension has been sent."));
+
+        Optional<ExtensionsSettings.Extension> extension = spy.lookupExtensionSettingsById("ad-extension");
+        assertTrue(extension.isPresent());
+        assertEquals(false, extension.get().getAdditionalSettings().get(boolSetting));
     }
 
 }
