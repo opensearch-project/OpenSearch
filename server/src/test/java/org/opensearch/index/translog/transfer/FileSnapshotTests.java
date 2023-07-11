6c7c932b5a49dccd1603be7068bbeb0b5531eea3
Raghuvansh Raj <raghraaj@amazon.com>
2023-07-11 18:06:01 +0530

    [Remote Store] Add multipart upload integration for translog and segment files (#7119)
    Signed-off-by: Raghuvansh Raj <raghraaj@amazon.com>

--- server/src/test/java/org/opensearch/index/translog/transfer/FileSnapshotTests.java
+++ server/src/test/java/org/opensearch/index/translog/transfer/FileSnapshotTests.java
@@ -28,15 +28,15 @@ public class FileSnapshotTests extends OpenSearchTestCase {
     public void testFileSnapshotPath() throws IOException {
         Path file = createTempFile();
         Files.writeString(file, "hello");
-        fileSnapshot = new FileSnapshot.TransferFileSnapshot(file, 12);
+        fileSnapshot = new FileSnapshot.TransferFileSnapshot(file, 12, null);
 
         assertFileSnapshotProperties(file);
 
-        try (FileSnapshot sameFileSnapshot = new FileSnapshot.TransferFileSnapshot(file, 12)) {
+        try (FileSnapshot sameFileSnapshot = new FileSnapshot.TransferFileSnapshot(file, 12, null)) {
             assertEquals(sameFileSnapshot, fileSnapshot);
         }
 
-        try (FileSnapshot sameFileDiffPTSnapshot = new FileSnapshot.TransferFileSnapshot(file, 34)) {
+        try (FileSnapshot sameFileDiffPTSnapshot = new FileSnapshot.TransferFileSnapshot(file, 34, null)) {
             assertNotEquals(sameFileDiffPTSnapshot, fileSnapshot);
         }
     }
