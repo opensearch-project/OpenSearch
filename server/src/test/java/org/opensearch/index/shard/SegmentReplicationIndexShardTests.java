b669533ab9a13fc6075510d54311a065f3414567
Varun Bansal <bansvaru@amazon.com>
2023-07-21 18:08:41 +0530

    Mute test - SegmentReplicationIndexShardTests#testNRTReplicaWithRemoteStorePromotedAsPrimaryCommitCommit (#8818)
    Signed-off-by: bansvaru <bansvaru@amazon.com>

--- server/src/test/java/org/opensearch/index/shard/SegmentReplicationIndexShardTests.java
+++ server/src/test/java/org/opensearch/index/shard/SegmentReplicationIndexShardTests.java
@@ -884,6 +884,7 @@ public class SegmentReplicationIndexShardTests extends OpenSearchIndexLevelRepli
         testNRTReplicaWithRemoteStorePromotedAsPrimary(true, false);
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/8817")
     public void testNRTReplicaWithRemoteStorePromotedAsPrimaryCommitCommit() throws Exception {
         testNRTReplicaWithRemoteStorePromotedAsPrimary(true, true);
     }
