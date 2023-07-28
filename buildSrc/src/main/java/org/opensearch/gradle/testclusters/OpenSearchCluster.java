5ae72ed36e04f90d7ce42cc79a1ae2310769c188
Stephen Crawford <steecraw@amazon.com>
2023-07-28 11:30:05 -0400

    spotless
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- buildSrc/src/main/java/org/opensearch/gradle/testclusters/OpenSearchCluster.java
+++ buildSrc/src/main/java/org/opensearch/gradle/testclusters/OpenSearchCluster.java
@@ -372,8 +372,7 @@ public class OpenSearchCluster implements TestClusterConfiguration, Named {
             nodeNames = nodes.stream().map(OpenSearchNode::getName).map(this::safeName).collect(Collectors.joining(","));
         }
         String httpProtocol = "http";
-        Optional<String> protocol = nodes.stream()
-            .map(OpenSearchNode::getHttpProtocol).findFirst();
+        Optional<String> protocol = nodes.stream().map(OpenSearchNode::getHttpProtocol).findFirst();
         if (protocol.isPresent()) {
             httpProtocol = protocol.get();
         }
