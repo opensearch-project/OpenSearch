46a6812bbe2ceea087935fc1ed7bc318f9b67a2a
Stephen Crawford <steecraw@amazon.com>
2023-07-28 11:19:51 -0400

    Swap issuer with subject
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- server/src/main/java/org/opensearch/identity/tokens/OnBehalfOfClaims.java
+++ server/src/main/java/org/opensearch/identity/tokens/OnBehalfOfClaims.java
@@ -14,7 +14,7 @@ package org.opensearch.identity.tokens;
 public class OnBehalfOfClaims {
 
     private final String audience;
-    private final String issuer;
+    private final String subject;
     private final Long expiration;
     private final Long not_before;
     private final Long issued_at;
@@ -22,14 +22,14 @@ public class OnBehalfOfClaims {
     /**
      * Constructor for OnBehalfOfClaims
      * @param aud the Audience for the token
-     * @param issuer the Issuer of the token
+     * @param subject the subject of the token
      * @param expiration the expiration time in seconds for the token
      * @param not_before the not_before time in seconds for the token
      * @param issued_at the issued_at time in seconds for the token
      */
-    public OnBehalfOfClaims(String aud, String issuer, Long expiration, Long not_before, Long issued_at) {
+    public OnBehalfOfClaims(String aud, String subject, Long expiration, Long not_before, Long issued_at) {
         this.audience = aud;
-        this.issuer = issuer;
+        this.subject = subject;
         this.expiration = expiration;
         this.not_before = not_before;
         this.issued_at = issued_at;
@@ -38,39 +38,39 @@ public class OnBehalfOfClaims {
     /**
      * A constructor that sets a default issued at time of the current time
      * @param aud the Audience for the token
-     * @param issuer the Issuer of the token
+     * @param subject the subject of the token
      * @param expiration the expiration time in seconds for the token
      * @param not_before the not_before time in seconds for the token
      */
-    public OnBehalfOfClaims(String aud, String issuer, Long expiration, Long not_before) {
-        this(aud, issuer, expiration, not_before, System.nanoTime() / 1000000);
+    public OnBehalfOfClaims(String aud, String subject, Long expiration, Long not_before) {
+        this(aud, subject, expiration, not_before, System.nanoTime() / 1000000);
     }
 
     /**
      * A constructor which sets a default not before time of the current time
      * @param aud the Audience for the token
-     * @param issuer the Issuer of the token
+     * @param subject the subject of the token
      * @param expiration the expiration time in seconds for the token
      */
-    public OnBehalfOfClaims(String aud, String issuer, Long expiration) {
-        this(aud, issuer, expiration, System.nanoTime() / 1000000);
+    public OnBehalfOfClaims(String aud, String subject, Long expiration) {
+        this(aud, subject, expiration, System.nanoTime() / 1000000);
     }
 
     /**
      * A constructor which sets the default expiration time of 5 minutes from the current time
      * @param aud the Audience for the token
-     * @param issuer the Issuer of the token
+     * @param subject the subject of the token
      */
-    public OnBehalfOfClaims(String aud, String issuer) {
-        this(aud, issuer, System.nanoTime() / 1000000 + 300000);
+    public OnBehalfOfClaims(String aud, String subject) {
+        this(aud, subject, System.nanoTime() / 1000000 + 300000);
     }
 
     public String getAudience() {
         return audience;
     }
 
-    public String getIssuer() {
-        return issuer;
+    public String getSubject() {
+        return subject;
     }
 
     public Long getExpiration() {
