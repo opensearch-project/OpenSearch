14ca8edab677316a32bf501067705da80cd4874f
Suraj Singh <surajrider@gmail.com>
2023-08-01 13:09:50 -0700

    Spotless fix
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/main/java/org/opensearch/index/shard/IndexShard.java
+++ server/src/main/java/org/opensearch/index/shard/IndexShard.java
@@ -198,7 +198,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
