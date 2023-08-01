a5707c79427fb2b5d6aa803449d94bc12247f9b2
Stefano Maglione <maglionestefanoinfo@gmail.com>
2023-08-01 19:35:22 +0200

    Revert "Added featureflag"
    This reverts commit 12f68718357f0e1cd1022c17bba2a31642931406.

--- server/src/test/java/org/opensearch/search/SearchServiceTests.java
+++ server/src/test/java/org/opensearch/search/SearchServiceTests.java
@@ -226,7 +226,7 @@ public class SearchServiceTests extends OpenSearchSingleNodeTestCase {
     }
 
     @Override
-    protected Settings featureFlagSettings() {
+    protected Settings nodeSettings() {
         return Settings.builder().put("search.default_search_timeout", "5s").put(FeatureFlags.CONCURRENT_SEGMENT_SEARCH, true).build();
     }
 
