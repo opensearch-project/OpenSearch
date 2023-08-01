dc795934f76aa80089654df6e20f16203b78d30e
Suraj Singh <surajrider@gmail.com>
2023-07-31 17:23:44 -0700

    Remove un-necessary char
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/main/java/org/opensearch/index/store/RemoteSegmentStoreDirectory.java
+++ server/src/main/java/org/opensearch/index/store/RemoteSegmentStoreDirectory.java
@@ -201,7 +201,7 @@ public final class RemoteSegmentStoreDirectory extends FilterDirectory implement
         return remoteSegmentMetadata;
     }
 
-    private RemoteSegmentMetadata readMetadataFile(String metadataFilename) throws IOException {S
+    private RemoteSegmentMetadata readMetadataFile(String metadataFilename) throws IOException {
         try (IndexInput indexInput = remoteMetadataDirectory.openInput(metadataFilename, IOContext.DEFAULT)) {
             byte[] metadataBytes = new byte[(int) indexInput.length()];
             indexInput.readBytes(metadataBytes, 0, (int) indexInput.length());
