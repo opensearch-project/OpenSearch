15b7de00f2ff75587560ac6e38ffb20da030178e
Jay Deng <jayd0104@gmail.com>
2023-08-09 12:21:04 -0700

    Do not evaluate shard_size and shard_min_doc_count at segment slice level (#9085)
    * Use BucketCountThresholds in InternalTerms and InternalAggregations and do not apply shard level thresholds at slice level for Concurrent Segment Search
    
    Signed-off-by: Jay Deng <jayd0104@gmail.com>
    
    * Addressing comments
    
    Signed-off-by: Jay Deng <jayd0104@gmail.com>
    
    * Re-introduce shardSize member to InternalMultiTerms and InternalMappedTerms
    
    Signed-off-by: Jay Deng <jayd0104@gmail.com>
    
    * Introduce LocalBucketCountThresholds for local size and min_doc_count values
    
    Signed-off-by: Jay Deng <jayd0104@gmail.com>
    
    ---------
    
    Signed-off-by: Jay Deng <jayd0104@gmail.com>

--- server/src/main/java/org/opensearch/search/aggregations/InternalAggregation.java
+++ server/src/main/java/org/opensearch/search/aggregations/InternalAggregation.java
@@ -40,6 +40,8 @@ import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.rest.action.search.RestSearchAction;
 import org.opensearch.script.ScriptService;
+import org.opensearch.search.aggregations.bucket.LocalBucketCountThresholds;
+import org.opensearch.search.aggregations.bucket.terms.TermsAggregator;
 import org.opensearch.search.aggregations.pipeline.PipelineAggregator;
 import org.opensearch.search.aggregations.pipeline.PipelineAggregator.PipelineTree;
 import org.opensearch.search.aggregations.support.AggregationPath;
@@ -158,6 +160,18 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
 
         public boolean isSliceLevel() {
             return this.isSliceLevel;
+        }
+
+        /**
+         * For slice level partial reduce we will apply shard level `shard_size` and `shard_min_doc_count` limits
+         * whereas for coordinator level partial reduce it will use top level `size` and `min_doc_count`
+         */
+        public LocalBucketCountThresholds asLocalBucketCountThresholds(TermsAggregator.BucketCountThresholds bucketCountThresholds) {
+            if (isSliceLevel()) {
+                return new LocalBucketCountThresholds(bucketCountThresholds.getShardMinDocCount(), bucketCountThresholds.getShardSize());
+            } else {
+                return new LocalBucketCountThresholds(bucketCountThresholds.getMinDocCount(), bucketCountThresholds.getRequiredSize());
+            }
         }
 
         public BigArrays bigArrays() {
