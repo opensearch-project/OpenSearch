471ca68e15e84d4471384821d8727b8921e82f48
Owais Kazi <owaiskazi19@gmail.com>
2023-08-14 11:42:45 -0700

    The spotless should take care of unified imports ordering (libs/ subprojects) (#9309)
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    (cherry picked from commit 301c766075a4a91870439482cab4cfb09d9299bd)

--- libs/core/src/main/java/org/opensearch/core/common/breaker/CircuitBreakingException.java
+++ libs/core/src/main/java/org/opensearch/core/common/breaker/CircuitBreakingException.java
@@ -35,8 +35,8 @@ import org.opensearch.LegacyESVersion;
 import org.opensearch.OpenSearchException;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
-import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.rest.RestStatus;
+import org.opensearch.core.xcontent.XContentBuilder;
 
 import java.io.IOException;
 
