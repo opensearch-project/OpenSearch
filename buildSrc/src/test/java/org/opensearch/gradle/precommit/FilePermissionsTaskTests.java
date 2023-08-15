314448d1cd11b5e69e36ecf3d0f51c2a901677af
Andriy Redko <andriy.redko@aiven.io>
2023-08-14 20:27:56 -0400

    [BUG] The spotless should take care of unified imports ordering (buildSrc/ subprojects) (#9314)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    Signed-off-by: Ivan Brusic <ivan.brusic@flocksafety.com>

--- buildSrc/src/test/java/org/opensearch/gradle/precommit/FilePermissionsTaskTests.java
+++ buildSrc/src/test/java/org/opensearch/gradle/precommit/FilePermissionsTaskTests.java
@@ -31,12 +31,8 @@
 
 package org.opensearch.gradle.precommit;
 
-import java.io.File;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.List;
-
 import com.carrotsearch.randomizedtesting.RandomizedTest;
+
 import org.apache.tools.ant.taskdefs.condition.Os;
 import org.opensearch.gradle.test.GradleUnitTestCase;
 import org.gradle.api.GradleException;
@@ -44,6 +40,11 @@ import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Assert;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
 
 public class FilePermissionsTaskTests extends GradleUnitTestCase {
 
