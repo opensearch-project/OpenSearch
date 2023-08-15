40301f5eb00b436f1174e5c25f628ab0aa563166
Ankit Kala <ankikala@amazon.com>
2023-08-16 00:21:04 +0530

    Fix spotless violations during merge
    Signed-off-by: Ankit Kala <ankikala@amazon.com>

--- server/src/main/java/org/opensearch/index/SegmentReplicationPressureService.java
+++ server/src/main/java/org/opensearch/index/SegmentReplicationPressureService.java
@@ -10,7 +10,6 @@ package org.opensearch.index;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.opensearch.core.action.ActionListener;
 import org.opensearch.cluster.action.shard.ShardStateAction;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.inject.Inject;
