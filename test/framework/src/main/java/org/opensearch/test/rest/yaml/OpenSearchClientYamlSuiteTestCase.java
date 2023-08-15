a643c89805b19ad9e37094b5803f8807de908738
Owais Kazi <owaiskazi19@gmail.com>
2023-08-15 10:34:17 -0700

    The spotless should take care of unified imports ordering (test/ subprojects) (#9351)
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    (cherry picked from commit a1faafed3ad88dd8f863e40766b7f035df0ed99b)

--- test/framework/src/main/java/org/opensearch/test/rest/yaml/OpenSearchClientYamlSuiteTestCase.java
+++ test/framework/src/main/java/org/opensearch/test/rest/yaml/OpenSearchClientYamlSuiteTestCase.java
@@ -34,6 +34,7 @@ package org.opensearch.test.rest.yaml;
 
 import com.carrotsearch.randomizedtesting.RandomizedTest;
 import com.carrotsearch.randomizedtesting.annotations.TimeoutSuite;
+
 import org.apache.http.HttpHost;
 import org.apache.lucene.tests.util.TimeUnits;
 import org.opensearch.Version;
