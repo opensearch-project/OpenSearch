2e2a1a704852425becfab1981c60c465e2e32bc6
Nick Knize <nknize@apache.org>
2023-08-14 20:06:51 -0500

    Refactor Compressors from CompressorFactory to CompressorRegistry for extensibility (#9262)
    * Refactor Compressors from CompressorFactory to CompressorRegistry for extensibility
    
    This commit refactors the CompressorFactory static singleton
    class and CompressorType enum to a formal CompressorRegistry and enables
    downstream implementations to register their own compression
    implementations for use in compressing Blob stores and MediaType data.
    This is different from Lucene's Codec compression extension points which
    expose different compression implementations for Lucene's Stored Fields.
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * fix missing javadoc and relocate the common compress to compress package
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * fix deprecated case
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * fix typo in compress build.gradle
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * PR cleanup
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * PR changes
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * update with initial annotations
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    * remove NONE singleton in CompressorRegistry and remove static block init
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    
    ---------
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- test/framework/src/main/java/org/opensearch/test/core/compress/AbstractCompressorTestCase.java
+++ test/framework/src/main/java/org/opensearch/test/core/compress/AbstractCompressorTestCase.java
@@ -6,11 +6,11 @@
  * compatible open source license.
  */
 
-package org.opensearch.common.compress;
+package org.opensearch.test.core.compress;
 
 import org.apache.lucene.tests.util.LineFileDocs;
 import org.apache.lucene.tests.util.TestUtil;
-import org.opensearch.core.common.compress.Compressor;
+import org.opensearch.core.compress.Compressor;
 import org.opensearch.test.OpenSearchTestCase;
 
 import java.io.ByteArrayInputStream;
@@ -22,7 +22,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
-abstract class AbstractCompressorTests extends OpenSearchTestCase {
+public abstract class AbstractCompressorTestCase extends OpenSearchTestCase {
 
     public void testRandom() throws IOException {
         Random r = random();
@@ -404,6 +404,6 @@ abstract class AbstractCompressorTests extends OpenSearchTestCase {
         assertArrayEquals(bytes, uncompressedOut.toByteArray());
     }
 
-    abstract Compressor compressor();
+    protected abstract Compressor compressor();
 
 }
