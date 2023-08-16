c3e79fd9cafd072e3b8520fdb6562ab35933986f
Vikas Bansal <43470111+vikasvb90@users.noreply.github.com>
2023-08-16 13:24:12 +0530

    Forked out CryptoProvider to allow registration of multiple crypto providers
    Signed-off-by: Vikas Bansal <43470111+vikasvb90@users.noreply.github.com>

--- plugins/crypto/src/main/java/org/opensearch/cryptoplugin/CryptoManagerImpl.java
+++ plugins/crypto/src/main/java/org/opensearch/cryptoplugin/CryptoManagerImpl.java
@@ -14,17 +14,13 @@ import com.amazonaws.encryptionsdk.caching.LocalCryptoMaterialsCache;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
-import org.opensearch.common.crypto.DecryptedRangedStreamProvider;
-import org.opensearch.common.io.InputStreamContainer;
+import org.opensearch.common.crypto.CryptoProvider;
 import org.opensearch.common.util.concurrent.AbstractRefCounted;
 import org.opensearch.crypto.CryptoManager;
 import org.opensearch.cryptoplugin.cache.MasterKeyCache;
 import org.opensearch.encryption.frame.FrameCryptoProvider;
-import org.opensearch.common.crypto.EncryptedHeaderContentSupplier;
 import org.opensearch.encryption.frame.core.AwsCrypto;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.security.SecureRandom;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -39,11 +35,12 @@ public class CryptoManagerImpl extends AbstractRefCounted implements CryptoManag
     private static final long cryptoCacheTTL = TimeUnit.DAYS.toMillis(5);
     private static final long cryptoCacheJitter = TimeUnit.MINUTES.toMillis(30); // - 30 minutes
 
-    private final FrameCryptoProvider frameCryptoProvider;
     private final Runnable onClose;
     private final MasterKeyCache masterKeyCache;
     private final String keyProviderName;
     private final String keyProviderType;
+
+    private final CryptoProvider cryptoProvider;
 
     public CryptoManagerImpl(
         String keyProviderName,
@@ -65,7 +62,7 @@ public class CryptoManagerImpl extends AbstractRefCounted implements CryptoManag
             .withMaxAge(masterKeyCacheTTL, TimeUnit.MILLISECONDS)
             .build();
 
-        this.frameCryptoProvider = new FrameCryptoProvider(new AwsCrypto(cachingMaterialsManager, cryptoAlgorithm), encryptionContext);
+        this.cryptoProvider = new FrameCryptoProvider(new AwsCrypto(cachingMaterialsManager, cryptoAlgorithm), encryptionContext);
         this.onClose = onClose;
         this.masterKeyCache = keyProvider;
         this.keyProviderName = keyProviderName;
@@ -88,58 +85,11 @@ public class CryptoManagerImpl extends AbstractRefCounted implements CryptoManag
         return keyProviderName;
     }
 
-    @Override
-    public Object initEncryptionMetadata() {
-        return frameCryptoProvider.initEncryptionMetadata();
-    }
-
-    @Override
-    public Object loadEncryptionMetadata(EncryptedHeaderContentSupplier encryptedHeaderContentSupplier) throws IOException {
-        return frameCryptoProvider.loadEncryptionMetadata(encryptedHeaderContentSupplier);
-    }
-
-    @Override
-    public long adjustContentSizeForPartialEncryption(Object cryptoContextObj, long contentSize) {
-        return frameCryptoProvider.adjustEncryptedStreamSize(cryptoContextObj, contentSize);
-    }
-
-    @Override
-    public long estimateEncryptedLengthOfEntireContent(Object cryptoContextObj, long contentLength) {
-        return frameCryptoProvider.estimateEncryptedLength(cryptoContextObj, contentLength);
-    }
-
-    @Override
-    public long estimateDecryptedLength(Object cryptoContextObj, long contentLength) {
-        return frameCryptoProvider.estimateDecryptedLength(cryptoContextObj, contentLength);
-    }
-
-    @Override
-    public InputStreamContainer createEncryptingStream(Object encryptionMetadata, InputStreamContainer inputStreamContainer) {
-        return frameCryptoProvider.createEncryptingStream(encryptionMetadata, inputStreamContainer);
-    }
-
-    @Override
-    public InputStreamContainer createEncryptingStreamOfPart(
-        Object cryptoContextObj,
-        InputStreamContainer streamContainer,
-        int totalStreams,
-        int streamIdx
-    ) {
-        return frameCryptoProvider.createEncryptingStreamOfPart(cryptoContextObj, streamContainer, totalStreams, streamIdx);
-    }
-
-    @Override
-    public InputStream createDecryptingStream(InputStream encryptingStream) {
-        return frameCryptoProvider.createDecryptingStream(encryptingStream);
-    }
-
-    @Override
-    public DecryptedRangedStreamProvider createDecryptingStreamOfRange(
-        Object cryptoContext,
-        long startPosOfRawContent,
-        long endPosOfRawContent
-    ) {
-        return frameCryptoProvider.createDecryptingStreamOfRange(cryptoContext, startPosOfRawContent, endPosOfRawContent);
+    /**
+     * @return Crypto provider
+     */
+    public CryptoProvider getCryptoProvider() {
+        return cryptoProvider;
     }
 
     @Override
