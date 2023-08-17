fe1cb4c50d0b212c6bf04bef6a47ad96c6e5d4b2
Daniel Widdis <widdis@gmail.com>
2023-08-16 20:59:48 -0700

    Fix flaky test by preventing duplicate keys in random mapping fields (#9184)
    * Prevent duplicate keys in random mapping fields
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Aliases were already unique. Used while loop for indices to keep size
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    Signed-off-by: Ivan Brusic <ivan.brusic@flocksafety.com>

--- test/framework/src/main/java/org/opensearch/index/RandomCreateIndexGenerator.java
+++ test/framework/src/main/java/org/opensearch/index/RandomCreateIndexGenerator.java
@@ -40,6 +40,8 @@ import org.opensearch.core.xcontent.MediaTypeRegistry;
 import org.opensearch.core.xcontent.XContentBuilder;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_NUMBER_OF_REPLICAS;
 import static org.opensearch.cluster.metadata.IndexMetadata.SETTING_NUMBER_OF_SHARDS;
@@ -112,8 +114,12 @@ public final class RandomCreateIndexGenerator {
         builder.startObject("properties");
 
         int fieldsNo = randomIntBetween(0, 5);
-        for (int i = 0; i < fieldsNo; i++) {
-            builder.startObject(randomAlphaOfLength(5));
+        Set<String> uniqueFields = new HashSet<>();
+        while (uniqueFields.size() < fieldsNo) {
+            uniqueFields.add(randomAlphaOfLength(5));
+        }
+        for (String uniqueField : uniqueFields) {
+            builder.startObject(uniqueField);
 
             if (allowObjectField && randomBoolean()) {
                 randomMappingFields(builder, false);
