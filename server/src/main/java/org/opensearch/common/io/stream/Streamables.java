003098cb8f01604a0b157dcc150667256e12e265
Nick Knize <nknize@apache.org>
2023-08-18 16:20:43 -0500

    [Remove] Joda from Serialization (#9350)
    In Legacy 7.0 joda time was removed in favor of java time to support
    nanoseconds properly. This commit removes joda from the serialization
    including ignoring the joda flag sent over the wire for DocValueFormat.
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>
    Signed-off-by: Ivan Brusic <ivan.brusic@flocksafety.com>

--- server/src/main/java/org/opensearch/common/io/stream/Streamables.java
+++ server/src/main/java/org/opensearch/common/io/stream/Streamables.java
@@ -9,16 +9,9 @@
 package org.opensearch.common.io.stream;
 
 import org.opensearch.common.geo.GeoPoint;
-import org.opensearch.common.time.DateUtils;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
 import org.opensearch.core.common.io.stream.Writeable.WriteableRegistry;
-import org.joda.time.DateTimeZone;
-import org.joda.time.ReadableInstant;
-
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 /**
  * This utility class registers generic types for streaming over the wire using
@@ -47,14 +40,6 @@ public final class Streamables {
      * Registers writers by class type
      */
     private static void registerWriters() {
-        /** {@link ReadableInstant} */
-        WriteableRegistry.registerWriter(ReadableInstant.class, (o, v) -> {
-            o.writeByte((byte) 13);
-            final ReadableInstant instant = (ReadableInstant) v;
-            o.writeString(instant.getZone().getID());
-            o.writeLong(instant.getMillis());
-        });
-        WriteableRegistry.registerClassAlias(ReadableInstant.class, ReadableInstant.class);
         /** {@link GeoPoint} */
         WriteableRegistry.registerWriter(GeoPoint.class, (o, v) -> {
             o.writeByte((byte) 22);
@@ -68,12 +53,6 @@ public final class Streamables {
      * NOTE: see {@code StreamOutput#WRITERS} for all registered ordinals
      */
     private static void registerReaders() {
-        /** {@link ZonedDateTime} */
-        WriteableRegistry.registerReader(Byte.valueOf((byte) 13), (i) -> {
-            final ZoneId zoneId = DateUtils.dateTimeZoneToZoneId(DateTimeZone.forID(i.readString()));
-            long millis = i.readLong();
-            return ZonedDateTime.ofInstant(Instant.ofEpochMilli(millis), zoneId);
-        });
         /** {@link GeoPoint} */
         WriteableRegistry.registerReader(Byte.valueOf((byte) 22), GeoPoint::new);
     }
