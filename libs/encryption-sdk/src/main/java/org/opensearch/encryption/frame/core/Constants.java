209687035ed04d129fb921c152312260ee4da806
Vikas Bansal <43470111+vikasvb90@users.noreply.github.com>
2023-08-21 08:37:34 +0530

    Moved crypto cache and crypto manager code to libs/encryption-sdk
    Signed-off-by: Vikas Bansal <43470111+vikasvb90@users.noreply.github.com>

--- libs/encryption-sdk/src/main/java/org/opensearch/encryption/frame/core/Constants.java
+++ libs/encryption-sdk/src/main/java/org/opensearch/encryption/frame/core/Constants.java
@@ -5,20 +5,25 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
+
+/*
+ * Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
+ * in compliance with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package org.opensearch.encryption.frame.core;
 
 public final class Constants {
-    /**
-     * Default length of the message identifier used to uniquely identify every ciphertext created by
-     * this library.
-     *
-     */
-    @Deprecated
-    public static final int MESSAGE_ID_LEN = 16;
 
-    private Constants() {
-        // Prevent instantiation
-    }
+    private Constants() {}
 
     /** Marker for identifying the final frame. */
     public static final int ENDFRAME_SEQUENCE_NUMBER = ~0; // is 0xFFFFFFFF
@@ -27,13 +32,13 @@ public final class Constants {
      * The identifier for non-final frames in the framing content type. This value is used as part of
      * the additional authenticated data (AAD) when encryption of content in a frame.
      */
-    public static final String FRAME_STRING_ID = "AWSKMSEncryptionClient Frame";
+    public static final String FRAME_STRING_ID = "Crypto Frame";
 
     /**
      * The identifier for the final frame in the framing content type. This value is used as part of
      * the additional authenticated data (AAD) when encryption of content in a frame.
      */
-    public static final String FINAL_FRAME_STRING_ID = "AWSKMSEncryptionClient Final Frame";
+    public static final String FINAL_FRAME_STRING_ID = "Crypto Final Frame";
 
     public static final long MAX_FRAME_NUMBER = (1L << 32) - 1;
 }
