d3ce29c4be3dfdc7a86af2d1f7b3c88907bd4159
Bukhtawar Khan <bukhtawa@amazon.com>
2023-08-24 14:42:30 +0530

    Fix up minor
    Signed-off-by: Bukhtawar Khan <bukhtawa@amazon.com>

--- server/src/main/java/org/opensearch/common/StreamLimiter.java
+++ server/src/main/java/org/opensearch/common/StreamLimiter.java
@@ -8,8 +8,6 @@
 
 package org.opensearch.common;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.lucene.store.RateLimiter;
 
 import java.io.IOException;
