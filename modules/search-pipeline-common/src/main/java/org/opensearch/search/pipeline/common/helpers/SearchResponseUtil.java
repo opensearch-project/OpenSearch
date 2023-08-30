dc22f6120dc40e166c84c5d132a7b708b366e336
Michael Froh <froh@amazon.com>
2023-08-30 18:58:39 +0000

    Add missing Javadoc and extra InternalSearchResponse case
    Signed-off-by: Michael Froh <froh@amazon.com>

--- modules/search-pipeline-common/src/main/java/org/opensearch/search/pipeline/common/helpers/SearchResponseUtil.java
+++ modules/search-pipeline-common/src/main/java/org/opensearch/search/pipeline/common/helpers/SearchResponseUtil.java
@@ -26,13 +26,15 @@ public final class SearchResponseUtil {
 
     /**
      * Construct a new {@link SearchResponse} based on an existing one, replacing just the {@link SearchHits}.
-     * @param newHits new search hits
+     * @param newHits new {@link SearchHits}
      * @param response the existing search response
-     * @return a new search response where the search hits have been replaced
+     * @return a new search response where the {@link SearchHits} has been replaced
      */
     public static SearchResponse replaceHits(SearchHits newHits, SearchResponse response) {
         SearchResponseSections searchResponseSections;
-        if (response.getAggregations() instanceof InternalAggregations) {
+        if (response.getAggregations() == null || response.getAggregations() instanceof InternalAggregations) {
+            // We either have no aggregations, or we have Writeable InternalAggregations.
+            // Either way, we can produce a Writeable InternalSearchResponse.
             searchResponseSections = new InternalSearchResponse(
                 newHits,
                 (InternalAggregations) response.getAggregations(),
@@ -43,6 +45,7 @@ public final class SearchResponseUtil {
                 response.getNumReducePhases()
             );
         } else {
+            // We have non-Writeable Aggregations, so the whole SearchResponseSections is non-Writeable.
             searchResponseSections = new SearchResponseSections(
                 newHits,
                 response.getAggregations(),
@@ -67,6 +70,12 @@ public final class SearchResponseUtil {
         );
     }
 
+    /**
+     * Convenience method when only replacing the {@link SearchHit} array within the {@link SearchHits} in a {@link SearchResponse}.
+     * @param newHits the new array of {@link SearchHit} elements.
+     * @param response the search response to update
+     * @return a {@link SearchResponse} where the underlying array of {@link SearchHit} within the {@link SearchHits} has been replaced.
+     */
     public static SearchResponse replaceHits(SearchHit[] newHits, SearchResponse response) {
         if (response.getHits() == null) {
             throw new IllegalStateException("Response must have hits");
