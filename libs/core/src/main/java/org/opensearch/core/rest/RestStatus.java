11aa0c3457d08852bd030011d9b3084061b44271
Andriy Redko <andriy.redko@aiven.io>
2023-08-30 11:56:43 -0400

    [BWC and API enforcement] Decorate the existing APIs with proper annotations (part 1) (#9520)
    * [BWC and API enforcement] Decorate the existing APIs with proper annotations (part 1)
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    * Address code review comments
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    ---------
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- libs/core/src/main/java/org/opensearch/core/rest/RestStatus.java
+++ libs/core/src/main/java/org/opensearch/core/rest/RestStatus.java
@@ -32,6 +32,7 @@
 
 package org.opensearch.core.rest;
 
+import org.opensearch.common.annotation.PublicApi;
 import org.opensearch.core.action.ShardOperationFailedException;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
@@ -47,6 +48,7 @@ import static java.util.Collections.unmodifiableMap;
  *
  * @opensearch.api
  */
+@PublicApi(since = "1.0.0")
 public enum RestStatus {
     /**
      * The client SHOULD continue with its request. This interim response is used to inform the client that the
