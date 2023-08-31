2ed633c5a83bf7758f28380ac10624dd9e991efb
Dev Agarwal <devagarwal1803@gmail.com>
2023-08-31 22:29:22 +0530

    Added sampler based on Blanket Probabilistic Sampling rate and Override for on demand (#9522)
    Signed-off-by: Dev Agarwal <devagarwal1803@gmail.com>
    Signed-off-by: Ivan Brusic <ivan.brusic@flocksafety.com>

--- plugins/telemetry-otel/src/test/java/org/opensearch/telemetry/OTelTelemetryPluginTests.java
+++ plugins/telemetry-otel/src/test/java/org/opensearch/telemetry/OTelTelemetryPluginTests.java
@@ -29,6 +29,8 @@ import static org.opensearch.telemetry.OTelTelemetrySettings.OTEL_TRACER_SPAN_EX
 import static org.opensearch.telemetry.OTelTelemetrySettings.TRACER_EXPORTER_BATCH_SIZE_SETTING;
 import static org.opensearch.telemetry.OTelTelemetrySettings.TRACER_EXPORTER_DELAY_SETTING;
 import static org.opensearch.telemetry.OTelTelemetrySettings.TRACER_EXPORTER_MAX_QUEUE_SIZE_SETTING;
+import static org.opensearch.telemetry.TelemetrySettings.TRACER_ENABLED_SETTING;
+import static org.opensearch.telemetry.TelemetrySettings.TRACER_SAMPLER_PROBABILITY;
 
 public class OTelTelemetryPluginTests extends OpenSearchTestCase {
 
@@ -42,7 +44,9 @@ public class OTelTelemetryPluginTests extends OpenSearchTestCase {
         // io.opentelemetry.sdk.OpenTelemetrySdk.close waits only for 10 seconds for shutdown to complete.
         Settings settings = Settings.builder().put(TRACER_EXPORTER_DELAY_SETTING.getKey(), "1s").build();
         oTelTracerModulePlugin = new OTelTelemetryPlugin(settings);
-        telemetry = oTelTracerModulePlugin.getTelemetry(null);
+        telemetry = oTelTracerModulePlugin.getTelemetry(
+            new TelemetrySettings(Settings.EMPTY, new ClusterSettings(settings, Set.of(TRACER_ENABLED_SETTING, TRACER_SAMPLER_PROBABILITY)))
+        );
         tracingTelemetry = telemetry.get().getTracingTelemetry();
     }
 
