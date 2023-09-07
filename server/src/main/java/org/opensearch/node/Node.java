3c5102e6f4178b21301d488c3d220235f65e2b9a
Varun Bansal <bansvaru@amazon.com>
2023-09-07 18:03:39 +0530

    [Remote Cluster State] Move remote cluster state related settings to node attrs (#9811)
    ---------
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    Signed-off-by: Ivan Brusic <ivan.brusic@flocksafety.com>

--- server/src/main/java/org/opensearch/node/Node.java
+++ server/src/main/java/org/opensearch/node/Node.java
@@ -272,6 +272,7 @@ import static org.opensearch.common.util.FeatureFlags.TELEMETRY;
 import static org.opensearch.env.NodeEnvironment.collectFileCacheDataPath;
 import static org.opensearch.index.ShardIndexingPressureSettings.SHARD_INDEXING_PRESSURE_ENABLED_ATTRIBUTE_KEY;
 import static org.opensearch.node.remotestore.RemoteStoreNodeAttribute.isRemoteStoreAttributePresent;
+import static org.opensearch.node.remotestore.RemoteStoreNodeAttribute.isRemoteStoreClusterStateEnabled;
 
 /**
  * A node represent a node within a cluster ({@code cluster.name}). The {@link #client()} can be used
@@ -681,7 +682,7 @@ public class Node implements Closeable {
                 threadPool::relativeTimeInMillis
             );
             final RemoteClusterStateService remoteClusterStateService;
-            if (RemoteClusterStateService.REMOTE_CLUSTER_STATE_ENABLED_SETTING.get(settings) == true) {
+            if (isRemoteStoreClusterStateEnabled(settings)) {
                 remoteClusterStateService = new RemoteClusterStateService(
                     nodeEnvironment.nodeId(),
                     repositoriesServiceReference::get,
