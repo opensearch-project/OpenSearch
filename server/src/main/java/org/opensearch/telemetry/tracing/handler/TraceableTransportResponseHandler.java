16cb218589624e178a0756e99aea4e5f0dc05ca3
Gagan Juneja <gagandeepjuneja@gmail.com>
2023-09-15 21:15:24 +0530

    Add tracing instrumentation in transport service (#10042)
    * Add instrumentation to transport service
    
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    
    * Add instrumentation to transport service
    
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    
    * Add javadoc
    
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    
    ---------
    
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    Co-authored-by: Gagan Juneja <gjjuneja@amazon.com>
    Signed-off-by: Ivan Brusic <ivan.brusic@flocksafety.com>

---
+++ server/src/main/java/org/opensearch/telemetry/tracing/handler/TraceableTransportResponseHandler.java
@@ -0,0 +1,107 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.telemetry.tracing.handler;
+
+import org.opensearch.common.util.FeatureFlags;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.transport.TransportResponse;
+import org.opensearch.telemetry.tracing.Span;
+import org.opensearch.telemetry.tracing.SpanScope;
+import org.opensearch.telemetry.tracing.Tracer;
+import org.opensearch.transport.TransportException;
+import org.opensearch.transport.TransportResponseHandler;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Tracer wrapped {@link TransportResponseHandler}
+ * @param <T> TransportResponse
+ */
+public class TraceableTransportResponseHandler<T extends TransportResponse> implements TransportResponseHandler<T> {
+
+    private final Span span;
+    private final TransportResponseHandler<T> delegate;
+    private final Tracer tracer;
+
+    /**
+     * Constructor.
+     *
+     * @param delegate delegate
+     * @param span span
+     * @param tracer tracer
+     */
+    private TraceableTransportResponseHandler(TransportResponseHandler<T> delegate, Span span, Tracer tracer) {
+        this.delegate = Objects.requireNonNull(delegate);
+        this.span = Objects.requireNonNull(span);
+        this.tracer = Objects.requireNonNull(tracer);
+    }
+
+    /**
+     * Factory method.
+     * @param delegate delegate
+     * @param span span
+     * @param tracer tracer
+     * @return transportResponseHandler
+     */
+    public static <S extends TransportResponse> TransportResponseHandler<S> create(
+        TransportResponseHandler<S> delegate,
+        Span span,
+        Tracer tracer
+    ) {
+        if (FeatureFlags.isEnabled(FeatureFlags.TELEMETRY) == true) {
+            return new TraceableTransportResponseHandler<S>(delegate, span, tracer);
+        } else {
+            return delegate;
+        }
+    }
+
+    @Override
+    public T read(StreamInput in) throws IOException {
+        return delegate.read(in);
+    }
+
+    @Override
+    public void handleResponse(T response) {
+        try (SpanScope scope = tracer.withSpanInScope(span)) {
+            delegate.handleResponse(response);
+        } finally {
+            span.endSpan();
+        }
+    }
+
+    @Override
+    public void handleException(TransportException exp) {
+        try (SpanScope scope = tracer.withSpanInScope(span)) {
+            delegate.handleException(exp);
+        } finally {
+            span.setError(exp);
+            span.endSpan();
+        }
+    }
+
+    @Override
+    public String executor() {
+        return delegate.executor();
+    }
+
+    @Override
+    public String toString() {
+        return delegate.toString();
+    }
+
+    @Override
+    public void handleRejection(Exception exp) {
+        try (SpanScope scope = tracer.withSpanInScope(span)) {
+            delegate.handleRejection(exp);
+        } finally {
+            span.endSpan();
+        }
+    }
+}
