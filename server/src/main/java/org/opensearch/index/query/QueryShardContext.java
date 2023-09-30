e1565821a28109c5c7f773e77764fc5104de7f88
Heemin Kim <heemin@amazon.com>
2023-09-30 09:02:49 -0700

    Pass parent filter to inner query in nested query (#10246)
    Pass parent filter to inner query so that inner query can utilize the information
    
    Signed-off-by: Heemin Kim <heemin@amazon.com>

--- server/src/main/java/org/opensearch/index/query/QueryShardContext.java
+++ server/src/main/java/org/opensearch/index/query/QueryShardContext.java
@@ -115,6 +115,7 @@ public class QueryShardContext extends QueryRewriteContext {
     private boolean mapUnmappedFieldAsString;
     private NestedScope nestedScope;
     private final ValuesSourceRegistry valuesSourceRegistry;
+    private BitSetProducer parentFilter;
 
     public QueryShardContext(
         int shardId,
@@ -621,5 +622,13 @@ public class QueryShardContext extends QueryRewriteContext {
 
     public AggregationUsageService getUsageService() {
         return valuesSourceRegistry.getUsageService();
+    }
+
+    public BitSetProducer getParentFilter() {
+        return parentFilter;
+    }
+
+    public void setParentFilter(BitSetProducer parentFilter) {
+        this.parentFilter = parentFilter;
     }
 }
