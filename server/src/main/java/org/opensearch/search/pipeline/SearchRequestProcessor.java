7f5889a3b4a625da1bab68d4b1bd998551b736e5
Michael Froh <froh@amazon.com>
2023-10-03 00:47:09 +0000

    Make search pipelines asynchronous
    If a search processor needs to make a call out to another
    service, we should not risk blocking on the transport thread. We should
    support async execution.
    
    Signed-off-by: Michael Froh <froh@amazon.com>

--- server/src/main/java/org/opensearch/search/pipeline/SearchRequestProcessor.java
+++ server/src/main/java/org/opensearch/search/pipeline/SearchRequestProcessor.java
@@ -9,10 +9,37 @@
 package org.opensearch.search.pipeline;
 
 import org.opensearch.action.search.SearchRequest;
+import org.opensearch.core.action.ActionListener;
 
 /**
  * Interface for a search pipeline processor that modifies a search request.
  */
 public interface SearchRequestProcessor extends Processor {
+
+    /**
+     * Transform a {@link SearchRequest}. Executed on the coordinator node before any {@link org.opensearch.action.search.SearchPhase}
+     * executes.
+     * <p>
+     * Implement this method if the processor makes no asynchronous calls.
+     * @param request the executed {@link SearchRequest}
+     * @return a new {@link SearchRequest} (or the input {@link SearchRequest} if no changes)
+     * @throws Exception if an error occurs during processing
+     */
     SearchRequest processRequest(SearchRequest request) throws Exception;
+
+    /**
+     * Transform a {@link SearchRequest}. Executed on the coordinator node before any {@link org.opensearch.action.search.SearchPhase}
+     * executes.
+     * <p>
+     * Expert method: Implement this if the processor needs to make asynchronous calls. Otherwise, implement processRequest.
+     * @param request the executed {@link SearchRequest}
+     * @param requestListener callback to be invoked on successful processing or on failure
+     */
+    default void asyncProcessRequest(SearchRequest request, ActionListener<SearchRequest> requestListener) {
+        try {
+            requestListener.onResponse(processRequest(request));
+        } catch (Exception e) {
+            requestListener.onFailure(e);
+        }
+    }
 }
--- server/src/main/java/org/opensearch/search/pipeline/SearchResponseProcessor.java
+++ server/src/main/java/org/opensearch/search/pipeline/SearchResponseProcessor.java
@@ -10,10 +10,37 @@ package org.opensearch.search.pipeline;
 
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
+import org.opensearch.core.action.ActionListener;
 
 /**
  * Interface for a search pipeline processor that modifies a search response.
  */
 public interface SearchResponseProcessor extends Processor {
+
+    /**
+     * Transform a {@link SearchResponse}, possibly based on the executed {@link SearchRequest}.
+     * <p>
+     * Implement this method if the processor makes no asynchronous calls.
+     * @param request the executed {@link SearchRequest}
+     * @param response the current {@link SearchResponse}, possibly modified by earlier processors
+     * @return a modified {@link SearchResponse} (or the input {@link SearchResponse} if no changes)
+     * @throws Exception if an error occurs during processing
+     */
     SearchResponse processResponse(SearchRequest request, SearchResponse response) throws Exception;
+
+    /**
+     * Transform a {@link SearchResponse}, possibly based on the executed {@link SearchRequest}.
+     * <p>
+     * Expert method: Implement this if the processor needs to make asynchronous calls. Otherwise, implement processResponse.
+     * @param request the executed {@link SearchRequest}
+     * @param response the current {@link SearchResponse}, possibly modified by earlier processors
+     * @param responseListener callback to be invoked on successful processing or on failure
+     */
+    default void asyncProcessResponse(SearchRequest request, SearchResponse response, ActionListener<SearchResponse> responseListener) {
+        try {
+            responseListener.onResponse(processResponse(request, response));
+        } catch (Exception e) {
+            responseListener.onFailure(e);
+        }
+    }
 }
