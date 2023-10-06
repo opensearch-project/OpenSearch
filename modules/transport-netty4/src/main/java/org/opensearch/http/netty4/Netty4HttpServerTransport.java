f1a6d233ea3a5d62731da33aed32e58f6cf828f3
Peter Nied <petern@amazon.com>
2023-10-06 02:30:42 +0000

    Fix missing parens
    Signed-off-by: Peter Nied <petern@amazon.com>

--- modules/transport-netty4/src/main/java/org/opensearch/http/netty4/Netty4HttpServerTransport.java
+++ modules/transport-netty4/src/main/java/org/opensearch/http/netty4/Netty4HttpServerTransport.java
@@ -353,7 +353,7 @@ public class Netty4HttpServerTransport extends AbstractHttpServerTransport {
             );
             decoder.setCumulator(ByteToMessageDecoder.COMPOSITE_CUMULATOR);
             ch.pipeline().addLast("decoder", decoder);
-            ch.pipeline().addLast("header_verifier", transport.createHeaderVerifier);
+            ch.pipeline().addLast("header_verifier", transport.createHeaderVerifier());
             ch.pipeline().addLast("decoder_compress", transport.createDecompressor());
             ch.pipeline().addLast("encoder", new HttpResponseEncoder());
             final HttpObjectAggregator aggregator = new HttpObjectAggregator(handlingSettings.getMaxContentLength());
