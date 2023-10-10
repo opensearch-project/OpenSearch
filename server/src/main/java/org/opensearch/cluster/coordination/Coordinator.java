3dc6ae3b872a56e87637c288ef3c5d28160a5ead
Niyati Aggarwal <niyatiagg4641@gmail.com>
2023-10-09 19:47:21 -0700

    making follower check timeout dynamic
    Signed-off-by: Niyati Aggarwal <niyatiagg4641@gmail.com>

--- server/src/main/java/org/opensearch/cluster/coordination/Coordinator.java
+++ server/src/main/java/org/opensearch/cluster/coordination/Coordinator.java
@@ -263,6 +263,7 @@ public class Coordinator extends AbstractLifecycleComponent implements Discovery
         this.leaderChecker = new LeaderChecker(settings, clusterSettings, transportService, this::onLeaderFailure, nodeHealthService);
         this.followersChecker = new FollowersChecker(
             settings,
+            clusterSettings,
             transportService,
             this::onFollowerCheckRequest,
             this::removeNode,
