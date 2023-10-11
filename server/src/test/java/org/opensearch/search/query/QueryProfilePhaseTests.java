9e919a99fe55ed47f5d9e3ffaca1ae5588b4917b
Ticheng Lin <51488860+ticheng-aws@users.noreply.github.com>
2023-10-11 07:31:18 -0700

    Fix flaky query profile phase tests with concurrent search enabled (#10547) (#10547)
    Signed-off-by: Ticheng Lin <ticheng@amazon.com>

--- server/src/test/java/org/opensearch/search/query/QueryProfilePhaseTests.java
+++ server/src/test/java/org/opensearch/search/query/QueryProfilePhaseTests.java
@@ -1102,10 +1102,6 @@ public class QueryProfilePhaseTests extends IndexShardTestCase {
     }
 
     public void testDisableTopScoreCollection() throws Exception {
-        assumeFalse(
-            "Concurrent search case muted pending fix: https://github.com/opensearch-project/OpenSearch/issues/10469",
-            executor != null
-        );
         Directory dir = newDirectory();
         IndexWriterConfig iwc = newIndexWriterConfig(new StandardAnalyzer());
         RandomIndexWriter w = new RandomIndexWriter(random(), dir, iwc);
@@ -1277,10 +1273,6 @@ public class QueryProfilePhaseTests extends IndexShardTestCase {
     }
 
     public void testMaxScore() throws Exception {
-        assumeFalse(
-            "Concurrent search case muted pending fix: https://github.com/opensearch-project/OpenSearch/issues/9932",
-            executor != null
-        );
         Directory dir = newDirectory();
         final Sort sort = new Sort(new SortField("filter", SortField.Type.STRING));
         IndexWriterConfig iwc = newIndexWriterConfig().setIndexSort(sort);
@@ -1400,10 +1392,6 @@ public class QueryProfilePhaseTests extends IndexShardTestCase {
     }
 
     public void testCollapseQuerySearchResults() throws Exception {
-        assumeFalse(
-            "Concurrent search case muted pending fix: https://github.com/opensearch-project/OpenSearch/issues/10139",
-            executor != null
-        );
         Directory dir = newDirectory();
         final Sort sort = new Sort(new SortField("user", SortField.Type.INT));
         IndexWriterConfig iwc = newIndexWriterConfig().setIndexSort(sort);
