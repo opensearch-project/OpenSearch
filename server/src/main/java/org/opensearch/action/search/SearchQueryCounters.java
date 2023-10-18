280beaecb94d794b3f6c57c66821a8e41f79e3f8
Siddhant Deshmukh <deshsid@amazon.com>
2023-10-17 22:34:55 -0700

    Address review comments and add complex query unit test
    Signed-off-by: Siddhant Deshmukh <deshsid@amazon.com>

--- server/src/main/java/org/opensearch/action/search/SearchQueryCounters.java
+++ server/src/main/java/org/opensearch/action/search/SearchQueryCounters.java
@@ -40,77 +40,77 @@ public class SearchQueryCounters {
         this.aggCounter = metricsRegistry.createCounter(
             "search.query.type.agg.count",
             "Counter for the number of top level agg search queries",
-            "0"
+            "1"
         );
         this.boolCounter = metricsRegistry.createCounter(
             "search.query.type.bool.count",
             "Counter for the number of top level and nested bool search queries",
-            "0"
+            "1"
         );
         this.functionScoreCounter = metricsRegistry.createCounter(
             "search.query.type.functionscore.count",
             "Counter for the number of top level and nested function score search queries",
-            "0"
+            "1"
         );
         this.matchCounter = metricsRegistry.createCounter(
             "search.query.type.match.count",
             "Counter for the number of top level and nested match search queries",
-            "0"
+            "1"
         );
         this.matchPhrasePrefixCounter = metricsRegistry.createCounter(
             "search.query.type.matchphrase.count",
             "Counter for the number of top level and nested match phrase prefix search queries",
-            "0"
+            "1"
         );
         this.multiMatchCounter = metricsRegistry.createCounter(
             "search.query.type.multimatch.count",
             "Counter for the number of top level and nested multi match search queries",
-            "0"
+            "1"
         );
         this.otherQueryCounter = metricsRegistry.createCounter(
             "search.query.type.other.count",
             "Counter for the number of top level and nested search queries that do not match any other categories",
-            "0"
+            "1"
         );
         this.queryStringQueryCounter = metricsRegistry.createCounter(
             "search.query.type.querystringquery.count",
             "Counter for the number of top level and nested queryStringQuery search queries",
-            "0"
+            "1"
         );
         this.rangeCounter = metricsRegistry.createCounter(
             "search.query.type.range.count",
             "Counter for the number of top level and nested range search queries",
-            "0"
+            "1"
         );
         this.regexCounter = metricsRegistry.createCounter(
             "search.query.type.regex.count",
             "Counter for the number of top level and nested regex search queries",
-            "0"
+            "1"
         );
         this.skippedCounter = metricsRegistry.createCounter(
             "search.query.type.skipped.count",
             "Counter for the number queries skipped due to error",
-            "0"
+            "1"
         );
         this.sortCounter = metricsRegistry.createCounter(
             "search.query.type.sort.count",
             "Counter for the number of top level sort search queries",
-            "0"
+            "1"
         );
         this.termCounter = metricsRegistry.createCounter(
             "search.query.type.term.count",
             "Counter for the number of top level and nested term search queries",
-            "0"
+            "1"
         );
         this.totalCounter = metricsRegistry.createCounter(
             "search.query.type.total.count",
             "Counter for the number of top level and nested search queries",
-            "0"
+            "1"
         );
         this.wildcardCounter = metricsRegistry.createCounter(
             "search.query.type.wildcard.count",
             "Counter for the number of top level and nested wildcard search queries",
-            "0"
+            "1"
         );
     }
 }
