5093cc71476f5796f3b91fb18ff867cfe37588c0
Dhwanil Patel <dhwanip@amazon.com>
2023-10-20 19:18:25 +0530

    Fix spotless failure (#10782)
    Signed-off-by: Dhwanil Patel <dhwanip@amazon.com>

--- server/src/test/java/org/opensearch/gateway/remote/RemoteClusterStateServiceTests.java
+++ server/src/test/java/org/opensearch/gateway/remote/RemoteClusterStateServiceTests.java
@@ -1282,7 +1282,12 @@ public class RemoteClusterStateServiceTests extends OpenSearchTestCase {
             .version(1L)
             .stateUUID("state-uuid")
             .metadata(
-                Metadata.builder().version(randomNonNegativeLong()).put(indexMetadata, true).clusterUUID("cluster-uuid").coordinationMetadata(coordinationMetadata).build()
+                Metadata.builder()
+                    .version(randomNonNegativeLong())
+                    .put(indexMetadata, true)
+                    .clusterUUID("cluster-uuid")
+                    .coordinationMetadata(coordinationMetadata)
+                    .build()
             );
     }
 
