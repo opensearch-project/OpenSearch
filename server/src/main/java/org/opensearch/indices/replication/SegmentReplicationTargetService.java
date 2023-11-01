349a5fe98cd78580eafaf0168133847f6ca091cd
Marc Handalian <handalm@amazon.com>
2023-11-01 04:02:10 +0000

    Refactor SegmentReplicationTargetService to only hold completed target state instead of the entire target.
    Signed-off-by: Marc Handalian <handalm@amazon.com>

--- server/src/main/java/org/opensearch/indices/replication/SegmentReplicationTargetService.java
+++ server/src/main/java/org/opensearch/indices/replication/SegmentReplicationTargetService.java
@@ -69,7 +69,7 @@ public class SegmentReplicationTargetService implements IndexEventListener {
 
     private final ReplicationCollection<SegmentReplicationTarget> onGoingReplications;
 
-    private final Map<ShardId, SegmentReplicationTarget> completedReplications = ConcurrentCollections.newConcurrentMap();
+    private final Map<ShardId, SegmentReplicationState> completedReplications = ConcurrentCollections.newConcurrentMap();
 
     private final SegmentReplicationSourceFactory sourceFactory;
 
@@ -191,7 +191,7 @@ public class SegmentReplicationTargetService implements IndexEventListener {
      */
     @Nullable
     public SegmentReplicationState getlatestCompletedEventSegmentReplicationState(ShardId shardId) {
-        return Optional.ofNullable(completedReplications.get(shardId)).map(SegmentReplicationTarget::state).orElse(null);
+        return completedReplications.get(shardId);
     }
 
     /**
@@ -518,7 +518,7 @@ public class SegmentReplicationTargetService implements IndexEventListener {
                 logger.debug(() -> new ParameterizedMessage("Finished replicating {} marking as done.", target.description()));
                 onGoingReplications.markAsDone(replicationId);
                 if (target.state().getIndex().recoveredFileCount() != 0 && target.state().getIndex().recoveredBytes() != 0) {
-                    completedReplications.put(target.shardId(), target);
+                    completedReplications.put(target.shardId(), target.state());
                 }
             }
 
