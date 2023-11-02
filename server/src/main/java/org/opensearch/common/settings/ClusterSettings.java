ae580960625d2d71ae3056909dbfb77af0fa136b
Poojita Raj <poojiraj@amazon.com>
2023-11-02 13:57:48 -0700

    Revert "Add cluster setting cluster.restrict.index.replication_type t… (#10866)"
    This reverts commit 8b2173910f754a48773b3283e1a511cbc1a9db78.
    
    Signed-off-by: Poojita Raj <poojiraj@amazon.com>

--- server/src/main/java/org/opensearch/common/settings/ClusterSettings.java
+++ server/src/main/java/org/opensearch/common/settings/ClusterSettings.java
@@ -691,8 +691,7 @@ public final class ClusterSettings extends AbstractScopedSettings {
                 AdmissionControlSettings.ADMISSION_CONTROL_TRANSPORT_LAYER_MODE,
                 CPUBasedAdmissionControllerSettings.CPU_BASED_ADMISSION_CONTROLLER_TRANSPORT_LAYER_MODE,
                 CPUBasedAdmissionControllerSettings.INDEXING_CPU_USAGE_LIMIT,
-                CPUBasedAdmissionControllerSettings.SEARCH_CPU_USAGE_LIMIT,
-                IndicesService.CLUSTER_RESTRICT_INDEX_REPLICATION_TYPE_SETTING
+                CPUBasedAdmissionControllerSettings.SEARCH_CPU_USAGE_LIMIT
             )
         )
     );
