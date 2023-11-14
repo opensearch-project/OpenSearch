9b0af163dee6c082b743070c8425087c13d5c8a9
Chaitanya Gohel <gashutos@amazon.com>
2023-11-14 20:52:43 +0530

    Removing missing value outdated comments
    Signed-off-by: Chaitanya Gohel <gashutos@amazon.com>

--- server/src/main/java/org/opensearch/index/fielddata/fieldcomparator/FloatValuesComparatorSource.java
+++ server/src/main/java/org/opensearch/index/fielddata/fieldcomparator/FloatValuesComparatorSource.java
@@ -95,8 +95,6 @@ public class FloatValuesComparatorSource extends IndexFieldData.XFieldComparator
         assert indexFieldData == null || fieldname.equals(indexFieldData.getFieldName());
 
         final float fMissingValue = (Float) missingObject(missingValue, reversed);
-        // NOTE: it's important to pass null as a missing value in the constructor so that
-        // the comparator doesn't check docsWithField since we replace missing values in select()
         return new FloatComparator(numHits, fieldname, fMissingValue, reversed, enableSkipping && this.enableSkipping) {
             @Override
             public LeafFieldComparator getLeafComparator(LeafReaderContext context) throws IOException {
