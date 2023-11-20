a62e3f3185bd4982509d89cd9a755edc9aad9b28
Siddhant Deshmukh <deshsid@amazon.com>
2023-11-20 14:54:43 -0800

    Capture query level and type for additional query types
    Signed-off-by: Siddhant Deshmukh <deshsid@amazon.com>

--- server/src/main/java/org/opensearch/action/search/SearchQueryCategorizingVisitor.java
+++ server/src/main/java/org/opensearch/action/search/SearchQueryCategorizingVisitor.java
@@ -10,14 +10,30 @@ package org.opensearch.action.search;
 
 import org.apache.lucene.search.BooleanClause;
 import org.opensearch.index.query.BoolQueryBuilder;
+import org.opensearch.index.query.BoostingQueryBuilder;
+import org.opensearch.index.query.ConstantScoreQueryBuilder;
+import org.opensearch.index.query.DisMaxQueryBuilder;
+import org.opensearch.index.query.DistanceFeatureQueryBuilder;
+import org.opensearch.index.query.ExistsQueryBuilder;
+import org.opensearch.index.query.FieldMaskingSpanQueryBuilder;
+import org.opensearch.index.query.FuzzyQueryBuilder;
+import org.opensearch.index.query.GeoBoundingBoxQueryBuilder;
+import org.opensearch.index.query.GeoDistanceQueryBuilder;
+import org.opensearch.index.query.GeoPolygonQueryBuilder;
+import org.opensearch.index.query.GeoShapeQueryBuilder;
+import org.opensearch.index.query.IntervalQueryBuilder;
+import org.opensearch.index.query.MatchAllQueryBuilder;
 import org.opensearch.index.query.MatchPhraseQueryBuilder;
 import org.opensearch.index.query.MatchQueryBuilder;
 import org.opensearch.index.query.MultiMatchQueryBuilder;
+import org.opensearch.index.query.PrefixQueryBuilder;
 import org.opensearch.index.query.QueryBuilder;
 import org.opensearch.index.query.QueryBuilderVisitor;
 import org.opensearch.index.query.QueryStringQueryBuilder;
 import org.opensearch.index.query.RangeQueryBuilder;
 import org.opensearch.index.query.RegexpQueryBuilder;
+import org.opensearch.index.query.ScriptQueryBuilder;
+import org.opensearch.index.query.SimpleQueryStringBuilder;
 import org.opensearch.index.query.TermQueryBuilder;
 import org.opensearch.index.query.WildcardQueryBuilder;
 import org.opensearch.index.query.functionscore.FunctionScoreQueryBuilder;
@@ -62,6 +78,38 @@ final class SearchQueryCategorizingVisitor implements QueryBuilderVisitor {
             searchQueryCounters.termCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
         } else if (qb instanceof WildcardQueryBuilder) {
             searchQueryCounters.wildcardCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } if (qb instanceof BoostingQueryBuilder) {
+            searchQueryCounters.boostCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof ConstantScoreQueryBuilder) {
+            searchQueryCounters.constantScoreCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof DisMaxQueryBuilder) {
+            searchQueryCounters.disMaxCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof DistanceFeatureQueryBuilder) {
+            searchQueryCounters.distanceFeatureCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof ExistsQueryBuilder) {
+            searchQueryCounters.existsCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof FieldMaskingSpanQueryBuilder) {
+            searchQueryCounters.fieldMaskingSpanCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof FuzzyQueryBuilder) {
+            searchQueryCounters.fuzzyCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof GeoBoundingBoxQueryBuilder) {
+            searchQueryCounters.geoBoundingBoxCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof GeoDistanceQueryBuilder) {
+            searchQueryCounters.geoDistanceCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof GeoPolygonQueryBuilder) {
+            searchQueryCounters.geoPolygonCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof GeoShapeQueryBuilder) {
+            searchQueryCounters.geoShapeCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof IntervalQueryBuilder) {
+            searchQueryCounters.intervalCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof MatchAllQueryBuilder) {
+            searchQueryCounters.matchallCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof PrefixQueryBuilder) {
+            searchQueryCounters.prefixCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof ScriptQueryBuilder) {
+            searchQueryCounters.scriptCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
+        } else if (qb instanceof SimpleQueryStringBuilder) {
+            searchQueryCounters.simpleQueryStringCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
         } else {
             searchQueryCounters.otherQueryCounter.add(1, Tags.create().addTag(LEVEL_TAG, level));
         }
--- server/src/main/java/org/opensearch/action/search/SearchQueryCounters.java
+++ server/src/main/java/org/opensearch/action/search/SearchQueryCounters.java
@@ -21,15 +21,30 @@ final class SearchQueryCounters {
     // Counters related to Query types
     public final Counter aggCounter;
     public final Counter boolCounter;
+    public final Counter boostCounter;
+    public final Counter constantScoreCounter;
+    public final Counter disMaxCounter;
+    public final Counter distanceFeatureCounter;
+    public final Counter existsCounter;
+    public final Counter fieldMaskingSpanCounter;
     public final Counter functionScoreCounter;
+    public final Counter fuzzyCounter;
+    public final Counter geoBoundingBoxCounter;
+    public final Counter geoDistanceCounter;
+    public final Counter geoPolygonCounter;
+    public final Counter geoShapeCounter;
+    public final Counter intervalCounter;
     public final Counter matchCounter;
+    public final Counter matchallCounter;
     public final Counter matchPhrasePrefixCounter;
     public final Counter multiMatchCounter;
     public final Counter otherQueryCounter;
+    public final Counter prefixCounter;
     public final Counter queryStringQueryCounter;
     public final Counter rangeCounter;
     public final Counter regexCounter;
-
+    public final Counter scriptCounter;
+    public final Counter simpleQueryStringCounter;
     public final Counter sortCounter;
     public final Counter skippedCounter;
     public final Counter termCounter;
@@ -48,14 +63,79 @@ final class SearchQueryCounters {
             "Counter for the number of top level and nested bool search queries",
             UNIT
         );
+        this.boostCounter = metricsRegistry.createCounter(
+            "search.query.type.boast.count",
+            "Counter for the number of top level and nested boast search queries",
+            UNIT
+        );
+        this.constantScoreCounter = metricsRegistry.createCounter(
+            "search.query.type.counstantscore.count",
+            "Counter for the number of top level and nested constant score search queries",
+            UNIT
+        );
+        this.disMaxCounter = metricsRegistry.createCounter(
+            "search.query.type.disMax.count",
+            "Counter for the number of top level and nested disjuntion max search queries",
+            UNIT
+        );
+        this.distanceFeatureCounter = metricsRegistry.createCounter(
+            "search.query.type.distanceFeature.count",
+            "Counter for the number of top level and nested distance feature search queries",
+            UNIT
+        );
+        this.existsCounter = metricsRegistry.createCounter(
+            "search.query.type.exists.count",
+            "Counter for the number of top level and nested exists search queries",
+            UNIT
+        );
+        this.fieldMaskingSpanCounter = metricsRegistry.createCounter(
+            "search.query.type.fieldmaskingspan.count",
+            "Counter for the number of top level and nested field masking span search queries",
+            UNIT
+        );
         this.functionScoreCounter = metricsRegistry.createCounter(
             "search.query.type.functionscore.count",
             "Counter for the number of top level and nested function score search queries",
             UNIT
         );
+        this.fuzzyCounter = metricsRegistry.createCounter(
+            "search.query.type.fuzzy.count",
+            "Counter for the number of top level and nested fuzzy search queries",
+            UNIT
+        );
+        this.geoBoundingBoxCounter = metricsRegistry.createCounter(
+            "search.query.type.geoboundingbox.count",
+            "Counter for the number of top level and nested geo bounding box queries",
+            UNIT
+        );
+        this.geoDistanceCounter = metricsRegistry.createCounter(
+            "search.query.type.geodistance.count",
+            "Counter for the number of top level and nested geo distance queries",
+            UNIT
+        );
+        this.geoPolygonCounter = metricsRegistry.createCounter(
+            "search.query.type.geopolygon.count",
+            "Counter for the number of top level and nested geo polygon queries",
+            UNIT
+        );
+        this.geoShapeCounter = metricsRegistry.createCounter(
+            "search.query.type.geoshape.count",
+            "Counter for the number of top level and nested geo shape queries",
+            UNIT
+        );
+        this.intervalCounter = metricsRegistry.createCounter(
+            "search.query.type.interval.count",
+            "Counter for the number of top level and nested interval queries",
+            UNIT
+        );
         this.matchCounter = metricsRegistry.createCounter(
             "search.query.type.match.count",
             "Counter for the number of top level and nested match search queries",
+            UNIT
+        );
+        this.matchallCounter = metricsRegistry.createCounter(
+            "search.query.type.matchall.count",
+            "Counter for the number of top level and nested match all search queries",
             UNIT
         );
         this.matchPhrasePrefixCounter = metricsRegistry.createCounter(
@@ -73,6 +153,11 @@ final class SearchQueryCounters {
             "Counter for the number of top level and nested search queries that do not match any other categories",
             UNIT
         );
+        this.prefixCounter = metricsRegistry.createCounter(
+            "search.query.type.prefix.count",
+            "Counter for the number of top level and nested search queries that match prefix queries",
+            UNIT
+        );
         this.queryStringQueryCounter = metricsRegistry.createCounter(
             "search.query.type.querystringquery.count",
             "Counter for the number of top level and nested queryStringQuery search queries",
@@ -86,6 +171,16 @@ final class SearchQueryCounters {
         this.regexCounter = metricsRegistry.createCounter(
             "search.query.type.regex.count",
             "Counter for the number of top level and nested regex search queries",
+            UNIT
+        );
+        this.scriptCounter = metricsRegistry.createCounter(
+            "search.query.type.script.count",
+            "Counter for the number of top level and nested script search queries",
+            UNIT
+        );
+        this.simpleQueryStringCounter = metricsRegistry.createCounter(
+            "search.query.type.simplequerystring.count",
+            "Counter for the number of top level and nested script simple query string search queries",
             UNIT
         );
         this.skippedCounter = metricsRegistry.createCounter(
