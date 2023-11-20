6f98e0a74e5e992a126f424c4b5f42c68ad943b0
Rishabh Maurya <rishabhmaurya05@gmail.com>
2023-11-20 15:54:20 -0800

    remove unused import
    Signed-off-by: Rishabh Maurya <rishabhmaurya05@gmail.com>

--- server/src/test/java/org/opensearch/index/query/SourceFieldMatchQueryTests.java
+++ server/src/test/java/org/opensearch/index/query/SourceFieldMatchQueryTests.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class SourceFieldMatchQueryTests extends MapperServiceTestCase {
