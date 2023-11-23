7e8139566fcc15a4510d263e03aa5d3d715b4681
Gao Binlong <gbinlong@amazon.com>
2023-11-23 10:39:16 +0800

    Format some code
    Signed-off-by: Gao Binlong <gbinlong@amazon.com>

--- modules/ingest-common/src/main/java/org/opensearch/ingest/common/RemoveProcessor.java
+++ modules/ingest-common/src/main/java/org/opensearch/ingest/common/RemoveProcessor.java
@@ -47,8 +47,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.opensearch.ingest.ConfigurationUtils.newConfigurationException;
