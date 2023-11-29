50e3666414b7f73c40f41960177e8098071550b9
Andriy Redko <andriy.redko@aiven.io>
2023-11-29 16:27:16 -0500

    Revert Rounding API visibility changes (#11392)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- server/src/main/java/org/opensearch/common/Rounding.java
+++ server/src/main/java/org/opensearch/common/Rounding.java
@@ -54,6 +54,7 @@ import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.IsoFields;
@@ -64,6 +65,7 @@ import java.time.zone.ZoneRules;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.OptionalLong;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -98,7 +100,7 @@ public abstract class Rounding implements Writeable {
             }
 
             @Override
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return extraLocalOffsetLookup;
             }
         },
@@ -109,7 +111,7 @@ public abstract class Rounding implements Writeable {
                 return DateUtils.roundYear(utcMillis);
             }
 
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return extraLocalOffsetLookup;
             }
         },
@@ -120,7 +122,7 @@ public abstract class Rounding implements Writeable {
                 return DateUtils.roundQuarterOfYear(utcMillis);
             }
 
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return extraLocalOffsetLookup;
             }
         },
@@ -131,7 +133,7 @@ public abstract class Rounding implements Writeable {
                 return DateUtils.roundMonthOfYear(utcMillis);
             }
 
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return extraLocalOffsetLookup;
             }
         },
@@ -140,7 +142,7 @@ public abstract class Rounding implements Writeable {
                 return DateUtils.roundFloor(utcMillis, this.ratio);
             }
 
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return ratio;
             }
         },
@@ -149,7 +151,7 @@ public abstract class Rounding implements Writeable {
                 return DateUtils.roundFloor(utcMillis, ratio);
             }
 
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return ratio;
             }
         },
@@ -164,7 +166,7 @@ public abstract class Rounding implements Writeable {
                 return DateUtils.roundFloor(utcMillis, ratio);
             }
 
-            public long extraLocalOffsetLookup() {
+            long extraLocalOffsetLookup() {
                 return ratio;
             }
         },
@@ -216,7 +218,7 @@ public abstract class Rounding implements Writeable {
          * look up so that we can see transitions that we might have rounded
          * down beyond.
          */
-        public abstract long extraLocalOffsetLookup();
+        abstract long extraLocalOffsetLookup();
 
         public byte getId() {
             return id;
@@ -487,7 +489,7 @@ public abstract class Rounding implements Writeable {
      *
      * @opensearch.internal
      */
-    public static class TimeUnitRounding extends Rounding {
+    static class TimeUnitRounding extends Rounding {
         static final byte ID = 1;
 
         private final DateTimeUnit unit;
@@ -513,14 +515,6 @@ public abstract class Rounding implements Writeable {
         @Override
         public byte id() {
             return ID;
-        }
-
-        public DateTimeUnit getUnit() {
-            return this.unit;
-        }
-
-        public ZoneId getTimeZone() {
-            return this.timeZone;
         }
 
         private LocalDateTime truncateLocalDateTime(LocalDateTime localDateTime) {
@@ -953,7 +947,7 @@ public abstract class Rounding implements Writeable {
      *
      * @opensearch.internal
      */
-    public static class TimeIntervalRounding extends Rounding {
+    static class TimeIntervalRounding extends Rounding {
         static final byte ID = 2;
 
         private final long interval;
@@ -978,14 +972,6 @@ public abstract class Rounding implements Writeable {
         @Override
         public byte id() {
             return ID;
-        }
-
-        public long getInterval() {
-            return this.interval;
-        }
-
-        public ZoneId getTimeZone() {
-            return this.timeZone;
         }
 
         @Override
@@ -1383,5 +1369,41 @@ public abstract class Rounding implements Writeable {
             default:
                 throw new OpenSearchException("unknown rounding id [" + id + "]");
         }
+    }
+
+    /**
+     * Extracts the interval value from the {@link Rounding} instance
+     * @param rounding {@link Rounding} instance
+     * @return the interval value from the {@link Rounding} instance or {@code OptionalLong.empty()}
+     * if the interval is not available
+     */
+    public static OptionalLong getInterval(Rounding rounding) {
+        long interval = 0;
+
+        if (rounding instanceof TimeUnitRounding) {
+            interval = (((TimeUnitRounding) rounding).unit).extraLocalOffsetLookup();
+            if (!isUTCTimeZone(((TimeUnitRounding) rounding).timeZone)) {
+                // Fast filter aggregation cannot be used if it needs time zone rounding
+                return OptionalLong.empty();
+            }
+        } else if (rounding instanceof TimeIntervalRounding) {
+            interval = ((TimeIntervalRounding) rounding).interval;
+            if (!isUTCTimeZone(((TimeIntervalRounding) rounding).timeZone)) {
+                // Fast filter aggregation cannot be used if it needs time zone rounding
+                return OptionalLong.empty();
+            }
+        } else {
+            return OptionalLong.empty();
+        }
+
+        return OptionalLong.of(interval);
+    }
+
+    /**
+     * Helper function for checking if the time zone requested for date histogram
+     * aggregation is utc or not
+     */
+    private static boolean isUTCTimeZone(final ZoneId zoneId) {
+        return "Z".equals(zoneId.getDisplayName(TextStyle.FULL, Locale.ENGLISH));
     }
 }
