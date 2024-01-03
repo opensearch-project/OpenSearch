5976dc74c8be4478e0a7900cd9a74cec6b6974eb
Marc Handalian <marc.handalian@gmail.com>
2024-01-02 17:46:13 -0800

    Set BestBucketsDeferringCollector as finished by default when there are no leaves
    With Lucene 9.9.0+ IndexSearcher#search is not invoked if there are no leaves, skipping directly to invoking reduce on the collectorManager.
    This collector would then throw in prepareSelectedBuckets thinking it has not yet finished collection.
    
    Signed-off-by: Marc Handalian <marc.handalian@gmail.com>

--- server/src/main/java/org/opensearch/search/aggregations/bucket/BestBucketsDeferringCollector.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/BestBucketsDeferringCollector.java
@@ -90,7 +90,7 @@ public class BestBucketsDeferringCollector extends DeferringBucketCollector {
     protected PackedLongValues.Builder docDeltasBuilder;
     protected PackedLongValues.Builder bucketsBuilder;
     protected long maxBucket = -1;
-    protected boolean finished = false;
+    protected boolean finished;
     protected LongHash selectedBuckets;
 
     /**
@@ -101,6 +101,7 @@ public class BestBucketsDeferringCollector extends DeferringBucketCollector {
     public BestBucketsDeferringCollector(SearchContext context, boolean isGlobal) {
         this.searchContext = context;
         this.isGlobal = isGlobal;
+        this.finished = context.searcher().getLeafContexts().isEmpty();
     }
 
     @Override
