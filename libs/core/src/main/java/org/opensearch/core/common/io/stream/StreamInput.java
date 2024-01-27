9938f111ce98d14eb6955fde06f703fa5847b100
Dharin Shah <8616130+Dharin-shah@users.noreply.github.com>
2024-01-27 23:14:36 +0100

    remove unused method
    Signed-off-by: Dharin Shah <8616130+Dharin-shah@users.noreply.github.com>

--- libs/core/src/main/java/org/opensearch/core/common/io/stream/StreamInput.java
+++ libs/core/src/main/java/org/opensearch/core/common/io/stream/StreamInput.java
@@ -633,48 +633,6 @@ public abstract class StreamInput extends InputStream {
     }
 
     /**
-     * Reads a {@link Map} from a data source using provided key and value readers and a map constructor.
-     *
-     * This method is a flexible utility for reading a map from a data source, such as a file or network stream.
-     * It uses the provided `keyReader` and `valueReader` to read each key-value pair. The size of the map is
-     * determined first by reading the array size. A map constructor is also provided to create a map of the
-     * appropriate size, allowing for optimized performance based on the expected number of entries.
-     *
-     * If the read size is zero, an empty map is returned. Otherwise, the method iterates over the size,
-     * reading keys and values and adding them to the map.
-     *
-     * Usage example:
-     * <pre><code>
-     * Map&lt;Integer, String&gt; map = readMap(StreamInput::readInteger, StreamInput::readString, HashMap::new);
-     * </code></pre>
-     *
-     * Note: If the map is empty, an immutable empty map is returned. Otherwise, a mutable map is created.
-     *
-     * @param keyReader The {@link Writeable.Reader} used to read the keys of the map.
-     * @param valueReader The {@link Writeable.Reader} used to read the values of the map.
-     * @param constructor A function that takes an integer (the initial size) and returns a new map.
-     *                    This allows for the creation of a map with an initial capacity matching the
-     *                    expected size, optimizing performance.
-     * @return A {@link Map} containing the keys and values read from the data source. This map is either
-     *         empty (and immutable) or mutable and filled with the read data.
-     * @throws IOException If an I/O error occurs during reading from the data source.
-     */
-    private <K, V> Map<K, V> readMap(Writeable.Reader<K> keyReader, Writeable.Reader<V> valueReader, IntFunction<Map<K, V>> constructor)
-        throws IOException {
-        int size = readArraySize();
-        if (size == 0) {
-            return Collections.emptyMap();
-        }
-        Map<K, V> map = constructor.apply(size);
-        for (int i = 0; i < size; i++) {
-            K key = keyReader.read(this);
-            V value = valueReader.read(this);
-            map.put(key, value);
-        }
-        return map;
-    }
-
-    /**
      * If the returned map contains any entries it will be mutable. If it is empty it might be immutable.
      */
     public <K, V> Map<K, V> readMap(Writeable.Reader<K> keyReader, Writeable.Reader<V> valueReader) throws IOException {
