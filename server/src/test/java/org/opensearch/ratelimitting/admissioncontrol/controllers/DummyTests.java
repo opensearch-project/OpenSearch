4d8ec8eafa9ac0f82769bc3add8caf60b01b4317
Ajay Kumar Movva <movvaam@amazon.com>
2024-02-01 12:30:08 +0530

    Added Dummy Commit
    Signed-off-by: Ajay Kumar Movva <movvaam@amazon.com>

---
+++ server/src/test/java/org/opensearch/ratelimitting/admissioncontrol/controllers/DummyTests.java
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.ratelimitting.admissioncontrol.controllers;
+
+import org.opensearch.test.OpenSearchTestCase;
+
+public class DummyTests extends OpenSearchTestCase {
+    public void testDummyCase() {
+        assert true;
+    }
+}
