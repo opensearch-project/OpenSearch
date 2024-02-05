e8a5a1ccbb57157cccb1f9b563c783279603b604
Mohit Godwani <81609427+mgodwan@users.noreply.github.com>
2024-02-05 23:27:53 +0530

    Enable Fuzzy codec for doc id fields using a bloom filter (#11027)
    * Enable Fuzzy codec for doc id fields using a bloom filter
    
    Signed-off-by: mgodwan <mgodwan@amazon.com>

--- server/src/main/java/org/opensearch/common/settings/FeatureFlagSettings.java
+++ server/src/main/java/org/opensearch/common/settings/FeatureFlagSettings.java
@@ -34,6 +34,7 @@ public class FeatureFlagSettings extends AbstractScopedSettings {
         FeatureFlags.IDENTITY_SETTING,
         FeatureFlags.TELEMETRY_SETTING,
         FeatureFlags.DATETIME_FORMATTER_CACHING_SETTING,
-        FeatureFlags.WRITEABLE_REMOTE_INDEX_SETTING
+        FeatureFlags.WRITEABLE_REMOTE_INDEX_SETTING,
+        FeatureFlags.DOC_ID_FUZZY_SET_SETTING
     );
 }
