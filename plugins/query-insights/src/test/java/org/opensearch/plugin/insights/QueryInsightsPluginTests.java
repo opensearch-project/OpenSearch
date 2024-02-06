b7926415c12357a38287ee9494fef4670258398c
Chenyang Ji <cyji@amazon.com>
2024-02-05 22:18:08 -0800

    Top N Queries by latency implementation (#11904)
    * Top N Queries by latency implementation
    * Increase JavaDoc coverage and update PR based comments
    * Refactor record and service to make them generic
    * refactor service for improving multithreading efficiency
    * rebase from master to pick up query insights plugin changes
    
    ---------
    
    Signed-off-by: Chenyang Ji <cyji@amazon.com>
    (cherry picked from commit 554cbf7bfd83d94e1b1b69528ce0d128454754f4)

--- plugins/query-insights/src/test/java/org/opensearch/plugin/insights/QueryInsightsPluginTests.java
+++ plugins/query-insights/src/test/java/org/opensearch/plugin/insights/QueryInsightsPluginTests.java
@@ -14,11 +14,16 @@ import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.ClusterSettings;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.core.action.ActionResponse;
+import org.opensearch.plugin.insights.core.listener.QueryInsightsListener;
 import org.opensearch.plugin.insights.core.service.QueryInsightsService;
+import org.opensearch.plugin.insights.rules.action.top_queries.TopQueriesAction;
+import org.opensearch.plugin.insights.rules.resthandler.top_queries.RestTopQueriesAction;
 import org.opensearch.plugin.insights.settings.QueryInsightsSettings;
 import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.rest.RestHandler;
 import org.opensearch.test.OpenSearchTestCase;
+import org.opensearch.threadpool.ExecutorBuilder;
+import org.opensearch.threadpool.ScalingExecutorBuilder;
 import org.opensearch.threadpool.ThreadPool;
 import org.junit.Before;
 
@@ -74,18 +79,29 @@ public class QueryInsightsPluginTests extends OpenSearchTestCase {
             null,
             null
         );
-        assertEquals(1, components.size());
+        assertEquals(2, components.size());
         assertTrue(components.get(0) instanceof QueryInsightsService);
+        assertTrue(components.get(1) instanceof QueryInsightsListener);
+    }
+
+    public void testGetExecutorBuilders() {
+        Settings.Builder settingsBuilder = Settings.builder();
+        Settings settings = settingsBuilder.build();
+        List<ExecutorBuilder<?>> executorBuilders = queryInsightsPlugin.getExecutorBuilders(settings);
+        assertEquals(1, executorBuilders.size());
+        assertTrue(executorBuilders.get(0) instanceof ScalingExecutorBuilder);
     }
 
     public void testGetRestHandlers() {
         List<RestHandler> components = queryInsightsPlugin.getRestHandlers(Settings.EMPTY, null, null, null, null, null, null);
-        assertEquals(0, components.size());
+        assertEquals(1, components.size());
+        assertTrue(components.get(0) instanceof RestTopQueriesAction);
     }
 
     public void testGetActions() {
         List<ActionPlugin.ActionHandler<? extends ActionRequest, ? extends ActionResponse>> components = queryInsightsPlugin.getActions();
-        assertEquals(0, components.size());
+        assertEquals(1, components.size());
+        assertTrue(components.get(0).getAction() instanceof TopQueriesAction);
     }
 
 }
