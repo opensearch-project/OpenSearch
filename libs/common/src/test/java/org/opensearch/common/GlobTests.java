79117b61590a2c37f968a54c468877a7f3d2f544
Robin Friedmann <robinfriedmann.rf@gmail.com>
2024-02-07 09:26:26 +0100

    Fix failing test(#12065) Signed-off-by: Robin Friedmann <robinfriedmann.rf@gmail.com>
    Refactored Glob.globMatch by using iterative approach; added unit tests (#12065)
    Signed-off-by: Robin Friedmann <robinfriedmann.rf@gmail.com>

--- libs/common/src/test/java/org/opensearch/common/GlobTests.java
+++ libs/common/src/test/java/org/opensearch/common/GlobTests.java
@@ -12,49 +12,64 @@ import org.opensearch.test.OpenSearchTestCase;
 
 public class GlobTests extends OpenSearchTestCase {
 
-
     public void testGlobMatchNoWildcard() {
         assertTrue(Glob.globMatch("test", "test"));
         assertFalse(Glob.globMatch("test", "testing"));
     }
-
 
     public void testGlobMatchWildcardAtBeginning() {
         assertTrue(Glob.globMatch("*test", "thisisatest"));
         assertFalse(Glob.globMatch("*test", "thisisatesting"));
     }
 
-
     public void testGlobMatchWildcardAtEnd() {
         assertTrue(Glob.globMatch("test*", "testthisisa"));
         assertFalse(Glob.globMatch("test*", "atestthisis"));
     }
-
 
     public void testGlobMatchWildcardAtMiddle() {
         assertTrue(Glob.globMatch("test*ing", "testthisisaing"));
         assertFalse(Glob.globMatch("test*ing", "testthisisa"));
     }
 
-
     public void testGlobMatchMultipleWildcards() {
         assertTrue(Glob.globMatch("*test*", "thisisatesting"));
         assertFalse(Glob.globMatch("*test*", "thisisatesing"));
+        assertTrue(Glob.globMatch("*test*test", "thisisatestingtest"));
+        assertFalse(Glob.globMatch("*test*test", "thisisatesting"));
     }
 
+    public void testGlobMatchMultipleCharactersWithSingleWildcard() {
+        assertTrue(Glob.globMatch("a*b", "acb"));
+        assertTrue(Glob.globMatch("a*b", "aab"));
+        assertTrue(Glob.globMatch("a*b", "aaab"));
+        assertFalse(Glob.globMatch("a*b", "ac"));
+    }
+
+    public void testGlobMatchWildcardWithEmptyString() {
+        assertTrue(Glob.globMatch("*", ""));
+        assertTrue(Glob.globMatch("a*", "a"));
+        assertFalse(Glob.globMatch("a*", ""));
+    }
+
+    public void testGlobMatchMultipleWildcardsWithMultipleCharacters() {
+        assertTrue(Glob.globMatch("a*b*c", "abc"));
+        assertTrue(Glob.globMatch("a*b*c", "axxxbxbc"));
+        assertTrue(Glob.globMatch("a*b*c", "aabc"));
+        assertTrue(Glob.globMatch("a*b*c", "abac"));
+        assertFalse(Glob.globMatch("a*b*c", "abca"));
+        assertFalse(Glob.globMatch("a*b*c", "ac"));
+    }
 
     public void testGlobMatchNullPattern() {
         assertFalse(Glob.globMatch(null, "test"));
     }
 
-
     public void testGlobMatchNullString() {
         assertFalse(Glob.globMatch("test", null));
     }
-
 
     public void testGlobMatchNullPatternAndString() {
         assertFalse(Glob.globMatch(null, null));
     }
 }
-
