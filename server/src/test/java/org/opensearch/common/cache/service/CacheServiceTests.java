0eff4a578a27a22e93bf12780b79d8de463ae049
Sagar Upadhyaya <sagar.upadhyaya.121@gmail.com>
2024-03-05 13:01:58 -0800

    Fixing CacheService test
    Signed-off-by: Sagar Upadhyaya <sagar.upadhyaya.121@gmail.com>

--- server/src/test/java/org/opensearch/common/cache/service/CacheServiceTests.java
+++ server/src/test/java/org/opensearch/common/cache/service/CacheServiceTests.java
@@ -10,6 +10,7 @@ package org.opensearch.common.cache.service;
 
 import org.opensearch.common.cache.CacheType;
 import org.opensearch.common.cache.ICache;
+import org.opensearch.common.cache.RemovalListener;
 import org.opensearch.common.cache.module.CacheModule;
 import org.opensearch.common.cache.settings.CacheSettings;
 import org.opensearch.common.cache.store.OpenSearchOnHeapCache;
@@ -61,7 +62,24 @@ public class CacheServiceTests extends OpenSearchTestCase {
             IllegalArgumentException.class,
             () -> cacheService.createCache(config, CacheType.INDICES_REQUEST_CACHE)
         );
-        assertEquals("No configuration exists for cache type: INDICES_REQUEST_CACHE", ex.getMessage());
+        assertEquals("No store name: [opensearch_onheap] is registered for cache type: INDICES_REQUEST_CACHE", ex.getMessage());
+    }
+
+    public void testWithCreateCacheWithDefaultStoreNameForIRC() {
+        CachePlugin mockPlugin1 = mock(CachePlugin.class);
+        ICache.Factory factory1 = mock(ICache.Factory.class);
+        Map<String, ICache.Factory> factoryMap = Map.of("cache1", factory1);
+        when(mockPlugin1.getCacheFactoryMap()).thenReturn(factoryMap);
+
+        CacheModule cacheModule = new CacheModule(List.of(mockPlugin1), Settings.EMPTY);
+        CacheConfig<String, String> config = mock(CacheConfig.class);
+        when(config.getSettings()).thenReturn(Settings.EMPTY);
+        when(config.getWeigher()).thenReturn((k, v) -> 100);
+        when(config.getRemovalListener()).thenReturn(mock(RemovalListener.class));
+
+        CacheService cacheService = cacheModule.getCacheService();
+        ICache<String, String> iCache = cacheService.createCache(config, CacheType.INDICES_REQUEST_CACHE);
+        assertTrue(iCache instanceof OpenSearchOnHeapCache);
     }
 
     public void testWithCreateCacheWithInvalidStoreNameAssociatedForCacheType() {
