79c59f67c3b94d073221e66ccc0f0d57550817cc
Arpit Bandejiya <abandeji@amazon.com>
2024-03-14 15:50:38 +0530

    Testing the allocation changes for all tests
    Signed-off-by: Arpit Bandejiya <abandeji@amazon.com>

--- server/src/main/java/org/opensearch/cluster/routing/allocation/allocator/BalancedShardsAllocator.java
+++ server/src/main/java/org/opensearch/cluster/routing/allocation/allocator/BalancedShardsAllocator.java
@@ -150,7 +150,7 @@ public class BalancedShardsAllocator implements ShardsAllocator {
      */
     public static final Setting<Boolean> PREFER_RANDOM_SHARD_ALLOCATION = Setting.boolSetting(
         "cluster.routing.allocation.balance.prefer_random_allocation",
-        false,
+        true,
         Property.Dynamic,
         Property.NodeScope
     );
