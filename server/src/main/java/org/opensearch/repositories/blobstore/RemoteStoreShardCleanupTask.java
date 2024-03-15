bc3374ae7fa408a086f399d8efcc404c724e0db1
Harish Bhakuni <harishbhakuni21@gmail.com>
2024-03-14 17:43:27 -0700

    Simplify remote directory cleanup after snapshot delete to … (#12672)
    * Simplify remote directory cleanup after snapshot delete to avoid concurrent cleanup task runs for same shard.
    
    Signed-off-by: Harish Bhakuni <hbhakuni@amazon.com>
    
    * Address PR Comments.
    
    Signed-off-by: Harish Bhakuni <hbhakuni@amazon.com>
    
    ---------
    
    Signed-off-by: Harish Bhakuni <hbhakuni@amazon.com>
    Co-authored-by: Harish Bhakuni <hbhakuni@amazon.com>

--- server/src/main/java/org/opensearch/repositories/blobstore/RemoteStoreShardCleanupTask.java
+++ server/src/main/java/org/opensearch/repositories/blobstore/RemoteStoreShardCleanupTask.java
@@ -12,10 +12,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.core.index.shard.ShardId;
 
-import java.util.Map;
 import java.util.Set;
 
-import static org.opensearch.common.util.concurrent.ConcurrentCollections.newConcurrentMap;
 import static org.opensearch.common.util.concurrent.ConcurrentCollections.newConcurrentSet;
 
 /**
@@ -25,7 +23,6 @@ public class RemoteStoreShardCleanupTask implements Runnable {
     private final Runnable task;
     private final String shardIdentifier;
     final static Set<String> ongoingRemoteDirectoryCleanups = newConcurrentSet();
-    final static Map<String, Runnable> pendingRemoteDirectoryCleanups = newConcurrentMap();
     private static final Logger staticLogger = LogManager.getLogger(RemoteStoreShardCleanupTask.class);
 
     public RemoteStoreShardCleanupTask(Runnable task, String indexUUID, ShardId shardId) {
@@ -39,25 +36,16 @@ public class RemoteStoreShardCleanupTask implements Runnable {
 
     @Override
     public void run() {
-        // TODO: this is the best effort at the moment since there is still a known race condition scenario in this
-        // method which needs to be handled where one of the thread just came out of while loop and removed the
-        // entry from ongoingRemoteDirectoryCleanup, and another thread added new pending task in the map.
-        // we need to introduce semaphores/locks to avoid that situation which introduces the overhead of lock object
-        // cleanups. however, there will be no scenario where two threads run cleanup for same shard at same time.
-        // <issue-link>
-        if (pendingRemoteDirectoryCleanups.put(shardIdentifier, task) == null) {
-            if (ongoingRemoteDirectoryCleanups.add(shardIdentifier)) {
-                while (pendingRemoteDirectoryCleanups.containsKey(shardIdentifier)) {
-                    Runnable newTask = pendingRemoteDirectoryCleanups.get(shardIdentifier);
-                    pendingRemoteDirectoryCleanups.remove(shardIdentifier);
-                    newTask.run();
-                }
+        // If there is already a same task ongoing for a shard, we need to skip the new task to avoid multiple
+        // concurrent cleanup of same shard.
+        if (ongoingRemoteDirectoryCleanups.add(shardIdentifier)) {
+            try {
+                task.run();
+            } finally {
                 ongoingRemoteDirectoryCleanups.remove(shardIdentifier);
-            } else {
-                staticLogger.debug("one task is already ongoing for shard {}, we can leave entry in pending", shardIdentifier);
             }
         } else {
-            staticLogger.debug("one cleanup task for shard {} is already in pending, we can skip this task", shardIdentifier);
+            staticLogger.warn("one cleanup task for shard {} is already ongoing, need to skip this task", shardIdentifier);
         }
     }
 }
