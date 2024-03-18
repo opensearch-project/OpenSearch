dbadbdc78f8d990696f908c399883e79ab4ba4ea
Andriy Redko <drreta@gmail.com>
2024-03-18 12:39:40 -0400

    Revert "Integrate IO Based AdmissionController to AdmissionControl Framework …"
    This reverts commit fbcc0d94d1183e15ea3c74c72894c527460b2ce3.
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- server/src/test/java/org/opensearch/ratelimitting/admissioncontrol/settings/CPUBasedAdmissionControlSettingsTests.java
+++ server/src/test/java/org/opensearch/ratelimitting/admissioncontrol/settings/CPUBasedAdmissionControlSettingsTests.java
@@ -20,7 +20,7 @@ import org.opensearch.threadpool.ThreadPool;
 import java.util.Arrays;
 import java.util.Set;
 
-public class CPUBasedAdmissionControllerSettingsTests extends OpenSearchTestCase {
+public class CPUBasedAdmissionControlSettingsTests extends OpenSearchTestCase {
     private ClusterService clusterService;
     private ThreadPool threadPool;
 
