4af9ba87115cc83ebce70ba0f41c7e4d9688eeac
Sagar Upadhyaya <sagar.upadhyaya.121@gmail.com>
2024-03-19 09:39:34 -0700

    Adding a ehcache issue reference for thread leak issue
    Signed-off-by: Sagar Upadhyaya <sagar.upadhyaya.121@gmail.com>

--- plugins/cache-ehcache/src/test/java/org/opensearch/cache/store/disk/EhcacheThreadLeakFilter.java
+++ plugins/cache-ehcache/src/test/java/org/opensearch/cache/store/disk/EhcacheThreadLeakFilter.java
@@ -11,10 +11,12 @@ package org.opensearch.cache.store.disk;
 import com.carrotsearch.randomizedtesting.ThreadFilter;
 
 /**
- * In Ehcache(as of 3.8.0), while calling remove/invalidate() on entries causes to start a daemon thread in the
+ * In Ehcache(as of 3.10.8), while calling remove/invalidate() on entries causes to start a daemon thread in the
  * background to clean up the stale offheap memory associated with the disk cache. And this thread is not closed even
  * after we try to close the cache or cache manager. Considering that it requires a node restart to switch between
  * different cache plugins, this shouldn't be a problem for now. Will be following up with ehcache on this.
+ *
+ * See: https://github.com/ehcache/ehcache3/issues/3204
  */
 public class EhcacheThreadLeakFilter implements ThreadFilter {
 
