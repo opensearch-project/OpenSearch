711c652498b2b92bcd84443d44b5db86fb982010
Mohammad Qureshi <qreshi@amazon.com>
2024-03-19 12:58:53 -0700

    Move DerivedFieldType to its own file and add an enum for DerivedFieldSupportedTypes
    Signed-off-by: Mohammad Qureshi <qreshi@amazon.com>

--- server/src/main/java/org/opensearch/index/mapper/DerivedFieldMapper.java
+++ server/src/main/java/org/opensearch/index/mapper/DerivedFieldMapper.java
@@ -10,18 +10,14 @@ package org.opensearch.index.mapper;
 
 import org.apache.lucene.document.FieldType;
 import org.apache.lucene.index.IndexOptions;
-import org.apache.lucene.search.Query;
-import org.opensearch.common.Nullable;
+import org.apache.lucene.index.IndexableField;
 import org.opensearch.core.xcontent.XContentBuilder;
-import org.opensearch.index.query.QueryShardContext;
 import org.opensearch.script.Script;
-import org.opensearch.search.lookup.SearchLookup;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
 
 /**
  * A field mapper for derived fields
@@ -30,7 +26,7 @@ import java.util.Map;
  */
 public class DerivedFieldMapper extends ParametrizedFieldMapper {
 
-    public static final String CONTENT_TYPE = "derived_field";
+    public static final String CONTENT_TYPE = "derived";
 
     /**
      * Default parameters for the boolean field mapper
@@ -59,7 +55,6 @@ public class DerivedFieldMapper extends ParametrizedFieldMapper {
      * @opensearch.internal
      */
     public static class Builder extends ParametrizedFieldMapper.Builder {
-
         // TODO: The type of parameter may change here if the actual underlying FieldType object is needed
         private final Parameter<String> type = Parameter.stringParam(
             "type",
@@ -76,7 +71,9 @@ public class DerivedFieldMapper extends ParametrizedFieldMapper {
             m -> toType(m).script
         ).setSerializerCheck((id, ic, value) -> value != null);
 
-        public Builder(String name) { super(name); }
+        public Builder(String name) {
+            super(name);
+        }
 
         @Override
         protected List<Parameter<?>> getParameters() {
@@ -85,69 +82,16 @@ public class DerivedFieldMapper extends ParametrizedFieldMapper {
 
         @Override
         public DerivedFieldMapper build(BuilderContext context) {
-            MappedFieldType ft = new DerivedFieldType(buildFullName(context));
+            FieldMapper fieldMapper = DerivedFieldSupportedTypes.getFieldMapperFromType(type.getValue(), name, context);
+            Function<Object, IndexableField> fieldFunction =
+                DerivedFieldSupportedTypes.getIndexableFieldGeneratorType(type.getValue(), name);
+            DerivedFieldType ft = new DerivedFieldType(buildFullName(context), type.getValue(), script.getValue(), fieldMapper, fieldFunction);
             return new DerivedFieldMapper(name, ft, multiFieldsBuilder.build(this, context), copyTo.build(), this);
         }
     }
 
     public static final TypeParser PARSER = new TypeParser((n, c) -> new Builder(n));
-
-    /**
-     * Field type for derived field mapper
-     *
-     * @opensearch.internal
-     */
-    public static final class DerivedFieldType extends MappedFieldType {
-
-        public DerivedFieldType(
-            String name,
-            boolean isIndexed,
-            boolean isStored,
-            boolean hasDocValues,
-            Map<String, String> meta
-        ) {
-            super(name, isIndexed, isStored, hasDocValues, TextSearchInfo.NONE, meta);
-        }
-
-        public DerivedFieldType(String name) { this(name, false, false, false, Collections.emptyMap()); }
-
-        @Override
-        public String typeName() {
-            return CONTENT_TYPE;
-        }
-
-        @Override
-        public ValueFetcher valueFetcher(QueryShardContext context, SearchLookup searchLookup, String format ) {
-            if (format != null) {
-                throw new IllegalArgumentException("Field [" + name() + "] of type [" + typeName() + "] doesn't support formats.");
-            }
-
-            // TODO Return to this during Query implementation. The derived fields don't typically exist in _source but
-            //  we may want fetch the field name from source if a 'script' is not provided.
-            return new SourceValueFetcher(name(), context) {
-                @Override
-                protected Object parseSourceValue(Object value) {
-                    return null;
-                }
-            };
-        }
-
-        @Override
-        public Query termQuery(Object value, @Nullable QueryShardContext context) {
-            throw new IllegalArgumentException("Field [" + name() + "] of type [" + typeName() + "] does not support term queries");
-        }
-
-        @Override
-        public Query existsQuery(QueryShardContext context) {
-            throw new IllegalArgumentException("Field [" + name() + "] of type [" + typeName() + "] does not support exist queries");
-        }
-
-        @Override
-        public boolean isAggregatable() { return false; }
-    }
-
     private final String type;
-
     private final Script script;
 
     protected DerivedFieldMapper(
@@ -163,7 +107,9 @@ public class DerivedFieldMapper extends ParametrizedFieldMapper {
     }
 
     @Override
-    public DerivedFieldType fieldType() { return (DerivedFieldType) super.fieldType(); }
+    public DerivedFieldType fieldType() {
+        return (DerivedFieldType) super.fieldType();
+    }
 
     @Override
     protected void parseCreateField(ParseContext context) throws IOException {
@@ -173,7 +119,9 @@ public class DerivedFieldMapper extends ParametrizedFieldMapper {
     }
 
     @Override
-    public ParametrizedFieldMapper.Builder getMergeBuilder() { return new Builder(simpleName()).init(this); }
+    public ParametrizedFieldMapper.Builder getMergeBuilder() {
+        return new Builder(simpleName()).init(this);
+    }
 
     @Override
     protected String contentType() {
@@ -187,7 +135,11 @@ public class DerivedFieldMapper extends ParametrizedFieldMapper {
         copyTo.toXContent(builder, params);
     }
 
-    public String getType() { return type; }
+    public String getType() {
+        return type;
+    }
 
-    public Script getScript() { return script; }
+    public Script getScript() {
+        return script;
+    }
 }
