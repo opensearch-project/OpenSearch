a350cef35c92b6270a98d5b30dab3669483319a2
Andriy Redko <andriy.redko@aiven.io>
2024-03-23 11:40:39 -0400

    Added missed API visibility annotations for public / experimental APIs (#12864) (#12867)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    (cherry picked from commit ac0bb8344a2dcd0b1c251e450710801c78b6249f)

--- server/src/main/java/org/opensearch/http/HttpServerTransport.java
+++ server/src/main/java/org/opensearch/http/HttpServerTransport.java
@@ -32,6 +32,7 @@
 
 package org.opensearch.http;
 
+import org.opensearch.common.annotation.PublicApi;
 import org.opensearch.common.lifecycle.LifecycleComponent;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.common.transport.BoundTransportAddress;
@@ -42,8 +43,9 @@ import org.opensearch.rest.RestRequest;
 /**
  * HTTP Transport server
  *
- * @opensearch.internal
+ * @opensearch.api
  */
+@PublicApi(since = "1.0.0")
 public interface HttpServerTransport extends LifecycleComponent, ReportingService<HttpInfo> {
 
     String HTTP_SERVER_WORKER_THREAD_NAME_PREFIX = "http_server_worker";
