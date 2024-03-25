88021247c3260b0d7874f68615ba5fb13c82bb9f
Olasoji Denloye <olasoji.denloye@intel.com>
2024-03-25 09:23:01 -0400

    Moved cryptodirectory plugin to sandbox
    Signed-off-by: Olasoji Denloye <olasoji.denloye@intel.com>

---
+++ sandbox/plugins/cryptodirectory/src/yamlRestTest/java/org/opensearch/index/store/CryptoDirectoryClientYamlTestSuiteIT.java
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.path.to.plugin;
+
+import com.carrotsearch.randomizedtesting.annotations.Name;
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+
+import org.opensearch.test.rest.yaml.ClientYamlTestCandidate;
+import org.opensearch.test.rest.yaml.OpenSearchClientYamlSuiteTestCase;
+
+public class CryptoDirectoryClientYamlTestSuiteIT extends OpenSearchClientYamlSuiteTestCase {
+
+    public RenameClientYamlTestSuiteIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
+        super(testCandidate);
+    }
+
+    @ParametersFactory
+    public static Iterable<Object[]> parameters() throws Exception {
+        return OpenSearchClientYamlSuiteTestCase.createParameters();
+    }
+}
