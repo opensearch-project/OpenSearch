0d2fd0136b8ed84c24e50fa8b3520cc0f1140857
Kiran Prakash <awskiran@amazon.com>
2024-04-01 15:23:54 -0700

    Update IndicesRequestCacheIT.java
    Signed-off-by: Kiran Prakash <awskiran@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/indices/IndicesRequestCacheIT.java
+++ server/src/internalClusterTest/java/org/opensearch/indices/IndicesRequestCacheIT.java
@@ -82,6 +82,7 @@ public class IndicesRequestCacheIT extends ParameterizedStaticSettingsOpenSearch
         super(settings);
     }
 
+    
     @ParametersFactory
     public static Collection<Object[]> parameters() {
         return Arrays.asList(
