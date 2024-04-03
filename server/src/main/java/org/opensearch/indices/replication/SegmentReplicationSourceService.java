ce239f77b08eef20e0a0995cc0736116c39ee503
Marc Handalian <marc.handalian@gmail.com>
2024-04-02 17:10:40 -0700

    Fix comment on beforeIndexShardClosed
    Signed-off-by: Marc Handalian <marc.handalian@gmail.com>

--- server/src/main/java/org/opensearch/indices/replication/SegmentReplicationSourceService.java
+++ server/src/main/java/org/opensearch/indices/replication/SegmentReplicationSourceService.java
@@ -217,7 +217,7 @@ public class SegmentReplicationSourceService extends AbstractLifecycleComponent 
 
     /**
      *
-     * After a primary shard is closed, cancel any ongoing replications to release incref'd segments.
+     * Before a primary shard is closed, cancel any ongoing replications to release incref'd segments.
      */
     @Override
     public void beforeIndexShardClosed(ShardId shardId, @Nullable IndexShard indexShard, Settings indexSettings) {
