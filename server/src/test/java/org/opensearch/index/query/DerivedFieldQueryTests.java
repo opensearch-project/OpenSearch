bcd2d8a5197e7aa4a9239d76fbedc85d3c554da8
Rishabh Maurya <rishabhmaurya05@gmail.com>
2024-04-11 14:08:03 -0700

    [Backport 2.x] [Derived Fields] PR4: Capability to define derived fields in search request (#13163)
    * Support derived fields definition in search request
    * adds support for fetch phase on derived fields
    * adds support for highlighting on derived fields
    * fix the version for backward compatibility
    ---------
    
    Signed-off-by: Rishabh Maurya <rishabhmaurya05@gmail.com>
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- server/src/test/java/org/opensearch/index/query/DerivedFieldQueryTests.java
+++ server/src/test/java/org/opensearch/index/query/DerivedFieldQueryTests.java
@@ -22,6 +22,7 @@ import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.opensearch.common.lucene.Lucene;
+import org.opensearch.index.mapper.DerivedFieldSupportedTypes;
 import org.opensearch.index.mapper.DerivedFieldValueFetcher;
 import org.opensearch.script.DerivedFieldScript;
 import org.opensearch.script.Script;
@@ -75,14 +76,17 @@ public class DerivedFieldQueryTests extends OpenSearchTestCase {
 
         // Create ValueFetcher from mocked DerivedFieldScript.Factory
         DerivedFieldScript.LeafFactory leafFactory = factory.newFactory((new Script("")).getParams(), searchLookup);
-        DerivedFieldValueFetcher valueFetcher = new DerivedFieldValueFetcher(leafFactory);
+        DerivedFieldValueFetcher valueFetcher = new DerivedFieldValueFetcher(
+            leafFactory,
+            null,
+            DerivedFieldSupportedTypes.getIndexableFieldGeneratorType("keyword", "ip_from_raw_request")
+        );
 
         // Create DerivedFieldQuery
         DerivedFieldQuery derivedFieldQuery = new DerivedFieldQuery(
             new TermQuery(new Term("ip_from_raw_request", "247.37.0.0")),
             valueFetcher,
             searchLookup,
-            (o -> new KeywordField("ip_from_raw_request", (String) o, Field.Store.NO)),
             Lucene.STANDARD_ANALYZER
         );
 
