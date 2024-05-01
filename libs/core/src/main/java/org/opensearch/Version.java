c9d62fb877f523a24ec28f52ddf1343bb5eeaacd
Andriy Redko <andriy.redko@aiven.io>
2024-05-01 17:34:49 -0400

    Bump 2.x to 2.15.0
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- libs/core/src/main/java/org/opensearch/Version.java
+++ libs/core/src/main/java/org/opensearch/Version.java
@@ -130,7 +130,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_2_13_0 = new Version(2130099, org.apache.lucene.util.Version.LUCENE_9_10_0);
     public static final Version V_2_13_1 = new Version(2130199, org.apache.lucene.util.Version.LUCENE_9_10_0);
     public static final Version V_2_14_0 = new Version(2140099, org.apache.lucene.util.Version.LUCENE_9_10_0);
-    public static final Version CURRENT = V_2_14_0;
+    public static final Version V_2_15_0 = new Version(2150099, org.apache.lucene.util.Version.LUCENE_9_10_0);
+    public static final Version CURRENT = V_2_15_0;
 
     public static Version fromId(int id) {
         final Version known = LegacyESVersion.idToVersion.get(id);
