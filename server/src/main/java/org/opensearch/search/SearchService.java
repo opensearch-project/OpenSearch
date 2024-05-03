c650e2d499b9910d5f0da87fd4db5847ce06fb72
bowenlan-amzn <bowenlan23@gmail.com>
2024-05-02 19:35:32 -0700

    Support multi ranges traversal when doing date histogram rewrite optimization (#13317)

--- server/src/main/java/org/opensearch/search/SearchService.java
+++ server/src/main/java/org/opensearch/search/SearchService.java
@@ -275,7 +275,7 @@ public class SearchService extends AbstractLifecycleComponent implements IndexEv
     // value 0 means rewrite filters optimization in aggregations will be disabled
     public static final Setting<Integer> MAX_AGGREGATION_REWRITE_FILTERS = Setting.intSetting(
         "search.max_aggregation_rewrite_filters",
-        72,
+        3000,
         0,
         Property.Dynamic,
         Property.NodeScope
