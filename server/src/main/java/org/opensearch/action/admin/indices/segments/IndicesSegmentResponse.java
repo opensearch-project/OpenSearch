d97ccb2e6ee03ba69a532d7f513cab3ec196f71e
Harsh Garg <gkharsh@amazon.com>
2024-05-27 11:51:08 +0530

    Optimizing get indices to segments map calculations for IndicesSegmentResponse class
    Signed-off-by: Harsh Garg <gkharsh@amazon.com>

--- server/src/main/java/org/opensearch/action/admin/indices/segments/IndicesSegmentResponse.java
+++ server/src/main/java/org/opensearch/action/admin/indices/segments/IndicesSegmentResponse.java
@@ -47,11 +47,9 @@ import org.opensearch.index.engine.Segment;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Transport response for retrieving indices segment information
@@ -87,19 +85,17 @@ public class IndicesSegmentResponse extends BroadcastResponse {
         }
         Map<String, IndexSegments> indicesSegments = new HashMap<>();
 
-        Set<String> indices = new HashSet<>();
+        Map<String, List<ShardSegments>> indicesShardSegments = new HashMap<>();
         for (ShardSegments shard : shards) {
-            indices.add(shard.getShardRouting().getIndexName());
-        }
-
-        for (String indexName : indices) {
-            List<ShardSegments> shards = new ArrayList<>();
-            for (ShardSegments shard : this.shards) {
-                if (shard.getShardRouting().getIndexName().equals(indexName)) {
-                    shards.add(shard);
-                }
+            List<ShardSegments> shardSegList = indicesShardSegments.get(shard.getShardRouting().getIndexName());
+            if (shardSegList == null) {
+                shardSegList = new ArrayList<>();
+                indicesShardSegments.put(shard.getShardRouting().getIndexName(), shardSegList);
             }
-            indicesSegments.put(indexName, new IndexSegments(indexName, shards.toArray(new ShardSegments[shards.size()])));
+            shardSegList.add(shard);
+        }
+        for (Map.Entry<String, List<ShardSegments>> entry : indicesShardSegments.entrySet()) {
+            indicesSegments.put(entry.getKey(), new IndexSegments(entry.getKey(), entry.getValue().toArray(new ShardSegments[0])));
         }
         this.indicesSegments = indicesSegments;
         return indicesSegments;
