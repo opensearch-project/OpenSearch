077e60b344e537e7c5e295d987f8d6523bc38adc
Shivansh Arora <hishiv@amazon.com>
2024-05-29 15:32:11 +0530

    Remove conflicting static method from Metadata.Custom interface
    Signed-off-by: Shivansh Arora <hishiv@amazon.com>

--- server/src/main/java/org/opensearch/gateway/remote/RemoteClusterStateService.java
+++ server/src/main/java/org/opensearch/gateway/remote/RemoteClusterStateService.java
@@ -148,7 +148,7 @@ public class RemoteClusterStateService implements Closeable {
     public static final ChecksumBlobStoreFormat<Metadata.Custom> CUSTOM_METADATA_FORMAT = new ChecksumBlobStoreFormat<>(
         "custom",
         METADATA_NAME_FORMAT,
-        Metadata.Custom::fromXContent
+        null // no need to reader here, as this object is only used to write/serialize the object
     );
 
     /**
