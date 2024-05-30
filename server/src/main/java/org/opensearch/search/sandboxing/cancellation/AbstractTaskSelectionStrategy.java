827b3cfd547adc7a6f5286ebcf35630850c0cb55
Kiran Prakash <awskiran@amazon.com>
2024-05-30 11:13:35 -0700

    Add java docs
    Signed-off-by: Kiran Prakash <awskiran@amazon.com>

--- server/src/main/java/org/opensearch/search/sandboxing/cancellation/AbstractTaskSelectionStrategy.java
+++ server/src/main/java/org/opensearch/search/sandboxing/cancellation/AbstractTaskSelectionStrategy.java
@@ -17,10 +17,31 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Represents an abstract task selection strategy.
+ * This class implements the TaskSelectionStrategy interface and provides a method to select tasks for cancellation based on a sorting condition.
+ * The specific sorting condition depends on the implementation.
+ */
 public abstract class AbstractTaskSelectionStrategy implements TaskSelectionStrategy {
 
+    /**
+     * Returns a comparator that defines the sorting condition for tasks.
+     * The specific sorting condition depends on the implementation.
+     *
+     * @return The comparator
+     */
     public abstract Comparator<Task> sortingCondition();
 
+    /**
+     * Selects tasks for cancellation based on the provided limit and resource type.
+     * The tasks are sorted based on the sorting condition and then selected until the accumulated resource usage reaches the limit.
+     *
+     * @param tasks The list of tasks from which to select
+     * @param limit The limit on the accumulated resource usage
+     * @param resourceType The type of resource to consider
+     * @return The list of selected tasks
+     * @throws IllegalArgumentException If the limit is less than zero
+     */
     @Override
     public List<Task> selectTasksForCancellation(List<Task> tasks, long limit, SandboxResourceType resourceType) {
         if (limit < 0) {
