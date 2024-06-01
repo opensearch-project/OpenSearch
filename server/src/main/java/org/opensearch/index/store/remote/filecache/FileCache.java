c9fe18009e5eeba5a542e50e798911f7ea0921e7
Andrew Ross <andrross@amazon.com>
2024-05-31 17:12:21 -0700

    Add constant for compatiblity
    Signed-off-by: Andrew Ross <andrross@amazon.com>

--- server/src/main/java/org/opensearch/index/store/remote/filecache/FileCache.java
+++ server/src/main/java/org/opensearch/index/store/remote/filecache/FileCache.java
@@ -10,6 +10,7 @@ package org.opensearch.index.store.remote.filecache;
 
 import org.apache.lucene.store.IndexInput;
 import org.opensearch.common.annotation.PublicApi;
+import org.opensearch.common.settings.Setting;
 import org.opensearch.core.common.breaker.CircuitBreaker;
 import org.opensearch.core.common.breaker.CircuitBreakingException;
 import org.opensearch.index.store.remote.utils.cache.CacheUsage;
@@ -47,6 +48,9 @@ import static org.opensearch.index.store.remote.directory.RemoteSnapshotDirector
  */
 @PublicApi(since = "2.7.0")
 public class FileCache implements RefCountedCache<Path, CachedIndexInput> {
+    // This constant moved, but exists here for backward compatibility
+    public static final Setting<Double> DATA_TO_FILE_CACHE_SIZE_RATIO_SETTING = FileCacheSettings.DATA_TO_FILE_CACHE_SIZE_RATIO_SETTING;
+
     private final SegmentedCache<Path, CachedIndexInput> theCache;
 
     private final CircuitBreaker circuitBreaker;
