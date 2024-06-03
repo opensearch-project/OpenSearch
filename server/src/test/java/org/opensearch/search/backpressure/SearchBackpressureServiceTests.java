99af1121fe32db2fbc6b4937f0c876675eadbb39
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-06-03 16:58:56 -0700

    remove wildcard import
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- server/src/test/java/org/opensearch/search/backpressure/SearchBackpressureServiceTests.java
+++ server/src/test/java/org/opensearch/search/backpressure/SearchBackpressureServiceTests.java
@@ -42,7 +42,13 @@ import org.junit.After;
 import org.junit.Before;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
