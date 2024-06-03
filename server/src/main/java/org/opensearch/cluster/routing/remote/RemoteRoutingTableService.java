e486165f703d31de3ba6252e863085859b13eba0
Himshikha Gupta <himshikh@amazon.com>
2024-06-03 16:50:55 +0530

    Addressing PR comments
    Signed-off-by: Himshikha Gupta <himshikh@amazon.com>

--- server/src/main/java/org/opensearch/cluster/routing/remote/RemoteRoutingTableService.java
+++ server/src/main/java/org/opensearch/cluster/routing/remote/RemoteRoutingTableService.java
@@ -10,6 +10,7 @@ package org.opensearch.cluster.routing.remote;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.opensearch.common.lifecycle.AbstractLifecycleComponent;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.io.IOUtils;
@@ -19,7 +20,6 @@ import org.opensearch.repositories.RepositoriesService;
 import org.opensearch.repositories.Repository;
 import org.opensearch.repositories.blobstore.BlobStoreRepository;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.function.Supplier;
 
@@ -30,7 +30,7 @@ import static org.opensearch.node.remotestore.RemoteStoreNodeAttribute.isRemoteR
  *
  * @opensearch.internal
  */
-public class RemoteRoutingTableService implements Closeable {
+public class RemoteRoutingTableService extends AbstractLifecycleComponent {
 
     /**
      * Cluster setting to specify if routing table should be published to remote store
@@ -53,13 +53,14 @@ public class RemoteRoutingTableService implements Closeable {
     }
 
     @Override
-    public void close() throws IOException {
+    protected void doClose() throws IOException {
         if (blobStoreRepository != null) {
             IOUtils.close(blobStoreRepository);
         }
     }
 
-    public void start() {
+    @Override
+    protected void doStart() {
         assert isRemoteRoutingTableEnabled(settings) == true : "Remote routing table is not enabled";
         final String remoteStoreRepo = settings.get(
             Node.NODE_ATTRIBUTES.getKey() + RemoteStoreNodeAttribute.REMOTE_STORE_ROUTING_TABLE_REPOSITORY_NAME_ATTRIBUTE_KEY
@@ -69,5 +70,8 @@ public class RemoteRoutingTableService implements Closeable {
         assert repository instanceof BlobStoreRepository : "Repository should be instance of BlobStoreRepository";
         blobStoreRepository = (BlobStoreRepository) repository;
     }
+
+    @Override
+    protected void doStop() {}
 
 }
