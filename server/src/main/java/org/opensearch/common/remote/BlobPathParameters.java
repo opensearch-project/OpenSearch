156eca3433ee0910f7fc21340f0052ada7e7533e
Sooraj Sinha <81695996+soosinha@users.noreply.github.com>
2024-06-06 08:23:46 +0530

    [Remote Cluster State] Remote state interfaces  (#13785)
    * Remote Writable Entity interfaces
    
    Signed-off-by: Sooraj Sinha <soosinha@amazon.com>

---
+++ server/src/main/java/org/opensearch/common/remote/BlobPathParameters.java
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.common.remote;
+
+import java.util.List;
+
+/**
+ * Parameters which can be used to construct a blob path
+ *
+ */
+public class BlobPathParameters {
+
+    private final List<String> pathTokens;
+    private final String filePrefix;
+
+    public BlobPathParameters(final List<String> pathTokens, final String filePrefix) {
+        this.pathTokens = pathTokens;
+        this.filePrefix = filePrefix;
+    }
+
+    public List<String> getPathTokens() {
+        return pathTokens;
+    }
+
+    public String getFilePrefix() {
+        return filePrefix;
+    }
+}
