opensearch-project/OpenSearch

commit ebe6f2c2878f036cb4c1147683c30091747d7632

Add GeoBounds aggregation on GeoShape field type.(#3980) (#4266) (#8301)
Enables geo_bounds aggregation to work with geo_shape field types.

This enhancement includes:
    * Addition of Doc values on the GeoShape Field.
    * Addition of GeoShape ValueSource level code interfaces for accessing the DocValues.
    * Addition of Missing Value feature in the GeoShape Aggregations.

Signed-off-by: Navneet Verma <navneev@amazon.com>
Co-authored-by: Navneet Verma <navneev@amazon.com>

libs/geo/src/main/java/org/opensearch/geometry/GeometryCollection.java
@@ -88,6 +88,15 @@ public class GeometryCollection<G extends Geometry> implements Geometry, Iterabl
         return shapes.get(i);
     }
 
+    /**
+     * Returns a {@link List} of All {@link Geometry} present in this collection.
+     *
+     * @return a {@link List} of All {@link Geometry}
+     */
+    public List<G> getAll() {
+        return shapes;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;

modules/geo/build.gradle
@@ -40,6 +40,7 @@ restResources {
     includeCore '_common', 'indices', 'index', 'search', 'bulk'
   }
 }
+
 artifacts {
   restTests(project.file('src/yamlRestTest/resources/rest-api-spec/test'))
 }

modules/geo/src/internalClusterTest/java/org/opensearch/geo/GeoModulePluginIntegTestCase.java
@@ -21,6 +21,9 @@ import java.util.Collections;
  * for the test cluster on which integration tests are running.
  */
 public abstract class GeoModulePluginIntegTestCase extends OpenSearchIntegTestCase {
+
+    protected static final double GEOHASH_TOLERANCE = 1E-5D;
+
     /**
      * Returns a collection of plugins that should be loaded on each node for doing the integration tests. As this
      * geo plugin is not getting packaged in a zip, we need to load it before the tests run.

modules/geo/src/internalClusterTest/java/org/opensearch/geo/search/MissingValueIT.java
@@ -8,52 +8,149 @@
 
 package org.opensearch.geo.search;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
 import org.opensearch.action.search.SearchResponse;
+import org.opensearch.common.geo.GeoPoint;
 import org.opensearch.geo.GeoModulePluginIntegTestCase;
+import org.opensearch.geo.search.aggregations.common.GeoBoundsHelper;
 import org.opensearch.geo.search.aggregations.metrics.GeoBounds;
 import org.opensearch.geo.tests.common.AggregationBuilders;
+import org.opensearch.geo.tests.common.RandomGeoGenerator;
+import org.opensearch.geo.tests.common.RandomGeoGeometryGenerator;
+import org.opensearch.geometry.Geometry;
+import org.opensearch.geometry.utils.WellKnownText;
 import org.opensearch.test.OpenSearchIntegTestCase;
 
 import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertAcked;
 import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertSearchResponse;
 import static org.hamcrest.Matchers.closeTo;
 
+/**
+ * Tests to validate if user specified a missingValue in the input while doing the aggregation
+ */
 @OpenSearchIntegTestCase.SuiteScopeTestCase
 public class MissingValueIT extends GeoModulePluginIntegTestCase {
 
+    private static final String INDEX_NAME = "idx";
+    private static final String GEO_SHAPE_FIELD_NAME = "myshape";
+    private static final String GEO_SHAPE_FIELD_TYPE = "type=geo_shape";
+    private static final String AGGREGATION_NAME = "bounds";
+    private static final String NON_EXISTENT_FIELD = "non_existing_field";
+    private static final WellKnownText WKT = WellKnownText.INSTANCE;
+    private static Geometry indexedGeometry;
+    private static GeoPoint indexedGeoPoint;
+    private GeoPoint bottomRight;
+    private GeoPoint topLeft;
+
     @Override
     protected void setupSuiteScopeCluster() throws Exception {
-        assertAcked(prepareCreate("idx").setMapping("date", "type=date", "location", "type=geo_point", "str", "type=keyword").get());
+        assertAcked(
+            prepareCreate(INDEX_NAME).setMapping(
+                "date",
+                "type=date",
+                "location",
+                "type=geo_point",
+                "str",
+                "type=keyword",
+                GEO_SHAPE_FIELD_NAME,
+                GEO_SHAPE_FIELD_TYPE
+            ).get()
+        );
+        indexedGeometry = RandomGeoGeometryGenerator.randomGeometry(random());
+        indexedGeoPoint = RandomGeoGenerator.randomPoint(random());
+        assert indexedGeometry != null;
         indexRandom(
             true,
-            client().prepareIndex("idx").setId("1").setSource(),
-            client().prepareIndex("idx")
+            client().prepareIndex(INDEX_NAME).setId("1").setSource(),
+            client().prepareIndex(INDEX_NAME)
                 .setId("2")
-                .setSource("str", "foo", "long", 3L, "double", 5.5, "date", "2015-05-07", "location", "1,2")
+                .setSource(
+                    "str",
+                    "foo",
+                    "long",
+                    3L,
+                    "double",
+                    5.5,
+                    "date",
+                    "2015-05-07",
+                    "location",
+                    indexedGeoPoint.toString(),
+                    GEO_SHAPE_FIELD_NAME,
+                    WKT.toWKT(indexedGeometry)
+                )
         );
     }
 
+    @Before
+    public void runBeforeEachTest() {
+        bottomRight = new GeoPoint(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
+        topLeft = new GeoPoint(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+
     public void testUnmappedGeoBounds() {
-        SearchResponse response = client().prepareSearch("idx")
-            .addAggregation(AggregationBuilders.geoBounds("bounds").field("non_existing_field").missing("2,1"))
+        final GeoPoint missingGeoPoint = RandomGeoGenerator.randomPoint(random());
+        GeoBoundsHelper.updateBoundsBottomRight(missingGeoPoint, bottomRight);
+        GeoBoundsHelper.updateBoundsTopLeft(missingGeoPoint, topLeft);
+        SearchResponse response = client().prepareSearch(INDEX_NAME)
+            .addAggregation(
+                AggregationBuilders.geoBounds(AGGREGATION_NAME)
+                    .field(NON_EXISTENT_FIELD)
+                    .wrapLongitude(false)
+                    .missing(missingGeoPoint.toString())
+            )
             .get();
         assertSearchResponse(response);
-        GeoBounds bounds = response.getAggregations().get("bounds");
-        assertThat(bounds.bottomRight().lat(), closeTo(2.0, 1E-5));
-        assertThat(bounds.bottomRight().lon(), closeTo(1.0, 1E-5));
-        assertThat(bounds.topLeft().lat(), closeTo(2.0, 1E-5));
-        assertThat(bounds.topLeft().lon(), closeTo(1.0, 1E-5));
+        validateResult(response.getAggregations().get(AGGREGATION_NAME));
     }
 
     public void testGeoBounds() {
-        SearchResponse response = client().prepareSearch("idx")
-            .addAggregation(AggregationBuilders.geoBounds("bounds").field("location").missing("2,1"))
+        GeoBoundsHelper.updateBoundsForGeoPoint(indexedGeoPoint, topLeft, bottomRight);
+        final GeoPoint missingGeoPoint = RandomGeoGenerator.randomPoint(random());
+        GeoBoundsHelper.updateBoundsForGeoPoint(missingGeoPoint, topLeft, bottomRight);
+        SearchResponse response = client().prepareSearch(INDEX_NAME)
+            .addAggregation(
+                AggregationBuilders.geoBounds(AGGREGATION_NAME).field("location").wrapLongitude(false).missing(missingGeoPoint.toString())
+            )
             .get();
         assertSearchResponse(response);
-        GeoBounds bounds = response.getAggregations().get("bounds");
-        assertThat(bounds.bottomRight().lat(), closeTo(1.0, 1E-5));
-        assertThat(bounds.bottomRight().lon(), closeTo(2.0, 1E-5));
-        assertThat(bounds.topLeft().lat(), closeTo(2.0, 1E-5));
-        assertThat(bounds.topLeft().lon(), closeTo(1.0, 1E-5));
+        validateResult(response.getAggregations().get(AGGREGATION_NAME));
+    }
+
+    public void testGeoBoundsWithMissingShape() {
+        // create GeoBounds for the indexed Field
+        GeoBoundsHelper.updateBoundsForGeometry(indexedGeometry, topLeft, bottomRight);
+        final Geometry missingGeometry = RandomGeoGeometryGenerator.randomGeometry(random());
+        assert missingGeometry != null;
+        GeoBoundsHelper.updateBoundsForGeometry(missingGeometry, topLeft, bottomRight);
+        final SearchResponse response = client().prepareSearch(INDEX_NAME)
+            .addAggregation(
+                AggregationBuilders.geoBounds(AGGREGATION_NAME)
+                    .wrapLongitude(false)
+                    .field(GEO_SHAPE_FIELD_NAME)
+                    .missing(WKT.toWKT(missingGeometry))
+            )
+            .get();
+        assertSearchResponse(response);
+        validateResult(response.getAggregations().get(AGGREGATION_NAME));
+    }
+
+    public void testUnmappedGeoBoundsOnGeoShape() {
+        // We cannot useGeometry other than Point as for GeoBoundsAggregation as the Default Value for the
+        // CoreValueSourceType is GeoPoint hence we need to use Point here.
+        final Geometry missingGeometry = RandomGeoGeometryGenerator.randomPoint(random());
+        final SearchResponse response = client().prepareSearch(INDEX_NAME)
+            .addAggregation(AggregationBuilders.geoBounds(AGGREGATION_NAME).field(NON_EXISTENT_FIELD).missing(WKT.toWKT(missingGeometry)))
+            .get();
+        GeoBoundsHelper.updateBoundsForGeometry(missingGeometry, topLeft, bottomRight);
+        assertSearchResponse(response);
+        validateResult(response.getAggregations().get(AGGREGATION_NAME));
+    }
+
+    private void validateResult(final GeoBounds bounds) {
+        MatcherAssert.assertThat(bounds.bottomRight().lat(), closeTo(bottomRight.lat(), GEOHASH_TOLERANCE));
+        MatcherAssert.assertThat(bounds.bottomRight().lon(), closeTo(bottomRight.lon(), GEOHASH_TOLERANCE));
+        MatcherAssert.assertThat(bounds.topLeft().lat(), closeTo(topLeft.lat(), GEOHASH_TOLERANCE));
+        MatcherAssert.assertThat(bounds.topLeft().lon(), closeTo(topLeft.lon(), GEOHASH_TOLERANCE));
     }
 }

modules/geo/src/internalClusterTest/java/org/opensearch/geo/search/aggregations/common/GeoBoundsHelper.java
@@ -1,0 +1,187 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.geo.search.aggregations.common;
+
+import org.junit.Assert;
+import org.opensearch.common.geo.GeoPoint;
+import org.opensearch.geometry.Geometry;
+import org.opensearch.geometry.GeometryCollection;
+import org.opensearch.geometry.Line;
+import org.opensearch.geometry.MultiLine;
+import org.opensearch.geometry.MultiPoint;
+import org.opensearch.geometry.MultiPolygon;
+import org.opensearch.geometry.Point;
+import org.opensearch.geometry.Polygon;
+import org.opensearch.geometry.Rectangle;
+import org.opensearch.geometry.ShapeType;
+
+import java.util.Locale;
+
+/**
+ * A helper class for finding the geo bounds for a shape or a point.
+ */
+public final class GeoBoundsHelper {
+
+    /**
+     * Updates the GeoBounds for the input GeoPoint in topLeft and bottomRight GeoPoints.
+     *
+     * @param geoPoint {@link GeoPoint}
+     * @param topLeft {@link GeoPoint}
+     * @param bottomRight {@link GeoPoint}
+     */
+    public static void updateBoundsForGeoPoint(final GeoPoint geoPoint, final GeoPoint topLeft, final GeoPoint bottomRight) {
+        updateBoundsBottomRight(geoPoint, bottomRight);
+        updateBoundsTopLeft(geoPoint, topLeft);
+    }
+
+    /**
+     * Find the bottom right for a point and put it in the currentBounds param.
+     *
+     * @param geoPoint {@link GeoPoint}
+     * @param currentBound {@link GeoPoint}
+     */
+    public static void updateBoundsBottomRight(final GeoPoint geoPoint, final GeoPoint currentBound) {
+        if (geoPoint.lat() < currentBound.lat()) {
+            currentBound.resetLat(geoPoint.lat());
+        }
+        if (geoPoint.lon() > currentBound.lon()) {
+            currentBound.resetLon(geoPoint.lon());
+        }
+    }
+
+    /**
+     * Find the top left for a point and put it in the currentBounds param.
+     *
+     * @param geoPoint {@link GeoPoint}
+     * @param currentBound {@link GeoPoint}
+     */
+    public static void updateBoundsTopLeft(final GeoPoint geoPoint, final GeoPoint currentBound) {
+        if (geoPoint.lat() > currentBound.lat()) {
+            currentBound.resetLat(geoPoint.lat());
+        }
+        if (geoPoint.lon() < currentBound.lon()) {
+            currentBound.resetLon(geoPoint.lon());
+        }
+    }
+
+    /**
+     * Find the bounds for an input shape.
+     *
+     * @param geometry {@link Geometry}
+     * @param geoShapeTopLeft {@link GeoPoint}
+     * @param geoShapeBottomRight {@link GeoPoint}
+     */
+    public static void updateBoundsForGeometry(
+        final Geometry geometry,
+        final GeoPoint geoShapeTopLeft,
+        final GeoPoint geoShapeBottomRight
+    ) {
+        final ShapeType shapeType = geometry.type();
+        switch (shapeType) {
+            case POINT:
+                updateBoundsTopLeft((Point) geometry, geoShapeTopLeft);
+                updateBoundsBottomRight((Point) geometry, geoShapeBottomRight);
+                return;
+            case MULTIPOINT:
+                ((MultiPoint) geometry).getAll().forEach(p -> updateBoundsTopLeft(p, geoShapeTopLeft));
+                ((MultiPoint) geometry).getAll().forEach(p -> updateBoundsBottomRight(p, geoShapeBottomRight));
+                return;
+            case POLYGON:
+                updateBoundsTopLeft((Polygon) geometry, geoShapeTopLeft);
+                updateBoundsBottomRight((Polygon) geometry, geoShapeBottomRight);
+                return;
+            case LINESTRING:
+                updateBoundsTopLeft((Line) geometry, geoShapeTopLeft);
+                updateBoundsBottomRight((Line) geometry, geoShapeBottomRight);
+                return;
+            case MULTIPOLYGON:
+                ((MultiPolygon) geometry).getAll().forEach(p -> updateBoundsTopLeft(p, geoShapeTopLeft));
+                ((MultiPolygon) geometry).getAll().forEach(p -> updateBoundsBottomRight(p, geoShapeBottomRight));
+                return;
+            case GEOMETRYCOLLECTION:
+                ((GeometryCollection<?>) geometry).getAll()
+                    .forEach(geo -> updateBoundsForGeometry(geo, geoShapeTopLeft, geoShapeBottomRight));
+                return;
+            case MULTILINESTRING:
+                ((MultiLine) geometry).getAll().forEach(line -> updateBoundsTopLeft(line, geoShapeTopLeft));
+                ((MultiLine) geometry).getAll().forEach(line -> updateBoundsBottomRight(line, geoShapeBottomRight));
+                return;
+            case ENVELOPE:
+                updateBoundsTopLeft((Rectangle) geometry, geoShapeTopLeft);
+                updateBoundsBottomRight((Rectangle) geometry, geoShapeBottomRight);
+                return;
+            default:
+                Assert.fail(String.format(Locale.ROOT, "The shape type %s is not supported", shapeType));
+        }
+    }
+
+    private static void updateBoundsTopLeft(final Point p, final GeoPoint currentBound) {
+        final GeoPoint geoPoint = new GeoPoint(p.getLat(), p.getLon());
+        updateBoundsTopLeft(geoPoint, currentBound);
+    }
+
+    private static void updateBoundsTopLeft(final Polygon polygon, final GeoPoint currentBound) {
+        for (int i = 0; i < polygon.getPolygon().length(); i++) {
+            double lat = polygon.getPolygon().getLats()[i];
+            double lon = polygon.getPolygon().getLons()[i];
+            final GeoPoint geoPoint = new GeoPoint(lat, lon);
+            updateBoundsTopLeft(geoPoint, currentBound);
+        }
+    }
+
+    private static void updateBoundsTopLeft(final Line line, final GeoPoint currentBound) {
+        for (int i = 0; i < line.length(); i++) {
+            double lat = line.getLats()[i];
+            double lon = line.getLons()[i];
+            final GeoPoint geoPoint = new GeoPoint(lat, lon);
+            updateBoundsTopLeft(geoPoint, currentBound);
+        }
+    }
+
+    private static void updateBoundsTopLeft(final Rectangle rectangle, final GeoPoint currentBound) {
+        if (rectangle.getMaxLat() > currentBound.lat()) {
+            currentBound.resetLat(rectangle.getMaxLat());
+        }
+        if (rectangle.getMinLon() < currentBound.lon()) {
+            currentBound.resetLon(rectangle.getMinLon());
+        }
+    }
+
+    private static void updateBoundsBottomRight(final Point p, final GeoPoint currentBound) {
+        final GeoPoint geoPoint = new GeoPoint(p.getLat(), p.getLon());
+        updateBoundsBottomRight(geoPoint, currentBound);
+    }
+
+    private static void updateBoundsBottomRight(final Polygon polygon, final GeoPoint currentBound) {
+        for (int i = 0; i < polygon.getPolygon().length(); i++) {
+            double lat = polygon.getPolygon().getLats()[i];
+            double lon = polygon.getPolygon().getLons()[i];
+            final GeoPoint geoPoint = new GeoPoint(lat, lon);
+            updateBoundsBottomRight(geoPoint, currentBound);
+        }
+    }
+
+    private static void updateBoundsBottomRight(final Line line, final GeoPoint currentBound) {
+        for (int i = 0; i < line.length(); i++) {
+            double lat = line.getLats()[i];
+            double lon = line.getLons()[i];
+            final GeoPoint geoPoint = new GeoPoint(lat, lon);
+            updateBoundsBottomRight(geoPoint, currentBound);
+        }
+    }
+
+    private static void updateBoundsBottomRight(final Rectangle rectangle, final GeoPoint currentBound) {
+        if (rectangle.getMinLat() < currentBound.lat()) {
+            currentBound.resetLat(rectangle.getMinLat());
+        }
+        if (rectangle.getMaxLon() > currentBound.lon()) {
+            currentBound.resetLon(rectangle.getMaxLon());
+        }
+    }
+}

modules/geo/src/internalClusterTest/java/org/opensearch/geo/search/aggregations/metrics/AbstractGeoAggregatorModulePluginTestCase.java
@@ -22,14 +22,20 @@ import org.opensearch.core.xcontent.ToXContent;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.geo.GeoModulePluginIntegTestCase;
+import org.opensearch.geo.search.aggregations.common.GeoBoundsHelper;
 import org.opensearch.geo.tests.common.RandomGeoGenerator;
+import org.opensearch.geo.tests.common.RandomGeoGeometryGenerator;
+import org.opensearch.geometry.Geometry;
 import org.opensearch.geometry.utils.Geohash;
+import org.opensearch.geometry.utils.StandardValidator;
+import org.opensearch.geometry.utils.WellKnownText;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.sort.SortBuilders;
 import org.opensearch.search.sort.SortOrder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.opensearch.common.xcontent.XContentFactory.jsonBuilder;
@@ -46,6 +52,7 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
 
     protected static final String SINGLE_VALUED_FIELD_NAME = "geo_value";
     protected static final String MULTI_VALUED_FIELD_NAME = "geo_values";
+    protected static final String GEO_SHAPE_FIELD_NAME = "shape";
     protected static final String NUMBER_FIELD_NAME = "l_values";
     protected static final String UNMAPPED_IDX_NAME = "idx_unmapped";
     protected static final String IDX_NAME = "idx";
@@ -57,11 +64,13 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
     protected static int numDocs;
     protected static int numUniqueGeoPoints;
     protected static GeoPoint[] singleValues, multiValues;
+    protected static Geometry[] geoShapesValues;
     protected static GeoPoint singleTopLeft, singleBottomRight, multiTopLeft, multiBottomRight, singleCentroid, multiCentroid,
-        unmappedCentroid;
+        unmappedCentroid, geoShapeTopLeft, geoShapeBottomRight;
     protected static ObjectIntMap<String> expectedDocCountsForGeoHash = null;
     protected static ObjectObjectMap<String, GeoPoint> expectedCentroidsForGeoHash = null;
-    protected static final double GEOHASH_TOLERANCE = 1E-5D;
+
+    protected static final WellKnownText WKT = new WellKnownText(true, new StandardValidator(true));
 
     @Override
     public void setupSuiteScopeCluster() throws Exception {
@@ -75,7 +84,9 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
                 NUMBER_FIELD_NAME,
                 "type=long",
                 "tag",
-                "type=keyword"
+                "type=keyword",
+                GEO_SHAPE_FIELD_NAME,
+                "type=geo_shape"
             )
         );
 
@@ -83,6 +94,8 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
         singleBottomRight = new GeoPoint(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
         multiTopLeft = new GeoPoint(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
         multiBottomRight = new GeoPoint(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
+        geoShapeTopLeft = new GeoPoint(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        geoShapeBottomRight = new GeoPoint(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
         singleCentroid = new GeoPoint(0, 0);
         multiCentroid = new GeoPoint(0, 0);
         unmappedCentroid = new GeoPoint(0, 0);
@@ -95,16 +108,20 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
         singleValues = new GeoPoint[numUniqueGeoPoints];
         for (int i = 0; i < singleValues.length; i++) {
             singleValues[i] = RandomGeoGenerator.randomPoint(random());
-            updateBoundsTopLeft(singleValues[i], singleTopLeft);
-            updateBoundsBottomRight(singleValues[i], singleBottomRight);
+            GeoBoundsHelper.updateBoundsForGeoPoint(singleValues[i], singleTopLeft, singleBottomRight);
         }
 
         multiValues = new GeoPoint[numUniqueGeoPoints];
         for (int i = 0; i < multiValues.length; i++) {
             multiValues[i] = RandomGeoGenerator.randomPoint(random());
-            updateBoundsTopLeft(multiValues[i], multiTopLeft);
-            updateBoundsBottomRight(multiValues[i], multiBottomRight);
+            GeoBoundsHelper.updateBoundsForGeoPoint(multiValues[i], multiTopLeft, multiBottomRight);
         }
+
+        geoShapesValues = new Geometry[numDocs];
+        IntStream.range(0, numDocs).forEach(iterator -> {
+            geoShapesValues[iterator] = RandomGeoGeometryGenerator.randomGeometry(random());
+            GeoBoundsHelper.updateBoundsForGeometry(geoShapesValues[iterator], geoShapeTopLeft, geoShapeBottomRight);
+        });
 
         List<IndexRequestBuilder> builders = new ArrayList<>();
 
@@ -132,6 +149,7 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
                             .endArray()
                             .field(NUMBER_FIELD_NAME, i)
                             .field("tag", "tag" + i)
+                            .field(GEO_SHAPE_FIELD_NAME, WKT.toWKT(geoShapesValues[i]))
                             .endObject()
                     )
             );
@@ -147,7 +165,9 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
             );
         }
 
-        assertAcked(prepareCreate(EMPTY_IDX_NAME).setMapping(SINGLE_VALUED_FIELD_NAME, "type=geo_point"));
+        assertAcked(
+            prepareCreate(EMPTY_IDX_NAME).setMapping(SINGLE_VALUED_FIELD_NAME, "type=geo_point", GEO_SHAPE_FIELD_NAME, "type=geo_shape")
+        );
 
         assertAcked(
             prepareCreate(DATELINE_IDX_NAME).setMapping(
@@ -273,23 +293,5 @@ public abstract class AbstractGeoAggregatorModulePluginTestCase extends GeoModul
         final double newLon = centroid.lon() + (location.lon() - centroid.lon()) / docCount;
         final double newLat = centroid.lat() + (location.lat() - centroid.lat()) / docCount;
         return centroid.reset(newLat, newLon);
-    }
-
-    private void updateBoundsBottomRight(GeoPoint geoPoint, GeoPoint currentBound) {
-        if (geoPoint.lat() < currentBound.lat()) {
-            currentBound.resetLat(geoPoint.lat());
-        }
-        if (geoPoint.lon() > currentBound.lon()) {
-            currentBound.resetLon(geoPoint.lon());
-        }
-    }
-
-    private void updateBoundsTopLeft(GeoPoint geoPoint, GeoPoint currentBound) {
-        if (geoPoint.lat() > currentBound.lat()) {
-            currentBound.resetLat(geoPoint.lat());
-        }
-        if (geoPoint.lon() < currentBound.lon()) {
-            currentBound.resetLon(geoPoint.lon());
-        }
     }
 }

commit ce1395957205a335a91d7e389ea92026b4d2a804

Demonstrate that arrays of `geo_shape` can no longer be ingested
This was first broken in commit ebe6f2c2878f036cb4c1147683c30091747d7632

Demonstrates #14193

Signed-off-by: Kurt McKee <contactme@kurtmckee.org>

modules/geo/src/yamlRestTest/resources/rest-api-spec/test/geo_shape/300_array_of_geo_shapes.yml
@@ -1,0 +1,67 @@
+setup:
+  - do:
+      indices.create:
+        index: test
+        body:
+          settings:
+            number_of_replicas: 0
+          mappings:
+            properties:
+              location:
+                type: "geo_shape"
+
+---
+"Single shape":
+  - do:
+      bulk:
+        refresh: true
+        body:
+          - index:
+              _index: test
+              _id: 1
+          - location:
+              type: "polygon"
+              coordinates: [
+                [
+                  [ -87.0, 41.0 ],
+                  [ -88.0, 41.0 ],
+                  [ -88.0, 42.0 ],
+                  [ -87.0, 42.0 ],
+                  [ -87.0, 41.0 ],
+                ]
+              ]
+
+  - match: {errors: false}
+
+---
+"Array of shapes":
+  - do:
+      bulk:
+        refresh: true
+        body:
+          - index:
+              _index: test
+              _id: 1
+          - location:
+              - type: "polygon"
+                coordinates: [
+                  [
+                    [ -87.0, 41.0 ],
+                    [ -88.0, 41.0 ],
+                    [ -88.0, 42.0 ],
+                    [ -87.0, 42.0 ],
+                    [ -87.0, 41.0 ],
+                  ]
+                ]
+              - type: "polygon"
+                coordinates: [
+                  [
+                    [ -97.0, 41.0 ],
+                    [ -98.0, 41.0 ],
+                    [ -98.0, 42.0 ],
+                    [ -97.0, 42.0 ],
+                    [ -97.0, 41.0 ],
+                  ]
+                ]
+
+  - match: {errors: false}