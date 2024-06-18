26004379151db7b1cb7e1b37a3f6b85d376631e9
Shivansh Arora <hishiv@amazon.com>
2024-06-18 18:45:37 +0530

    Add unittests for RemoteClusterStateAttributesManager
    Signed-off-by: Shivansh Arora <hishiv@amazon.com>

---
+++ server/src/test/java/org/opensearch/gateway/remote/RemoteStateTestUtil.java
@@ -0,0 +1,28 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.gateway.remote;
+
+import org.opensearch.core.action.ActionListener;
+
+public class RemoteStateTestUtil {
+    public static class TestCapturingListener<T> implements ActionListener<T> {
+        T result;
+        Exception failure;
+
+        @Override
+        public void onResponse(T result) {
+            this.result = result;
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            this.failure = e;
+        }
+    }
+}
