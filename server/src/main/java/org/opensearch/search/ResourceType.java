bcccedbba41df6e69cd73a118e4106c3a09f47ca
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-06-21 09:06:41 -0700

    Bug/sbp cancellation (#13474)
    * change cancellation logic to fix disparity bw trackers and resource duress
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add additional tests for searchBackpressureService and refactor code
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add enumMap instead of list for tracking taskResourceUsageTrackets
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add nodeNotInDuress test for nodeDuressTrackers class
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add entry in CHANGELOG
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove wildcard import
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * streamline imports
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add additional test case to test the circuit breaker for SBP logic
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add missing javadoc to resourece type enum
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add javadoc to a method
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix javadoc warnings
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix javadoc warnings
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    ---------
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

---
+++ server/src/main/java/org/opensearch/search/ResourceType.java
@@ -0,0 +1,41 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.search;
+
+/**
+ * Enum to hold the resource type
+ */
+public enum ResourceType {
+    CPU("cpu"),
+    JVM("jvm");
+
+    private final String name;
+
+    ResourceType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The string match here is case-sensitive
+     * @param s name matching the resource type name
+     * @return a {@link ResourceType}
+     */
+    public static ResourceType fromName(String s) {
+        for (ResourceType resourceType : values()) {
+            if (resourceType.getName().equals(s)) {
+                return resourceType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown resource type: [" + s + "]");
+    }
+
+    private String getName() {
+        return name;
+    }
+}
