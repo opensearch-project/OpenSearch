926b4746334cfaed199ebd034b81ce2717461558
Veljko Potparic <veljko_velid@yahoo.com>
2024-06-27 06:54:56 +0200

    Introduce Serbian analyzer
    Signed-off-by: Veljko Potparic <veljko_velid@yahoo.com>

--- modules/analysis-common/src/main/java/org/opensearch/analysis/common/StemmerTokenFilterFactory.java
+++ modules/analysis-common/src/main/java/org/opensearch/analysis/common/StemmerTokenFilterFactory.java
@@ -96,6 +96,7 @@ import org.tartarus.snowball.ext.NorwegianStemmer;
 import org.tartarus.snowball.ext.PortugueseStemmer;
 import org.tartarus.snowball.ext.RomanianStemmer;
 import org.tartarus.snowball.ext.RussianStemmer;
+import org.tartarus.snowball.ext.SerbianStemmer;
 import org.tartarus.snowball.ext.SpanishStemmer;
 import org.tartarus.snowball.ext.SwedishStemmer;
 import org.tartarus.snowball.ext.TurkishStemmer;
@@ -257,7 +258,10 @@ public class StemmerTokenFilterFactory extends AbstractTokenFilterFactory {
             } else if ("russian".equalsIgnoreCase(language)) {
                 return new SnowballFilter(tokenStream, new RussianStemmer());
             } else if ("light_russian".equalsIgnoreCase(language) || "lightRussian".equalsIgnoreCase(language)) {
-                return new RussianLightStemFilter(tokenStream);
+            return new RussianLightStemFilter(tokenStream);
+
+            } else if ("serbian".equalsIgnoreCase(language)) {
+                return new SnowballFilter(tokenStream, new SerbianStemmer());
 
                 // Spanish stemmers
             } else if ("spanish".equalsIgnoreCase(language)) {
