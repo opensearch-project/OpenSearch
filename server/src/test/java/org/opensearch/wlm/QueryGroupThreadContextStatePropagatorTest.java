3dd00c6386e73fcca38ab41407c3a0a649b616b4
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-07-01 12:29:50 -0700

    add query group header propagator
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

---
+++ server/src/test/java/org/opensearch/wlm/QueryGroupThreadContextStatePropagatorTest.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.wlm;
+
+import org.junit.Test;
+import org.opensearch.test.OpenSearchTestCase;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class QueryGroupThreadContextStatePropagatorTest extends OpenSearchTestCase {
+
+
+    public void testTransients() {
+        QueryGroupThreadContextStatePropagator sut = new QueryGroupThreadContextStatePropagator();
+        Map<String, Object> source = Map.of("queryGroupId", "adgarja0r235te");
+        Map<String, Object> transients = sut.transients(source);
+        assertEquals("adgarja0r235te", transients.get("queryGroupId"));
+    }
+
+
+    public void testHeaders() {
+        QueryGroupThreadContextStatePropagator sut = new QueryGroupThreadContextStatePropagator();
+        Map<String, Object> source = Map.of("queryGroupId", "adgarja0r235te");
+        Map<String, String> headers = sut.headers(source);
+        assertEquals("adgarja0r235te", headers.get("queryGroupId"));
+    }
+}
