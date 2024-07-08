fff79aec7a77247f3f4b67e0dc9c30f74f6bf3c6
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-07-08 14:39:39 -0700

    Adding QueryGroup schema (#13669)
    * rebase with opensearch/main
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add resourceLimitGroupId propagation logic from coordinator to data nodes
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add sandbox schema
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add resourceLimitGroupTests
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add resourceLimitGroupMetadata tests
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * run spotlessApply
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add mode field in ResourceLimitGroup schema
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix breaking testcases
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add task cancellation skeleton
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add multitenant labels in searchSource builder
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * write custom xcontent parser for ResourceLimitGroup
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove unrelated changes
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove non-existing import fro cluster settings
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove non releated changes
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add _id as the resourceLimitGroup key
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add change to register resource limit group metadata
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add updatedAt in resource limit group
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * rename resourceLimitGroup to queryGroup
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address the comments on PR
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * rename the mode member var to resiliency mode
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add change in CHANGELOG
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add tests for custom namedWritable QueryGroupMetadata
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * structure resourceLimits into an object
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add QueryGroup.toXContent test case
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix precommit errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix precommit errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix assemble errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix checkstyle errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    ---------
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- server/src/main/java/org/opensearch/cluster/ClusterModule.java
+++ server/src/main/java/org/opensearch/cluster/ClusterModule.java
@@ -48,6 +48,7 @@ import org.opensearch.cluster.metadata.MetadataIndexStateService;
 import org.opensearch.cluster.metadata.MetadataIndexTemplateService;
 import org.opensearch.cluster.metadata.MetadataMappingService;
 import org.opensearch.cluster.metadata.MetadataUpdateSettingsService;
+import org.opensearch.cluster.metadata.QueryGroupMetadata;
 import org.opensearch.cluster.metadata.RepositoriesMetadata;
 import org.opensearch.cluster.metadata.WeightedRoutingMetadata;
 import org.opensearch.cluster.routing.DelayedAllocationService;
@@ -212,6 +213,8 @@ public class ClusterModule extends AbstractModule {
             DecommissionAttributeMetadata::new,
             DecommissionAttributeMetadata::readDiffFrom
         );
+
+        registerMetadataCustom(entries, QueryGroupMetadata.TYPE, QueryGroupMetadata::new, QueryGroupMetadata::readDiffFrom);
         // Task Status (not Diffable)
         entries.add(new Entry(Task.Status.class, PersistentTasksNodeService.Status.NAME, PersistentTasksNodeService.Status::new));
         return entries;
