f7722480c3e537edaf62391180e07cd337c7013f
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-07-09 14:38:52 -0700

    [Backport 2.x] Adding QueryGroup schema (#13669) (#14690)
    * Adding QueryGroup schema (#13669)
    
    * rebase with opensearch/main
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add resourceLimitGroupId propagation logic from coordinator to data nodes
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add sandbox schema
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add resourceLimitGroupTests
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add resourceLimitGroupMetadata tests
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * run spotlessApply
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add mode field in ResourceLimitGroup schema
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix breaking testcases
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add task cancellation skeleton
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add multitenant labels in searchSource builder
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * write custom xcontent parser for ResourceLimitGroup
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove unrelated changes
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove non-existing import fro cluster settings
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * remove non releated changes
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add _id as the resourceLimitGroup key
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add change to register resource limit group metadata
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add updatedAt in resource limit group
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * rename resourceLimitGroup to queryGroup
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address the comments on PR
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * rename the mode member var to resiliency mode
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add change in CHANGELOG
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add tests for custom namedWritable QueryGroupMetadata
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * structure resourceLimits into an object
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add QueryGroup.toXContent test case
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix precommit errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix precommit errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix assemble errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * fix checkstyle errors
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    ---------
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add min supported version in query group metadata
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    ---------
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    Signed-off-by: kkewwei <kkewwei@163.com>

--- server/src/main/java/org/opensearch/cluster/metadata/Metadata.java
+++ server/src/main/java/org/opensearch/cluster/metadata/Metadata.java
@@ -1393,6 +1393,25 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata>, To
             return this;
         }
 
+        public Builder queryGroups(final Map<String, QueryGroup> queryGroups) {
+            this.customs.put(QueryGroupMetadata.TYPE, new QueryGroupMetadata(queryGroups));
+            return this;
+        }
+
+        public Builder put(final QueryGroup queryGroup) {
+            Objects.requireNonNull(queryGroup, "queryGroup should not be null");
+            Map<String, QueryGroup> existing = new HashMap<>(getQueryGroups());
+            existing.put(queryGroup.get_id(), queryGroup);
+            return queryGroups(existing);
+        }
+
+        private Map<String, QueryGroup> getQueryGroups() {
+            return Optional.ofNullable(this.customs.get(QueryGroupMetadata.TYPE))
+                .map(o -> (QueryGroupMetadata) o)
+                .map(QueryGroupMetadata::queryGroups)
+                .orElse(Collections.emptyMap());
+        }
+
         public Custom getCustom(String type) {
             return customs.get(type);
         }
