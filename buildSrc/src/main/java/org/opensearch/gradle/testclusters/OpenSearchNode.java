4a0f60f6ee62e2f4d35531c54fdf262005dce4f7
bowenlan-amzn <bowenlan23@gmail.com>
2024-07-15 10:43:12 -0700

    revert use_real_memory change in OpenSearchNode
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>

--- buildSrc/src/main/java/org/opensearch/gradle/testclusters/OpenSearchNode.java
+++ buildSrc/src/main/java/org/opensearch/gradle/testclusters/OpenSearchNode.java
@@ -1179,7 +1179,7 @@ public class OpenSearchNode implements TestClusterConfiguration {
         // Temporarily disable the real memory usage circuit breaker. It depends on real memory usage which we have no full control
         // over and the REST client will not retry on circuit breaking exceptions yet (see #31986 for details). Once the REST client
         // can retry on circuit breaking exceptions, we can revert again to the default configuration.
-        baseConfig.put("indices.breaker.total.use_real_memory", "true");
+        baseConfig.put("indices.breaker.total.use_real_memory", "false");
         // Don't wait for state, just start up quickly. This will also allow new and old nodes in the BWC case to become the master
         baseConfig.put("discovery.initial_state_timeout", "0s");
 
