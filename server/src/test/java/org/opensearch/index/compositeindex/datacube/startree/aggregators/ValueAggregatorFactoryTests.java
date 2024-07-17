a2cef8fcdeeaed4ca11f6c6d69a9c9cb5cd4d2c5
Sarthak Aggarwal <sarthagg@amazon.com>
2024-07-17 15:26:49 +0530

    Star Tree Implementation [OnHeap] (#14512)
    ---------
    Signed-off-by: Sarthak Aggarwal <sarthagg@amazon.com>

---
+++ server/src/test/java/org/opensearch/index/compositeindex/datacube/startree/aggregators/ValueAggregatorFactoryTests.java
@@ -0,0 +1,27 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.index.compositeindex.datacube.startree.aggregators;
+
+import org.opensearch.index.compositeindex.datacube.MetricStat;
+import org.opensearch.index.compositeindex.datacube.startree.aggregators.numerictype.StarTreeNumericType;
+import org.opensearch.test.OpenSearchTestCase;
+
+public class ValueAggregatorFactoryTests extends OpenSearchTestCase {
+
+    public void testGetValueAggregatorForSumType() {
+        ValueAggregator aggregator = ValueAggregatorFactory.getValueAggregator(MetricStat.SUM);
+        assertNotNull(aggregator);
+        assertEquals(SumValueAggregator.class, aggregator.getClass());
+    }
+
+    public void testGetAggregatedValueTypeForSumType() {
+        StarTreeNumericType starTreeNumericType = ValueAggregatorFactory.getAggregatedValueType(MetricStat.SUM);
+        assertEquals(SumValueAggregator.VALUE_AGGREGATOR_TYPE, starTreeNumericType);
+    }
+}
