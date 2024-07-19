403605c8d5a648a333f9e643ce7219a66de8bd4f
Kiran Prakash <awskiran@amazon.com>
2024-07-19 14:40:47 -0700

    remove unused
    Signed-off-by: Kiran Prakash <awskiran@amazon.com>

--- server/src/main/java/org/opensearch/search/querygroup/QueryGroupService.java
+++ server/src/main/java/org/opensearch/search/querygroup/QueryGroupService.java
@@ -14,7 +14,6 @@ import org.opensearch.cluster.metadata.QueryGroup;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.inject.Inject;
 import org.opensearch.common.lifecycle.AbstractLifecycleComponent;
-import org.opensearch.common.unit.TimeValue;
 import org.opensearch.search.querygroup.tracker.QueryGroupUsageTracker;
 import org.opensearch.threadpool.Scheduler;
 import org.opensearch.threadpool.ThreadPool;
@@ -23,7 +22,6 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Main service which will run periodically to track and cancel resource constraint violating tasks in QueryGroups
@@ -43,16 +41,8 @@ public class QueryGroupService extends AbstractLifecycleComponent {
      * @param threadPool threadPool this will be used to schedule the service
      */
     @Inject
-    public QueryGroupService(
-        QueryGroupUsageTracker queryGroupUsageTracker,
-        // QueryGroupServiceSettings queryGroupServiceSettings,
-        // QueryGroupPruner queryGroupPruner,
-        ClusterService clusterService,
-        ThreadPool threadPool
-    ) {
+    public QueryGroupService(QueryGroupUsageTracker queryGroupUsageTracker, ClusterService clusterService, ThreadPool threadPool) {
         this.queryGroupUsageTracker = queryGroupUsageTracker;
-        // this.queryGroupServiceSettings = queryGroupServiceSettings;
-        // this.queryGroupPruner = queryGroupPruner;
         this.clusterService = clusterService;
         this.threadPool = threadPool;
     }
@@ -75,16 +65,7 @@ public class QueryGroupService extends AbstractLifecycleComponent {
      */
     @Override
     protected void doStart() {
-        scheduledFuture = threadPool.scheduleWithFixedDelay(() -> {
-            try {
-                doRun();
-            } catch (Exception e) {
-                logger.debug("Exception occurred in QueryGroup service", e);
-            }
-        },
-            new TimeValue(1, TimeUnit.SECONDS), // TODO get this from QueryGroupServiceSettings
-            ThreadPool.Names.GENERIC
-        );
+        // This method is intentionally left empty to ensure the service is never scheduled
     }
 
     @Override
