d4d958a629e912c3bb8e8ace3ad5cdfe147a4d75
Marc Handalian <marc.handalian@gmail.com>
2024-07-22 15:31:04 -0700

    Fix license header and add changelog entry
    Signed-off-by: Marc Handalian <marc.handalian@gmail.com>

--- server/src/test/java/org/opensearch/search/aggregations/bucket/terms/DerivedFieldAggregationTests.java
+++ server/src/test/java/org/opensearch/search/aggregations/bucket/terms/DerivedFieldAggregationTests.java
@@ -1,3 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
 package org.opensearch.search.aggregations.bucket.terms;
 
 import org.apache.lucene.document.Document;
