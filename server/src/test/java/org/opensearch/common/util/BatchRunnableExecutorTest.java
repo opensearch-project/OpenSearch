f78903721272d62358eeeae84171dd68cb8039b1
Rishab Nahata <rnnahata@amazon.com>
2024-07-23 16:57:15 +0530

    Fix BatchRunnableExecutorTest test
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- server/src/test/java/org/opensearch/common/util/BatchRunnableExecutorTest.java
+++ server/src/test/java/org/opensearch/common/util/BatchRunnableExecutorTest.java
@@ -8,6 +8,7 @@
 
 package org.opensearch.common.util;
 
+import org.mockito.ArgumentCaptor;
 import org.opensearch.common.unit.TimeValue;
 import org.opensearch.common.util.concurrent.TimeoutAwareRunnable;
 import org.opensearch.test.OpenSearchTestCase;
@@ -17,11 +18,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BatchRunnableExecutorTest extends OpenSearchTestCase {
     private Supplier<TimeValue> timeoutSupplier;
@@ -30,12 +27,16 @@ public class BatchRunnableExecutorTest extends OpenSearchTestCase {
     private TimeoutAwareRunnable runnable3;
     private List<TimeoutAwareRunnable> runnableList;
 
-    public void testRunWithoutTimeout() {
+    public void setupRunnables() {
         timeoutSupplier = mock(Supplier.class);
         runnable1 = mock(TimeoutAwareRunnable.class);
         runnable2 = mock(TimeoutAwareRunnable.class);
         runnable3 = mock(TimeoutAwareRunnable.class);
         runnableList = Arrays.asList(runnable1, runnable2, runnable3);
+    }
+
+    public void testRunWithoutTimeout() {
+        setupRunnables();
         when(timeoutSupplier.get()).thenReturn(TimeValue.timeValueSeconds(1));
         BatchRunnableExecutor executor = new BatchRunnableExecutor(runnableList, timeoutSupplier);
         executor.run();
@@ -48,11 +49,7 @@ public class BatchRunnableExecutorTest extends OpenSearchTestCase {
     }
 
     public void testRunWithTimeout() {
-        timeoutSupplier = mock(Supplier.class);
-        runnable1 = mock(TimeoutAwareRunnable.class);
-        runnable2 = mock(TimeoutAwareRunnable.class);
-        runnable3 = mock(TimeoutAwareRunnable.class);
-        runnableList = Arrays.asList(runnable1, runnable2, runnable3);
+        setupRunnables();
         when(timeoutSupplier.get()).thenReturn(TimeValue.timeValueNanos(1));
         BatchRunnableExecutor executor = new BatchRunnableExecutor(runnableList, timeoutSupplier);
         executor.run();
@@ -64,32 +61,27 @@ public class BatchRunnableExecutorTest extends OpenSearchTestCase {
         verify(runnable3, never()).run();
     }
 
-    // public void testRunWithPartialTimeout() {
-    // timeoutSupplier = mock(Supplier.class);
-    // runnable1 = mock(TimeoutAwareRunnable.class);
-    // runnable2 = mock(TimeoutAwareRunnable.class);
-    // runnable3 = mock(TimeoutAwareRunnable.class);
-    // runnableList = Arrays.asList(runnable1, runnable2, runnable3);
-    // when(timeoutSupplier.get()).thenReturn(TimeValue.timeValueMillis(100));
-    // BatchRunnableExecutor executor = new BatchRunnableExecutor(runnableList, timeoutSupplier);
-    // doAnswer(invocationOnMock -> {
-    // Thread.sleep(1000);
-    // return null;
-    // }).when(runnable1).run();
-    // executor.run();
-    // verify(runnable1, times(1)).run();
-    // verify(runnable2, never()).run();
-    // verify(runnable3, never()).run();
-    // verify(runnable2, times(1)).onTimeout();
-    // verify(runnable3, times(1)).onTimeout();
-    // }
+    public void testRunWithPartialTimeout() {
+        setupRunnables();
+        when(timeoutSupplier.get()).thenReturn(TimeValue.timeValueMillis(50));
+        BatchRunnableExecutor executor = new BatchRunnableExecutor(runnableList, timeoutSupplier);
+        doAnswer(invocation -> {
+            Thread.sleep(100);
+            return null;
+        }).when(runnable1).run();
+        executor.run();
+        ArgumentCaptor<Runnable> runnableCaptor = ArgumentCaptor.forClass(Runnable.class);
+        verify(runnable1, atMost(1)).run();
+        verify(runnable2, atMost(1)).run();
+        verify(runnable3, atMost(1)).run();
+        verify(runnable2, atMost(1)).onTimeout();
+        verify(runnable3, atMost(1)).onTimeout();
+        verify(runnable2, atMost(1)).onTimeout();
+        verify(runnable3, atMost(1)).onTimeout();
+    }
 
     public void testRunWithEmptyRunnableList() {
-        timeoutSupplier = mock(Supplier.class);
-        runnable1 = mock(TimeoutAwareRunnable.class);
-        runnable2 = mock(TimeoutAwareRunnable.class);
-        runnable3 = mock(TimeoutAwareRunnable.class);
-        runnableList = Arrays.asList(runnable1, runnable2, runnable3);
+        setupRunnables();
         BatchRunnableExecutor executor = new BatchRunnableExecutor(Collections.emptyList(), timeoutSupplier);
         executor.run();
         verify(runnable1, never()).onTimeout();
