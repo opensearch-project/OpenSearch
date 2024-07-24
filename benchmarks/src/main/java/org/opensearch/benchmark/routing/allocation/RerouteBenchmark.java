fe6e5d94b18dacf12a2427dd389eaa8e787212c7
Rishab Nahata <rnnahata@amazon.com>
2024-07-25 02:39:18 +0530

    Fix imports
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- benchmarks/src/main/java/org/opensearch/benchmark/routing/allocation/RerouteBenchmark.java
+++ benchmarks/src/main/java/org/opensearch/benchmark/routing/allocation/RerouteBenchmark.java
@@ -8,8 +8,17 @@
 
 package org.opensearch.benchmark.routing.allocation;
 
-import org.openjdk.jmh.annotations.*;
-
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.opensearch.Version;
 import org.opensearch.cluster.ClusterName;
 import org.opensearch.cluster.ClusterState;
