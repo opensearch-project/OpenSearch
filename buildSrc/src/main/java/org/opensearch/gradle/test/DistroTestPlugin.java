3a70538b3e197ffb7db04072f69b5565501f9efe
Andriy Redko <andriy.redko@aiven.io>
2024-07-30 11:22:25 -0400

    OpenJDK Update (July 2024 Patch releases) (#15003)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- buildSrc/src/main/java/org/opensearch/gradle/test/DistroTestPlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/test/DistroTestPlugin.java
@@ -75,9 +75,9 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class DistroTestPlugin implements Plugin<Project> {
-    private static final String SYSTEM_JDK_VERSION = "8u412-b08";
+    private static final String SYSTEM_JDK_VERSION = "8.0.422+5.1";
     private static final String SYSTEM_JDK_VENDOR = "adoptium";
-    private static final String GRADLE_JDK_VERSION = "11.0.23+9";
+    private static final String GRADLE_JDK_VERSION = "11.0.24+8";
     private static final String GRADLE_JDK_VENDOR = "adoptium";
 
     // all distributions used by distro tests. this is temporary until tests are per distribution
