21f37a6fc0e30861ffc177bf139369b25ba795c2
Kiran Prakash <awskiran@amazon.com>
2024-08-01 15:21:12 -0700

    remove QueryGroupTestHelpers
    Signed-off-by: Kiran Prakash <awskiran@amazon.com>

--- server/src/test/java/org/opensearch/wlm/QueryGroupLevelResourceUsageViewTests.java
+++ server/src/test/java/org/opensearch/wlm/QueryGroupLevelResourceUsageViewTests.java
@@ -8,14 +8,15 @@
 
 package org.opensearch.wlm;
 
+import org.opensearch.action.search.SearchAction;
+import org.opensearch.core.tasks.TaskId;
 import org.opensearch.search.ResourceType;
 import org.opensearch.tasks.Task;
 import org.opensearch.test.OpenSearchTestCase;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
-import static org.opensearch.wlm.QueryGroupTestHelpers.getRandomTask;
 
 public class QueryGroupLevelResourceUsageViewTests extends OpenSearchTestCase {
     Map<ResourceType, Long> resourceUsage;
@@ -48,5 +49,16 @@ public class QueryGroupLevelResourceUsageViewTests extends OpenSearchTestCase {
 
     private boolean assertResourceUsageData(Map<ResourceType, Long> resourceUsageData) {
         return resourceUsageData.get(ResourceType.fromName("memory")) == 34L && resourceUsageData.get(ResourceType.fromName("cpu")) == 12L;
+    }
+
+    private Task getRandomTask(long id) {
+        return new Task(
+            id,
+            "transport",
+            SearchAction.NAME,
+            "test description",
+            new TaskId(randomLong() + ":" + randomLong()),
+            Collections.emptyMap()
+        );
     }
 }
