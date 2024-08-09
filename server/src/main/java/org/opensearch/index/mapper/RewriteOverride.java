c9b921276747851a6b8cfc20a3f21fa23ad7e9d8
Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
2024-08-09 14:09:51 -0700

    Fix enum docs
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>

--- server/src/main/java/org/opensearch/index/mapper/RewriteOverride.java
+++ server/src/main/java/org/opensearch/index/mapper/RewriteOverride.java
@@ -10,11 +10,10 @@ package org.opensearch.index.mapper;
 
 import org.opensearch.common.annotation.PublicApi;
 
-/*
-* A custom rewrite override for a query. Default executes the query as is and other values determine which structure to use at run-time.
+/** A custom rewrite override for a query. Default executes the query as is and other values determine which structure to use at run-time.
 *
 * @opensearch.internal
-* */
+*/
 @PublicApi(since = "2.17.0")
 public enum RewriteOverride {
 
