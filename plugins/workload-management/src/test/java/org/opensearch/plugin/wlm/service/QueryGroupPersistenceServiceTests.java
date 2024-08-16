d4615c79a4b958152442a95085481f5a015853b0
Ruirui Zhang <mariazrr@amazon.com>
2024-08-15 17:29:02 -0700

    fix IT Signed-off-by: Ruirui Zhang <mariazrr@amazon.com>

--- plugins/workload-management/src/test/java/org/opensearch/plugin/wlm/service/QueryGroupPersistenceServiceTests.java
+++ plugins/workload-management/src/test/java/org/opensearch/plugin/wlm/service/QueryGroupPersistenceServiceTests.java
@@ -6,14 +6,6 @@
  * compatible open source license.
  */
 
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
-
 package org.opensearch.plugin.wlm.service;
 
 import org.opensearch.cluster.ClusterName;
