8ca0e7b857b1d1e5d277a25cc0ebe807abc1858a
Dmitry Kryukov <dk2k@ya.ru>
2024-08-23 19:07:14 +0300

    Removed unneeded calls of String.format()
    Signed-off-by: Dmitry Kryukov <dk2k@ya.ru>

--- server/src/main/java/org/opensearch/gateway/remote/RemoteManifestManager.java
+++ server/src/main/java/org/opensearch/gateway/remote/RemoteManifestManager.java
@@ -140,7 +140,7 @@ public class RemoteManifestManager {
         CountDownLatch latch = new CountDownLatch(1);
 
         LatchedActionListener completionListener = new LatchedActionListener<>(ActionListener.wrap(resp -> {
-            logger.trace(String.format(Locale.ROOT, "Manifest file uploaded successfully."));
+            logger.trace("Manifest file uploaded successfully.");
         }, ex -> { exceptionReference.set(ex); }), latch);
 
         RemoteClusterMetadataManifest remoteClusterMetadataManifest = new RemoteClusterMetadataManifest(
@@ -154,13 +154,13 @@ public class RemoteManifestManager {
         try {
             if (latch.await(getMetadataManifestUploadTimeout().millis(), TimeUnit.MILLISECONDS) == false) {
                 RemoteStateTransferException ex = new RemoteStateTransferException(
-                    String.format(Locale.ROOT, "Timed out waiting for transfer of manifest file to complete")
+                    "Timed out waiting for transfer of manifest file to complete"
                 );
                 throw ex;
             }
         } catch (InterruptedException ex) {
             RemoteStateTransferException exception = new RemoteStateTransferException(
-                String.format(Locale.ROOT, "Timed out waiting for transfer of manifest file to complete - %s"),
+                "Timed out waiting for transfer of manifest file to complete - %s",
                 ex
             );
             Thread.currentThread().interrupt();
