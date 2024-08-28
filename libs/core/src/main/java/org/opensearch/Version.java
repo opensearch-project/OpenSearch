b0c8db4e5c435752e620c3e25da8b1f405f6ef24
Andriy Redko <andriy.redko@aiven.io>
2024-08-28 10:34:15 -0400

    Added bwc version 1.3.20
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- libs/core/src/main/java/org/opensearch/Version.java
+++ libs/core/src/main/java/org/opensearch/Version.java
@@ -99,6 +99,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_1_3_17 = new Version(1031799, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_3_18 = new Version(1031899, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_1_3_19 = new Version(1031999, org.apache.lucene.util.Version.LUCENE_8_10_1);
+    public static final Version V_1_3_20 = new Version(1032099, org.apache.lucene.util.Version.LUCENE_8_10_1);
     public static final Version V_2_0_0 = new Version(2000099, org.apache.lucene.util.Version.LUCENE_9_1_0);
     public static final Version V_2_0_1 = new Version(2000199, org.apache.lucene.util.Version.LUCENE_9_1_0);
     public static final Version V_2_0_2 = new Version(2000299, org.apache.lucene.util.Version.LUCENE_9_1_0);
