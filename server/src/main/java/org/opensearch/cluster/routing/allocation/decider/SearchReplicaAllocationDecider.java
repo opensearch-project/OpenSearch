0855d9f136beac7f3ef2602bdc74dfc9017c155e
Marc Handalian <marc.handalian@gmail.com>
2024-08-30 16:59:05 -0700

    Fix license header and description on SearchReplicaAllocationDecider
    Signed-off-by: Marc Handalian <marc.handalian@gmail.com>

--- server/src/main/java/org/opensearch/cluster/routing/allocation/decider/SearchReplicaAllocationDecider.java
+++ server/src/main/java/org/opensearch/cluster/routing/allocation/decider/SearchReplicaAllocationDecider.java
@@ -6,30 +6,6 @@
  * compatible open source license.
  */
 
-/*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-/*
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
- */
-
 package org.opensearch.cluster.routing.allocation.decider;
 
 import org.opensearch.cluster.node.DiscoveryNode;
@@ -49,32 +25,11 @@ import static org.opensearch.cluster.node.DiscoveryNodeFilters.IP_VALIDATOR;
 import static org.opensearch.cluster.node.DiscoveryNodeFilters.OpType.OR;
 
 /**
- * This {@link AllocationDecider} control shard allocation by include and
- * exclude filters via dynamic cluster and index routing settings.
- * <p>
- * This filter is used to make explicit decision on which nodes certain shard
- * can / should be allocated. The decision if a shard can be allocated, must not
- * be allocated or should be allocated is based on either cluster wide dynamic
- * settings ({@code cluster.routing.allocation.*}) or index specific dynamic
- * settings ({@code index.routing.allocation.*}). All of those settings can be
- * changed at runtime via the cluster or the index update settings API.
- * </p>
- * Note: Cluster settings are applied first and will override index specific
- * settings such that if a shard can be allocated according to the index routing
- * settings it wont be allocated on a node if the cluster specific settings
- * would disallow the allocation. Filters are applied in the following order:
- * <ol>
- * <li>{@code required} - filters required allocations.
- * If any {@code required} filters are set the allocation is denied if the index is <b>not</b> in the set of {@code required} to allocate
- * on the filtered node</li>
- * <li>{@code include} - filters "allowed" allocations.
- * If any {@code include} filters are set the allocation is denied if the index is <b>not</b> in the set of {@code include} filters for
- * the filtered node</li>
- * <li>{@code exclude} - filters "prohibited" allocations.
- * If any {@code exclude} filters are set the allocation is denied if the index is in the set of {@code exclude} filters for the
- * filtered node</li>
- * </ol>
- *
+ * This allocation decider is similar to FilterAllocationDecider but provides
+ * the option to filter specifically for search replicas.
+ * The filter behaves similar to an include for any defined node attribute.
+ * A search replica can be allocated to only nodes with one of the specified attributes while
+ * other shard types will be rejected from nodes with any othe attributes.
  * @opensearch.internal
  */
 public class SearchReplicaAllocationDecider extends AllocationDecider {
