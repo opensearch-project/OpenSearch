579f2aaef81cdee80028fceba1f509dc69e108c6
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-08-30 14:07:43 -0700

    Add support for tracking failures at query group level (#15527)
    * add workload managementRequestFailureListener
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add unit tests
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add CHANGELOG
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * add missing javadoc
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * refactor
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * address comments
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    * rename listener instance
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>
    
    ---------
    
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- server/src/main/java/org/opensearch/wlm/QueryGroupService.java
+++ server/src/main/java/org/opensearch/wlm/QueryGroupService.java
@@ -9,11 +9,59 @@
 package org.opensearch.wlm;
 
 import org.opensearch.core.concurrency.OpenSearchRejectedExecutionException;
+import org.opensearch.wlm.stats.QueryGroupState;
+import org.opensearch.wlm.stats.QueryGroupStats;
+import org.opensearch.wlm.stats.QueryGroupStats.QueryGroupStatsHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * This is stub at this point in time and will be replace by an acutal one in couple of days
+ * As of now this is a stub and main implementation PR will be raised soon.Coming PR will collate these changes with core QueryGroupService changes
  */
 public class QueryGroupService {
+    // This map does not need to be concurrent since we will process the cluster state change serially and update
+    // this map with new additions and deletions of entries. QueryGroupState is thread safe
+    private final Map<String, QueryGroupState> queryGroupStateMap;
+
+    public QueryGroupService() {
+        this(new HashMap<>());
+    }
+
+    public QueryGroupService(Map<String, QueryGroupState> queryGroupStateMap) {
+        this.queryGroupStateMap = queryGroupStateMap;
+    }
+
+    /**
+     * updates the failure stats for the query group
+     * @param queryGroupId query group identifier
+     */
+    public void incrementFailuresFor(final String queryGroupId) {
+        QueryGroupState queryGroupState = queryGroupStateMap.get(queryGroupId);
+        // This can happen if the request failed for a deleted query group
+        // or new queryGroup is being created and has not been acknowledged yet
+        if (queryGroupState == null) {
+            return;
+        }
+        queryGroupState.failures.inc();
+    }
+
+    /**
+     *
+     * @return node level query group stats
+     */
+    public QueryGroupStats nodeStats() {
+        final Map<String, QueryGroupStatsHolder> statsHolderMap = new HashMap<>();
+        for (Map.Entry<String, QueryGroupState> queryGroupsState : queryGroupStateMap.entrySet()) {
+            final String queryGroupId = queryGroupsState.getKey();
+            final QueryGroupState currentState = queryGroupsState.getValue();
+
+            statsHolderMap.put(queryGroupId, QueryGroupStatsHolder.from(currentState));
+        }
+
+        return new QueryGroupStats(statsHolderMap);
+    }
+
     /**
      *
      * @param queryGroupId query group identifier
