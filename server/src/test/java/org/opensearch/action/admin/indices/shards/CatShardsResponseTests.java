24d16a2f1d29f66247f31cc2e2ea17be4d81e79f
Somesh Gupta <someshgupta987@gmail.com>
2024-08-31 19:06:52 +0530

    Fixed test file
    Signed-off-by: Somesh Gupta <someshgupta987@gmail.com>
    
    Added license

--- server/src/test/java/org/opensearch/action/admin/indices/shards/CatShardsResponseTests.java
+++ server/src/test/java/org/opensearch/action/admin/indices/shards/CatShardsResponseTests.java
@@ -1,28 +1,50 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.action.admin.indices.shards;
 
 import org.opensearch.action.admin.cluster.shards.CatShardsResponse;
 import org.opensearch.action.admin.cluster.state.ClusterStateResponse;
 import org.opensearch.action.admin.indices.stats.IndicesStatsResponse;
 import org.opensearch.cluster.ClusterName;
-import org.junit.Before;
-import org.junit.Test;
+import org.opensearch.test.OpenSearchTestCase;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
-public class CatShardsResponseTests {
+public class CatShardsResponseTests extends OpenSearchTestCase {
 
-    private CatShardsResponse catShardsResponse;
+    private final CatShardsResponse catShardsResponse = new CatShardsResponse();
 
-    @Before
-    public void setUp() {
-        catShardsResponse = new CatShardsResponse();
-    }
-
-    @Test
     public void testGetAndSetClusterStateResponse() {
-        assertNull(catShardsResponse.getClusterStateResponse());
-
         ClusterName clusterName = new ClusterName("1");
         ClusterStateResponse clusterStateResponse = new ClusterStateResponse(clusterName, null, false);
         catShardsResponse.setClusterStateResponse(clusterStateResponse);
@@ -30,10 +52,7 @@ public class CatShardsResponseTests {
         assertEquals(clusterStateResponse, catShardsResponse.getClusterStateResponse());
     }
 
-    @Test
     public void testGetAndSetIndicesStatsResponse() {
-        assertNull(catShardsResponse.getIndicesStatsResponse());
-
         final IndicesStatsResponse indicesStatsResponse = new IndicesStatsResponse(null, 0, 0, 0, null);
         catShardsResponse.setIndicesStatsResponse(indicesStatsResponse);
 
