40027c6e5161458d6cfb6d9ba5045eb6dc2f6476
Bharathwaj G <bharath78910@gmail.com>
2024-09-03 15:22:51 +0530

    [Star tree] Star tree index validations (#15533)
    ---------
    
    Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

--- server/src/main/java/org/opensearch/common/settings/IndexScopedSettings.java
+++ server/src/main/java/org/opensearch/common/settings/IndexScopedSettings.java
@@ -249,6 +249,8 @@ public final class IndexScopedSettings extends AbstractScopedSettings {
                 StarTreeIndexSettings.DEFAULT_METRICS_LIST,
                 StarTreeIndexSettings.DEFAULT_DATE_INTERVALS,
                 StarTreeIndexSettings.STAR_TREE_MAX_DATE_INTERVALS_SETTING,
+                StarTreeIndexSettings.STAR_TREE_MAX_BASE_METRICS_SETTING,
+                StarTreeIndexSettings.IS_COMPOSITE_INDEX_SETTING,
 
                 IndexSettings.INDEX_CONTEXT_CREATED_VERSION,
                 IndexSettings.INDEX_CONTEXT_CURRENT_VERSION,
