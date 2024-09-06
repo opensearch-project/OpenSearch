af5682ef1536e5aa72341b917a10fb6873bf9197
Shivansh Arora <hishiv@amazon.com>
2024-09-06 15:21:38 +0530

    apply spotless
    Signed-off-by: Shivansh Arora <hishiv@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/gateway/remote/RemoteStatePublicationIT.java
+++ server/src/internalClusterTest/java/org/opensearch/gateway/remote/RemoteStatePublicationIT.java
@@ -38,8 +38,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
 import java.util.stream.Collectors;
