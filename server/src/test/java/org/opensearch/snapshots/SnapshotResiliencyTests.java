2961a6fc2e0430ca9ced350ab1a5f6cb304cae97
rajiv-kv <157019998+rajiv-kv@users.noreply.github.com>
2024-09-12 11:54:18 +0530

    using remote cluster-state as fallback (#15424)
    Signed-off-by: Rajiv Kumar Vaidyanathan <rajivkv@amazon.com>

--- server/src/test/java/org/opensearch/snapshots/SnapshotResiliencyTests.java
+++ server/src/test/java/org/opensearch/snapshots/SnapshotResiliencyTests.java
@@ -2413,7 +2413,8 @@ public class SnapshotResiliencyTests extends OpenSearchTestCase {
                         clusterService,
                         threadPool,
                         actionFilters,
-                        indexNameExpressionResolver
+                        indexNameExpressionResolver,
+                        null
                     )
                 );
                 actions.put(
@@ -2455,7 +2456,8 @@ public class SnapshotResiliencyTests extends OpenSearchTestCase {
                         clusterService,
                         threadPool,
                         actionFilters,
-                        indexNameExpressionResolver
+                        indexNameExpressionResolver,
+                        null
                     )
                 );
 
