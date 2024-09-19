f7e4ac3d1dd7002c1d196c17cc47f48227f978f3
Craig Perkins <cwperx@amazon.com>
2024-09-19 16:55:21 -0400

    Remove identity-related feature flagged code from the RestController (#15430)
    * Add authenticate to IdentityPlugin interface
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Handle null
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Fix tests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Fix ActionModuleTests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add to CHANGELOG
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add DelegatingRestHandlerTests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Address forbiddenApi
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove authenticate from IdentityPlugin and keep RestController feature flagged code removed
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Move RestTokenExtractor to identity-shiro plugin
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove change in IdentityService
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove changes in ActionModuleTests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add tests for RestTokenExtractor
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove DelegatingRestHandler
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Call super instead of keeping a reference to the delegated restHandler
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Address code review comments
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    ---------
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    Signed-off-by: Craig Perkins <craig5008@gmail.com>

--- plugins/identity-shiro/src/main/java/org/opensearch/identity/shiro/ShiroIdentityPlugin.java
+++ plugins/identity-shiro/src/main/java/org/opensearch/identity/shiro/ShiroIdentityPlugin.java
@@ -13,20 +13,28 @@ import org.apache.logging.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.mgt.SecurityManager;
 import org.opensearch.client.Client;
+import org.opensearch.client.node.NodeClient;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.annotation.ExperimentalApi;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.common.io.stream.NamedWriteableRegistry;
+import org.opensearch.core.rest.RestStatus;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.env.Environment;
 import org.opensearch.env.NodeEnvironment;
 import org.opensearch.identity.PluginSubject;
 import org.opensearch.identity.Subject;
+import org.opensearch.identity.tokens.AuthToken;
 import org.opensearch.identity.tokens.TokenManager;
+import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.IdentityPlugin;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.repositories.RepositoriesService;
+import org.opensearch.rest.BytesRestResponse;
+import org.opensearch.rest.RestChannel;
+import org.opensearch.rest.RestHandler;
+import org.opensearch.rest.RestRequest;
 import org.opensearch.script.ScriptService;
 import org.opensearch.threadpool.ThreadPool;
 import org.opensearch.watcher.ResourceWatcherService;
@@ -34,14 +42,12 @@ import org.opensearch.watcher.ResourceWatcherService;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 /**
  * Identity implementation with Shiro
- *
- * @opensearch.experimental
  */
-@ExperimentalApi
-public final class ShiroIdentityPlugin extends Plugin implements IdentityPlugin {
+public final class ShiroIdentityPlugin extends Plugin implements IdentityPlugin, ActionPlugin {
     private Logger log = LogManager.getLogger(this.getClass());
 
     private final Settings settings;
@@ -101,6 +107,37 @@ public final class ShiroIdentityPlugin extends Plugin implements IdentityPlugin 
     }
 
     @Override
+    public UnaryOperator<RestHandler> getRestHandlerWrapper(ThreadContext threadContext) {
+        return AuthcRestHandler::new;
+    }
+
+    class AuthcRestHandler extends RestHandler.Wrapper {
+
+        public AuthcRestHandler(RestHandler original) {
+            super(original);
+        }
+
+        @Override
+        public void handleRequest(RestRequest request, RestChannel channel, NodeClient client) throws Exception {
+            try {
+                final AuthToken token = ShiroTokenExtractor.extractToken(request);
+                // If no token was found, continue executing the request
+                if (token == null) {
+                    // Authentication did not fail so return true. Authorization is handled at the action level.
+                    super.handleRequest(request, channel, client);
+                    return;
+                }
+                ShiroSubject shiroSubject = (ShiroSubject) getCurrentSubject();
+                shiroSubject.authenticate(token);
+                // Caller was authorized, forward the request to the handler
+                super.handleRequest(request, channel, client);
+            } catch (final Exception e) {
+                final BytesRestResponse bytesRestResponse = new BytesRestResponse(RestStatus.UNAUTHORIZED, e.getMessage());
+                channel.sendResponse(bytesRestResponse);
+            }
+        }
+    }
+
     public PluginSubject getPluginSubject(Plugin plugin) {
         return new ShiroPluginSubject(threadPool);
     }
