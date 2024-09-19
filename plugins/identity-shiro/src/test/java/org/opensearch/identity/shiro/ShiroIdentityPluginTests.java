1bc81d3eee07e96b6c6393510b41d194276e204c
Craig Perkins <cwperx@amazon.com>
2024-09-19 16:55:21 -0400

    Remove identity-related feature flagged code from the RestController (#15430)
    * Add authenticate to IdentityPlugin interface
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Handle null
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Fix tests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Fix ActionModuleTests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add to CHANGELOG
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add DelegatingRestHandlerTests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Address forbiddenApi
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove authenticate from IdentityPlugin and keep RestController feature flagged code removed
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Move RestTokenExtractor to identity-shiro plugin
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove change in IdentityService
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove changes in ActionModuleTests
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add tests for RestTokenExtractor
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Remove DelegatingRestHandler
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Call super instead of keeping a reference to the delegated restHandler
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Address code review comments
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    ---------
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    Signed-off-by: Craig Perkins <craig5008@gmail.com>

--- plugins/identity-shiro/src/test/java/org/opensearch/identity/shiro/ShiroIdentityPluginTests.java
+++ plugins/identity-shiro/src/test/java/org/opensearch/identity/shiro/ShiroIdentityPluginTests.java
@@ -13,7 +13,7 @@ import org.opensearch.common.settings.Settings;
 import org.opensearch.identity.IdentityService;
 import org.opensearch.plugins.IdentityPlugin;
 import org.opensearch.test.OpenSearchTestCase;
-import org.opensearch.threadpool.TestThreadPool;
+import org.opensearch.threadpool.ThreadPool;
 
 import java.util.List;
 
@@ -21,27 +21,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
 
 public class ShiroIdentityPluginTests extends OpenSearchTestCase {
 
     public void testSingleIdentityPluginSucceeds() {
-        TestThreadPool threadPool = new TestThreadPool(getTestName());
         IdentityPlugin identityPlugin1 = new ShiroIdentityPlugin(Settings.EMPTY);
         List<IdentityPlugin> pluginList1 = List.of(identityPlugin1);
-        IdentityService identityService1 = new IdentityService(Settings.EMPTY, threadPool, pluginList1);
+        IdentityService identityService1 = new IdentityService(Settings.EMPTY, mock(ThreadPool.class), pluginList1);
         assertThat(identityService1.getTokenManager(), is(instanceOf(ShiroTokenManager.class)));
-        terminate(threadPool);
     }
 
     public void testMultipleIdentityPluginsFail() {
-        TestThreadPool threadPool = new TestThreadPool(getTestName());
         IdentityPlugin identityPlugin1 = new ShiroIdentityPlugin(Settings.EMPTY);
         IdentityPlugin identityPlugin2 = new ShiroIdentityPlugin(Settings.EMPTY);
         IdentityPlugin identityPlugin3 = new ShiroIdentityPlugin(Settings.EMPTY);
         List<IdentityPlugin> pluginList = List.of(identityPlugin1, identityPlugin2, identityPlugin3);
-        Exception ex = assertThrows(OpenSearchException.class, () -> new IdentityService(Settings.EMPTY, threadPool, pluginList));
+        Exception ex = assertThrows(
+            OpenSearchException.class,
+            () -> new IdentityService(Settings.EMPTY, mock(ThreadPool.class), pluginList)
+        );
         assert (ex.getMessage().contains("Multiple identity plugins are not supported,"));
-        terminate(threadPool);
     }
 
 }
