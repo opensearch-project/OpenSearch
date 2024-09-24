eeb2f3997bb84f33f13b848e125051ecf2c2a1c7
Ashish Singh <ssashish@amazon.com>
2024-09-24 11:07:30 +0530

    Fix flaky test in RemoteStoreMigrationSettingsUpdateIT (#16048)
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- test/framework/src/main/java/org/opensearch/test/OpenSearchIntegTestCase.java
+++ test/framework/src/main/java/org/opensearch/test/OpenSearchIntegTestCase.java
@@ -403,7 +403,7 @@ public abstract class OpenSearchIntegTestCase extends OpenSearchTestCase {
 
     private static Boolean segmentsPathFixedPrefix;
 
-    private static Boolean snapshotShardPathFixedPrefix;
+    protected static Boolean snapshotShardPathFixedPrefix;
 
     private Path remoteStoreRepositoryPath;
 
@@ -2904,7 +2904,7 @@ public abstract class OpenSearchIntegTestCase extends OpenSearchTestCase {
         settings.put(RemoteStoreSettings.CLUSTER_REMOTE_STORE_PINNED_TIMESTAMP_ENABLED.getKey(), randomBoolean());
         settings.put(RemoteStoreSettings.CLUSTER_REMOTE_STORE_SEGMENTS_PATH_PREFIX.getKey(), translogPathFixedPrefix ? "a" : "");
         settings.put(RemoteStoreSettings.CLUSTER_REMOTE_STORE_TRANSLOG_PATH_PREFIX.getKey(), segmentsPathFixedPrefix ? "b" : "");
-        settings.put(BlobStoreRepository.SNAPSHOT_SHARD_PATH_PREFIX_SETTING.getKey(), segmentsPathFixedPrefix ? "c" : "");
+        settings.put(BlobStoreRepository.SNAPSHOT_SHARD_PATH_PREFIX_SETTING.getKey(), snapshotShardPathFixedPrefix ? "c" : "");
         return settings.build();
     }
 
