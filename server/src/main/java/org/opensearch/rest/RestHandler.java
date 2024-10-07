ec978f8e2989106d5a20baf3610b9be4d2a2c981
Harsh Garg <gkharsh@amazon.com>
2024-10-07 20:33:39 +0530

    Fixing testRestHandlerWrapper UT
    Signed-off-by: Harsh Garg <gkharsh@amazon.com>

--- server/src/main/java/org/opensearch/rest/RestHandler.java
+++ server/src/main/java/org/opensearch/rest/RestHandler.java
@@ -194,6 +194,11 @@ public interface RestHandler {
         }
 
         @Override
+        public boolean isActionPaginated() {
+            return delegate.isActionPaginated();
+        }
+
+        @Override
         public boolean supportsStreaming() {
             return delegate.supportsStreaming();
         }
