2d7316bb2fc92eada5cbeaca487cc8982a0f2778
Kaushal Kumar <ravi.kaushal97@gmail.com>
2024-10-07 16:05:55 -0700

    fix precommit errors
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- server/src/main/java/org/opensearch/wlm/QueryGroupsStateAccessor.java
+++ server/src/main/java/org/opensearch/wlm/QueryGroupsStateAccessor.java
@@ -13,6 +13,10 @@ import org.opensearch.wlm.stats.QueryGroupState;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is used to decouple {@link QueryGroupService} and {@link org.opensearch.wlm.cancellation.QueryGroupTaskCancellationService} to share the
+ * {@link QueryGroupState}s
+ */
 public class QueryGroupsStateAccessor {
     // This map does not need to be concurrent since we will process the cluster state change serially and update
     // this map with new additions and deletions of entries. QueryGroupState is thread safe
@@ -26,18 +30,34 @@ public class QueryGroupsStateAccessor {
         this.queryGroupStateMap = queryGroupStateMap;
     }
 
+    /**
+     * returns the query groups state
+     */
     public Map<String, QueryGroupState> getQueryGroupStateMap() {
         return queryGroupStateMap;
     }
 
+    /**
+     * return QueryGroupState for the given queryGroupId
+     * @param queryGroupId
+     * @return QueryGroupState for the given queryGroupId, if id is invalid return default query group state
+     */
     public QueryGroupState getQueryGroupState(String queryGroupId) {
         return queryGroupStateMap.getOrDefault(queryGroupId, queryGroupStateMap.get(QueryGroupTask.DEFAULT_QUERY_GROUP_ID_SUPPLIER.get()));
     }
 
+    /**
+     * adds new QueryGroupState against given queryGroupId
+     * @param queryGroupId
+     */
     public void addNewQueryGroup(String queryGroupId) {
         this.queryGroupStateMap.putIfAbsent(queryGroupId, new QueryGroupState());
     }
 
+    /**
+     * removes QueryGroupState against given queryGroupId
+     * @param queryGroupId
+     */
     public void removeQueryGroup(String queryGroupId) {
         this.queryGroupStateMap.remove(queryGroupId);
     }
