4142a28cc4a2ef26e201b3d3e74a486186f7913e
Daniel Widdis <widdis@gmail.com>
2024-10-17 12:42:21 -0700

    Run spotless, add license header
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- libs/core/src/test/java/org/opensearch/core/common/io/stream/WriteableTests.java
+++ libs/core/src/test/java/org/opensearch/core/common/io/stream/WriteableTests.java
@@ -1,7 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
 package org.opensearch.core.common.io.stream;
 
-import org.junit.Assert;
 import org.opensearch.test.OpenSearchTestCase;
+import org.junit.Assert;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,7 +21,10 @@ public class WriteableTests extends OpenSearchTestCase {
             Writeable.WriteableRegistry.registerClassAlias(StringBuilder.class, AtomicInteger.class);
             Assert.fail("expected exception not thrown");
         } catch (IllegalArgumentException illegalArgumentException) {
-            Assert.assertEquals("Streamable custom class already registered [java.lang.StringBuilder]", illegalArgumentException.getMessage());
+            Assert.assertEquals(
+                "Streamable custom class already registered [java.lang.StringBuilder]",
+                illegalArgumentException.getMessage()
+            );
         }
     }
 }
