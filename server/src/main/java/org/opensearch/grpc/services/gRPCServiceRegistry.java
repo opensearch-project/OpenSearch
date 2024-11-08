d889ca6222075e5e575afbcea2e95205d83a87d2
Finn Carroll <carrofin@amazon.com>
2024-11-07 17:43:55 -0800

    Comment unimplemented func stubs
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- server/src/main/java/org/opensearch/grpc/services/gRPCServiceRegistry.java
+++ server/src/main/java/org/opensearch/grpc/services/gRPCServiceRegistry.java
@@ -1,28 +1,26 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
-
-package org.opensearch.grpc.services;
-
-import io.grpc.BindableService;
-import java.util.ArrayList;
-
-import org.opensearch.server.proto.action.admin.cluster.node.stats.NodesStatsProto;
-
-public class gRPCServiceRegistry {
-    private final ArrayList<BindableService> services = new ArrayList<>();
-
-    gRPCServiceRegistry() { }
-
-    public void addService(BindableService bindableService) {
-        services.add(bindableService);
-    }
-
-    public ArrayList<BindableService> getServices() {
-        return services;
-    }
-}
+///*
+// * SPDX-License-Identifier: Apache-2.0
+// *
+// * The OpenSearch Contributors require contributions made to
+// * this file be licensed under the Apache-2.0 license or a
+// * compatible open source license.
+// */
+//
+//package org.opensearch.grpc.services;
+//
+//import io.grpc.BindableService;
+//import java.util.ArrayList;
+//
+//public class gRPCServiceRegistry {
+//    private final ArrayList<BindableService> services = new ArrayList<>();
+//
+//    gRPCServiceRegistry() { }
+//
+//    public void addService(BindableService bindableService) {
+//        services.add(bindableService);
+//    }
+//
+//    public ArrayList<BindableService> getServices() {
+//        return services;
+//    }
+//}
