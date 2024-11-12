67cc9a643c1eab9df7d6838148ecd50f29fec768
Yupeng Fu <yupeng@uber.com>
2024-11-11 21:31:32 -0800

    stream poller wip
    Signed-off-by: Yupeng Fu <yupeng@uber.com>

---
+++ server/src/main/java/org/opensearch/indices/ingest/StreamPoller.java
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.indices.ingest;
+
+public interface StreamPoller {
+
+    void start();;
+
+    void pause();
+
+    void resume();
+
+    void close();
+
+    void resetPointer();
+
+    boolean isPaused();
+
+    boolean isClosed();
+
+    enum State {
+        NONE,
+        CLOSED,
+        PAUSED,
+        POLLING,
+        PROCESSING,
+    }
+}
