130291797df2bfecf305914977b8dfe4b051ff41
Craig Perkins <cwperx@amazon.com>
2024-11-22 16:42:06 -0500

    Handle NodeList
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- buildSrc/src/main/groovy/org/opensearch/gradle/plugin/OptionalDependenciesPlugin.groovy
+++ buildSrc/src/main/groovy/org/opensearch/gradle/plugin/OptionalDependenciesPlugin.groovy
@@ -79,7 +79,7 @@ class OptionalDependenciesPlugin implements Plugin<Project> {
 
                                 if (foundDep) {
                                     if (foundDep.optional) {
-                                        foundDep.optional.value = 'true'
+                                        foundDep.optional[0].value = 'true'
                                     } else {
                                         foundDep.appendNode(OPTIONAL_IDENTIFIER, 'true')
                                     }
