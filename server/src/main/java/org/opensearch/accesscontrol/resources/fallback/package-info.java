b58308e4cd8ed1daf5100d4998733e8e4917dab5
Darshit Chanpura <dchanp@amazon.com>
2024-12-17 11:28:03 -0500

    Adds missing package-info
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

---
+++ server/src/main/java/org/opensearch/accesscontrol/resources/fallback/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/**
+ * This package defines all default implementations of  Resource Sharing and Access Control which will be utilized when security is disabled
+ */
+package org.opensearch.accesscontrol.resources.fallback;
