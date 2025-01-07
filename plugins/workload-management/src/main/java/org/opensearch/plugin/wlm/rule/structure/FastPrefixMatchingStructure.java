8e4ac67595e79360ee8aa3c86a95fbf8b5cef15e
Kaushal Kumar <ravi.kaushal97@gmail.com>
2025-01-07 15:33:52 -0800

    fix javadoc errors
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- plugins/workload-management/src/main/java/org/opensearch/plugin/wlm/rule/structure/FastPrefixMatchingStructure.java
+++ plugins/workload-management/src/main/java/org/opensearch/plugin/wlm/rule/structure/FastPrefixMatchingStructure.java
@@ -10,10 +10,31 @@ package org.opensearch.plugin.wlm.rule.structure;
 
 import java.util.List;
 
+/**
+ * Common interface which exposes methods to add/search/delete Rule attributes
+ */
 public interface FastPrefixMatchingStructure {
+    /**
+     * Inserts the rule output against the attribute value denoted by key
+     * @param key
+     * @param value
+     */
     void insert(String key, String value);
 
+    /**
+     * Searches for a key in structure.
+     *
+     * @param key The key to search for.
+     * @return A list of string values associated with the key or its prefixes.
+     *         Returns an empty list if no matches are found.
+     */
     List<String> search(String key);
 
+    /**
+     * Deletes a key from the structure.
+     *
+     * @param key The key to be deleted.
+     * @return true if the key was successfully deleted, false otherwise.
+     */
     boolean delete(String key);
 }
