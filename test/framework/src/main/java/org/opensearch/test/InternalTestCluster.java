b9b4badafdbc83a3cda2333de40dffb8542bae06
Rishabh Maurya <rishabhmaurya05@gmail.com>
2025-01-19 19:46:02 -0800

    remove unnecessary imports
    Signed-off-by: Rishabh Maurya <rishabhmaurya05@gmail.com>

--- test/framework/src/main/java/org/opensearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/opensearch/test/InternalTestCluster.java
@@ -749,6 +749,7 @@ public final class InternalTestCluster extends TestCluster {
         final Settings.Builder updatedSettings = Settings.builder();
 
         updatedSettings.put(Environment.PATH_HOME_SETTING.getKey(), baseDir);
+
         if (numDataPaths > 1) {
             updatedSettings.putList(
                 Environment.PATH_DATA_SETTING.getKey(),
