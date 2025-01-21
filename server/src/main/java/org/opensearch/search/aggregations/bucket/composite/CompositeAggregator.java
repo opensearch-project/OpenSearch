491e2a70c34432a91079ffed66f6f6cef8036547
Finn Carroll <carrofin@amazon.com>
2025-01-21 09:14:25 -0800

    Spotless apply
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- server/src/main/java/org/opensearch/search/aggregations/bucket/composite/CompositeAggregator.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/composite/CompositeAggregator.java
@@ -89,7 +89,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.LongUnaryOperator;
