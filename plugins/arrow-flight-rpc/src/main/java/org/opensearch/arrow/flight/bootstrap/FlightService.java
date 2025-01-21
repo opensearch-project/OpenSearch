5b1b588f02a952067dc7bef46d3eea06744a6c19
Rishabh Maurya <rishabhmaurya05@gmail.com>
2025-01-20 18:21:01 -0800

    Remove StreamManagerWrapper and Node.java changes from PR
    Signed-off-by: Rishabh Maurya <rishabhmaurya05@gmail.com>

--- plugins/arrow-flight-rpc/src/main/java/org/opensearch/arrow/flight/bootstrap/FlightService.java
+++ plugins/arrow-flight-rpc/src/main/java/org/opensearch/arrow/flight/bootstrap/FlightService.java
@@ -8,6 +8,7 @@
 
 package org.opensearch.arrow.flight.bootstrap;
 
+import org.apache.arrow.flight.NoOpFlightProducer;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.util.AutoCloseables;
@@ -17,8 +18,6 @@ import org.apache.logging.log4j.Logger;
 import org.opensearch.arrow.flight.bootstrap.tls.DefaultSslContextProvider;
 import org.opensearch.arrow.flight.bootstrap.tls.DisabledSslContextProvider;
 import org.opensearch.arrow.flight.bootstrap.tls.SslContextProvider;
-import org.opensearch.arrow.flight.impl.BaseFlightProducer;
-import org.opensearch.arrow.flight.impl.FlightStreamManager;
 import org.opensearch.arrow.spi.StreamManager;
 import org.opensearch.client.Client;
 import org.opensearch.cluster.service.ClusterService;
@@ -105,7 +104,7 @@ public class FlightService extends NetworkPlugin.AuxTransport {
                 client
             );
             initializeStreamManager(clientManager);
-            serverComponents.setFlightProducer(new BaseFlightProducer(clientManager, (FlightStreamManager) streamManager, allocator));
+            serverComponents.setFlightProducer(new NoOpFlightProducer());
             serverComponents.start();
 
         } catch (Exception e) {
@@ -166,7 +165,6 @@ public class FlightService extends NetworkPlugin.AuxTransport {
     }
 
     private void initializeStreamManager(FlightClientManager clientManager) {
-        streamManager = new FlightStreamManager(() -> allocator);
-        ((FlightStreamManager) streamManager).setClientManager(clientManager);
+        streamManager = null;
     }
 }
