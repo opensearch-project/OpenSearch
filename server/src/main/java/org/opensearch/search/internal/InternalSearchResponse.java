8dce9b4ae8ee854e90041a774ef91674665a1da5
Junwei Dai <junweid@amazon.com>
2025-01-23 10:51:56 -0800

    bring back the constructor InternalSearchResponse
    Signed-off-by: Junwei Dai <junweid@amazon.com>

--- server/src/main/java/org/opensearch/search/internal/InternalSearchResponse.java
+++ server/src/main/java/org/opensearch/search/internal/InternalSearchResponse.java
@@ -111,6 +111,29 @@ public class InternalSearchResponse extends SearchResponseSections implements Wr
         );
     }
 
+    public InternalSearchResponse(
+        SearchHits hits,
+        InternalAggregations aggregations,
+        Suggest suggest,
+        SearchProfileShardResults profileResults,
+        boolean timedOut,
+        Boolean terminatedEarly,
+        int numReducePhases,
+        List<SearchExtBuilder> searchExtBuilderList
+    ) {
+        this(
+            hits,
+            aggregations,
+            suggest,
+            profileResults,
+            timedOut,
+            terminatedEarly,
+            numReducePhases,
+            searchExtBuilderList,
+            Collections.emptyList()
+        );
+    }
+
     public InternalSearchResponse(StreamInput in) throws IOException {
         super(
             new SearchHits(in),
