39bc545ecb6e10e7600e16398d940b4e8b2e1db1
Ruirui Zhang <mariazrr@amazon.com>
2025-01-23 13:35:31 -0800

    add get rule api logic Signed-off-by: Ruirui Zhang <mariazrr@amazon.com>

---
+++ plugins/workload-management/src/test/java/org/opensearch/plugin/wlm/rule/action/GetRuleRequestTests.java
@@ -0,0 +1,53 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.plugin.wlm.rule.action;
+
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.test.OpenSearchTestCase;
+import org.opensearch.wlm.Rule;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.opensearch.plugin.wlm.RuleTestUtils.PATTERN_ONE;
+import static org.opensearch.plugin.wlm.RuleTestUtils._ID_ONE;
+
+public class GetRuleRequestTests extends OpenSearchTestCase {
+
+    /**
+     * Test case to verify the serialization and deserialization of GetRuleRequest
+     */
+    public void testSerialization() throws IOException {
+        GetRuleRequest request = new GetRuleRequest(_ID_ONE, Map.of(Rule.RuleAttribute.INDEX_PATTERN, Set.of(PATTERN_ONE)));
+        assertEquals(_ID_ONE, request.get_id());
+        BytesStreamOutput out = new BytesStreamOutput();
+        request.writeTo(out);
+        StreamInput streamInput = out.bytes().streamInput();
+        GetRuleRequest otherRequest = new GetRuleRequest(streamInput);
+        assertEquals(request.get_id(), otherRequest.get_id());
+        assertEquals(request.getAttributeFilters(), otherRequest.getAttributeFilters());
+    }
+
+    /**
+     * Test case to verify the serialization and deserialization of GetRuleRequest when name is null
+     */
+    public void testSerializationWithNull() throws IOException {
+        GetRuleRequest request = new GetRuleRequest((String) null, new HashMap<>());
+        assertNull(request.get_id());
+        BytesStreamOutput out = new BytesStreamOutput();
+        request.writeTo(out);
+        StreamInput streamInput = out.bytes().streamInput();
+        GetRuleRequest otherRequest = new GetRuleRequest(streamInput);
+        assertEquals(request.get_id(), otherRequest.get_id());
+        assertEquals(request.getAttributeFilters(), otherRequest.getAttributeFilters());
+    }
+}
