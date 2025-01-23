0b453379aed003edbf2057746b706aae7e7e4208
Finn Carroll <carrofin@amazon.com>
2025-01-22 18:15:05 -0800

    Add SecureNetty4GrpcServerTransport.SETTING_GRPC_PORT to plugin settings.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- plugins/transport-grpc/src/main/java/org/opensearch/transport/grpc/GrpcPlugin.java
+++ plugins/transport-grpc/src/main/java/org/opensearch/transport/grpc/GrpcPlugin.java
@@ -26,7 +26,6 @@ import java.util.function.Supplier;
 
 import static org.opensearch.transport.grpc.Netty4GrpcServerTransport.SETTING_GRPC_BIND_HOST;
 import static org.opensearch.transport.grpc.Netty4GrpcServerTransport.SETTING_GRPC_HOST;
-import static org.opensearch.transport.grpc.Netty4GrpcServerTransport.SETTING_GRPC_PORT;
 import static org.opensearch.transport.grpc.Netty4GrpcServerTransport.SETTING_GRPC_PUBLISH_HOST;
 import static org.opensearch.transport.grpc.Netty4GrpcServerTransport.SETTING_GRPC_PUBLISH_PORT;
 import static org.opensearch.transport.grpc.Netty4GrpcServerTransport.SETTING_GRPC_WORKER_COUNT;
@@ -75,7 +74,8 @@ public final class GrpcPlugin extends Plugin implements NetworkPlugin {
     @Override
     public List<Setting<?>> getSettings() {
         return List.of(
-            SETTING_GRPC_PORT,
+            Netty4GrpcServerTransport.SETTING_GRPC_PORT,
+            SecureNetty4GrpcServerTransport.SETTING_GRPC_PORT,
             SETTING_GRPC_HOST,
             SETTING_GRPC_PUBLISH_HOST,
             SETTING_GRPC_BIND_HOST,
