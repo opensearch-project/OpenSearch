2afd5a7d5031582614df6dc8afb284da973b9d40
Tejas Shah <shatejas@amazon.com>
2025-01-27 12:48:15 -0800

    Adds KNN specific enums for profiling, exposes profiler in ContextIndexSearcher
    Signed-off-by: Tejas Shah <shatejas@amazon.com>

--- server/src/main/java/org/opensearch/search/profile/query/QueryTimingType.java
+++ server/src/main/java/org/opensearch/search/profile/query/QueryTimingType.java
@@ -48,7 +48,9 @@ public enum QueryTimingType {
     SCORE,
     SHALLOW_ADVANCE,
     COMPUTE_MAX_SCORE,
-    SET_MIN_COMPETITIVE_SCORE;
+    SET_MIN_COMPETITIVE_SCORE,
+    EXACT_KNN_SEARCH,
+    ANN_SEARCH;
 
     @Override
     public String toString() {
