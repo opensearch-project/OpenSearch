ba62f371d185e5580c1db1603e757abe04efc14d
xuxiong1 <xiongxug@outlook.com>
2025-02-13 23:32:35 +0800

    add tests
    Signed-off-by: xuxiong1 <xiongxug@outlook.com>

--- server/src/test/java/org/opensearch/cluster/metadata/IngestionSourceTests.java
+++ server/src/test/java/org/opensearch/cluster/metadata/IngestionSourceTests.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class IngestionSourceTests extends OpenSearchTestCase {
 
-    private final IngestionSource.PointerInitReset pointerInitReset = new IngestionSource.PointerInitReset("pointerInitReset", 0);
+    private final IngestionSource.PointerInitReset pointerInitReset = new IngestionSource.PointerInitReset("pointerInitReset", 1000L);
 
     public void testConstructorAndGetters() {
         Map<String, Object> params = new HashMap<>();
@@ -23,7 +23,8 @@ public class IngestionSourceTests extends OpenSearchTestCase {
         IngestionSource source = new IngestionSource("type", pointerInitReset, params);
 
         assertEquals("type", source.getType());
-        assertEquals("pointerInitReset", source.getPointerInitReset());
+        assertEquals("pointerInitReset", source.getPointerInitReset().getType());
+        assertEquals(1000L, source.getPointerInitReset().getValue());
         assertEquals(params, source.params());
     }
 
@@ -63,7 +64,7 @@ public class IngestionSourceTests extends OpenSearchTestCase {
         params.put("key", "value");
         IngestionSource source = new IngestionSource("type", pointerInitReset, params);
 
-        String expected = "IngestionSource{type='type',pointer_init_reset='pointerInitReset', params={key=value}}";
+        String expected = "IngestionSource{type='type',pointer_init_reset='PointerInitReset{type='pointerInitReset', value=1000}', params={key=value}}";
         assertEquals(expected, source.toString());
     }
 }
