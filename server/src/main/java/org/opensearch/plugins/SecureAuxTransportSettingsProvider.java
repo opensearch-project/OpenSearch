fa1aa41afbd3f05c11f90bd76aba8c9c9e648650
Finn Carroll <carrofin@amazon.com>
2025-02-20 15:11:00 -0800

    Javadocs for SecureTransportParameters.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- server/src/main/java/org/opensearch/plugins/SecureAuxTransportSettingsProvider.java
+++ server/src/main/java/org/opensearch/plugins/SecureAuxTransportSettingsProvider.java
@@ -30,20 +30,54 @@ public interface SecureAuxTransportSettingsProvider {
      */
     Optional<SecureAuxTransportSettingsProvider.SecureTransportParameters> parameters(Settings settings);
 
+    /**
+     * Parameters for configuring secure transport connections.
+     * Provides access to SSL/TLS configuration settings.
+     */
     @ExperimentalApi
     interface SecureTransportParameters {
+        /**
+         * Determines if dual mode is enabled for handling both TLS and plaintext connections.
+         * When enabled, the server can accept both secure and insecure connections on the same port.
+         * @return true if dual mode is enabled, false otherwise
+         */
         boolean dualModeEnabled();
 
+        /**
+         * Get the SSL provider implementation to use (e.g., "JDK", "OPENSSL").
+         * @return the name of the SSL provider
+         */
         String sslProvider();
 
+        /**
+         * Get the client authentication mode (e.g., "NONE", "OPTIONAL", "REQUIRE").
+         * Determines whether client certificates are requested/required during handshake.
+         * @return the client authentication setting
+         */
         String clientAuth();
 
+        /**
+         * Get enabled TLS protocols (e.g., "TLSv1.2", "TLSv1.3").
+         * @return the enabled protocols
+         */
         Iterable<String> protocols();
 
+        /**
+         * Get enabled cipher suites for TLS connections.
+         * @return the enabled cipher suites
+         */
         Iterable<String> cipherSuites();
 
+        /**
+         * KeyManagerFactory which manages the server's identity credentials.
+         * @return the key manager factory
+         */
         KeyManagerFactory keyManagerFactory();
 
+        /**
+         * TrustManagerFactory which determines trusted client certificates.
+         * @return the trust manager factory
+         */
         TrustManagerFactory trustManagerFactory();
     }
 }
