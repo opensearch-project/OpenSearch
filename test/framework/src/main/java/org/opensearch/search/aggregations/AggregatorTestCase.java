4e7d10378c8c64e5154fdd91b6e4979496807c2a
Asim M <asim.seng@gmail.com>
2025-02-21 22:41:35 +0000

    Introduce `execution_hint` for Cardinality aggregation (#17312) - backport to 2.19 (#17420)
    * Introduce `execution_hint` for Cardinality aggregation (#17312) - backport to 2.19
    
    ---------
    
    Signed-off-by: Siddharth Rayabharam <maitreya2954@gmail.com>
    Signed-off-by: Asim Mahmood <asim.seng@gmail.com>
    Signed-off-by: Asim M <asim.seng@gmail.com>
    Co-authored-by: Siddharth Rayabharam <maitreya2954@gmail.com>
    Co-authored-by: Craig Perkins <craig5008@gmail.com>
    (cherry picked from commit e3a6ccadc942c64e83bd224031bc4d1c6ab14623)
    
    * Update changelog
    
    Signed-off-by: Asim Mahmood <asim.seng@gmail.com>
    
    ---------
    
    Signed-off-by: Asim Mahmood <asim.seng@gmail.com>

--- test/framework/src/main/java/org/opensearch/search/aggregations/AggregatorTestCase.java
+++ test/framework/src/main/java/org/opensearch/search/aggregations/AggregatorTestCase.java
@@ -1328,6 +1328,7 @@ public abstract class AggregatorTestCase extends OpenSearchTestCase {
     protected static class CountingAggregator extends Aggregator {
         private final AtomicInteger collectCounter;
         public final Aggregator delegate;
+        private LeafBucketCollector selectedCollector;
 
         public CountingAggregator(AtomicInteger collectCounter, Aggregator delegate) {
             this.collectCounter = collectCounter;
@@ -1336,6 +1337,10 @@ public abstract class AggregatorTestCase extends OpenSearchTestCase {
 
         public AtomicInteger getCollectCount() {
             return collectCounter;
+        }
+
+        public LeafBucketCollector getSelectedCollector() {
+            return selectedCollector;
         }
 
         @Override
@@ -1378,7 +1383,8 @@ public abstract class AggregatorTestCase extends OpenSearchTestCase {
             return new LeafBucketCollector() {
                 @Override
                 public void collect(int doc, long bucket) throws IOException {
-                    delegate.getLeafCollector(ctx).collect(doc, bucket);
+                    selectedCollector = delegate.getLeafCollector(ctx);
+                    selectedCollector.collect(doc, bucket);
                     collectCounter.incrementAndGet();
                 }
             };
