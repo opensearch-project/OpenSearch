dd9acdb458e4d652c5fd545e872b9e36933a6878
Finn Carroll <carrofin@amazon.com>
2025-02-26 09:47:39 -0800

    Add list services UT.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- plugins/transport-grpc/src/test/java/org/opensearch/transport/grpc/Netty4GrpcServerTransportTests.java
+++ plugins/transport-grpc/src/test/java/org/opensearch/transport/grpc/Netty4GrpcServerTransportTests.java
@@ -8,6 +8,7 @@
 
 package org.opensearch.transport.grpc;
 
+import io.grpc.reflection.v1alpha.ServiceResponse;
 import org.opensearch.common.network.NetworkService;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.core.common.transport.TransportAddress;
@@ -48,6 +49,19 @@ public class Netty4GrpcServerTransportTests extends OpenSearchTestCase {
             final TransportAddress remoteAddress = randomFrom(transport.getBoundAddress().boundAddresses());
             try (NettyGrpcClient client = new NettyGrpcClient.Builder().setAddress(remoteAddress).build()) {
                 assertEquals(client.checkHealth(), HealthCheckResponse.ServingStatus.SERVING);
+            }
+            transport.stop();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void testGrpcTransportListServices() {
+        try (Netty4GrpcServerTransport transport = new Netty4GrpcServerTransport(createSettings(), services, networkService)) {
+            transport.start();
+            final TransportAddress remoteAddress = randomFrom(transport.getBoundAddress().boundAddresses());
+            try (NettyGrpcClient client = new NettyGrpcClient.Builder().setAddress(remoteAddress).build()) {
+                assertTrue(client.listServices().size() > 1);
             }
             transport.stop();
         } catch (Exception e) {
