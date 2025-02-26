0c4f27d49cc1f0e6e3403a33ff2426752acc1639
Rajat Gupta <gptrajat@amazon.com>
2025-02-26 10:02:29 -0800

    Modified terminate script
    Signed-off-by: Rajat Gupta <gptrajat@amazon.com>

--- qa/systemd-test/src/test/resources/scripts/terminate.sh
+++ qa/systemd-test/src/test/resources/scripts/terminate.sh
@@ -10,11 +10,3 @@ if kill -15 $1 2>/dev/null; then
 else
     echo "Failed to send SIGTERM to process $1"
 fi
-
-sleep 2
-
-if kill -0 $1 2>/dev/null; then
-    echo "Process $1 is still running"
-else
-    echo "Process $1 has terminated"
-fi
