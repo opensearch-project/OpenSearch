7ab3cba63182d5c43040cb932f1b74d6cc3b1854
Aleksandr Tuliakov <tulyakov@yandex-team.ru>
2025-03-05 15:19:39 +0300

    Replace working with exceptions with custom comparator
    Signed-off-by: Aleksandr Tuliakov <tulyakov@yandex-team.ru>

---
+++ server/src/main/java/org/apache/lucene/codecs/lucene90/Lucene90StoredFieldsFormatComparator.java
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.apache.lucene.codecs.lucene90;
+
+/**
+ * This class is a custom comparator for Lucene90StoredFieldsFormat
+ */
+public class Lucene90StoredFieldsFormatComparator {
+    public static boolean equal(Lucene90StoredFieldsFormat one, Lucene90StoredFieldsFormat two) {
+        return one.mode == two.mode;
+    }
+}
