c83500db863e6804f5922f1842ec13ff51d1fb82
Ruirui Zhang <mariazrr@amazon.com>
2025-03-12 13:52:18 -0700

    add update rule api logic Signed-off-by: Ruirui Zhang <mariazrr@amazon.com>

---
+++ plugins/workload-management/src/main/java/org/opensearch/plugin/wlm/querygroup/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/**
+ * This package holds constructs for the Query Group's in-memory storage and processing with the cluster state
+ */
+package org.opensearch.plugin.wlm.querygroup;
