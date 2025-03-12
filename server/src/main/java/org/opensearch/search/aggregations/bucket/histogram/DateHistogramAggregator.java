a3827ce0b0139420d422510d3fe436af4368b2d9
shreyah963 <shreyab963@gmail.com>
2025-03-12 11:58:37 -0700

    Added Singleton DocValue Functionality
    Signed-off-by: shreyah963 <shreyab963@gmail.com>

--- server/src/main/java/org/opensearch/search/aggregations/bucket/histogram/DateHistogramAggregator.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/histogram/DateHistogramAggregator.java
@@ -31,6 +31,8 @@
 
 package org.opensearch.search.aggregations.bucket.histogram;
 
+import org.apache.lucene.index.DocValues;
+import org.apache.lucene.index.NumericDocValues;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.SortedNumericDocValues;
 import org.apache.lucene.search.ScoreMode;
@@ -195,19 +197,33 @@ class DateHistogramAggregator extends BucketsAggregator implements SizedBucketAg
         return filterRewriteOptimizationContext.tryOptimize(ctx, this::incrementBucketDocCount, segmentMatchAll(context, ctx));
     }
 
-    @Override
     public LeafBucketCollector getLeafCollector(LeafReaderContext ctx, LeafBucketCollector sub) throws IOException {
         if (valuesSource == null) {
             return LeafBucketCollector.NO_OP_COLLECTOR;
         }
 
-        SortedNumericDocValues values = valuesSource.longValues(ctx);
+        final SortedNumericDocValues values = valuesSource.longValues(ctx);
+        final NumericDocValues singleton = DocValues.unwrapSingleton(values);
+
+        if (singleton != null) {
+            // Optimized path for single-valued fields
+            return new LeafBucketCollectorBase(sub, values) {
+                @Override
+                public void collect(int doc, long owningBucketOrd) throws IOException {
+                    if (singleton.advanceExact(doc)) {
+                        long value = singleton.longValue();
+                        collectValue(sub, doc, owningBucketOrd, value);
+                    }
+                }
+            };
+        }
+
+        // Original path for multi-valued fields
         return new LeafBucketCollectorBase(sub, values) {
             @Override
             public void collect(int doc, long owningBucketOrd) throws IOException {
                 if (values.advanceExact(doc)) {
                     int valuesCount = values.docValueCount();
-
                     long previousRounded = Long.MIN_VALUE;
                     for (int i = 0; i < valuesCount; ++i) {
                         long value = values.nextValue();
@@ -216,20 +232,25 @@ class DateHistogramAggregator extends BucketsAggregator implements SizedBucketAg
                         if (rounded == previousRounded) {
                             continue;
                         }
-                        if (hardBounds == null || hardBounds.contain(rounded)) {
-                            long bucketOrd = bucketOrds.add(owningBucketOrd, rounded);
-                            if (bucketOrd < 0) { // already seen
-                                bucketOrd = -1 - bucketOrd;
-                                collectExistingBucket(sub, doc, bucketOrd);
-                            } else {
-                                collectBucket(sub, doc, bucketOrd);
-                            }
-                        }
+                        collectValue(sub, doc, owningBucketOrd, value);
                         previousRounded = rounded;
                     }
                 }
             }
         };
+    }
+
+    private void collectValue(LeafBucketCollector sub, int doc, long owningBucketOrd, long value) throws IOException {
+        long rounded = preparedRounding.round(value);
+        if (hardBounds == null || hardBounds.contain(rounded)) {
+            long bucketOrd = bucketOrds.add(owningBucketOrd, rounded);
+            if (bucketOrd < 0) { // already seen
+                bucketOrd = -1 - bucketOrd;
+                collectExistingBucket(sub, doc, bucketOrd);
+            } else {
+                collectBucket(sub, doc, bucketOrd);
+            }
+        }
     }
 
     private String fetchStarTreeCalendarUnit() {
