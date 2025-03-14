439943288ae61173ba2704821560e3a21c338742
Varun Bharadwaj <varunbharadwaj1995@gmail.com>
2025-03-14 08:59:44 -0700

    Handle race condition on calling flush before poller is initialized
    Signed-off-by: Varun Bharadwaj <varunbharadwaj1995@gmail.com>

--- server/src/main/java/org/opensearch/index/engine/IngestionEngine.java
+++ server/src/main/java/org/opensearch/index/engine/IngestionEngine.java
@@ -215,8 +215,14 @@ public class IngestionEngine extends InternalEngine {
                 commitData.put(HISTORY_UUID_KEY, historyUUID);
                 commitData.put(Engine.MIN_RETAINED_SEQNO, Long.toString(softDeletesPolicy.getMinRetainedSeqNo()));
 
-                // ingestion engine needs to record batch start pointer
-                commitData.put(StreamPoller.BATCH_START, streamPoller.getBatchStartPointer().asString());
+                /*
+                 * Ingestion engine needs to record batch start pointer.
+                 * Batch start pointer can be null at index creation time, if flush is called before the stream
+                 * poller has been completely initialized.
+                 */
+                if (streamPoller.getBatchStartPointer() != null) {
+                    commitData.put(StreamPoller.BATCH_START, streamPoller.getBatchStartPointer().asString());
+                }
                 final String currentForceMergeUUID = forceMergeUUID;
                 if (currentForceMergeUUID != null) {
                     commitData.put(FORCE_MERGE_UUID_KEY, currentForceMergeUUID);
