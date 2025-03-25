488467cd5b90d051aed99cfe8346afd7da2276c5
Michael Froh <froh@amazon.com>
2025-03-24 17:08:44 -0700

    Delegate TransportUpdateAction to TransportBulkAction
    This is consistent with TransportIndexAction and TransportDeleteAction,
    and ensures that updates behave consistently, whether sent via the
    `_update` API or via the `_bulk` API.
    
    Signed-off-by: Michael Froh <froh@amazon.com>

--- server/src/main/java/org/opensearch/action/update/TransportUpdateAction.java
+++ server/src/main/java/org/opensearch/action/update/TransportUpdateAction.java
@@ -32,121 +32,35 @@
 
 package org.opensearch.action.update;
 
-import org.opensearch.ExceptionsHelper;
-import org.opensearch.ResourceAlreadyExistsException;
-import org.opensearch.action.ActionRunnable;
-import org.opensearch.action.DocWriteRequest;
 import org.opensearch.action.RoutingMissingException;
-import org.opensearch.action.admin.indices.create.CreateIndexRequest;
-import org.opensearch.action.admin.indices.create.CreateIndexResponse;
-import org.opensearch.action.delete.DeleteRequest;
-import org.opensearch.action.delete.DeleteResponse;
-import org.opensearch.action.index.IndexRequest;
-import org.opensearch.action.index.IndexResponse;
+import org.opensearch.action.bulk.BulkItemResponse;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.TransportBulkAction;
 import org.opensearch.action.support.ActionFilters;
-import org.opensearch.action.support.AutoCreateIndex;
-import org.opensearch.action.support.TransportActions;
-import org.opensearch.action.support.single.instance.TransportInstanceSingleOperationAction;
-import org.opensearch.cluster.ClusterState;
-import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.action.support.HandledTransportAction;
+import org.opensearch.action.support.WriteRequest;
 import org.opensearch.cluster.metadata.Metadata;
-import org.opensearch.cluster.routing.PlainShardIterator;
-import org.opensearch.cluster.routing.ShardIterator;
-import org.opensearch.cluster.routing.ShardRouting;
-import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.collect.Tuple;
 import org.opensearch.common.inject.Inject;
-import org.opensearch.common.logging.DeprecationLogger;
-import org.opensearch.common.settings.Settings;
-import org.opensearch.common.xcontent.XContentHelper;
 import org.opensearch.core.action.ActionListener;
-import org.opensearch.core.common.bytes.BytesReference;
-import org.opensearch.core.common.io.stream.NotSerializableExceptionWrapper;
-import org.opensearch.core.common.io.stream.StreamInput;
-import org.opensearch.core.index.shard.ShardId;
-import org.opensearch.core.rest.RestStatus;
-import org.opensearch.core.xcontent.MediaType;
-import org.opensearch.index.IndexNotFoundException;
-import org.opensearch.index.IndexService;
-import org.opensearch.index.IndexSettings;
-import org.opensearch.index.engine.VersionConflictEngineException;
-import org.opensearch.index.shard.IndexShard;
-import org.opensearch.index.shard.IndexingStats.Stats.DocStatusStats;
-import org.opensearch.indices.IndicesService;
 import org.opensearch.tasks.Task;
-import org.opensearch.threadpool.ThreadPool;
-import org.opensearch.threadpool.ThreadPool.Names;
 import org.opensearch.transport.TransportService;
-import org.opensearch.transport.client.node.NodeClient;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-
-import static org.opensearch.ExceptionsHelper.unwrapCause;
-import static org.opensearch.action.bulk.TransportSingleItemBulkWriteAction.toSingleItemBulkRequest;
-import static org.opensearch.action.bulk.TransportSingleItemBulkWriteAction.wrapBulkResponse;
 
 /**
- * Transport action for updating an index
+ * Performs the update operation.
+ * <p>
+ * Deprecated use TransportBulkAction with a single item instead
  *
  * @opensearch.internal
  */
-public class TransportUpdateAction extends TransportInstanceSingleOperationAction<UpdateRequest, UpdateResponse> {
-    private static final DeprecationLogger deprecationLogger = DeprecationLogger.getLogger(TransportUpdateAction.class);
-    private final AutoCreateIndex autoCreateIndex;
-    private final UpdateHelper updateHelper;
-    private final IndicesService indicesService;
-    private final NodeClient client;
-    private final ClusterService clusterService;
+@Deprecated
+public class TransportUpdateAction extends HandledTransportAction<UpdateRequest, UpdateResponse> {
+
+    private final TransportBulkAction bulkAction;
 
     @Inject
-    public TransportUpdateAction(
-        ThreadPool threadPool,
-        ClusterService clusterService,
-        TransportService transportService,
-        UpdateHelper updateHelper,
-        ActionFilters actionFilters,
-        IndexNameExpressionResolver indexNameExpressionResolver,
-        IndicesService indicesService,
-        AutoCreateIndex autoCreateIndex,
-        NodeClient client
-    ) {
-        super(
-            UpdateAction.NAME,
-            threadPool,
-            clusterService,
-            transportService,
-            actionFilters,
-            indexNameExpressionResolver,
-            UpdateRequest::new
-        );
-        this.updateHelper = updateHelper;
-        this.indicesService = indicesService;
-        this.autoCreateIndex = autoCreateIndex;
-        this.client = client;
-        this.clusterService = clusterService;
-    }
-
-    @Override
-    protected String executor(ShardId shardId) {
-        final IndexService indexService = indicesService.indexServiceSafe(shardId.getIndex());
-        return indexService.getIndexSettings().getIndexMetadata().isSystem() ? Names.SYSTEM_WRITE : Names.WRITE;
-    }
-
-    @Override
-    protected UpdateResponse newResponse(StreamInput in) throws IOException {
-        return new UpdateResponse(in);
-    }
-
-    @Override
-    protected boolean retryOnFailure(Exception e) {
-        return TransportActions.isShardNotAvailableException(e);
-    }
-
-    @Override
-    protected void resolveRequest(ClusterState state, UpdateRequest request) {
-        resolveAndValidateRouting(state.metadata(), request.concreteIndex(), request);
+    public TransportUpdateAction(TransportService transportService, ActionFilters actionFilters, TransportBulkAction bulkAction) {
+        super(UpdateAction.NAME, transportService, actionFilters, UpdateRequest::new);
+        this.bulkAction = bulkAction;
     }
 
     public static void resolveAndValidateRouting(Metadata metadata, String concreteIndex, UpdateRequest request) {
@@ -158,240 +72,23 @@ public class TransportUpdateAction extends TransportInstanceSingleOperationActio
     }
 
     @Override
-    protected void doExecute(Task task, final UpdateRequest request, final ActionListener<UpdateResponse> listener) {
-        if (request.isRequireAlias() && (clusterService.state().getMetadata().hasAlias(request.index()) == false)) {
-            IndexNotFoundException e = new IndexNotFoundException(
-                "[" + DocWriteRequest.REQUIRE_ALIAS + "] request flag is [true] and [" + request.index() + "] is not an alias",
-                request.index()
-            );
-
-            incDocStatusStats(e);
-            throw e;
-        }
-        // if we don't have a master, we don't have metadata, that's fine, let it find a cluster-manager using create index API
-        if (autoCreateIndex.shouldAutoCreate(request.index(), clusterService.state())) {
-            client.admin()
-                .indices()
-                .create(
-                    new CreateIndexRequest().index(request.index()).cause("auto(update api)").clusterManagerNodeTimeout(request.timeout()),
-                    new ActionListener<CreateIndexResponse>() {
-                        @Override
-                        public void onResponse(CreateIndexResponse result) {
-                            innerExecute(task, request, listener);
-                        }
-
-                        @Override
-                        public void onFailure(Exception e) {
-                            if (unwrapCause(e) instanceof ResourceAlreadyExistsException) {
-                                // we have the index, do it
-                                try {
-                                    innerExecute(task, request, listener);
-                                } catch (Exception inner) {
-                                    inner.addSuppressed(e);
-                                    listener.onFailure(inner);
-                                }
-                            } else {
-                                listener.onFailure(e);
-                            }
-                        }
-                    }
-                );
-        } else {
-            innerExecute(task, request, listener);
-        }
-    }
-
-    private void innerExecute(final Task task, final UpdateRequest request, final ActionListener<UpdateResponse> listener) {
-        super.doExecute(task, request, ActionListener.wrap(listener::onResponse, e -> {
-            incDocStatusStats(e);
-            listener.onFailure(e);
-        }));
-    }
-
-    @Override
-    protected ShardIterator shards(ClusterState clusterState, UpdateRequest request) {
-        if (request.getShardId() != null) {
-            return clusterState.routingTable().index(request.concreteIndex()).shard(request.getShardId().getId()).primaryShardIt();
-        }
-        ShardIterator shardIterator = clusterService.operationRouting()
-            .indexShards(clusterState, request.concreteIndex(), request.id(), request.routing());
-        ShardRouting shard;
-        while ((shard = shardIterator.nextOrNull()) != null) {
-            if (shard.primary()) {
-                return new PlainShardIterator(shardIterator.shardId(), Collections.singletonList(shard));
+    protected void doExecute(Task task, UpdateRequest request, ActionListener<UpdateResponse> listener) {
+        // The following is mostly copied from TransportSingleItemBulkAction
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(request);
+        bulkRequest.setRefreshPolicy(request.getRefreshPolicy());
+        bulkRequest.timeout(request.timeout());
+        bulkRequest.waitForActiveShards(request.waitForActiveShards());
+        request.setRefreshPolicy(WriteRequest.RefreshPolicy.NONE);
+        bulkAction.execute(task, bulkRequest, ActionListener.wrap(bulkItemResponses -> {
+            assert bulkItemResponses.getItems().length == 1 : "expected only one item in bulk request";
+            BulkItemResponse bulkItemResponse = bulkItemResponses.getItems()[0];
+            if (bulkItemResponse.isFailed() == false) {
+                final UpdateResponse response = bulkItemResponse.getResponse();
+                listener.onResponse(response);
+            } else {
+                listener.onFailure(bulkItemResponse.getFailure().getCause());
             }
-        }
-        return new PlainShardIterator(shardIterator.shardId(), Collections.emptyList());
-    }
-
-    @Override
-    protected void shardOperation(final UpdateRequest request, final ActionListener<UpdateResponse> listener) {
-        shardOperation(request, listener, 0);
-    }
-
-    protected void shardOperation(final UpdateRequest request, final ActionListener<UpdateResponse> listener, final int retryCount) {
-        final ShardId shardId = request.getShardId();
-        final IndexService indexService = indicesService.indexServiceSafe(shardId.getIndex());
-        final IndexShard indexShard = indexService.getShard(shardId.getId());
-        final UpdateHelper.Result result = updateHelper.prepare(request, indexShard, threadPool::absoluteTimeInMillis);
-        switch (result.getResponseResult()) {
-            case CREATED:
-                IndexRequest upsertRequest = result.action();
-                // we fetch it from the index request so we don't generate the bytes twice, its already done in the index request
-                final BytesReference upsertSourceBytes = upsertRequest.source();
-                client.bulk(toSingleItemBulkRequest(upsertRequest), wrapBulkResponse(ActionListener.<IndexResponse>wrap(response -> {
-                    UpdateResponse update = new UpdateResponse(
-                        response.getShardInfo(),
-                        response.getShardId(),
-                        response.getId(),
-                        response.getSeqNo(),
-                        response.getPrimaryTerm(),
-                        response.getVersion(),
-                        response.getResult()
-                    );
-                    if (request.fetchSource() != null && request.fetchSource().fetchSource()) {
-                        Tuple<? extends MediaType, Map<String, Object>> sourceAndContent = XContentHelper.convertToMap(
-                            upsertSourceBytes,
-                            true,
-                            upsertRequest.getContentType()
-                        );
-                        update.setGetResult(
-                            UpdateHelper.extractGetResult(
-                                request,
-                                request.concreteIndex(),
-                                response.getSeqNo(),
-                                response.getPrimaryTerm(),
-                                response.getVersion(),
-                                sourceAndContent.v2(),
-                                sourceAndContent.v1(),
-                                upsertSourceBytes
-                            )
-                        );
-                    } else {
-                        update.setGetResult(null);
-                    }
-                    update.setForcedRefresh(response.forcedRefresh());
-                    listener.onResponse(update);
-                }, exception -> handleUpdateFailureWithRetry(listener, request, exception, retryCount))));
-
-                break;
-            case UPDATED:
-                IndexRequest indexRequest = result.action();
-                // we fetch it from the index request so we don't generate the bytes twice, its already done in the index request
-                final BytesReference indexSourceBytes = indexRequest.source();
-                final Settings indexSettings = indexService.getIndexSettings().getSettings();
-                if (IndexSettings.DEFAULT_PIPELINE.exists(indexSettings) || IndexSettings.FINAL_PIPELINE.exists(indexSettings)) {
-                    deprecationLogger.deprecate(
-                        "update_operation_with_ingest_pipeline",
-                        "the index ["
-                            + indexRequest.index()
-                            + "] has a default ingest pipeline or a final ingest pipeline, the support of the ingest pipelines for update operation causes unexpected result and will be removed in 3.0.0"
-                    );
-                }
-                client.bulk(toSingleItemBulkRequest(indexRequest), wrapBulkResponse(ActionListener.<IndexResponse>wrap(response -> {
-                    UpdateResponse update = new UpdateResponse(
-                        response.getShardInfo(),
-                        response.getShardId(),
-                        response.getId(),
-                        response.getSeqNo(),
-                        response.getPrimaryTerm(),
-                        response.getVersion(),
-                        response.getResult()
-                    );
-                    update.setGetResult(
-                        UpdateHelper.extractGetResult(
-                            request,
-                            request.concreteIndex(),
-                            response.getSeqNo(),
-                            response.getPrimaryTerm(),
-                            response.getVersion(),
-                            result.updatedSourceAsMap(),
-                            result.updateSourceContentType(),
-                            indexSourceBytes
-                        )
-                    );
-                    update.setForcedRefresh(response.forcedRefresh());
-                    listener.onResponse(update);
-                }, exception -> handleUpdateFailureWithRetry(listener, request, exception, retryCount))));
-                break;
-            case DELETED:
-                DeleteRequest deleteRequest = result.action();
-                client.bulk(toSingleItemBulkRequest(deleteRequest), wrapBulkResponse(ActionListener.<DeleteResponse>wrap(response -> {
-                    UpdateResponse update = new UpdateResponse(
-                        response.getShardInfo(),
-                        response.getShardId(),
-                        response.getId(),
-                        response.getSeqNo(),
-                        response.getPrimaryTerm(),
-                        response.getVersion(),
-                        response.getResult()
-                    );
-                    update.setGetResult(
-                        UpdateHelper.extractGetResult(
-                            request,
-                            request.concreteIndex(),
-                            response.getSeqNo(),
-                            response.getPrimaryTerm(),
-                            response.getVersion(),
-                            result.updatedSourceAsMap(),
-                            result.updateSourceContentType(),
-                            null
-                        )
-                    );
-                    update.setForcedRefresh(response.forcedRefresh());
-                    listener.onResponse(update);
-                }, exception -> handleUpdateFailureWithRetry(listener, request, exception, retryCount))));
-                break;
-            case NOOP:
-                UpdateResponse update = result.action();
-                IndexService indexServiceOrNull = indicesService.indexService(shardId.getIndex());
-                if (indexServiceOrNull != null) {
-                    IndexShard shard = indexService.getShardOrNull(shardId.getId());
-                    if (shard != null) {
-                        shard.noopUpdate();
-                    }
-                }
-
-                DocStatusStats stats = new DocStatusStats();
-                stats.inc(RestStatus.OK);
-
-                indicesService.addDocStatusStats(stats);
-                listener.onResponse(update);
-
-                break;
-            default:
-                throw new IllegalStateException("Illegal result " + result.getResponseResult());
-        }
-    }
-
-    private void handleUpdateFailureWithRetry(
-        final ActionListener<UpdateResponse> listener,
-        final UpdateRequest request,
-        final Exception failure,
-        int retryCount
-    ) {
-        final Throwable cause = unwrapCause(failure);
-        if (cause instanceof VersionConflictEngineException) {
-            if (retryCount < request.retryOnConflict()) {
-                logger.trace(
-                    "Retry attempt [{}] of [{}] on version conflict on [{}][{}][{}]",
-                    retryCount + 1,
-                    request.retryOnConflict(),
-                    request.index(),
-                    request.getShardId(),
-                    request.id()
-                );
-                threadPool.executor(executor(request.getShardId()))
-                    .execute(ActionRunnable.wrap(listener, l -> shardOperation(request, l, retryCount + 1)));
-                return;
-            }
-        }
-        listener.onFailure(cause instanceof Exception ? (Exception) cause : new NotSerializableExceptionWrapper(cause));
-    }
-
-    private void incDocStatusStats(final Exception e) {
-        DocStatusStats stats = new DocStatusStats();
-        stats.inc(ExceptionsHelper.status(e));
-        indicesService.addDocStatusStats(stats);
+        }, listener::onFailure));
     }
 }
