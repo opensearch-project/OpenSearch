cd072e3604aa84e6ec941e3d081e9bf3c50c7195
Gulshan <kumargu@amazon.com>
2025-04-01 20:54:25 +0530

    Add a policy parser for java agent
    Signed-off-by: Gulshan <kumargu@amazon.com>

---
+++ libs/agent-sm/agent-policy/src/main/java/org/opensearch/secure_sm/policy/GrantEntry.java
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.secure_sm.policy;
+
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class GrantEntry {
+    public String codeBase;
+    private final LinkedList<PermissionEntry> permissionEntries = new LinkedList<>();
+
+    public void add(PermissionEntry entry) {
+        permissionEntries.add(entry);
+    }
+
+    public List<PermissionEntry> permissionElements() {
+        return Collections.unmodifiableList(permissionEntries);
+    }
+
+    public void write(PrintWriter out) {
+        out.print("grant");
+        if (codeBase != null) {
+            out.print(" Codebase \"");
+            out.print(codeBase);
+            out.print("\"");
+        }
+        out.println(" {");
+        for (PermissionEntry pe : permissionEntries) {
+            out.print("  permission ");
+            out.print(pe.permission);
+            if (pe.name != null) {
+                out.print(" \"");
+                out.print(pe.name);
+                out.print("\"");
+            }
+            if (pe.action != null) {
+                out.print(", \"");
+                out.print(pe.action);
+                out.print("\"");
+            }
+            out.println(";");
+        }
+        out.println("};");
+    }
+}
