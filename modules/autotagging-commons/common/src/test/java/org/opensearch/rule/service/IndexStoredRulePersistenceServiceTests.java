89f1d01a0cfc8f62ab3640ad01c8c89751eb9c96
Ruirui Zhang <mariazrr@amazon.com>
2025-04-03 13:17:23 -0700

    modify based on comments Signed-off-by: Ruirui Zhang <mariazrr@amazon.com>

--- modules/autotagging-commons/common/src/test/java/org/opensearch/rule/service/IndexStoredRulePersistenceServiceTests.java
+++ modules/autotagging-commons/common/src/test/java/org/opensearch/rule/service/IndexStoredRulePersistenceServiceTests.java
@@ -33,10 +33,14 @@ import org.opensearch.rule.RulePersistenceService;
 import org.opensearch.rule.RuleQueryMapper;
 import org.opensearch.rule.autotagging.Rule;
 import org.opensearch.rule.utils.RuleTestUtils;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.common.bytes.BytesArray;
+import org.opensearch.core.rest.RestStatus;
+import org.opensearch.index.query.QueryBuilder;
+import org.opensearch.rule.action.GetRuleResponse;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.SearchHits;
 import org.opensearch.test.OpenSearchTestCase;
-import org.opensearch.threadpool.ThreadPool;
 import org.opensearch.transport.client.Client;
 
 import java.util.HashMap;
@@ -44,14 +48,18 @@ import java.util.HashMap;
 import org.mockito.ArgumentCaptor;
 
 import static org.opensearch.rule.XContentRuleParserTests.VALID_JSON;
-import static org.opensearch.rule.utils.RuleTestUtils.TEST_INDEX_NAME;
-import static org.opensearch.rule.utils.RuleTestUtils._ID_ONE;
+import static org.opensearch.rule.RuleTestUtils.TEST_INDEX_NAME;
+import static org.opensearch.rule.RuleTestUtils._ID_ONE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.opensearch.rule.RuleTestUtils.ATTRIBUTE_MAP;
+import static org.opensearch.rule.RuleTestUtils.TEST_INDEX_NAME;
+import static org.opensearch.rule.RuleTestUtils._ID_ONE;
+import static org.opensearch.rule.RuleTestUtils.setUpIndexStoredRulePersistenceService;
+import static org.opensearch.rule.utils.IndexStoredRuleParserTests.VALID_JSON;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -59,32 +67,8 @@ import static org.mockito.Mockito.when;
 @SuppressWarnings("unchecked")
 public class IndexStoredRulePersistenceServiceTests extends OpenSearchTestCase {
 
-    public static final int MAX_VALUES_PER_PAGE = 50;
-
     public void testGetRuleByIdSuccess() {
-        GetRuleRequest getRuleRequest = mock(GetRuleRequest.class);
-        when(getRuleRequest.getId()).thenReturn(_ID_ONE);
-        when(getRuleRequest.getAttributeFilters()).thenReturn(new HashMap<>());
-        QueryBuilder queryBuilder = mock(QueryBuilder.class);
-        RuleQueryMapper<QueryBuilder> mockRuleQueryMapper = mock(RuleQueryMapper.class);
-        RuleEntityParser mockRuleEntityParser = mock(RuleEntityParser.class);
-        Rule mockRule = mock(Rule.class);
-
-        when(mockRuleEntityParser.parse(anyString())).thenReturn(mockRule);
-        when(mockRuleQueryMapper.from(getRuleRequest)).thenReturn(queryBuilder);
-        when(queryBuilder.filter(any())).thenReturn(queryBuilder);
-
-        SearchRequestBuilder searchRequestBuilder = mock(SearchRequestBuilder.class);
-        Client client = setUpMockClient(searchRequestBuilder);
-
-        RulePersistenceService rulePersistenceService = new IndexStoredRulePersistenceService(
-            TEST_INDEX_NAME,
-            client,
-            MAX_VALUES_PER_PAGE,
-            mockRuleEntityParser,
-            mockRuleQueryMapper
-        );
-
+        IndexStoredRulePersistenceService rulePersistenceService = setUpIndexStoredRulePersistenceService(new HashMap<>());
         SearchResponse searchResponse = mock(SearchResponse.class);
         SearchHits searchHits = new SearchHits(new SearchHit[] { new SearchHit(1) }, new TotalHits(1, TotalHits.Relation.EQUAL_TO), 1.0f);
         when(searchResponse.getHits()).thenReturn(searchHits);
@@ -92,58 +76,22 @@ public class IndexStoredRulePersistenceServiceTests extends OpenSearchTestCase {
         hit.sourceRef(new BytesArray(VALID_JSON));
 
         ActionListener<GetRuleResponse> listener = mock(ActionListener.class);
-
-        doAnswer((invocation) -> {
-            ActionListener<SearchResponse> actionListener = invocation.getArgument(0);
-            actionListener.onResponse(searchResponse);
-            return null;
-        }).when(searchRequestBuilder).execute(any(ActionListener.class));
-
-        when(getRuleRequest.getFeatureType()).thenReturn(RuleTestUtils.MockRuleFeatureType.INSTANCE);
-        rulePersistenceService.getRule(getRuleRequest, listener);
+        rulePersistenceService.handleGetRuleResponse(_ID_ONE, searchResponse, listener);
 
         ArgumentCaptor<GetRuleResponse> responseCaptor = ArgumentCaptor.forClass(GetRuleResponse.class);
         verify(listener).onResponse(responseCaptor.capture());
         GetRuleResponse response = responseCaptor.getValue();
         assertEquals(response.getRules().size(), 1);
+        assertEquals(RestStatus.OK, response.getRestStatus());
     }
 
     public void testGetRuleByIdNotFound() {
-        GetRuleRequest getRuleRequest = mock(GetRuleRequest.class);
-        when(getRuleRequest.getId()).thenReturn(_ID_ONE);
-        QueryBuilder queryBuilder = mock(QueryBuilder.class);
-        RuleQueryMapper<QueryBuilder> mockRuleQueryMapper = mock(RuleQueryMapper.class);
-        RuleEntityParser mockRuleEntityParser = mock(RuleEntityParser.class);
-        Rule mockRule = mock(Rule.class);
-
-        when(mockRuleEntityParser.parse(anyString())).thenReturn(mockRule);
-        when(mockRuleQueryMapper.from(getRuleRequest)).thenReturn(queryBuilder);
-        when(queryBuilder.filter(any())).thenReturn(queryBuilder);
-
-        SearchRequestBuilder searchRequestBuilder = mock(SearchRequestBuilder.class);
-        Client client = setUpMockClient(searchRequestBuilder);
-
-        RulePersistenceService rulePersistenceService = new IndexStoredRulePersistenceService(
-            TEST_INDEX_NAME,
-            client,
-            MAX_VALUES_PER_PAGE,
-            mockRuleEntityParser,
-            mockRuleQueryMapper
-        );
-
+        IndexStoredRulePersistenceService rulePersistenceService = setUpIndexStoredRulePersistenceService(new HashMap<>());
         SearchResponse searchResponse = mock(SearchResponse.class);
         when(searchResponse.getHits()).thenReturn(new SearchHits(new SearchHit[] {}, new TotalHits(0, TotalHits.Relation.EQUAL_TO), 1.0f));
 
         ActionListener<GetRuleResponse> listener = mock(ActionListener.class);
-
-        doAnswer(invocationOnMock -> {
-            ActionListener<SearchResponse> actionListener = invocationOnMock.getArgument(0);
-            actionListener.onResponse(searchResponse);
-            return null;
-        }).when(searchRequestBuilder).execute(any(ActionListener.class));
-
-        when(getRuleRequest.getFeatureType()).thenReturn(RuleTestUtils.MockRuleFeatureType.INSTANCE);
-        rulePersistenceService.getRule(getRuleRequest, listener);
+        rulePersistenceService.handleGetRuleResponse(_ID_ONE, searchResponse, listener);
 
         ArgumentCaptor<Exception> exceptionCaptor = ArgumentCaptor.forClass(Exception.class);
         verify(listener).onFailure(exceptionCaptor.capture());
@@ -151,24 +99,17 @@ public class IndexStoredRulePersistenceServiceTests extends OpenSearchTestCase {
         assertTrue(exception instanceof ResourceNotFoundException);
     }
 
-    private Client setUpMockClient(SearchRequestBuilder searchRequestBuilder) {
-        Client client = mock(Client.class);
-        ClusterService clusterService = mock(ClusterService.class);
-        ClusterState clusterState = mock(ClusterState.class);
-        Metadata metadata = mock(Metadata.class);
-        ThreadPool threadPool = mock(ThreadPool.class);
-
-        ThreadContext threadContext = new ThreadContext(Settings.EMPTY);
-        when(client.threadPool()).thenReturn(threadPool);
-        when(threadPool.getThreadContext()).thenReturn(threadContext);
-        when(clusterService.state()).thenReturn(clusterState);
-        when(clusterState.metadata()).thenReturn(metadata);
-
+    public void testGetRuleWithAttributes() {
+        IndexStoredRulePersistenceService rulePersistenceService = setUpIndexStoredRulePersistenceService(new HashMap<>());
+        ActionListener<GetRuleResponse> listener = mock(ActionListener.class);
+        SearchRequestBuilder searchRequestBuilder = mock(SearchRequestBuilder.class);
+        Client client = rulePersistenceService.getClient();
         when(client.prepareSearch(TEST_INDEX_NAME)).thenReturn(searchRequestBuilder);
         when(searchRequestBuilder.setQuery(any(QueryBuilder.class))).thenReturn(searchRequestBuilder);
         when(searchRequestBuilder.setSize(anyInt())).thenReturn(searchRequestBuilder);
-
-        return client;
+        rulePersistenceService.getRuleFromIndex(null, ATTRIBUTE_MAP, null, listener);
+        verify(client).prepareSearch(TEST_INDEX_NAME);
+        verify(searchRequestBuilder).setQuery(any());
     }
 
     public void testDeleteRule_successful() {
