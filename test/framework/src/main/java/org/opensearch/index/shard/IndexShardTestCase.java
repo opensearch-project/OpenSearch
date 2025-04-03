74c406fbdd38d8f07152c5cdf6a48a3a2e8e9578
Ashish Singh <ssashish@amazon.com>
2025-04-03 23:37:55 +0530

    Implement parallel shard refresh behind cluster settings
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- test/framework/src/main/java/org/opensearch/index/shard/IndexShardTestCase.java
+++ test/framework/src/main/java/org/opensearch/index/shard/IndexShardTestCase.java
@@ -722,7 +722,10 @@ public abstract class IndexShardTestCase extends OpenSearchTestCase {
                 DefaultRemoteStoreSettings.INSTANCE,
                 false,
                 discoveryNodes,
-                mockReplicationStatsProvider
+                mockReplicationStatsProvider,
+                false,
+                () -> Boolean.FALSE,
+                () -> indexSettings.getRefreshInterval()
             );
             indexShard.addShardFailureCallback(DEFAULT_SHARD_FAILURE_HANDLER);
             if (remoteStoreStatsTrackerFactory != null) {
