0c0a349b40709ec98d728bc6ae0e48a4a564a7d0
Andrew Ross <andrross@amazon.com>
2025-04-03 13:41:31 -0700

    Fix socket interceptor and add unit test
    The optimization of using a Stream instead of a Collection caused problems
    with class not found and/or illegal access errors when using the lambda function
    in the `Stream::forEach` call in the intercept method.
    
    Signed-off-by: Andrew Ross <andrross@amazon.com>

--- libs/agent-sm/agent/src/test/java/org/opensearch/javaagent/AgentTests.java
+++ libs/agent-sm/agent/src/test/java/org/opensearch/javaagent/AgentTests.java
@@ -8,21 +8,9 @@
 
 package org.opensearch.javaagent;
 
-import org.opensearch.javaagent.bootstrap.AgentPolicy;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.security.Policy;
-import java.util.Set;
-
-public class AgentTests {
-    @SuppressWarnings("removal")
-    @BeforeClass
-    public static void setUp() {
-        AgentPolicy.setPolicy(new Policy() {
-        }, Set.of(), (caller, chain) -> caller.getName().equalsIgnoreCase("worker.org.gradle.process.internal.worker.GradleWorkerMain"));
-    }
-
+public class AgentTests extends AgentTestCase {
     @Test(expected = SecurityException.class)
     public void testSystemExitIsForbidden() {
         System.exit(0);
