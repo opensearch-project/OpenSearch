6d534b20dc2d6b564ffae5b573da6dec78ec609b
Ruirui Zhang <mariazrr@amazon.com>
2025-04-04 00:55:59 -0700

    modify based on comments Signed-off-by: Ruirui Zhang <mariazrr@amazon.com>

--- modules/autotagging-commons/common/src/test/java/org/opensearch/rule/action/CreateRuleResponseTests.java
+++ modules/autotagging-commons/common/src/test/java/org/opensearch/rule/action/CreateRuleResponseTests.java
@@ -12,7 +12,6 @@ import org.opensearch.autotagging.Rule;
 import org.opensearch.common.io.stream.BytesStreamOutput;
 import org.opensearch.common.xcontent.json.JsonXContent;
 import org.opensearch.core.common.io.stream.StreamInput;
-import org.opensearch.core.rest.RestStatus;
 import org.opensearch.core.xcontent.ToXContent;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.test.OpenSearchTestCase;
@@ -20,9 +19,9 @@ import org.opensearch.test.OpenSearchTestCase;
 import java.io.IOException;
 import java.util.Map;
 
-import static org.opensearch.rule.RuleTestUtils._ID_ONE;
-import static org.opensearch.rule.RuleTestUtils.assertEqualRules;
-import static org.opensearch.rule.RuleTestUtils.ruleOne;
+import static org.opensearch.rule.utils.RuleTestUtils._ID_ONE;
+import static org.opensearch.rule.utils.RuleTestUtils.assertEqualRules;
+import static org.opensearch.rule.utils.RuleTestUtils.ruleOne;
 import static org.mockito.Mockito.mock;
 
 public class CreateRuleResponseTests extends OpenSearchTestCase {
@@ -31,12 +30,11 @@ public class CreateRuleResponseTests extends OpenSearchTestCase {
      * Test case to verify serialization and deserialization of CreateRuleResponse
      */
     public void testSerialization() throws IOException {
-        CreateRuleResponse response = new CreateRuleResponse(_ID_ONE, ruleOne, RestStatus.OK);
+        CreateRuleResponse response = new CreateRuleResponse(_ID_ONE, ruleOne);
         BytesStreamOutput out = new BytesStreamOutput();
         response.writeTo(out);
         StreamInput streamInput = out.bytes().streamInput();
         CreateRuleResponse otherResponse = new CreateRuleResponse(streamInput);
-        assertEquals(response.getRestStatus(), otherResponse.getRestStatus());
         Rule responseRule = response.getRule();
         Rule otherResponseRule = otherResponse.getRule();
         assertEqualRules(Map.of(_ID_ONE, responseRule), Map.of(_ID_ONE, otherResponseRule), false);
@@ -47,13 +45,13 @@ public class CreateRuleResponseTests extends OpenSearchTestCase {
      */
     public void testToXContentCreateRule() throws IOException {
         XContentBuilder builder = JsonXContent.contentBuilder().prettyPrint();
-        CreateRuleResponse response = new CreateRuleResponse(_ID_ONE, ruleOne, RestStatus.OK);
+        CreateRuleResponse response = new CreateRuleResponse(_ID_ONE, ruleOne);
         String actual = response.toXContent(builder, mock(ToXContent.Params.class)).toString();
         String expected = "{\n"
             + "  \"_id\" : \"AgfUO5Ja9yfvhdONlYi3TQ==\",\n"
             + "  \"description\" : \"description_1\",\n"
             + "  \"mock_attribute_one\" : [\n"
-            + "    \"pattern_1\"\n"
+            + "    \"mock_attribute_one\"\n"
             + "  ],\n"
             + "  \"mock_feature_type\" : \"feature_value_one\",\n"
             + "  \"updated_at\" : \"2024-01-26T08:58:57.558Z\"\n"
