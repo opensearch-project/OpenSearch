3dd78452253e4eb1cdb74c9995f64a42eff25fcc
Yupeng Fu <yupeng@uber.com>
2025-04-05 11:06:43 -0700

    Add a builder to ingestion source
    Signed-off-by: Yupeng Fu <yupeng@uber.com>

--- server/src/test/java/org/opensearch/cluster/metadata/IngestionSourceTests.java
+++ server/src/test/java/org/opensearch/cluster/metadata/IngestionSourceTests.java
@@ -26,7 +26,10 @@ public class IngestionSourceTests extends OpenSearchTestCase {
     public void testConstructorAndGetters() {
         Map<String, Object> params = new HashMap<>();
         params.put("key", "value");
-        IngestionSource source = new IngestionSource("type", pointerInitReset, DROP, params);
+        IngestionSource source = new IngestionSource.Builder("type").setParams(params)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
 
         assertEquals("type", source.getType());
         assertEquals(StreamPoller.ResetState.REWIND_BY_OFFSET, source.getPointerInitReset().getType());
@@ -38,36 +41,57 @@ public class IngestionSourceTests extends OpenSearchTestCase {
     public void testEquals() {
         Map<String, Object> params1 = new HashMap<>();
         params1.put("key", "value");
-        IngestionSource source1 = new IngestionSource("type", pointerInitReset, DROP, params1);
+        IngestionSource source1 = new IngestionSource.Builder("type").setParams(params1)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
 
         Map<String, Object> params2 = new HashMap<>();
         params2.put("key", "value");
-        IngestionSource source2 = new IngestionSource("type", pointerInitReset, DROP, params2);
+        IngestionSource source2 = new IngestionSource.Builder("type").setParams(params2)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
         assertTrue(source1.equals(source2));
         assertTrue(source2.equals(source1));
 
-        IngestionSource source3 = new IngestionSource("differentType", pointerInitReset, DROP, params1);
+        IngestionSource source3 = new IngestionSource.Builder("differentType").setParams(params1)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
         assertFalse(source1.equals(source3));
     }
 
     public void testHashCode() {
         Map<String, Object> params1 = new HashMap<>();
         params1.put("key", "value");
-        IngestionSource source1 = new IngestionSource("type", pointerInitReset, DROP, params1);
+        IngestionSource source1 = new IngestionSource.Builder("type").setParams(params1)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
 
         Map<String, Object> params2 = new HashMap<>();
         params2.put("key", "value");
-        IngestionSource source2 = new IngestionSource("type", pointerInitReset, DROP, params2);
+        IngestionSource source2 = new IngestionSource.Builder("type").setParams(params2)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
         assertEquals(source1.hashCode(), source2.hashCode());
 
-        IngestionSource source3 = new IngestionSource("differentType", pointerInitReset, DROP, params1);
+        IngestionSource source3 = new IngestionSource.Builder("differentType").setParams(params1)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
         assertNotEquals(source1.hashCode(), source3.hashCode());
     }
 
     public void testToString() {
         Map<String, Object> params = new HashMap<>();
         params.put("key", "value");
-        IngestionSource source = new IngestionSource("type", pointerInitReset, DROP, params);
+        IngestionSource source = new IngestionSource.Builder("type").setParams(params)
+            .setPointerInitReset(pointerInitReset)
+            .setErrorStrategy(DROP)
+            .build();
         String expected =
             "IngestionSource{type='type',pointer_init_reset='PointerInitReset{type='REWIND_BY_OFFSET', value=1000}',error_strategy='DROP', params={key=value}}";
         assertEquals(expected, source.toString());
