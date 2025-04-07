185b100b0c8d1fae452e2a1a73d36c8df52d9645
Varun Bharadwaj <varunbharadwaj1995@gmail.com>
2025-04-07 14:13:31 -0700

    Support updates and deletes in ingestion flow
    Signed-off-by: Varun Bharadwaj <varunbharadwaj1995@gmail.com>

---
+++ server/src/main/java/org/opensearch/indices/pollingingest/IngestionUtils.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.indices.pollingingest;
+
+import org.opensearch.common.UUIDs;
+import org.opensearch.common.xcontent.XContentHelper;
+import org.opensearch.core.common.bytes.BytesArray;
+import org.opensearch.core.common.bytes.BytesReference;
+import org.opensearch.core.xcontent.MediaTypeRegistry;
+
+import java.util.Map;
+
+/**
+ * Holds common utils for pull-based ingestion.
+ */
+public final class IngestionUtils {
+
+    private IngestionUtils() {}
+
+    public static Map<String, Object> getParsedPayloadMap(byte[] payload) {
+        BytesReference payloadBR = new BytesArray(payload);
+        Map<String, Object> payloadMap = XContentHelper.convertToMap(payloadBR, false, MediaTypeRegistry.xContentType(payloadBR)).v2();
+        return payloadMap;
+    }
+
+    public static String generateID() {
+        return UUIDs.base64UUID();
+    }
+}
