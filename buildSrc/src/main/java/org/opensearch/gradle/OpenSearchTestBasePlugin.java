cb317a241f2741deab16676726a1fafbbdc843c0
Igonin <iigonin@sternad.de>
2025-04-08 11:42:19 +0200

    Add FIPS build-tooling
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    # Conflicts:
    #	CHANGELOG.md

--- buildSrc/src/main/java/org/opensearch/gradle/OpenSearchTestBasePlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/OpenSearchTestBasePlugin.java
@@ -161,11 +161,13 @@ public class OpenSearchTestBasePlugin implements Plugin<Project> {
                 test.systemProperty("tests.seed", BuildParams.getTestSeed());
             }
 
-            var securityFile = "java.security";
-            test.systemProperty(
-                "java.security.properties",
-                project.getRootProject().getLayout().getProjectDirectory() + "/distribution/src/config/" + securityFile
-            );
+            if (BuildParams.isInFipsJvm()) {
+                test.systemProperty(
+                    "java.security.properties",
+                    project.getRootProject().getLayout().getProjectDirectory() + "/distribution/src/config/fips_java.security"
+                );
+            }
+
             // don't track these as inputs since they contain absolute paths and break cache relocatability
             File gradleHome = project.getGradle().getGradleUserHomeDir();
             String gradleVersion = project.getGradle().getGradleVersion();
