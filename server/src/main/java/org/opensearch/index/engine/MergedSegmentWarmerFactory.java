e012362e1ed49f5cbe6ac6f76ce1758c1d469fb2
guojialiang <guojialiang.2012@bytedance.com>
2025-04-10 20:19:00 +0800

    Introduce MergedSegmentWarmerFactory for Local/Remote merged segment pre copy
    Signed-off-by: guojialiang <guojialiang.2012@bytedance.com>

--- server/src/main/java/org/opensearch/index/engine/MergedSegmentWarmerFactory.java
+++ server/src/main/java/org/opensearch/index/engine/MergedSegmentWarmerFactory.java
@@ -60,8 +60,10 @@ public class MergedSegmentWarmerFactory {
     public IndexWriter.IndexReaderWarmer get(IndexShard shard) {
         if (shard.indexSettings().isAssignedOnRemoteNode()) {
             return new RemoteStoreMergedSegmentWarmer(transportService, recoverySettings, clusterService);
-        } else {
+        } else if (shard.indexSettings().isSegRepLocalEnabled()) {
             return new LocalMergedSegmentWarmer(transportService, recoverySettings, clusterService);
+        } else {
+            return null;
         }
     }
 }
