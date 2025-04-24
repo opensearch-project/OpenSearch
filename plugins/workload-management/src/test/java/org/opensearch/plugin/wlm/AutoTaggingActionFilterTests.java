75520b879a3a89ce492824b125fe728460cda94e
Kaushal Kumar <ravi.kaushal97@gmail.com>
2025-04-24 15:28:47 -0700

    fix UTs
    Signed-off-by: Kaushal Kumar <ravi.kaushal97@gmail.com>

--- plugins/workload-management/src/test/java/org/opensearch/plugin/wlm/AutoTaggingActionFilterTests.java
+++ plugins/workload-management/src/test/java/org/opensearch/plugin/wlm/AutoTaggingActionFilterTests.java
@@ -8,9 +8,13 @@
 
 package org.opensearch.plugin.wlm;
 
+import org.opensearch.action.ActionRequest;
 import org.opensearch.action.admin.cluster.node.tasks.cancel.CancelTasksRequest;
 import org.opensearch.action.search.SearchRequest;
+import org.opensearch.action.support.ActionFilterChain;
 import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.action.ActionResponse;
 import org.opensearch.core.common.io.stream.StreamOutput;
 import org.opensearch.rule.InMemoryRuleProcessingService;
 import org.opensearch.rule.autotagging.Attribute;
@@ -57,10 +61,11 @@ public class AutoTaggingActionFilterTests extends OpenSearchTestCase {
 
     public void testApplyForValidRequest() {
         SearchRequest request = mock(SearchRequest.class);
+        ActionFilterChain<ActionRequest, ActionResponse> mockFilterChain = mock(TestActionFilterChain.class);
         when(request.indices()).thenReturn(new String[] { "foo" });
         try (ThreadContext.StoredContext context = threadPool.getThreadContext().stashContext()) {
             when(ruleProcessingService.evaluateLabel(anyList())).thenReturn(Optional.of("TestQG_ID"));
-            autoTaggingActionFilter.apply(mock(Task.class), "Test", request, null, null);
+            autoTaggingActionFilter.apply(mock(Task.class), "Test", request, null, mockFilterChain);
 
             assertEquals("TestQG_ID", threadPool.getThreadContext().getHeader(WorkloadGroupTask.WORKLOAD_GROUP_ID_HEADER));
             verify(ruleProcessingService, times(1)).evaluateLabel(anyList());
@@ -68,8 +73,9 @@ public class AutoTaggingActionFilterTests extends OpenSearchTestCase {
     }
 
     public void testApplyForInValidRequest() {
+        ActionFilterChain<ActionRequest, ActionResponse> mockFilterChain = mock(TestActionFilterChain.class);
         CancelTasksRequest request = new CancelTasksRequest();
-        autoTaggingActionFilter.apply(mock(Task.class), "Test", request, null, null);
+        autoTaggingActionFilter.apply(mock(Task.class), "Test", request, null, mockFilterChain);
 
         verify(ruleProcessingService, times(0)).evaluateLabel(anyList());
     }
@@ -111,5 +117,12 @@ public class AutoTaggingActionFilterTests extends OpenSearchTestCase {
 
         @Override
         public void writeTo(StreamOutput out) throws IOException {}
+    }
+
+    private static class TestActionFilterChain implements ActionFilterChain<ActionRequest, ActionResponse> {
+        @Override
+        public void proceed(Task task, String action, ActionRequest request, ActionListener<ActionResponse> listener) {
+
+        }
     }
 }
