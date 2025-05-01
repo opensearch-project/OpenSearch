a0ae3b1c31b0b9cdf9b1ed2eea3433c6d4734db1
Andriy Redko <drreta@gmail.com>
2025-05-01 17:43:38 -0400

    Update bundled JDK to 11.0.27+6
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- buildSrc/src/main/java/org/opensearch/gradle/test/DistroTestPlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/test/DistroTestPlugin.java
@@ -75,9 +75,9 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class DistroTestPlugin implements Plugin<Project> {
-    private static final String SYSTEM_JDK_VERSION = "8u442-b06";
+    private static final String SYSTEM_JDK_VERSION = "8u452-b09";
     private static final String SYSTEM_JDK_VENDOR = "adoptium";
-    private static final String GRADLE_JDK_VERSION = "11.0.26+4";
+    private static final String GRADLE_JDK_VERSION = "11.0.27+6";
     private static final String GRADLE_JDK_VENDOR = "adoptium";
 
     // all distributions used by distro tests. this is temporary until tests are per distribution
