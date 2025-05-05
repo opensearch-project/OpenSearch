00225511ca49a9658762b653972ff988feda2b62
Daniel Widdis <widdis@gmail.com>
2025-05-05 14:09:43 -0700

    Null check field names in QueryStringQueryBuilder (#18194)
    * Null check field names in QueryStringQuerybuilder
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Restrict test to run on >=3.1.0
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    Signed-off-by: Andrew Ross <andrross@amazon.com>
    Co-authored-by: Andrew Ross <andrross@amazon.com>
    (cherry picked from commit 379e4643fe13740f6ef7075de98de53fe05902c8)

--- server/src/test/java/org/opensearch/index/query/QueryStringQueryBuilderTests.java
+++ server/src/test/java/org/opensearch/index/query/QueryStringQueryBuilderTests.java
@@ -73,6 +73,7 @@ import org.opensearch.common.settings.Settings;
 import org.opensearch.common.unit.Fuzziness;
 import org.opensearch.common.util.FeatureFlags;
 import org.opensearch.common.xcontent.json.JsonXContent;
+import org.opensearch.core.common.ParsingException;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.index.mapper.FieldNamesFieldMapper;
 import org.opensearch.index.mapper.MapperService;
@@ -1020,6 +1021,18 @@ public class QueryStringQueryBuilderTests extends AbstractQueryTestCase<QueryStr
             .add(new MatchNoDocsQuery("empty fields"), Occur.SHOULD)
             .build();
         assertThat(expectedQuery, equalTo(query));
+    }
+
+    public void testNullFieldInFieldsArray() throws IOException {
+        String queryAsString = "{\n"
+            + "  \"query_string\" : {\n"
+            + "    \"query\" : \"test\",\n"
+            + "    \"fields\" : [ \"field1\", null, \"field2\" ]\n"
+            + "  }\n"
+            + "}";
+
+        ParsingException e = expectThrows(ParsingException.class, () -> parseQuery(queryAsString));
+        assertEquals("[query_string] field name in [fields] cannot be null", e.getMessage());
     }
 
     public void testToQueryTextParsing() throws IOException {
