46955ff787ab990b12d1537b08512314be66f542
kkewwei <kewei.11@bytedance.com>
2025-05-06 02:04:00 +0800

    Fix flaky RepositoriesServiceIT.testCreatSnapAndUpdateReposityCauseInfiniteLoop (#18199)
    Signed-off-by: kkewwei <kewei.11@bytedance.com>
    Signed-off-by: kkewwei <kkewwei@163.com>

--- server/src/internalClusterTest/java/org/opensearch/repositories/RepositoriesServiceIT.java
+++ server/src/internalClusterTest/java/org/opensearch/repositories/RepositoriesServiceIT.java
@@ -127,7 +127,6 @@ public class RepositoriesServiceIT extends OpenSearchIntegTestCase {
         assertThrows(RepositoryException.class, () -> createRepository(repositoryName, FsRepository.TYPE, repoSettings));
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/15842")
     public void testCreatSnapAndUpdateReposityCauseInfiniteLoop() throws InterruptedException {
         // create index
         internalCluster();
@@ -173,16 +172,20 @@ public class RepositoriesServiceIT extends OpenSearchIntegTestCase {
         });
         thread.start();
 
-        logger.info("--> begin to reset repository");
-        repoSettings = Settings.builder().put("location", randomRepoPath()).put("max_snapshot_bytes_per_sec", "300mb");
-        OpenSearchIntegTestCase.putRepositoryWithNoSettingOverrides(
-            client().admin().cluster(),
-            repositoryName,
-            FsRepository.TYPE,
-            true,
-            repoSettings
-        );
-        logger.info("--> finish to reset repository");
+        try {
+            logger.info("--> begin to reset repository");
+            repoSettings = Settings.builder().put("location", randomRepoPath()).put("max_snapshot_bytes_per_sec", "300mb");
+            OpenSearchIntegTestCase.putRepositoryWithNoSettingOverrides(
+                client().admin().cluster(),
+                repositoryName,
+                FsRepository.TYPE,
+                true,
+                repoSettings
+            );
+            logger.info("--> finish to reset repository");
+        } catch (IllegalStateException e) {
+            assertThat(e, hasToString(containsString(("trying to modify or unregister repository that is currently used"))));
+        }
 
         // after updating repository, snapshot should be success
         createSnapshot.run();
