8ac48654db6347ca69f288563fda7eb543d63476
Igonin <iigonin@sternad.de>
2025-05-14 13:51:52 +0200

    add additional '-Ptests.fips.enabled=true' parameter; set KEYSTORE_PASSWORD when running docker tests.
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>

--- buildSrc/src/main/java/org/opensearch/gradle/info/FipsBuildParams.java
+++ buildSrc/src/main/java/org/opensearch/gradle/info/FipsBuildParams.java
@@ -8,23 +8,35 @@
 
 package org.opensearch.gradle.info;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 public class FipsBuildParams {
 
+    @Deprecated
+    public static final String FIPS_BUILD_PARAM_FOR_TESTS = "tests.fips.enabled";
     public static final String FIPS_BUILD_PARAM = "crypto.standard";
+    public static final String DEFAULT_FIPS_MODE = "FIPS-140-3";
 
+    private static Boolean fipsBuildParamForTests;
+    private static String fipsBuildParam;
     private static String fipsMode;
 
     public static void init(Function<String, Object> fipsValue) {
-        fipsMode = (String) fipsValue.apply(FIPS_BUILD_PARAM);
-        fipsMode = fipsMode == null ? "any-supported" : fipsMode;
+        fipsBuildParamForTests = Boolean.parseBoolean((String) fipsValue.apply(FIPS_BUILD_PARAM_FOR_TESTS));
+        fipsBuildParam = (String) fipsValue.apply(FIPS_BUILD_PARAM);
+
+        if (fipsBuildParamForTests) {
+            fipsMode = DEFAULT_FIPS_MODE;
+        } else {
+            fipsMode = Objects.requireNonNullElse(fipsBuildParam, "any-supported");
+        }
     }
 
     private FipsBuildParams() {}
 
     public static boolean isInFipsMode() {
-        return "FIPS-140-3".equals(fipsMode);
+        return DEFAULT_FIPS_MODE.equals(fipsMode);
     }
 
     public static String getFipsMode() {
