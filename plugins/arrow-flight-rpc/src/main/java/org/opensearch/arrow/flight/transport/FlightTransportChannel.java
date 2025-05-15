0edf9c97efef1277df5a698e58aba0549643f780
Rishabh Maurya <rishabhmaurya05@gmail.com>
2025-05-14 18:48:45 -0700

    vectorized version of StreamInput and StreamOutput
    Signed-off-by: Rishabh Maurya <rishabhmaurya05@gmail.com>

---
+++ plugins/arrow-flight-rpc/src/main/java/org/opensearch/arrow/flight/transport/FlightTransportChannel.java
@@ -0,0 +1,102 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.arrow.flight.transport;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.Version;
+import org.opensearch.common.lease.Releasable;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.transport.TransportResponse;
+import org.opensearch.search.query.QuerySearchResult;
+import org.opensearch.transport.TcpChannel;
+import org.opensearch.transport.TcpTransportChannel;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * A TCP transport channel for Arrow Flight, supporting only streaming responses.
+ *
+ * @opensearch.internal
+ */
+public class FlightTransportChannel extends TcpTransportChannel {
+    private static final Logger logger = LogManager.getLogger(FlightTransportChannel.class);
+
+    private final AtomicBoolean streamOpen = new AtomicBoolean(true);
+
+    public FlightTransportChannel(
+        FlightOutboundHandler outboundHandler,
+        TcpChannel channel,
+        String action,
+        long requestId,
+        Version version,
+        Set<String> features,
+        boolean compressResponse,
+        boolean isHandshake,
+        Releasable breakerRelease
+    ) {
+        super(outboundHandler, channel, action, requestId, version, features, compressResponse, isHandshake, breakerRelease);
+    }
+
+    @Override
+    public void sendResponse(Exception exception) throws IOException {
+        try {
+            outboundHandler.sendErrorResponse(version, features, getChannel(), requestId, action, exception);
+            logger.debug("Sent error response for action [{}] with requestId [{}]", action, requestId);
+        } finally {
+            if (streamOpen.compareAndSet(true, false)) {
+                release(true);
+            }
+        }
+    }
+
+    @Override
+    public void sendResponseBatch(TransportResponse response) {
+        if (!streamOpen.get()) {
+            throw new RuntimeException("Stream is closed for requestId [" + requestId + "]");
+        }
+        if (response instanceof QuerySearchResult && ((QuerySearchResult) response).getShardSearchRequest() != null) {
+            ((QuerySearchResult) response).getShardSearchRequest().setOutboundNetworkTime(System.currentTimeMillis());
+        }
+        ((FlightOutboundHandler) outboundHandler).sendResponseBatch(
+            version,
+            features,
+            getChannel(),
+            requestId,
+            action,
+            response,
+            compressResponse,
+            isHandshake,
+            ActionListener.wrap(
+                (resp) -> logger.debug("Response batch sent for action [{}] with requestId [{}]", action, requestId),
+                e -> logger.error("Failed to send response batch for action [{}] with requestId [{}]", action, requestId, e)
+            )
+        );
+    }
+
+    @Override
+    public void completeStream() {
+        if (streamOpen.compareAndSet(true, false)) {
+            ((FlightOutboundHandler) outboundHandler).completeStream(
+                version,
+                features,
+                getChannel(),
+                requestId,
+                action,
+                ActionListener.wrap(
+                    (resp) -> logger.debug("Stream completed for action [{}] with requestId [{}]", action, requestId),
+                    e -> logger.error("Failed to complete stream for action [{}] with requestId [{}]", action, requestId, e)
+                )
+            );
+            release(false);
+        }
+    }
+}
---
+++ server/src/main/java/org/opensearch/cluster/StreamNodeConnectionsService.java
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.cluster;
+
+import org.opensearch.common.annotation.ExperimentalApi;
+import org.opensearch.common.inject.Inject;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.transport.StreamTransportService;
+
+@ExperimentalApi
+public class StreamNodeConnectionsService extends NodeConnectionsService {
+    @Inject
+    public StreamNodeConnectionsService(Settings settings, ThreadPool threadPool, StreamTransportService streamTransportService) {
+        super(settings, threadPool, streamTransportService);
+    }
+}
