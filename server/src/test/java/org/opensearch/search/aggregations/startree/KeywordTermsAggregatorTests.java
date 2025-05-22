d4f6597ca5762d8cdf2d0cb95be9b829901b3c07
Sandesh Kumar <sandeshkr419@gmail.com>
2025-05-21 18:45:54 -0700

    Support for star-tree keyword aggregation dfs mode
    Signed-off-by: Sandesh Kumar <sandeshkr419@gmail.com>

--- server/src/test/java/org/opensearch/search/aggregations/startree/KeywordTermsAggregatorTests.java
+++ server/src/test/java/org/opensearch/search/aggregations/startree/KeywordTermsAggregatorTests.java
@@ -45,7 +45,7 @@ import org.opensearch.index.mapper.MapperService;
 import org.opensearch.index.mapper.NumberFieldMapper;
 import org.opensearch.index.query.QueryBuilder;
 import org.opensearch.index.query.TermQueryBuilder;
-import org.opensearch.search.aggregations.Aggregator;
+import org.opensearch.search.aggregations.Aggregator.SubAggCollectionMode;
 import org.opensearch.search.aggregations.AggregatorTestCase;
 import org.opensearch.search.aggregations.bucket.terms.InternalTerms;
 import org.opensearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
@@ -66,6 +66,8 @@ import static org.opensearch.search.aggregations.AggregationBuilders.max;
 import static org.opensearch.search.aggregations.AggregationBuilders.min;
 import static org.opensearch.search.aggregations.AggregationBuilders.sum;
 import static org.opensearch.search.aggregations.AggregationBuilders.terms;
+import static org.opensearch.search.aggregations.Aggregator.SubAggCollectionMode.BREADTH_FIRST;
+import static org.opensearch.search.aggregations.Aggregator.SubAggCollectionMode.DEPTH_FIRST;
 import static org.opensearch.test.InternalAggregationTestCase.DEFAULT_MAX_BUCKETS;
 
 public class KeywordTermsAggregatorTests extends AggregatorTestCase {
@@ -155,8 +157,7 @@ public class KeywordTermsAggregatorTests extends AggregatorTestCase {
 
         Query query = new MatchAllDocsQuery();
         QueryBuilder queryBuilder = null;
-        TermsAggregationBuilder termsAggregationBuilder = terms("terms_agg").field(CLIENTIP)
-            .collectMode(Aggregator.SubAggCollectionMode.BREADTH_FIRST);
+        TermsAggregationBuilder termsAggregationBuilder = terms("terms_agg").field(CLIENTIP);
         testCase(indexSearcher, query, queryBuilder, termsAggregationBuilder, starTree, supportedDimensions);
 
         ValuesSourceAggregationBuilder[] aggBuilders = {
@@ -170,9 +171,7 @@ public class KeywordTermsAggregatorTests extends AggregatorTestCase {
             query = new MatchAllDocsQuery();
             queryBuilder = null;
 
-            termsAggregationBuilder = terms("terms_agg").field(CLIENTIP)
-                .subAggregation(aggregationBuilder)
-                .collectMode(Aggregator.SubAggCollectionMode.BREADTH_FIRST);
+            termsAggregationBuilder = terms("terms_agg").field(CLIENTIP).subAggregation(aggregationBuilder);
             testCase(indexSearcher, query, queryBuilder, termsAggregationBuilder, starTree, supportedDimensions);
 
             // Numeric-terms query with keyword terms aggregation
@@ -204,43 +203,47 @@ public class KeywordTermsAggregatorTests extends AggregatorTestCase {
         CompositeIndexFieldInfo starTree,
         LinkedHashMap<Dimension, MappedFieldType> supportedDimensions
     ) throws IOException {
-        InternalTerms starTreeAggregation = searchAndReduceStarTree(
-            createIndexSettings(),
-            indexSearcher,
-            query,
-            queryBuilder,
-            termsAggregationBuilder,
-            starTree,
-            supportedDimensions,
-            null,
-            DEFAULT_MAX_BUCKETS,
-            false,
-            null,
-            true,
-            STATUS_FIELD_TYPE,
-            SIZE_FIELD_NAME,
-            CLIENTIP_FIELD_NAME
-        );
+        for (SubAggCollectionMode collectionMode : List.of(DEPTH_FIRST, BREADTH_FIRST)) {
+            termsAggregationBuilder.collectMode(collectionMode);
 
-        InternalTerms defaultAggregation = searchAndReduceStarTree(
-            createIndexSettings(),
-            indexSearcher,
-            query,
-            queryBuilder,
-            termsAggregationBuilder,
-            null,
-            null,
-            null,
-            DEFAULT_MAX_BUCKETS,
-            false,
-            null,
-            false,
-            STATUS_FIELD_TYPE,
-            SIZE_FIELD_NAME,
-            CLIENTIP_FIELD_NAME
-        );
+            InternalTerms starTreeAggregation = searchAndReduceStarTree(
+                createIndexSettings(),
+                indexSearcher,
+                query,
+                queryBuilder,
+                termsAggregationBuilder,
+                starTree,
+                supportedDimensions,
+                null,
+                DEFAULT_MAX_BUCKETS,
+                false,
+                null,
+                true,
+                STATUS_FIELD_TYPE,
+                SIZE_FIELD_NAME,
+                CLIENTIP_FIELD_NAME
+            );
 
-        assertEquals(defaultAggregation.getBuckets().size(), starTreeAggregation.getBuckets().size());
-        assertEquals(defaultAggregation.getBuckets(), starTreeAggregation.getBuckets());
+            InternalTerms defaultAggregation = searchAndReduceStarTree(
+                createIndexSettings(),
+                indexSearcher,
+                query,
+                queryBuilder,
+                termsAggregationBuilder,
+                null,
+                null,
+                null,
+                DEFAULT_MAX_BUCKETS,
+                false,
+                null,
+                false,
+                STATUS_FIELD_TYPE,
+                SIZE_FIELD_NAME,
+                CLIENTIP_FIELD_NAME
+            );
+
+            assertEquals(defaultAggregation.getBuckets().size(), starTreeAggregation.getBuckets().size());
+            assertEquals(defaultAggregation.getBuckets(), starTreeAggregation.getBuckets());
+        }
     }
 }
