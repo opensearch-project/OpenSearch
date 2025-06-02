32cd2e6b5ef902a7cd70f3b0760fc53a0eb83b10
Iwan Igonin <83668556+beanuwave@users.noreply.github.com>
2025-06-02 13:52:14 +0200

    Add FIPS build tooling (#17907)
    * Add FIPS build-tooling
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    # Conflicts:
    #	CHANGELOG.md
    
    # Conflicts:
    #	CHANGELOG.md
    
    * Update distribution/tools/plugin-cli/src/test/java/org/opensearch/tools/cli/plugin/InstallPluginCommandTests.java
    
    Co-authored-by: Andriy Redko <drreta@gmail.com>
    Signed-off-by: Iwan Igonin <83668556+beanuwave@users.noreply.github.com>
    
    * Update server/src/main/java/org/opensearch/bootstrap/SecurityProviderManager.java
    
    Co-authored-by: Andriy Redko <drreta@gmail.com>
    Signed-off-by: Iwan Igonin <83668556+beanuwave@users.noreply.github.com>
    
    * add additional '-Ptests.fips.enabled=true' parameter; set KEYSTORE_PASSWORD when running docker tests.
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * prevent bc deps propagation outside their modules
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * change TestUtilsTests to test for cryptographic equality
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * use 'fipsOnly' for all BC declarations
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * fix: do not allow other values than 'FIPS-140-3' for 'crypto.standard'
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * exclude BC from client & server compile scope
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * Refactor BootstrapForTesting & Randomness
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    * revert Randomness.java
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    # Conflicts:
    #	server/src/main/java/org/opensearch/common/Randomness.java
    
    * rename SecurityProviderManager#excludeSunJCE to SecurityProviderManager#removeNonCompliantFipsProviders
    
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>
    
    ---------
    
    Signed-off-by: Iwan Igonin <83668556+beanuwave@users.noreply.github.com>
    Signed-off-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Igonin <iigonin@sternad.de>
    Co-authored-by: Andriy Redko <drreta@gmail.com>
    Co-authored-by: Benny Goerzig <benny.goerzig@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>
    Co-authored-by: Kai Sternad <k.sternad@sternad.de>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -35,6 +35,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 - Added File Cache Stats - Involves Block level as well as full file level stats ([#17538](https://github.com/opensearch-project/OpenSearch/issues/17479))
 - Added File Cache Pinning ([#17617](https://github.com/opensearch-project/OpenSearch/issues/13648))
 - Support consumer reset in Resume API for pull-based ingestion. This PR includes a breaking change for the experimental pull-based ingestion feature. ([#18332](https://github.com/opensearch-project/OpenSearch/pull/18332))
+- Add FIPS build tooling ([#4254](https://github.com/opensearch-project/security/issues/4254))
 
 ### Changed
 - Create generic DocRequest to better categorize ActionRequests ([#18269](https://github.com/opensearch-project/OpenSearch/pull/18269)))
--- libs/ssl-config/src/main/java/org/opensearch/common/ssl/PemUtils.java
+++ libs/ssl-config/src/main/java/org/opensearch/common/ssl/PemUtils.java
@@ -34,6 +34,8 @@ package org.opensearch.common.ssl;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
 import org.bouncycastle.openssl.PEMEncryptedKeyPair;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
@@ -49,6 +51,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.PrivateKey;
+import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
@@ -60,7 +63,15 @@ import java.util.function.Supplier;
 
 final class PemUtils {
 
-    public static final String BCFIPS = "BCFIPS";
+    static {
+        var highestPriority = 1;
+        if (Security.getProvider(BouncyCastleFipsProvider.PROVIDER_NAME) == null) {
+            Security.insertProviderAt(new BouncyCastleFipsProvider(), highestPriority++);
+        }
+        if (Security.getProvider(BouncyCastleJsseProvider.PROVIDER_NAME) == null) {
+            Security.insertProviderAt(new BouncyCastleJsseProvider(), highestPriority);
+        }
+    }
 
     PemUtils() {
         throw new IllegalStateException("Utility class should not be instantiated");
@@ -114,12 +125,14 @@ final class PemUtils {
 
             if (object instanceof PKCS8EncryptedPrivateKeyInfo) { // encrypted private key in pkcs8-format
                 var privateKeyInfo = (PKCS8EncryptedPrivateKeyInfo) object;
-                var inputDecryptorProvider = new JcePKCSPBEInputDecryptorProviderBuilder().setProvider(BCFIPS)
-                    .build(passwordSupplier.get());
+                var inputDecryptorProvider = new JcePKCSPBEInputDecryptorProviderBuilder().setProvider(
+                    BouncyCastleFipsProvider.PROVIDER_NAME
+                ).build(passwordSupplier.get());
                 return privateKeyInfo.decryptPrivateKeyInfo(inputDecryptorProvider);
             } else if (object instanceof PEMEncryptedKeyPair) { // encrypted private key
                 var encryptedKeyPair = (PEMEncryptedKeyPair) object;
-                var decryptorProvider = new JcePEMDecryptorProviderBuilder().setProvider(BCFIPS).build(passwordSupplier.get());
+                var decryptorProvider = new JcePEMDecryptorProviderBuilder().setProvider(BouncyCastleFipsProvider.PROVIDER_NAME)
+                    .build(passwordSupplier.get());
                 var keyPair = encryptedKeyPair.decryptKeyPair(decryptorProvider);
                 return keyPair.getPrivateKeyInfo();
             } else if (object instanceof PEMKeyPair) { // unencrypted private key
