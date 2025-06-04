bee07499be74bcf2987f9e54affd79694d75c84e
Karen X <karenxyr@gmail.com>
2025-06-04 17:04:07 -0400

    [GRPC] Add integration tests for Bulk and Search GRPC endpoints (#18366)
    * Add integration tests for Bulk and Search GRPC endpoints
    
    Signed-off-by: Karen Xu <karenxyr@gmail.com>
    
    * fix assertion for shard info and remove changelog entry
    Signed-off-by: Karen Xu <karenxyr@gmail.com>
    
    * use a common GRPC test base
    
    Signed-off-by: Karen Xu <karenxyr@gmail.com>
    
    ---------
    
    Signed-off-by: Karen Xu <karenxyr@gmail.com>

---
+++ plugins/transport-grpc/src/internalClusterTest/java/org/opensearch/plugin/transport/grpc/SearchServiceIT.java
@@ -0,0 +1,63 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.plugin.transport.grpc;
+
+import org.opensearch.plugin.transport.grpc.ssl.NettyGrpcClient;
+import org.opensearch.protobufs.SearchRequest;
+import org.opensearch.protobufs.SearchRequestBody;
+import org.opensearch.protobufs.SearchResponse;
+import org.opensearch.protobufs.services.SearchServiceGrpc;
+
+import io.grpc.ManagedChannel;
+
+/**
+ * Integration tests for the SearchService gRPC service.
+ */
+public class SearchServiceIT extends GrpcTransportBaseIT {
+
+    /**
+     * Tests the search operation via gRPC.
+     */
+    public void testSearchServiceSearch() throws Exception {
+        // Create a test index
+        String indexName = "test-search-index";
+        createTestIndex(indexName);
+
+        // Add a document to the index
+        indexTestDocument(indexName, "1", DEFAULT_DOCUMENT_SOURCE);
+
+        // Create a gRPC client
+        try (NettyGrpcClient client = createGrpcClient()) {
+            // Create a SearchService stub
+            ManagedChannel channel = client.getChannel();
+            SearchServiceGrpc.SearchServiceBlockingStub searchStub = SearchServiceGrpc.newBlockingStub(channel);
+
+            // Create a search request
+            SearchRequestBody requestBody = SearchRequestBody.newBuilder().setFrom(0).setSize(10).build();
+
+            SearchRequest searchRequest = SearchRequest.newBuilder()
+                .addIndex(indexName)
+                .setRequestBody(requestBody)
+                .setQ("field1:value1")
+                .build();
+
+            // Execute the search request
+            SearchResponse searchResponse = searchStub.search(searchRequest);
+
+            // Verify the response
+            assertNotNull("Search response should not be null", searchResponse);
+            assertTrue(
+                "Search response should have hits",
+                searchResponse.getResponseBody().getHits().getTotal().getTotalHits().getValue() > 0
+            );
+            assertEquals("Search response should have one hit", 1, searchResponse.getResponseBody().getHits().getHitsCount());
+            assertEquals("Hit should have correct ID", "1", searchResponse.getResponseBody().getHits().getHits(0).getId());
+        }
+    }
+}
