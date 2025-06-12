93d2077a14945e4f863e2d851db3b0e433e9969b
Sawan Srivastava <sawan1210@gmail.com>
2025-06-12 23:43:53 +0000

    Updated tests to fit new query hierarchy
    Signed-off-by: Sawan Srivastava <sawan1210@gmail.com>

--- server/src/main/java/org/opensearch/index/mapper/DateFieldMapper.java
+++ server/src/main/java/org/opensearch/index/mapper/DateFieldMapper.java
@@ -602,7 +602,6 @@ public final class DateFieldMapper extends ParametrizedFieldMapper {
             }
 
             return query;
-//            return nowUsed[0] ? new DateRangeIncludingNowQuery(query) : query;
         }
 
         public long parseToLong(Object value, boolean roundUp, @Nullable ZoneId zone, DateMathParser dateParser, LongSupplier now) {
