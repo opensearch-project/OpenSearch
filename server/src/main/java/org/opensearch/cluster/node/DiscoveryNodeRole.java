3756209f334aa2e1a4f4e19610588c8dc9a33649
Du Tran <quangdutran809@gmail.com>
2025-06-26 12:48:32 +0300

    Replace LegacyESVersion.fromString with Version.fromString - Remove unused constant
    Signed-off-by: Du Tran <quangdutran809@gmail.com>

--- server/src/main/java/org/opensearch/cluster/node/DiscoveryNodeRole.java
+++ server/src/main/java/org/opensearch/cluster/node/DiscoveryNodeRole.java
@@ -345,16 +345,6 @@ public abstract class DiscoveryNodeRole implements Comparable<DiscoveryNodeRole>
     );
 
     /**
-     * The version that {@link #REMOTE_CLUSTER_CLIENT_ROLE} is introduced. Nodes before this version do not have that role even
-     * they can connect to remote clusters.
-     */
-    public static final Version REMOTE_CLUSTER_CLIENT_ROLE_VERSION = Version.fromString("7.8.0");
-
-    static SortedSet<DiscoveryNodeRole> LEGACY_ROLES = Collections.unmodifiableSortedSet(
-        new TreeSet<>(Arrays.asList(DATA_ROLE, INGEST_ROLE, MASTER_ROLE))
-    );
-
-    /**
      * Represents an unknown role. This can occur if a newer version adds a role that an older version does not know about, or a newer
      * version removes a role that an older version knows about.
      */
