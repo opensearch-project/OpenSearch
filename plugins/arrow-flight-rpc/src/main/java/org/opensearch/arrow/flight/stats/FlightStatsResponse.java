d3123d5f244699cf6d26000a54edce1b22934bcf
Rishabh Maurya <rishabhmaurya05@gmail.com>
2025-07-21 17:42:11 -0700

    stream transport metrics and integration
    Signed-off-by: Rishabh Maurya <rishabhmaurya05@gmail.com>

--- plugins/arrow-flight-rpc/src/main/java/org/opensearch/arrow/flight/stats/FlightStatsResponse.java
+++ plugins/arrow-flight-rpc/src/main/java/org/opensearch/arrow/flight/stats/FlightStatsResponse.java
@@ -21,9 +21,6 @@ import org.opensearch.core.xcontent.XContentBuilder;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Response containing Flight transport statistics from multiple nodes
- */
 class FlightStatsResponse extends BaseNodesResponse<FlightNodeStats> implements ToXContentObject {
 
     public FlightStatsResponse(StreamInput in) throws IOException {
@@ -59,12 +56,11 @@ class FlightStatsResponse extends BaseNodesResponse<FlightNodeStats> implements 
                     ? nodeStats.getNode().getStreamAddress().toString()
                     : nodeStats.getNode().getAddress().toString()
             );
-            nodeStats.getFlightStats().toXContent(builder, params);
+            nodeStats.getMetrics().toXContent(builder, params);
             builder.endObject();
         }
         builder.endObject();
 
-        // Cluster-wide aggregated stats
         builder.startObject("cluster_stats");
         aggregateClusterStats(builder, params);
         builder.endObject();
@@ -74,163 +70,125 @@ class FlightStatsResponse extends BaseNodesResponse<FlightNodeStats> implements 
     }
 
     private void aggregateClusterStats(XContentBuilder builder, Params params) throws IOException {
-        // Performance aggregates
-        long totalServerRequests = 0;
-        long totalServerRequestsCurrent = 0;
-        long totalServerBatches = 0;
-        long totalClientBatches = 0;
-        long totalClientResponses = 0;
-        long totalBytesSent = 0;
-        long totalBytesReceived = 0;
-        long totalServerRequestTime = 0;
-        long totalServerBatchTime = 0;
-        long totalClientBatchTime = 0;
+        long totalClientCallsStarted = 0;
+        long totalClientCallsCompleted = 0;
+        long totalClientCallDuration = 0;
+        long totalClientRequestBytes = 0;
 
-        // Reliability aggregates
-        long totalClientApplicationErrors = 0;
-        long totalClientTransportErrors = 0;
-        long totalServerApplicationErrors = 0;
-        long totalServerTransportErrors = 0;
-        long totalClientStreamsCompleted = 0;
-        long totalServerStreamsCompleted = 0;
-        long totalUptime = 0;
+        long totalClientBatchesRequested = 0;
+        long totalClientBatchesReceived = 0;
+        long totalClientBatchReceivedBytes = 0;
+        long totalClientBatchProcessingTime = 0;
 
-        // Resource aggregates
-        long totalArrowAllocated = 0;
-        long totalArrowPeak = 0;
-        long totalDirectMemory = 0;
-        int totalClientThreadsActive = 0;
-        int totalClientThreadsTotal = 0;
-        int totalServerThreadsActive = 0;
-        int totalServerThreadsTotal = 0;
-        int totalConnections = 0;
-        int totalChannels = 0;
+        long totalServerCallsStarted = 0;
+        long totalServerCallsCompleted = 0;
+        long totalServerCallDuration = 0;
+        long totalServerRequestBytes = 0;
+
+        long totalServerBatchesSent = 0;
+        long totalServerBatchSentBytes = 0;
+        long totalServerBatchProcessingTime = 0;
 
         for (FlightNodeStats nodeStats : getNodes()) {
-            FlightTransportStats stats = nodeStats.getFlightStats();
+            FlightMetrics metrics = nodeStats.getMetrics();
 
-            // Performance
-            totalServerRequests += stats.performance.serverRequestsReceived;
-            totalServerRequestsCurrent += stats.performance.serverRequestsCurrent;
-            totalServerBatches += stats.performance.serverBatchesSent;
-            totalClientBatches += stats.performance.clientBatchesReceived;
-            totalClientResponses += stats.performance.clientResponsesReceived;
-            totalBytesSent += stats.performance.bytesSent;
-            totalBytesReceived += stats.performance.bytesReceived;
-            totalServerRequestTime += stats.performance.serverRequestTotalMillis;
-            totalServerBatchTime += stats.performance.serverBatchTotalMillis;
-            totalClientBatchTime += stats.performance.clientBatchTotalMillis;
+            FlightMetrics.ClientCallMetrics clientCallMetrics = metrics.getClientCallMetrics();
+            totalClientCallsStarted += clientCallMetrics.getStarted();
+            totalClientCallsCompleted += clientCallMetrics.getCompleted();
+            totalClientCallDuration += clientCallMetrics.getDuration().getSum();
+            totalClientRequestBytes += clientCallMetrics.getRequestBytes().getSum();
 
-            // Reliability
-            totalClientApplicationErrors += stats.reliability.clientApplicationErrors;
-            totalClientTransportErrors += stats.reliability.clientTransportErrors;
-            totalServerApplicationErrors += stats.reliability.serverApplicationErrors;
-            totalServerTransportErrors += stats.reliability.serverTransportErrors;
-            totalClientStreamsCompleted += stats.reliability.clientStreamsCompleted;
-            totalServerStreamsCompleted += stats.reliability.serverStreamsCompleted;
-            totalUptime = Math.max(totalUptime, stats.reliability.uptimeMillis);
+            FlightMetrics.ClientBatchMetrics clientBatchMetrics = metrics.getClientBatchMetrics();
+            totalClientBatchesRequested += clientBatchMetrics.getBatchesRequested();
+            totalClientBatchesReceived += clientBatchMetrics.getBatchesReceived();
+            totalClientBatchReceivedBytes += clientBatchMetrics.getReceivedBytes().getSum();
+            totalClientBatchProcessingTime += clientBatchMetrics.getProcessingTime().getSum();
 
-            // Resources
-            totalArrowAllocated += stats.resourceUtilization.arrowAllocatedBytes;
-            totalArrowPeak = Math.max(totalArrowPeak, stats.resourceUtilization.arrowPeakBytes);
-            totalDirectMemory += stats.resourceUtilization.directMemoryBytes;
-            totalClientThreadsActive += stats.resourceUtilization.clientThreadsActive;
-            totalClientThreadsTotal += stats.resourceUtilization.clientThreadsTotal;
-            totalServerThreadsActive += stats.resourceUtilization.serverThreadsActive;
-            totalServerThreadsTotal += stats.resourceUtilization.serverThreadsTotal;
-            totalConnections += stats.resourceUtilization.connectionsActive;
-            totalChannels += stats.resourceUtilization.channelsActive;
+            FlightMetrics.ServerCallMetrics serverCallMetrics = metrics.getServerCallMetrics();
+            totalServerCallsStarted += serverCallMetrics.getStarted();
+            totalServerCallsCompleted += serverCallMetrics.getCompleted();
+            totalServerCallDuration += serverCallMetrics.getDuration().getSum();
+            totalServerRequestBytes += serverCallMetrics.getRequestBytes().getSum();
+
+            FlightMetrics.ServerBatchMetrics serverBatchMetrics = metrics.getServerBatchMetrics();
+            totalServerBatchesSent += serverBatchMetrics.getBatchesSent();
+            totalServerBatchSentBytes += serverBatchMetrics.getSentBytes().getSum();
+            totalServerBatchProcessingTime += serverBatchMetrics.getProcessingTime().getSum();
         }
 
-        // Performance stats
-        builder.startObject("performance");
-        builder.field("server_requests_total", totalServerRequests);
-        builder.field("server_requests_current", totalServerRequestsCurrent);
-        builder.field("server_batches_sent", totalServerBatches);
-        builder.field("client_batches_received", totalClientBatches);
-        builder.field("client_responses_received", totalClientResponses);
-        builder.field("bytes_sent", totalBytesSent);
-        if (params.paramAsBoolean("human", false)) {
-            builder.field("bytes_sent_human", new ByteSizeValue(totalBytesSent).toString());
+        builder.startObject("client");
+
+        builder.startObject("calls");
+        builder.field("started", totalClientCallsStarted);
+        builder.field("completed", totalClientCallsCompleted);
+        builder.humanReadableField(
+            "duration_nanos",
+            "duration",
+            new TimeValue(totalClientCallDuration, java.util.concurrent.TimeUnit.NANOSECONDS)
+        );
+        if (totalClientCallsCompleted > 0) {
+            long avgDurationNanos = totalClientCallDuration / totalClientCallsCompleted;
+            builder.humanReadableField(
+                "avg_duration_nanos",
+                "avg_duration",
+                new TimeValue(avgDurationNanos, java.util.concurrent.TimeUnit.NANOSECONDS)
+            );
         }
-        builder.field("bytes_received", totalBytesReceived);
-        if (params.paramAsBoolean("human", false)) {
-            builder.field("bytes_received_human", new ByteSizeValue(totalBytesReceived).toString());
-        }
-        if (totalServerRequests > 0) {
-            long avgRequestTime = totalServerRequestTime / totalServerRequests;
-            builder.field("server_request_avg_millis", avgRequestTime);
-            if (params.paramAsBoolean("human", false)) {
-                builder.field("server_request_avg_time", TimeValue.timeValueMillis(avgRequestTime).toString());
-            }
-        }
-        if (totalServerBatches > 0) {
-            long avgBatchTime = totalServerBatchTime / totalServerBatches;
-            builder.field("server_batch_avg_millis", avgBatchTime);
-            if (params.paramAsBoolean("human", false)) {
-                builder.field("server_batch_avg_time", TimeValue.timeValueMillis(avgBatchTime).toString());
-            }
-        }
-        if (totalClientBatches > 0) {
-            long avgClientBatchTime = totalClientBatchTime / totalClientBatches;
-            builder.field("client_batch_avg_millis", avgClientBatchTime);
-            if (params.paramAsBoolean("human", false)) {
-                builder.field("client_batch_avg_time", TimeValue.timeValueMillis(avgClientBatchTime).toString());
-            }
-        }
+        builder.humanReadableField("request_bytes", "request", new ByteSizeValue(totalClientRequestBytes));
+        builder.humanReadableField("response_bytes", "response", new ByteSizeValue(totalClientBatchReceivedBytes));
         builder.endObject();
 
-        // Reliability stats
-        builder.startObject("reliability");
-        builder.field("client_application_errors", totalClientApplicationErrors);
-        builder.field("client_transport_errors", totalClientTransportErrors);
-        builder.field("server_application_errors", totalServerApplicationErrors);
-        builder.field("server_transport_errors", totalServerTransportErrors);
-        builder.field("client_streams_completed", totalClientStreamsCompleted);
-        builder.field("server_streams_completed", totalServerStreamsCompleted);
-        builder.field("cluster_uptime_millis", totalUptime);
-        if (params.paramAsBoolean("human", false)) {
-            builder.field("cluster_uptime", TimeValue.timeValueMillis(totalUptime).toString());
-        }
-
-        long totalErrors = totalClientApplicationErrors + totalClientTransportErrors + totalServerApplicationErrors
-            + totalServerTransportErrors;
-        long totalStreams = totalClientStreamsCompleted + totalServerStreamsCompleted + totalErrors;
-        if (totalStreams > 0) {
-            builder.field("cluster_error_rate_percent", (totalErrors * 100.0) / totalStreams);
-            builder.field(
-                "cluster_success_rate_percent",
-                ((totalClientStreamsCompleted + totalServerStreamsCompleted) * 100.0) / totalStreams
+        builder.startObject("batches");
+        builder.field("requested", totalClientBatchesRequested);
+        builder.field("received", totalClientBatchesReceived);
+        builder.humanReadableField("received_bytes", "received_size", new ByteSizeValue(totalClientBatchReceivedBytes));
+        if (totalClientBatchesReceived > 0) {
+            long avgProcessingTimeNanos = totalClientBatchProcessingTime / totalClientBatchesReceived;
+            builder.humanReadableField(
+                "avg_processing_time_nanos",
+                "avg_processing_time",
+                new TimeValue(avgProcessingTimeNanos, java.util.concurrent.TimeUnit.NANOSECONDS)
             );
         }
         builder.endObject();
 
-        // Resource utilization stats
-        builder.startObject("resource_utilization");
-        builder.field("arrow_allocated_bytes_total", totalArrowAllocated);
-        if (params.paramAsBoolean("human", false)) {
-            builder.field("arrow_allocated_total", new ByteSizeValue(totalArrowAllocated).toString());
+        builder.endObject();
+
+        builder.startObject("server");
+
+        builder.startObject("calls");
+        builder.field("started", totalServerCallsStarted);
+        builder.field("completed", totalServerCallsCompleted);
+        builder.humanReadableField(
+            "duration_nanos",
+            "duration",
+            new TimeValue(totalServerCallDuration, java.util.concurrent.TimeUnit.NANOSECONDS)
+        );
+        if (totalServerCallsCompleted > 0) {
+            long avgDurationNanos = totalServerCallDuration / totalServerCallsCompleted;
+            builder.humanReadableField(
+                "avg_duration_nanos",
+                "avg_duration",
+                new TimeValue(avgDurationNanos, java.util.concurrent.TimeUnit.NANOSECONDS)
+            );
         }
-        builder.field("arrow_peak_bytes_max", totalArrowPeak);
-        if (params.paramAsBoolean("human", false)) {
-            builder.field("arrow_peak_max", new ByteSizeValue(totalArrowPeak).toString());
+        builder.humanReadableField("request_bytes", "request", new ByteSizeValue(totalServerRequestBytes));
+        builder.humanReadableField("response_bytes", "response", new ByteSizeValue(totalServerBatchSentBytes));
+        builder.endObject();
+
+        builder.startObject("batches");
+        builder.field("sent", totalServerBatchesSent);
+        builder.humanReadableField("sent_bytes", "sent_size", new ByteSizeValue(totalServerBatchSentBytes));
+        if (totalServerBatchesSent > 0) {
+            long avgProcessingTimeNanos = totalServerBatchProcessingTime / totalServerBatchesSent;
+            builder.humanReadableField(
+                "avg_processing_time_nanos",
+                "avg_processing_time",
+                new TimeValue(avgProcessingTimeNanos, java.util.concurrent.TimeUnit.NANOSECONDS)
+            );
         }
-        builder.field("direct_memory_bytes_total", totalDirectMemory);
-        if (params.paramAsBoolean("human", false)) {
-            builder.field("direct_memory_total", new ByteSizeValue(totalDirectMemory).toString());
-        }
-        builder.field("client_threads_active", totalClientThreadsActive);
-        builder.field("client_threads_total", totalClientThreadsTotal);
-        builder.field("server_threads_active", totalServerThreadsActive);
-        builder.field("server_threads_total", totalServerThreadsTotal);
-        builder.field("connections_active", totalConnections);
-        builder.field("channels_active", totalChannels);
-        if (totalClientThreadsTotal > 0) {
-            builder.field("client_thread_utilization_percent", (totalClientThreadsActive * 100.0) / totalClientThreadsTotal);
-        }
-        if (totalServerThreadsTotal > 0) {
-            builder.field("server_thread_utilization_percent", (totalServerThreadsActive * 100.0) / totalServerThreadsTotal);
-        }
+        builder.endObject();
+
         builder.endObject();
     }
 }
