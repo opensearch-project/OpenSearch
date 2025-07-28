972ce140f70826460a52d8fce4a8575460250dd1
Sawan Srivastava <sawan1210@gmail.com>
2025-07-28 19:59:11 +0000

    more like Lucene's ConjunctionDISI
    Signed-off-by: Sawan Srivastava <sawan1210@gmail.com>

--- server/src/main/java/org/opensearch/search/approximate/ApproximateConjunctionScorer.java
+++ server/src/main/java/org/opensearch/search/approximate/ApproximateConjunctionScorer.java
@@ -38,6 +38,19 @@ public class ApproximateConjunctionScorer extends Scorer {
         this.score = boost;
     }
 
+    /**
+     * Creates a new ApproximateConjunctionScorer with an existing conjunction.
+     *
+     * @param boost The boost factor
+     * @param scoreMode The score mode
+     * @param conjunctionDISI The existing conjunction to reuse
+     */
+    public ApproximateConjunctionScorer(float boost, ScoreMode scoreMode, ApproximateConjunctionDISI conjunctionDISI) {
+        // Reuse the existing conjunction instead of creating a new one
+        this.approximateConjunctionDISI = conjunctionDISI;
+        this.score = boost;
+    }
+
     @Override
     public DocIdSetIterator iterator() {
         return approximateConjunctionDISI;
