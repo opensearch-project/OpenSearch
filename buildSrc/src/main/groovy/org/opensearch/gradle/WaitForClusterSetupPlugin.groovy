ce6d933b5557d17b172b6fb6c31d17cc76592551
Craig Perkins <cwperx@amazon.com>
2025-08-01 10:14:10 -0400

    Remove extension
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- buildSrc/src/main/groovy/org/opensearch/gradle/WaitForClusterSetupPlugin.groovy
+++ buildSrc/src/main/groovy/org/opensearch/gradle/WaitForClusterSetupPlugin.groovy
@@ -14,16 +14,6 @@ import org.gradle.api.Project
 class WaitForClusterSetupPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.extensions.create('clusterSetup', ClusterSetupExtension)
-        
         project.task('waitForClusterSetup', type: WaitForClusterSetupTask)
     }
-}
-
-class ClusterSetupExtension {
-    boolean securityEnabled = false
-    String protocol = 'http'
-    String username = 'admin'
-    String password = 'admin'
-    int timeoutMs = 180000
 }
