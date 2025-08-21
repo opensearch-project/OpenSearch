026e3cdb579db1fa9b1ac6e9b300f56754e0486d
Himanshu Kakar <hkakar@microsoft.com>
2025-08-21 14:05:18 +0530

    added the missing import
    Signed-off-by: Himanshu Kakar <hkakar@microsoft.com>

--- server/src/main/java/org/opensearch/index/store/CompositeDirectory.java
+++ server/src/main/java/org/opensearch/index/store/CompositeDirectory.java
@@ -32,6 +32,7 @@ import org.opensearch.threadpool.ThreadPool;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
