27e5939899af56432cd84b404845992256b0578e
Arafat Mridul <mridularafat735@gmail.com>
2025-08-21 19:28:29 +0600

    Changed value inside add()

---
+++ Assignment/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("5 + 5" + add(15, 5));
+        System.out.println("5 - 5" + subtract(5, 5));
+        System.out.println("5 * 5" + multiply(5, 5));
+        System.out.println("5 / 5" + divide(5, 5));
+    }
+    
+    public static float add(float num1, float num2) {
+        return num1 + num2;
+    }
+    
+    public static float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+    
+    public static float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+    
+    public static float divide(float num1, float num2) {
+        if(num2 == 0) return Float.MAX_VALUE;
+        return num1 / num2;
+    }
+}
