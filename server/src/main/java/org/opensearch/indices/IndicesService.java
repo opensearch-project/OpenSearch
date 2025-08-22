ad95bff862ee33f3bc894c75ec467cc14350a126
Anthony Leong <aj.leong623@gmail.com>
2025-08-22 12:01:29 -0700

    added integration tests
    Signed-off-by: Anthony Leong <aj.leong623@gmail.com>

--- server/src/main/java/org/opensearch/indices/IndicesService.java
+++ server/src/main/java/org/opensearch/indices/IndicesService.java
@@ -1367,9 +1367,10 @@ public class IndicesService extends AbstractLifecycleComponent
      *
      * @param stats Instance storing {@link DocStatusStats}
      */
+    /*@Deprecated(since = "3.3.0")
     public void addDocStatusStats(final org.opensearch.index.shard.IndexingStats.Stats.DocStatusStats stats) {
         oldShardsStats.indexingStats.getTotal().getDocStatusStats().add(stats);
-    }
+    }*/
 
     /**
      * Accumulate stats from the passed Object
