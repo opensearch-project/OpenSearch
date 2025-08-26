87352b75b3b7b5e545b302fbe13f4293dcf0eb06
Atri Sharma <atri.jiit@gmail.com>
2025-08-26 09:03:31 +0530

    Spotless checks
    Signed-off-by: Atri Sharma <atri.jiit@gmail.com>

--- server/src/main/java/org/opensearch/search/query/rewriters/package-info.java
+++ server/src/main/java/org/opensearch/search/query/rewriters/package-info.java
@@ -8,24 +8,24 @@
 
 /**
  * Query rewriting optimizations for improving search performance.
- * 
+ *
  * <p>This package contains various query rewriters that transform queries
  * into more efficient forms while maintaining semantic equivalence.
- * 
+ *
  * <p>The rewriters include:
  * <ul>
- *   <li>{@link org.opensearch.search.query.rewriters.BooleanFlatteningRewriter} - 
+ *   <li>{@link org.opensearch.search.query.rewriters.BooleanFlatteningRewriter} -
  *       Flattens nested boolean queries with single clauses</li>
- *   <li>{@link org.opensearch.search.query.rewriters.MatchAllRemovalRewriter} - 
+ *   <li>{@link org.opensearch.search.query.rewriters.MatchAllRemovalRewriter} -
  *       Removes redundant match_all queries from boolean clauses</li>
- *   <li>{@link org.opensearch.search.query.rewriters.TermsMergingRewriter} - 
+ *   <li>{@link org.opensearch.search.query.rewriters.TermsMergingRewriter} -
  *       Merges multiple term queries on the same field into a single terms query</li>
- *   <li>{@link org.opensearch.search.query.rewriters.MustNotToShouldRewriter} - 
+ *   <li>{@link org.opensearch.search.query.rewriters.MustNotToShouldRewriter} -
  *       Transforms must_not queries to should queries for better performance</li>
- *   <li>{@link org.opensearch.search.query.rewriters.MustToFilterRewriter} - 
+ *   <li>{@link org.opensearch.search.query.rewriters.MustToFilterRewriter} -
  *       Moves scoring-irrelevant queries from must to filter clauses</li>
  * </ul>
- * 
+ *
  * @opensearch.internal
  */
 package org.opensearch.search.query.rewriters;
