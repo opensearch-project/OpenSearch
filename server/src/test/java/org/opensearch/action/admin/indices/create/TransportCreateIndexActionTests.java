ea26c3ced78ae6284e898d644a6f20deed79ba09
Nils Bandener <nils.bandener@eliatra.com>
2025-09-11 23:38:28 +0200

    Additional actions
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>

--- server/src/test/java/org/opensearch/action/admin/indices/create/TransportCreateIndexActionTests.java
+++ server/src/test/java/org/opensearch/action/admin/indices/create/TransportCreateIndexActionTests.java
@@ -8,11 +8,16 @@
 
 package org.opensearch.action.admin.indices.create;
 
+import org.opensearch.action.admin.indices.alias.Alias;
+import org.opensearch.action.admin.indices.alias.IndicesAliasesAction;
 import org.opensearch.action.support.ActionFilter;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.cluster.ClusterState;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.metadata.MetadataCreateIndexService;
+import org.opensearch.cluster.metadata.ResolvedIndices;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.index.mapper.MappingTransformerRegistry;
 import org.opensearch.test.OpenSearchTestCase;
@@ -40,8 +45,6 @@ public class TransportCreateIndexActionTests extends OpenSearchTestCase {
     private ThreadPool threadPool;
     @Mock
     private MetadataCreateIndexService createIndexService;
-    @Mock
-    private IndexNameExpressionResolver indexNameExpressionResolver;
 
     @Mock
     private MappingTransformerRegistry mappingTransformerRegistry;
@@ -66,14 +69,12 @@ public class TransportCreateIndexActionTests extends OpenSearchTestCase {
             threadPool,
             createIndexService,
             actionFilters,
-            indexNameExpressionResolver,
+            new IndexNameExpressionResolver(new ThreadContext(Settings.EMPTY)),
             mappingTransformerRegistry
         );
     }
 
     public void testClusterManagerOperation_usesTransformedMapping() {
-        when(indexNameExpressionResolver.resolveDateMathExpression(any())).thenReturn("test-index");
-
         // Arrange: Create a test request
         final CreateIndexRequest request = new CreateIndexRequest("test-index");
         request.mapping("{\"properties\": {\"field\": {\"type\": \"text\"}}}");
@@ -83,11 +84,7 @@ public class TransportCreateIndexActionTests extends OpenSearchTestCase {
 
         // Capture ActionListener passed to applyTransformers
         final ArgumentCaptor<ActionListener<String>> listenerCaptor = ArgumentCaptor.forClass(ActionListener.class);
-        doNothing().when(mappingTransformerRegistry).applyTransformers(
-            anyString(),
-            any(),
-            listenerCaptor.capture()
-        );
+        doNothing().when(mappingTransformerRegistry).applyTransformers(anyString(), any(), listenerCaptor.capture());
 
         // Act: Call the method
         action.clusterManagerOperation(request, clusterState, responseListener);
@@ -96,12 +93,22 @@ public class TransportCreateIndexActionTests extends OpenSearchTestCase {
         listenerCaptor.getValue().onResponse(transformedMapping);
 
         // Assert: Capture request sent to createIndexService
-        ArgumentCaptor<CreateIndexClusterStateUpdateRequest> updateRequestCaptor =
-            ArgumentCaptor.forClass(CreateIndexClusterStateUpdateRequest.class);
+        ArgumentCaptor<CreateIndexClusterStateUpdateRequest> updateRequestCaptor = ArgumentCaptor.forClass(
+            CreateIndexClusterStateUpdateRequest.class
+        );
         verify(createIndexService, times(1)).createIndex(updateRequestCaptor.capture(), any());
 
         // Ensure transformed mapping is passed correctly
         CreateIndexClusterStateUpdateRequest capturedRequest = updateRequestCaptor.getValue();
         assertEquals(transformedMapping, capturedRequest.mappings());
     }
+
+    public void testResolveIndices() {
+        assertEquals(ResolvedIndices.of("test_index"), action.resolveIndices(new CreateIndexRequest("test_index")));
+        assertEquals(
+            ResolvedIndices.of("test_index").withLocalSubActions(IndicesAliasesAction.INSTANCE, ResolvedIndices.Local.of("test_alias")),
+            action.resolveIndices(new CreateIndexRequest("test_index").alias(new Alias("test_alias")))
+        );
+    }
+
 }
