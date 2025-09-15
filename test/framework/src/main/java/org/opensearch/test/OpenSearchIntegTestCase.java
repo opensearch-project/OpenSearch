11c7e246167737547c7df4c16d89836a1a784e12
Finn Carroll <carrofin@amazon.com>
2025-09-15 14:41:41 -0700

    Remove unused test deps.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- test/framework/src/main/java/org/opensearch/test/OpenSearchIntegTestCase.java
+++ test/framework/src/main/java/org/opensearch/test/OpenSearchIntegTestCase.java
@@ -2110,7 +2110,7 @@ public abstract class OpenSearchIntegTestCase extends OpenSearchTestCase {
         );
     }
 
-    protected NodeConfigurationSource getNodeConfigSource() {
+    private NodeConfigurationSource getNodeConfigSource() {
         Settings.Builder initialNodeSettings = Settings.builder();
         if (addMockTransportService()) {
             initialNodeSettings.put(NetworkModule.TRANSPORT_TYPE_KEY, getTestTransportType());
