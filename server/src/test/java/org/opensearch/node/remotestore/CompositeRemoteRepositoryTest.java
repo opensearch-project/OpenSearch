d85ae39dd0c4618791309771ca2be132a678d67c
Pranit Kumar <pranikum@amazon.com>
2025-09-22 10:33:08 +0530

    Fix imports
    Signed-off-by: Pranit Kumar <pranikum@amazon.com>

--- server/src/test/java/org/opensearch/node/remotestore/CompositeRemoteRepositoryTest.java
+++ server/src/test/java/org/opensearch/node/remotestore/CompositeRemoteRepositoryTest.java
@@ -12,9 +12,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opensearch.cluster.metadata.RepositoryMetadata;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.node.NodeRoleSettings;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class CompositeRemoteRepositoryTest {
 
