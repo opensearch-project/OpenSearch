dae836318349b9884cdfa4659c139183d38fed23
Finn Carroll <carrofin@amazon.com>
2025-09-22 17:25:15 -0700

    Populate package-info.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- modules/transport-grpc/transport-grpc-server-spi/src/main/java/org/opensearch/transport/grpc/server/spi/package-info.java
+++ modules/transport-grpc/transport-grpc-server-spi/src/main/java/org/opensearch/transport/grpc/server/spi/package-info.java
@@ -7,6 +7,29 @@
  */
 
 /**
- * TODO - REFERENCE PREVIOUS SPI COMMENT
+ * Service Provider Interface (SPI) for extending gRPC with additional gRPC BindableService implementations.
+ * <p>
+ * Enables plugins extending the grpc-transport module to inject their own services into the gRPC server by extending
+ * {@link org.opensearch.transport.grpc.server.spi.GrpcServiceFactory} interface. Provides some additional resources
+ * such as OpenSearch client, settings, thread pool, enabling more complex service implementations which can execute
+ * requests on the OpenSearch server.
+ * </p>
+ * <p>
+ * This SPI contains on interface:
+ * </p>
+ * <ul>
+ *   <li>{@link org.opensearch.transport.grpc.server.spi.GrpcServiceFactory} -
+ *       Interface for implementing custom query converters</li>
+ * </ul>
+ * <p>
+ * The SPI mechanism leverages OpenSearch's {@code ExtensiblePlugin} framework. Plugins must:
+ * <ul>
+ *   <li>Implement {@link org.opensearch.transport.grpc.server.spi.GrpcServiceFactory}</li>
+ *   <li>Create a {@code META-INF/services} file listing their converter implementation</li>
+ *   <li>Declare {@code transport-grpc} in their plugin descriptor's {@code extended.plugins} list</li>
+ * </ul>
+ * </p>
+ *
+ * @since 3.3.0
  */
 package org.opensearch.transport.grpc.server.spi;
