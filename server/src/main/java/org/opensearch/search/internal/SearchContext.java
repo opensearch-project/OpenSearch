3f78994931f020ea6a84eb1698f2c3da2fb2ea70
Atri Sharma <atri.jiit@gmail.com>
2025-09-24 23:16:49 +0530

    Add spotless output
    Signed-off-by: Atri Sharma <atri.jiit@gmail.com>

--- server/src/main/java/org/opensearch/search/internal/SearchContext.java
+++ server/src/main/java/org/opensearch/search/internal/SearchContext.java
@@ -78,10 +78,10 @@ import org.opensearch.search.fetch.subphase.highlight.SearchHighlightContext;
 import org.opensearch.search.profile.Profilers;
 import org.opensearch.search.query.QuerySearchResult;
 import org.opensearch.search.query.ReduceableSearchResult;
+import org.opensearch.search.query.StreamingSearchMode;
 import org.opensearch.search.rescore.RescoreContext;
 import org.opensearch.search.sort.SortAndFormats;
 import org.opensearch.search.suggest.SuggestionSearchContext;
-import org.opensearch.search.query.StreamingSearchMode;
 
 import java.util.Collection;
 import java.util.HashMap;
