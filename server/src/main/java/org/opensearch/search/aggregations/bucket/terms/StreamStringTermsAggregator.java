48b08fb794071a7e951fe4a3af18c37494d3bf68
Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
2025-10-01 18:26:56 -0700

    Expand streaming aggregations to numeric terms (#19335)
    * Expand streaming aggregations to numeric terms
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Add tests
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Changelog
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Fix gradle assemble
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * update test coverage
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * remove custom stream package
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * remove custom stream package
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * remove unused results
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    ---------
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>

--- server/src/main/java/org/opensearch/search/aggregations/bucket/terms/StreamStringTermsAggregator.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/terms/StreamStringTermsAggregator.java
@@ -46,12 +46,12 @@ public class StreamStringTermsAggregator extends AbstractStringTermsAggregator {
     private final ValuesSource.Bytes.WithOrdinals valuesSource;
     protected int segmentsWithSingleValuedOrds = 0;
     protected int segmentsWithMultiValuedOrds = 0;
-    protected final ResultStrategy<?, ?, ?> resultStrategy;
+    protected final ResultStrategy<?, ?> resultStrategy;
 
     public StreamStringTermsAggregator(
         String name,
         AggregatorFactories factories,
-        Function<StreamStringTermsAggregator, ResultStrategy<?, ?, ?>> resultStrategy,
+        Function<StreamStringTermsAggregator, ResultStrategy<?, ?>> resultStrategy,
         ValuesSource.Bytes.WithOrdinals valuesSource,
         BucketOrder order,
         DocValueFormat format,
@@ -72,11 +72,6 @@ public class StreamStringTermsAggregator extends AbstractStringTermsAggregator {
         super.doReset();
         valueCount = 0;
         sortedDocValuesPerBatch = null;
-    }
-
-    @Override
-    protected boolean tryPrecomputeAggregationForLeaf(LeafReaderContext ctx) throws IOException {
-        return false;
     }
 
     @Override
@@ -143,10 +138,9 @@ public class StreamStringTermsAggregator extends AbstractStringTermsAggregator {
     /**
      * Strategy for building results.
      */
-    abstract class ResultStrategy<
-        R extends InternalAggregation,
-        B extends InternalMultiBucketAggregation.InternalBucket,
-        TB extends InternalMultiBucketAggregation.InternalBucket> implements Releasable {
+    public abstract class ResultStrategy<R extends InternalAggregation, B extends InternalMultiBucketAggregation.InternalBucket>
+        implements
+            Releasable {
 
         // build aggregation batch for stream search
         InternalAggregation[] buildAggregationsBatch(long[] owningBucketOrds) throws IOException {
@@ -247,7 +241,12 @@ public class StreamStringTermsAggregator extends AbstractStringTermsAggregator {
         abstract B buildFinalBucket(long ordinal, long docCount) throws IOException;
     }
 
-    class StandardTermsResults extends ResultStrategy<StringTerms, StringTerms.Bucket, GlobalOrdinalsStringTermsAggregator.OrdBucket> {
+    /**
+     * StandardTermsResults for string terms
+     *
+     * @opensearch.internal
+     */
+    public class StandardTermsResults extends ResultStrategy<StringTerms, StringTerms.Bucket> {
         @Override
         String describe() {
             return "streaming_terms";
@@ -314,7 +313,7 @@ public class StreamStringTermsAggregator extends AbstractStringTermsAggregator {
 
             StringTerms.Bucket result = new StringTerms.Bucket(term, docCount, null, showTermDocCountError, 0, format);
             result.bucketOrd = ordinal;
-            result.docCountError = 0;
+            result.setDocCountError(0);
             return result;
         }
 
