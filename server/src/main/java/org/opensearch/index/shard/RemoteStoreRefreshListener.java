996a79ebda3f97e6277e253658fdf9ecd467c8b2
Hyunsang Han <gustkd3@gmail.com>
2025-10-09 09:01:26 +0900

    Refactor metadata upload optimization
    Signed-off-by: Hyunsang Han <gustkd3@gmail.com>

--- server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
+++ server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
@@ -267,15 +267,12 @@ public final class RemoteStoreRefreshListener extends ReleasableRetryableRefresh
                         public void onResponse(Void unused) {
                             try {
                                 logger.debug("New segments upload successful");
-                                Collection<String> filteredFiles = localSegmentsPostRefresh.stream()
-                                    .filter(file -> !skipUpload(file))
-                                    .collect(Collectors.toList());
-                                if (!filteredFiles.isEmpty()) {
+                                if (localSegmentsPostRefresh.stream().allMatch(file -> skipUpload(file))) {
+                                    logger.debug("Skipping metadata upload - no new segments were uploaded");
+                                } else {
                                     // Start metadata file upload
                                     uploadMetadata(localSegmentsPostRefresh, segmentInfos, checkpoint);
                                     logger.debug("Metadata upload successful");
-                                } else {
-                                    logger.debug("Skipping metadata upload - no new segments were uploaded");
                                 }
                                 clearStaleFilesFromLocalSegmentChecksumMap(localSegmentsPostRefresh);
                                 onSuccessfulSegmentsSync(
