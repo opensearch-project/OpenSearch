546257ccbe4662b613287d9e45b3dc2dc925ca9f
Bharathwaj G <bharath78910@gmail.com>
2025-10-31 23:25:32 +0530

    local recovery via catalogsnapshot (#19841)
    Signed-off-by: bharath-techie <bharath78910@gmail.com>

--- server/src/main/java/org/opensearch/index/engine/exec/WriterFileSet.java
+++ server/src/main/java/org/opensearch/index/engine/exec/WriterFileSet.java
@@ -8,12 +8,17 @@
 
 package org.opensearch.index.engine.exec;
 
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+import org.opensearch.core.common.io.stream.Writeable;
+
+import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
-public class WriterFileSet implements Serializable {
+public class WriterFileSet implements Serializable, Writeable {
 
     private final String directory;
     private final long writerGeneration;
@@ -23,6 +28,30 @@ public class WriterFileSet implements Serializable {
         this.files = new HashSet<>();
         this.writerGeneration = writerGeneration;
         this.directory = directory.toString();
+    }
+
+    public WriterFileSet(StreamInput in) throws IOException {
+        this.directory = in.readString();
+        this.writerGeneration = in.readLong();
+
+        int fileCount = in.readVInt();
+        this.files = new HashSet<>(fileCount);
+        for (int i = 0; i < fileCount; i++) {
+            this.files.add(in.readString());
+        }
+    }
+
+    /**
+     * Serialize this WriterFileSet to StreamOutput
+     */
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeString(directory);
+        out.writeLong(writerGeneration);
+        out.writeVInt(files.size());
+        for (String file : files) {
+            out.writeString(file);
+        }
     }
 
     public void add(String file) {
