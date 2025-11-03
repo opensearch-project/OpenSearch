c161a3ff0ea6e1e263100e0f405d0758f5dfbfc2
Sandesh Kumar <sandeshkr419@gmail.com>
2025-11-03 14:49:12 -0800

    Revert Accidental Missing import
    Signed-off-by: Sandesh Kumar <sandeshkr419@gmail.com>

--- server/src/test/java/org/opensearch/index/engine/LeafSorterOptimizationTests.java
+++ server/src/test/java/org/opensearch/index/engine/LeafSorterOptimizationTests.java
@@ -31,6 +31,7 @@ import java.util.Comparator;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
