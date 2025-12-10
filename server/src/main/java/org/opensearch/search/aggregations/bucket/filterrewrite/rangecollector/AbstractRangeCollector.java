3b8467c4720e924b9708e37a5a3340a434a9f73a
Anthony Leong <aj.leong623@gmail.com>
2025-12-09 21:58:13 -0800

    spotless
    Signed-off-by: Anthony Leong <aj.leong623@gmail.com>

--- server/src/main/java/org/opensearch/search/aggregations/bucket/filterrewrite/rangecollector/AbstractRangeCollector.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/filterrewrite/rangecollector/AbstractRangeCollector.java
@@ -53,13 +53,13 @@ public abstract class AbstractRangeCollector implements RangeCollector {
     @Override
     public boolean withinLowerBound(byte[] value) {
         return Ranges.withinLowerBound(value, ranges.getLowers()[activeIndex]);
-        //return ranges.withinLowerBound(value, ranges.getLowers()[activeIndex]);
+        // return ranges.withinLowerBound(value, ranges.getLowers()[activeIndex]);
     }
 
     @Override
     public boolean withinUpperBound(byte[] value) {
         return Ranges.withinLowerBound(value, ranges.getLowers()[activeIndex]);
-        //return ranges.withinUpperBound(value, ranges.getUppers()[activeIndex]);
+        // return ranges.withinUpperBound(value, ranges.getUppers()[activeIndex]);
     }
 
     @Override
