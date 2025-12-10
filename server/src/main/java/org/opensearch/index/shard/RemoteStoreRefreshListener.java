98319a3f017670aea4ef334a5db686b00479bd93
sjs004 <simarjeet.singh004@gmail.com>
2025-12-10 17:34:16 +0530

    Move the optimisation to upload Metadata method
    Signed-off-by: sjs004 <simarjeet.singh004@gmail.com>

--- server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
+++ server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
@@ -449,8 +449,8 @@ public final class RemoteStoreRefreshListener extends ReleasableRetryableRefresh
             && this.lastUploadedLuceneGeneration == currentLuceneGeneration
             && this.lastUploadedCheckpointVersion == currentCheckpointVersion) {
 
-            // LOGIC: The index state (segment files, primary authority, and replication progress)
-            // is identical to the last state successfully uploaded. Skip the costly remote I/O.
+            // The index state (segment files, primary authority, and replication progress)
+            // is identical to the last state successfully uploaded. Skip the remote I/O.
             return;
         }
 
