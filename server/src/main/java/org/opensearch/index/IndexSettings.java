3f2917ca4dafd0aa4e02b19c30233f120fb71324
Prudhvi Godithi <pgodithi@amazon.com>
2025-12-18 13:56:24 -0800

    Default Enable
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- server/src/main/java/org/opensearch/index/IndexSettings.java
+++ server/src/main/java/org/opensearch/index/IndexSettings.java
@@ -835,7 +835,7 @@ public final class IndexSettings {
 
     public static final Setting<Boolean> INDEX_DOC_ID_FUZZY_SET_ENABLED_SETTING = Setting.boolSetting(
         "index.optimize_doc_id_lookup.fuzzy_set.enabled",
-        false,
+        true,
         Property.IndexScope,
         Property.Dynamic
     );
