5660fcabf2b32609ddf1d51156d6275a125741af
Harsh Garg <gkharsh@amazon.com>
2026-01-19 09:26:10 +0530

    Updating documentation for new remote publication tests
    Signed-off-by: Harsh Garg <gkharsh@amazon.com>

--- qa/rolling-upgrade/src/test/java/org/opensearch/upgrades/RemotePublicationClusterStateIT.java
+++ qa/rolling-upgrade/src/test/java/org/opensearch/upgrades/RemotePublicationClusterStateIT.java
@@ -14,8 +14,26 @@ import org.opensearch.common.settings.Settings;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+/**
+ * Integration tests for remote publication enabled clusters during rolling upgrades.
+ * <p>
+ * The test verifies that cluster state (indices, templates, settings) is correctly
+ * published and persisted to remote storage during version upgrades.
+ * <p>
+ * For debugging failing tests:
+ * <ul>
+ *   <li>Check remote cluster state configuration in test cluster settings</li>
+ *   <li>Check logs for remote publication errors during upgrade phases</li>
+ *   <li>If the failures are related to deserialization of remote cluster state on new version nodes, verify if any new
+ *   attribute(s) added to ClusterState components are correctly deserialized in corresponding RemoteEntities</li>
+ * </ul>
+ * <p>
+ * See: <a href="https://docs.opensearch.org/latest/tuning-your-cluster/availability-and-recovery/remote-store/remote-cluster-state/">
+ * Remote Cluster State Documentation</a>
+ */
 @SuppressWarnings("unchecked")
 public class RemotePublicationClusterStateIT extends AbstractRollingTestCase {
     private static final String templateName = "my_template";
@@ -63,35 +81,114 @@ public class RemotePublicationClusterStateIT extends AbstractRollingTestCase {
 
     private static void createIndexTemplate() throws Exception {
         Request putIndexTemplate = new Request("PUT", "_template/" + templateName);
-        String indexTemplateJson = "{\"index_patterns\": [\"pattern-*\", \"log-*\"],\"settings\": {\"number_of_shards\": 3,\"number_of_replicas\": 1,\"index.refresh_interval\": \"5s\"},\"mappings\": {\"properties\": {\"timestamp\": {\"type\": \"date\"},\"message\": {\"type\": \"text\",\"fields\": {\"keyword\": {\"type\": \"keyword\",\"ignore_above\": 256}}},\"level\": {\"type\": \"keyword\"}}},\"aliases\": {\"mydata\": {}},\"version\": 1}";
+        String indexTemplateJson = """
+            {
+                "index_patterns": ["pattern-*", "log-*"],
+                "settings": {
+                    "number_of_shards": 3,
+                    "number_of_replicas": 1,
+                    "index.refresh_interval": "5s"
+                },
+                "mappings": {
+                    "properties": {
+                        "timestamp": {
+                            "type": "date"
+                        },
+                        "message": {
+                            "type": "text",
+                            "fields": {
+                                "keyword": {
+                                    "type": "keyword",
+                                    "ignore_above": 256
+                                }
+                            }
+                        },
+                        "level": {
+                            "type": "keyword"
+                        }
+                    }
+                },
+                "aliases": {
+                    "mydata": {}
+                },
+                "version": 1
+            }""";
         putIndexTemplate.setJsonEntity(indexTemplateJson);
         client().performRequest(putIndexTemplate);
     }
 
     private static void createComponentTemplate() throws Exception {
         Request putComponentTemplate = new Request("PUT", "_component_template/" + componentTemplateName);
-        String componentTemplateJson = "{\"template\": {\"mappings\": {\"properties\": {\"name\": {\"type\": \"text\"}}}},\"version\": 1}";
+        String componentTemplateJson = """
+            {
+                "template": {
+                    "mappings": {
+                        "properties": {
+                            "name": {
+                                "type": "text"
+                            }
+                        }
+                    }
+                },
+                "version": 1
+            }""";
         putComponentTemplate.setJsonEntity(componentTemplateJson);
         client().performRequest(putComponentTemplate);
     }
 
     private static void createComposableTemplate() throws Exception {
         Request putComposableTemplate = new Request("PUT", "_index_template/composable_template");
-        String composableTemplateJson = "{\"index_patterns\": [\"te-*\", \"bar-*\"],\"template\": {\"mappings\": {\"properties\": {\"email\": {\"type\": \"keyword\"}}}},\"version\": 1,\"composed_of\": [\"" + componentTemplateName + "\"]}";
+        String composableTemplateJson = String.format(Locale.ROOT, """
+            {
+                "index_patterns": ["te-*", "bar-*"],
+                "template": {
+                    "mappings": {
+                        "properties": {
+                            "email": {
+                                "type": "keyword"
+                            }
+                        }
+                    }
+                },
+                "version": 1,
+                "composed_of": ["%s"]
+            }""", componentTemplateName);
         putComposableTemplate.setJsonEntity(composableTemplateJson);
         client().performRequest(putComposableTemplate);
     }
 
     private static void addPersistentSettings() throws Exception {
         Request putSettings = new Request("PUT", "_cluster/settings");
-        String settingsJson = "{\"persistent\": {\"cluster\": {\"remote\": {\"cluster\": {\"seeds\": [\"127.0.0.1:9300\"]}}}}}";
+        String settingsJson = """
+            {
+                "persistent": {
+                    "cluster": {
+                        "remote": {
+                            "cluster": {
+                                "seeds": ["127.0.0.1:9300"]
+                            }
+                        }
+                    }
+                }
+            }""";
         putSettings.setJsonEntity(settingsJson);
         assertOK(client().performRequest(putSettings));
     }
 
     private static void addTransientSettings() throws Exception {
         Request putSettings = new Request("PUT", "_cluster/settings");
-        String settingsJson = "{\"transient\": {\"cluster\": {\"remote\": {\"cluster\": {\"seeds\": [\"127.0.0.1:9300\"]}}}}}";
+        String settingsJson = """
+            {
+                "transient": {
+                    "cluster": {
+                        "remote": {
+                            "cluster": {
+                                "seeds": ["127.0.0.1:9300"]
+                            }
+                        }
+                    }
+                }
+            }""";
         putSettings.setJsonEntity(settingsJson);
         assertOK(client().performRequest(putSettings));
     }
