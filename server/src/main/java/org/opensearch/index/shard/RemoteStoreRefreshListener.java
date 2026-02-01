1df35446a4cfd1c3eec946799059dcf457ae5a8e
Kamal Nayan <askkamal@amazon.com>
2026-02-01 13:42:15 +0530

    Removed unnecessary changes

--- server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
+++ server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
@@ -21,7 +21,6 @@ import org.opensearch.common.logging.Loggers;
 import org.opensearch.common.unit.TimeValue;
 import org.opensearch.common.util.UploadListener;
 import org.opensearch.core.action.ActionListener;
-import org.opensearch.index.engine.EngineNotInitializedException;
 import org.opensearch.index.engine.InternalEngine;
 import org.opensearch.index.engine.exec.FileMetadata;
 import org.opensearch.index.engine.exec.bridge.Indexer;
@@ -487,13 +486,7 @@ public final class RemoteStoreRefreshListener extends ReleasableRetryableRefresh
         userData.put(SequenceNumbers.MAX_SEQ_NO, Long.toString(maxSeqNo));
         catalogSnapshotCloned.setUserData(userData, false);
 
-        Indexer indexer = indexShard.getIndexer();
-
-        if(indexer == null) {
-            throw new EngineNotInitializedException("Engine is not initialized");
-        }
-
-        Translog.TranslogGeneration translogGeneration = indexer.translogManager().getTranslogGeneration();
+        Translog.TranslogGeneration translogGeneration = indexShard.getIndexer().translogManager().getTranslogGeneration();
         if (translogGeneration == null) {
             throw new UnsupportedOperationException("Encountered null TranslogGeneration while uploading metadata to remote segment store");
         } else {
