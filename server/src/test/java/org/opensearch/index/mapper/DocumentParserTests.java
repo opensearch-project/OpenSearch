d97a6c3b38a7293fdcf5ece63bd7b7aaade04153
krocky-cooky <kurotaku9679.sub@gmail.com>
2026-02-04 18:57:51 +0900

    add a testcase for geopoint array format
    Signed-off-by: krocky-cooky <kurotaku9679.sub@gmail.com>

--- server/src/test/java/org/opensearch/index/mapper/DocumentParserTests.java
+++ server/src/test/java/org/opensearch/index/mapper/DocumentParserTests.java
@@ -3532,4 +3532,28 @@ public class DocumentParserTests extends MapperServiceTestCase {
         assertNotNull(copiedFields);
         assertTrue(copiedFields.length > 0);
     }
+
+    public void testGeoPointArrayWithCopyTo() throws Exception {
+        DocumentMapper mapper = createDocumentMapper(mapping(b -> {
+            b.startObject("point");
+            {
+                b.field("type", "geo_point");
+                b.field("copy_to", "location_copy");
+            }
+            b.endObject();
+            b.startObject("location_copy").field("type", "geo_point").endObject();
+        }));
+
+        ParsedDocument doc = mapper.parse(source(b -> {
+            b.array("point", 74.00, 40.71);
+        }));
+
+        // Verify that the geo_point field itself exists
+        assertNotNull(doc.rootDoc().getField("point"));
+        
+        // Verify that the field was copied to the copy_to target
+        IndexableField[] copiedFields = doc.rootDoc().getFields("location_copy");
+        assertNotNull(copiedFields);
+        assertTrue(copiedFields.length > 0);
+    }
 }
