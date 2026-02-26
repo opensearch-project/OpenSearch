bc2f573e58de13011fb497bad0590c1b55b50e4b
Mikhail Urmich <m.urmich@jobware.de>
2026-02-26 10:06:54 +0100

    FetchSourceContextTests ii
    Signed-off-by: Mikhail Urmich <m.urmich@jobware.de>

--- server/src/test/java/org/opensearch/search/fetch/subphase/FetchSourceContextTests.java
+++ server/src/test/java/org/opensearch/search/fetch/subphase/FetchSourceContextTests.java
@@ -19,6 +19,7 @@ import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.common.Strings;
 import org.opensearch.core.common.bytes.BytesReference;
 import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.fetch.FetchContext;
 import org.opensearch.search.fetch.FetchSubPhase.HitContext;
@@ -36,9 +37,29 @@ import static org.mockito.Mockito.when;
 // ISSUE-20612 Source Validation
 public class FetchSourceContextTests extends OpenSearchTestCase {
 
-    public void testFetchSource() throws IOException {
-        XContentBuilder source = XContentFactory.jsonBuilder().startObject().field("field", "value").endObject();
-        FetchSourceContext.fromXContent(source);
+    private XContentParser createSourceParser(XContentBuilder source) throws IOException {
+        XContentParser parser = createParser(source);
+        parser.nextToken(); // move to start object
+        parser.nextToken(); // move to field name "_source"
+        parser.nextToken(); // move to _source value to parse
+        return parser;
     }
 
+    public void testFetchSource() throws IOException {
+        XContentBuilder source = XContentFactory.jsonBuilder()
+            .startObject().field("_source", true).endObject();
+        XContentParser parser = createSourceParser(source);
+
+        FetchSourceContext result = FetchSourceContext.fromXContent(parser);
+        assertEquals(FetchSourceContext.FETCH_SOURCE, result);
+    }
+
+    public void testDoNotFetchSource() throws IOException {
+        XContentBuilder source = XContentFactory.jsonBuilder()
+            .startObject().field("_source", false).endObject();
+        XContentParser parser = createSourceParser(source);
+
+        FetchSourceContext result = FetchSourceContext.fromXContent(parser);
+        assertEquals(FetchSourceContext.DO_NOT_FETCH_SOURCE, result);
+    }
 }
