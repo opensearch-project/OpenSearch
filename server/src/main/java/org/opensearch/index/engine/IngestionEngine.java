aab0a3d1caef97443308dee5ba3cfa560498539c
Rishab Nahata <rishabnahata07@gmail.com>
2026-03-11 03:13:58 +0530

    Wire IngestService from Node to IngestionEngine for pull-based ingestion (#20757)
    * Wire IngestService from Node to IngestionEngine for pull-based ingestion
    
    Signed-off-by: Rishab Nahata <rishabnahata07@gmail.com>

--- server/src/main/java/org/opensearch/index/engine/IngestionEngine.java
+++ server/src/main/java/org/opensearch/index/engine/IngestionEngine.java
@@ -40,6 +40,7 @@ import org.opensearch.indices.pollingingest.IngestionErrorStrategy;
 import org.opensearch.indices.pollingingest.IngestionSettings;
 import org.opensearch.indices.pollingingest.PollingIngestStats;
 import org.opensearch.indices.pollingingest.StreamPoller;
+import org.opensearch.ingest.IngestService;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -60,11 +61,17 @@ public class IngestionEngine extends InternalEngine {
     private StreamPoller streamPoller;
     private final IngestionConsumerFactory ingestionConsumerFactory;
     private final DocumentMapperForType documentMapperForType;
+    private final IngestService ingestService;
     private volatile IngestionShardPointer lastCommittedBatchStartPointer;
 
     public IngestionEngine(EngineConfig engineConfig, IngestionConsumerFactory ingestionConsumerFactory) {
+        this(engineConfig, ingestionConsumerFactory, null);
+    }
+
+    public IngestionEngine(EngineConfig engineConfig, IngestionConsumerFactory ingestionConsumerFactory, IngestService ingestService) {
         super(engineConfig);
         this.ingestionConsumerFactory = Objects.requireNonNull(ingestionConsumerFactory);
+        this.ingestService = ingestService;
         this.documentMapperForType = engineConfig.getDocumentMapperForTypeSupplier().get();
         registerDynamicIndexSettingsHandlers();
     }
