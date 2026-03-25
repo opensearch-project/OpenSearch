b48cc63f4c99f4793ff842ff08e1fd843dff4cc5
Craig Perkins <cwperx@amazon.com>
2026-03-25 14:43:16 -0400

    Address feedback
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- distribution/tools/plugin-cli/src/main/java/org/opensearch/tools/cli/plugin/PluginHelper.java
+++ distribution/tools/plugin-cli/src/main/java/org/opensearch/tools/cli/plugin/PluginHelper.java
@@ -31,10 +31,12 @@ public class PluginHelper {
      * @throws IOException if any I/O exception occurs while performing a file operation
      */
     public static Path verifyIfPluginExists(Path pluginPath, String pluginName) throws IOException {
-        List<Path> pluginSubFolders = Files.walk(pluginPath, 1).filter(Files::isDirectory).collect(Collectors.toList());
+        List<Path> pluginSubFolders = Files.walk(pluginPath, 1)
+            .filter(Files::isDirectory)
+            .filter(f -> !f.getFileName().toString().equals("lib"))
+            .collect(Collectors.toList());
         for (Path customPluginFolderPath : pluginSubFolders) {
             if (customPluginFolderPath != pluginPath
-                && "lib".equals(customPluginFolderPath.getFileName().toString()) == false
                 && !((customPluginFolderPath.getFileName().toString()).contains(".installing"))
                 && !((customPluginFolderPath.getFileName().toString()).contains(".removing"))) {
                 final PluginInfo info = PluginInfo.readFromProperties(customPluginFolderPath);
