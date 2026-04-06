a13b67e3c4755253c7522499861c01636d52fe46
Abhishek Som <abhissom@amazon.com>
2026-04-07 01:30:30 +0530

    Added support for Terms query to convert in RelNode format
    - Added TermsQueryTranslator to convert terms queries to Calcite Inbuilt MakeIn function which uses SEARCH and EQUALS operator
    - Registered TermsQueryTranslator in QueryRegistryFactory
    - Added integration tests for terms query including validation tests for boost, _name, and value_type
    - Updated README to document Terms query support

---
+++ sandbox/plugins/dsl-query-executor/src/main/java/org/opensearch/dsl/query/TermsQueryTranslator.java
@@ -0,0 +1,66 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.dsl.query;
+
+import org.apache.calcite.rel.type.RelDataTypeField;
+import org.apache.calcite.rex.RexNode;
+import org.opensearch.dsl.converter.ConversionContext;
+import org.opensearch.dsl.converter.ConversionException;
+import org.opensearch.index.query.AbstractQueryBuilder;
+import org.opensearch.index.query.QueryBuilder;
+import org.opensearch.index.query.TermsQueryBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Converts a {@link TermsQueryBuilder} to a Calcite IN RexNode.
+ */
+public class TermsQueryTranslator implements QueryTranslator {
+
+    @Override
+    public Class<? extends QueryBuilder> getQueryType() {
+        return TermsQueryBuilder.class;
+    }
+
+    @Override
+    public RexNode convert(QueryBuilder query, ConversionContext ctx) throws ConversionException {
+
+        TermsQueryBuilder termsQuery = (TermsQueryBuilder) query;
+
+        if (termsQuery.boost() != AbstractQueryBuilder.DEFAULT_BOOST) {
+            throw new ConversionException("Terms query does not support non-default boost");
+        }
+        if (termsQuery.queryName() != null) {
+            throw new ConversionException("Terms query does not support _name");
+        }
+        if (termsQuery.valueType() != TermsQueryBuilder.ValueType.DEFAULT) {
+            throw new ConversionException("Terms query does not support non-default value_type");
+        }
+
+        String fieldName = termsQuery.fieldName();
+        List<?> values = termsQuery.values();
+
+        if (values == null || values.isEmpty()) {
+            throw new ConversionException("Terms query must have values");
+        }
+
+        RelDataTypeField field = ctx.getRowType().getField(fieldName, false, false);
+        if (field == null) {
+            throw new ConversionException("Field '" + fieldName + "' not found in schema");
+        }
+
+        RexNode fieldRef = ctx.getRexBuilder().makeInputRef(field.getType(), field.getIndex());
+        List<RexNode> literals = values.stream()
+            .map(value -> ctx.getRexBuilder().makeLiteral(value, field.getType(), false))
+            .collect(Collectors.toList());
+
+        return ctx.getRexBuilder().makeIn(fieldRef, literals);
+    }
+}
---
+++ sandbox/plugins/dsl-query-executor/src/test/java/org/opensearch/dsl/query/TermsQueryTranslatorTests.java
@@ -0,0 +1,94 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.dsl.query;
+
+import org.apache.calcite.rex.RexCall;
+import org.apache.calcite.rex.RexInputRef;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.SqlKind;
+import org.opensearch.dsl.TestUtils;
+import org.opensearch.dsl.converter.ConversionContext;
+import org.opensearch.dsl.converter.ConversionException;
+import org.opensearch.index.query.QueryBuilders;
+import org.opensearch.index.query.TermsQueryBuilder;
+import org.opensearch.test.OpenSearchTestCase;
+
+public class TermsQueryTranslatorTests extends OpenSearchTestCase {
+
+    private final TermsQueryTranslator translator = new TermsQueryTranslator();
+    private final ConversionContext ctx = TestUtils.createContext();
+
+    public void testSingleValueUsesEquals() throws ConversionException {
+        RexNode result = translator.convert(QueryBuilders.termsQuery("name", "laptop"), ctx);
+
+        assertTrue(result instanceof RexCall);
+        RexCall call = (RexCall) result;
+        assertEquals(SqlKind.EQUALS, call.getKind());
+        assertEquals(2, call.getOperands().size());
+    }
+
+    public void testMultipleStringValuesUsesSearch() throws ConversionException {
+        RexNode result = translator.convert(QueryBuilders.termsQuery("name", "laptop", "phone"), ctx);
+
+        assertTrue(result instanceof RexCall);
+        RexCall call = (RexCall) result;
+        assertEquals(SqlKind.SEARCH, call.getKind());
+    }
+
+    public void testResolvesCorrectFieldIndex() throws ConversionException {
+        RexNode result = translator.convert(QueryBuilders.termsQuery("brand", "brandX", "brandY"), ctx);
+
+        RexCall call = (RexCall) result;
+        assertEquals(SqlKind.SEARCH, call.getKind());
+        RexInputRef fieldRef = (RexInputRef) call.getOperands().get(0);
+        assertEquals(2, fieldRef.getIndex());
+    }
+
+    public void testIntegerValues() throws ConversionException {
+        RexNode result = translator.convert(QueryBuilders.termsQuery("price", new Object[]{1200, 1500}), ctx);
+
+        RexCall call = (RexCall) result;
+        assertEquals(SqlKind.SEARCH, call.getKind());
+        RexInputRef fieldRef = (RexInputRef) call.getOperands().get(0);
+        assertEquals(1, fieldRef.getIndex());
+    }
+
+    public void testDoubleValuesUsesSearch() throws ConversionException {
+        RexNode result = translator.convert(
+            QueryBuilders.termsQuery("rating", new Object[]{4.5, 4.8, 5.0}), ctx);
+
+        RexCall call = (RexCall) result;
+        assertEquals(SqlKind.SEARCH, call.getKind());
+    }
+
+    public void testThrowsForUnknownField() {
+        expectThrows(ConversionException.class,
+            () -> translator.convert(QueryBuilders.termsQuery("nonexistent", "value"), ctx));
+    }
+
+    public void testThrowsForBoost() {
+        expectThrows(ConversionException.class,
+            () -> translator.convert(QueryBuilders.termsQuery("name", "laptop").boost(2.0f), ctx));
+    }
+
+    public void testThrowsForQueryName() {
+        expectThrows(ConversionException.class,
+            () -> translator.convert(QueryBuilders.termsQuery("name", "laptop").queryName("my_query"), ctx));
+    }
+
+    public void testThrowsForValueType() {
+        expectThrows(ConversionException.class,
+            () -> translator.convert(QueryBuilders.termsQuery("name", "laptop")
+                .valueType(TermsQueryBuilder.ValueType.BITMAP), ctx));
+    }
+
+    public void testReportsCorrectQueryType() {
+        assertEquals(TermsQueryBuilder.class, translator.getQueryType());
+    }
+}
