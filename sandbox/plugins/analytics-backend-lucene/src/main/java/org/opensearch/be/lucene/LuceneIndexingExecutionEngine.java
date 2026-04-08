02b44131bdc8026a3d510c77251586c7bcf0ff45
Arpit Bandejiya <abandeji@amazon.com>
2026-04-09 01:53:22 +0530

    Fix loggerCheck
    Signed-off-by: Arpit Bandejiya <abandeji@amazon.com>

--- sandbox/plugins/analytics-backend-lucene/src/main/java/org/opensearch/be/lucene/LuceneIndexingExecutionEngine.java
+++ sandbox/plugins/analytics-backend-lucene/src/main/java/org/opensearch/be/lucene/LuceneIndexingExecutionEngine.java
@@ -10,6 +10,7 @@ package org.opensearch.be.lucene;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.misc.store.HardlinkCopyDirectoryWrapper;
 import org.apache.lucene.store.Directory;
@@ -129,7 +130,10 @@ public class LuceneIndexingExecutionEngine implements IndexingExecutionEngine<Da
                 try {
                     IOUtils.rm(sourcePath);
                 } catch (IOException e) {
-                    logger.warn("Failed to delete source directory [{}] after addIndexes", sourcePath, e);
+                    logger.warn(
+                        () -> new ParameterizedMessage("Failed to delete source directory [{}] after addIndexes", sourcePath),
+                        e
+                    );
                 }
             }
         }
