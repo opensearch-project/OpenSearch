44c5c005702d7f8436dab43f84856e202723584f
Rishab Nahata <rishabnahata07@gmail.com>
2026-04-08 12:33:42 +0530

    Add multi-partition consumer factory methods to IngestionConsumerFactory
    Signed-off-by: Rishab Nahata <rishabnahata07@gmail.com>

--- plugins/ingestion-kafka/src/main/java/org/opensearch/plugin/kafka/KafkaConsumerFactory.java
+++ plugins/ingestion-kafka/src/main/java/org/opensearch/plugin/kafka/KafkaConsumerFactory.java
@@ -11,6 +11,8 @@ package org.opensearch.plugin.kafka;
 import org.opensearch.cluster.metadata.IngestionSource;
 import org.opensearch.index.IngestionConsumerFactory;
 
+import java.util.List;
+
 /**
  * Factory for creating Kafka consumers
  */
@@ -20,6 +22,8 @@ public class KafkaConsumerFactory implements IngestionConsumerFactory<KafkaParti
      * Configuration for the Kafka source
      */
     protected KafkaSourceConfig config;
+
+    private volatile int cachedPartitionCount = -1;
 
     /**
      * Constructor.
@@ -40,5 +44,34 @@ public class KafkaConsumerFactory implements IngestionConsumerFactory<KafkaParti
     @Override
     public KafkaOffset parsePointerFromString(String pointer) {
         return new KafkaOffset(Long.valueOf(pointer));
+    }
+
+    @Override
+    public int getSourcePartitionCount() {
+        if (cachedPartitionCount > 0) {
+            return cachedPartitionCount;
+        }
+        assert config != null;
+        // Create a temporary consumer to query partition metadata
+        try (KafkaPartitionConsumer tempConsumer = new KafkaPartitionConsumer(
+            "partition-count-query",
+            config,
+            0
+        )) {
+            cachedPartitionCount = tempConsumer.getPartitionCount();
+            return cachedPartitionCount;
+        }
+    }
+
+    @Override
+    public KafkaPartitionConsumer createMultiPartitionShardConsumer(String clientId, int shardId, List<Integer> partitionIds) {
+        assert config != null;
+        if (partitionIds.size() == 1) {
+            return new KafkaPartitionConsumer(clientId, config, partitionIds.get(0));
+        }
+        // TODO: Return KafkaMultiPartitionConsumer once implemented (PR 3)
+        throw new UnsupportedOperationException(
+            "Multi-partition consumer not yet implemented. Assign a single partition per shard or use partition_strategy=fixed."
+        );
     }
 }
