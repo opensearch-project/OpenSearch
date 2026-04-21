a8fb43fe0a85ac1d734cba6d2c83702146462fe9
Koustubh Gupta <thorkous@amazon.com>
2026-04-22 01:10:49 +0530

    Added delete Index engine abstraction.
    Signed-off-by: Koustubh Gupta <thorkous@amazon.com>

---
+++ server/src/main/java/org/opensearch/index/engine/dataformat/DeleteResult.java
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.index.engine.dataformat;
+
+/**
+ * Result of a document delete operation. Sealed to ensure exhaustive handling
+ * of success and failure cases.
+ */
+public sealed interface DeleteResult {
+
+    /**
+     * Indicates a successful delete.
+     *
+     * @param version the document version after deletion
+     * @param term the primary term of the shard
+     * @param seqNo the sequence number assigned to this delete operation
+     */
+    record Success(long version, long term, long seqNo) implements DeleteResult {
+    }
+
+    /**
+     * Indicates a failed delete.
+     *
+     * @param cause the exception that caused the failure
+     * @param version the document version at the time of failure
+     * @param term the primary term of the shard
+     * @param seqNo the sequence number assigned to this delete operation
+     */
+    record Failure(Exception cause, long version, long term, long seqNo) implements DeleteResult {
+    }
+}
