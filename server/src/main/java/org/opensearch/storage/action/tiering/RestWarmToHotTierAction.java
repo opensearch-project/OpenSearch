2105e1d876c8f4ccef4e5b1bd135a34214d222c7
Kavya Aggarwal <154978013+GeekGlider@users.noreply.github.com>
2026-04-23 13:59:51 +0530

    Add server-side implementation for HotToWarm, WarmToHot, and CancelTiering APIs (#21295)
    Signed-off-by: kavyaagg <kavyaagg@amazon.com>

--- server/src/main/java/org/opensearch/storage/action/tiering/RestWarmToHotTierAction.java
+++ server/src/main/java/org/opensearch/storage/action/tiering/RestWarmToHotTierAction.java
@@ -8,19 +8,35 @@
 
 package org.opensearch.storage.action.tiering;
 
+import org.opensearch.action.ActionType;
+import org.opensearch.action.support.clustermanager.AcknowledgedResponse;
+
+import static org.opensearch.storage.common.tiering.TieringUtils.W2H_TIERING_TYPE_KEY;
+
 /**
  * REST handler for moving indices to the hot tier.
- * getMigrationType and getTierAction will be added in the implementation PR.
  */
 public class RestWarmToHotTierAction extends RestBaseTierAction {
 
+    private static final String TARGET_TIER = "hot";
+
     /** Constructs a new RestWarmToHotTierAction. */
     public RestWarmToHotTierAction() {
-        super("hot");
+        super(TARGET_TIER);
     }
 
     @Override
     public String getName() {
         return "hot_tier_action";
+    }
+
+    @Override
+    protected String getMigrationType() {
+        return W2H_TIERING_TYPE_KEY;
+    }
+
+    @Override
+    protected ActionType<AcknowledgedResponse> getTierAction() {
+        return WarmToHotTierAction.INSTANCE;
     }
 }
