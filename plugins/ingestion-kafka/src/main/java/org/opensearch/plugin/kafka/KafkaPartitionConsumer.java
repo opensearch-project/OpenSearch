a17caa667c82e7860f0f5e2ba0e74b2accb54a13
Varun Bharadwaj <varunbharadwaj1995@gmail.com>
2026-04-28 18:04:41 -0700

    support configurable kafka metadata read timeout value
    Signed-off-by: Varun Bharadwaj <varunbharadwaj1995@gmail.com>

--- plugins/ingestion-kafka/src/main/java/org/opensearch/plugin/kafka/KafkaPartitionConsumer.java
+++ plugins/ingestion-kafka/src/main/java/org/opensearch/plugin/kafka/KafkaPartitionConsumer.java
@@ -45,8 +45,6 @@ public class KafkaPartitionConsumer implements IngestionShardConsumer<KafkaOffse
      * The Kafka consumer
      */
     protected final Consumer<byte[], byte[]> consumer;
-    // TODO: make this configurable
-    private final int timeoutMillis = 1000;
 
     private long lastFetchedOffset = -1;
     final String clientId;
@@ -76,7 +74,7 @@ public class KafkaPartitionConsumer implements IngestionShardConsumer<KafkaOffse
         this.config = config;
         String topic = config.getTopic();
         List<PartitionInfo> partitionInfos = AccessController.doPrivileged(
-            (PrivilegedAction<List<PartitionInfo>>) () -> consumer.partitionsFor(topic, Duration.ofMillis(timeoutMillis))
+            (PrivilegedAction<List<PartitionInfo>>) () -> consumer.partitionsFor(topic, Duration.ofMillis(config.getTopicMetadataFetchTimeoutMs()))
         );
         if (partitionInfos == null) {
             throw new IllegalArgumentException("Topic " + topic + " does not exist");
