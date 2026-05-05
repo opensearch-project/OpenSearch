8e4d1a24e6446760d8e819360795cee8c01862b4
Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
2026-05-05 08:35:11 +0530

    Add timestamp filter support for queries in analytics core (#21436)
    * Add timestamp filter support for queries in analytics core
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * Introduces RexNodeTransformer SPI so backends can declare RexNode transformations
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * pr comments
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * Changed RexNode to RexCall to be specific
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * removed caching of converter
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * Refactored after adding BackendPlanAdapter
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * Replaced SqlKind.OTHER to OTHER_FUNCTION
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * spotless fix
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * Added todo to support date_nanos
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * fix failing test
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    * spotless fix
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>
    
    ---------
    
    Signed-off-by: Vinay Krishna Pudyodu <vinkrish.neo@gmail.com>

--- sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/DataFusionAnalyticsBackendPlugin.java
+++ sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/DataFusionAnalyticsBackendPlugin.java
@@ -19,11 +19,13 @@ import org.opensearch.analytics.spi.FilterCapability;
 import org.opensearch.analytics.spi.FragmentConvertor;
 import org.opensearch.analytics.spi.ProjectCapability;
 import org.opensearch.analytics.spi.ScalarFunction;
+import org.opensearch.analytics.spi.ScalarFunctionAdapter;
 import org.opensearch.analytics.spi.ScanCapability;
 import org.opensearch.analytics.spi.SearchExecEngineProvider;
 import org.opensearch.index.engine.dataformat.DataFormatRegistry;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -135,6 +137,11 @@ public class DataFusionAnalyticsBackendPlugin implements AnalyticsSearchBackendP
                     }
                 }
                 return Set.copyOf(caps);
+            }
+
+            @Override
+            public Map<ScalarFunction, ScalarFunctionAdapter> scalarFunctionAdapters() {
+                return Map.of(ScalarFunction.TIMESTAMP, new TimestampFunctionAdapter());
             }
         };
     }
