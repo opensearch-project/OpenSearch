b007896d3d54a18040c8f3604a3b493a7fb09735
Radhakrishnan Pachyappan <gingeekrishna@gmail.com>
2026-05-27 02:20:52 +0530

    test(search): guard advanceExact return value in BitmapIndexQueryTests helper
    SortedNumericDocValues.advanceExact() returns false when the document
    has no doc values for the field; calling docValueCount()/nextValue()
    on a miss is an API violation. Guard the loop body with the return value,
    consistent with the Bitmap64IndexQueryTests helper.
    
    Signed-off-by: Radhakrishnan Pachyappan <gingeekrishna@gmail.com>

--- server/src/test/java/org/opensearch/search/query/BitmapIndexQueryTests.java
+++ server/src/test/java/org/opensearch/search/query/BitmapIndexQueryTests.java
@@ -108,9 +108,10 @@ public class BitmapIndexQueryTests extends OpenSearchTestCase {
             DocIdSetIterator disi = scorer.iterator();
             int docId;
             while ((docId = disi.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
-                dv.advanceExact(docId);
-                for (int count = 0; count < dv.docValueCount(); ++count) {
-                    actual.add((int) dv.nextValue());
+                if (dv.advanceExact(docId)) {
+                    for (int count = 0; count < dv.docValueCount(); ++count) {
+                        actual.add((int) dv.nextValue());
+                    }
                 }
             }
         }
