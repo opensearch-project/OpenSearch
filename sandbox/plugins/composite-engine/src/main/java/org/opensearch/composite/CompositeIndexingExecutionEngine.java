8de1c5b70dc6b1556b068be1050d014692961aa4
Arpit Bandejiya <abandeji@amazon.com>
2026-05-28 11:45:55 +0530

    Fix spotless
    Signed-off-by: Arpit Bandejiya <abandeji@amazon.com>

--- sandbox/plugins/composite-engine/src/main/java/org/opensearch/composite/CompositeIndexingExecutionEngine.java
+++ sandbox/plugins/composite-engine/src/main/java/org/opensearch/composite/CompositeIndexingExecutionEngine.java
@@ -42,13 +42,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.stream.Collectors;
 import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * A composite {@link IndexingExecutionEngine} that orchestrates indexing across
