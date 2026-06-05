1f045a03619361879c15fcf958ca2f53573871fd
Kamal Nayan <askkamal@amazon.com>
2026-06-05 19:03:30 +0530

    Added lucene temp file as excluded subdirectories list
    Signed-off-by: Kamal Nayan <askkamal@amazon.com>

--- server/src/main/java/org/opensearch/index/store/SubdirectoryAwareDirectory.java
+++ server/src/main/java/org/opensearch/index/store/SubdirectoryAwareDirectory.java
@@ -39,7 +39,7 @@ import java.util.stream.Collectors;
  */
 public class SubdirectoryAwareDirectory extends FilterDirectory {
     private static final Logger logger = LogManager.getLogger(SubdirectoryAwareDirectory.class);
-    private static final Set<String> EXCLUDED_SUBDIRECTORIES = Set.of("index/", "translog/", "_state/");
+    private static final Set<String> EXCLUDED_SUBDIRECTORIES = Set.of("index/", "translog/", "_state/", "lucene/");
     private final ShardPath shardPath;
 
     /**
