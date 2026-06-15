d848d7c7b18e7948451565e75d0b1bf437ce7ca7
Michael Froh <msfroh@apache.org>
2026-06-15 15:44:40 -0700

    Address comment from @zhaih
    Signed-off-by: Michael Froh <msfroh@apache.org>

--- server/src/test/java/org/opensearch/cluster/deployment/DeploymentStateServiceTests.java
+++ server/src/test/java/org/opensearch/cluster/deployment/DeploymentStateServiceTests.java
@@ -6,39 +6,37 @@
  * compatible open source license.
  */
 
-package org.opensearch.action.admin.cluster.deployment;
+package org.opensearch.cluster.deployment;
 
 import org.opensearch.cluster.ClusterName;
 import org.opensearch.cluster.ClusterState;
-import org.opensearch.cluster.deployment.Deployment;
-import org.opensearch.cluster.deployment.DeploymentManagerService;
-import org.opensearch.cluster.deployment.DeploymentState;
-import org.opensearch.cluster.deployment.DeploymentStateService;
 import org.opensearch.cluster.metadata.Metadata;
 import org.opensearch.test.OpenSearchTestCase;
 
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class TransportGetDeploymentActionTests extends OpenSearchTestCase {
+import static org.opensearch.cluster.deployment.DeploymentState.DRAIN;
+
+public class DeploymentStateServiceTests extends OpenSearchTestCase {
 
     private ClusterState emptyState() {
         return ClusterState.builder(new ClusterName("test")).metadata(Metadata.builder().build()).build();
     }
 
     public void testGetSpecificDeployment() {
-        ClusterState state = DeploymentManagerService.innerStartDeployment("dep-1", Map.of("zone", "zone-1"), emptyState());
+        ClusterState state = DeploymentManagerService.innerTransitionDeployment("dep-1", Map.of("zone", "zone-1"), emptyState(), DRAIN);
         AtomicReference<ClusterState> stateRef = new AtomicReference<>(state);
         DeploymentStateService service = new DeploymentStateService(stateRef::get);
 
         Deployment deployment = service.getDeployment("dep-1");
         assertNotNull(deployment);
-        assertEquals(DeploymentState.DRAIN, deployment.getState());
+        assertEquals(DRAIN, deployment.getState());
         assertEquals(Map.of("zone", "zone-1"), deployment.getNodeAttributes());
     }
 
     public void testGetNonexistentDeployment() {
-        ClusterState state = DeploymentManagerService.innerStartDeployment("dep-1", Map.of("zone", "zone-1"), emptyState());
+        ClusterState state = DeploymentManagerService.innerTransitionDeployment("dep-1", Map.of("zone", "zone-1"), emptyState(), DRAIN);
         AtomicReference<ClusterState> stateRef = new AtomicReference<>(state);
         DeploymentStateService service = new DeploymentStateService(stateRef::get);
 
@@ -48,8 +46,8 @@ public class TransportGetDeploymentActionTests extends OpenSearchTestCase {
 
     public void testGetAllDeployments() {
         ClusterState state = emptyState();
-        state = DeploymentManagerService.innerStartDeployment("dep-1", Map.of("zone", "zone-1"), state);
-        state = DeploymentManagerService.innerStartDeployment("dep-2", Map.of("zone", "zone-2"), state);
+        state = DeploymentManagerService.innerTransitionDeployment("dep-1", Map.of("zone", "zone-1"), state, DRAIN);
+        state = DeploymentManagerService.innerTransitionDeployment("dep-2", Map.of("zone", "zone-2"), state, DRAIN);
         AtomicReference<ClusterState> stateRef = new AtomicReference<>(state);
         DeploymentStateService service = new DeploymentStateService(stateRef::get);
 
