d0d798a75079c47ac2838c5ec840913f21f73194
Nils Bandener <nils.bandener@eliatra.com>
2026-07-06 22:26:45 +0200

    Restored WriterFileSet

--- server/src/test/java/org/opensearch/index/engine/exec/WriterFileSetTests.java
+++ server/src/test/java/org/opensearch/index/engine/exec/WriterFileSetTests.java
@@ -14,7 +14,6 @@ import org.opensearch.test.OpenSearchTestCase;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -49,26 +48,6 @@ public class WriterFileSetTests extends OpenSearchTestCase {
         assertEquals(original.writerGeneration(), deserialized.writerGeneration());
         assertEquals(original.files(), deserialized.files());
         assertEquals(original.numRows(), deserialized.numRows());
-        assertEquals(original.perFileMetadata(), deserialized.perFileMetadata());
-    }
-
-    public void testPerFileMetadataRoundTrip() throws Exception {
-        WriterFileSet original = new WriterFileSet(
-            "/tmp/original",
-            3L,
-            Set.of("_0.parquet"),
-            42L,
-            Map.of("_0.parquet", Map.of("opensearch.pme.encrypted", "true", "opensearch.pme.kms.instance_id", "kms-1"))
-        );
-
-        WriterFileSet deserialized = copyWriteable(
-            original,
-            new NamedWriteableRegistry(Collections.emptyList()),
-            in -> new WriterFileSet(in, "/tmp/other")
-        );
-
-        assertEquals(original.perFileMetadata(), deserialized.perFileMetadata());
-        assertEquals("true", deserialized.metadataForFile("_0.parquet").get("opensearch.pme.encrypted"));
     }
 
     public void testStreamRoundTripPreservesFormatVersion() throws Exception {
@@ -95,9 +74,6 @@ public class WriterFileSetTests extends OpenSearchTestCase {
         for (int i = 0; i < fileCount; i++) {
             files.add(randomAlphaOfLength(6) + "." + randomFrom("cfs", "si", "dat", "parquet"));
         }
-        Map<String, Map<String, String>> metadata = randomBoolean()
-            ? Map.of()
-            : Map.of(files.iterator().next(), Map.of("opensearch.pme.encrypted", "true"));
-        return new WriterFileSet(directory, randomNonNegativeLong(), files, randomIntBetween(0, 10000), 0L, metadata);
+        return new WriterFileSet(directory, randomNonNegativeLong(), files, randomIntBetween(0, 10000), 0L);
     }
 }
--- server/src/test/java/org/opensearch/index/engine/exec/coord/DataformatAwareCatalogSnapshotTests.java
+++ server/src/test/java/org/opensearch/index/engine/exec/coord/DataformatAwareCatalogSnapshotTests.java
@@ -458,7 +458,7 @@ public class DataformatAwareCatalogSnapshotTests extends OpenSearchTestCase {
         for (int i = 0; i < fileCount; i++) {
             files.add(randomAlphaOfLength(6) + "." + randomFrom(extensions));
         }
-        return new WriterFileSet(directory, writerGeneration, files, randomIntBetween(0, 10000), 0L, randomPerFileMetadata(files));
+        return new WriterFileSet(directory, writerGeneration, files, randomIntBetween(0, 10000), 0L);
     }
 
     private Segment randomSegment() {
@@ -540,15 +540,7 @@ public class DataformatAwareCatalogSnapshotTests extends OpenSearchTestCase {
         for (int i = 0; i < fileCount; i++) {
             files.add(randomAlphaOfLength(6) + "." + randomFrom(extensions));
         }
-        return new WriterFileSet(directory, writerGeneration, files, randomIntBetween(0, 10000), 0L, randomPerFileMetadata(files));
-    }
-
-    private Map<String, Map<String, String>> randomPerFileMetadata(Set<String> files) {
-        if (randomBoolean()) {
-            return Map.of();
-        }
-        String file = randomFrom(files.toArray(new String[0]));
-        return Map.of(file, Map.of("opensearch.pme.encrypted", "true", "opensearch.pme.kms.instance_id", randomAlphaOfLength(5)));
+        return new WriterFileSet(directory, writerGeneration, files, randomIntBetween(0, 10000), 0L);
     }
 
     private void assertSnapshotFieldsEqual(String context, DataformatAwareCatalogSnapshot expected, DataformatAwareCatalogSnapshot actual) {
@@ -587,7 +579,6 @@ public class DataformatAwareCatalogSnapshotTests extends OpenSearchTestCase {
                 assertEquals(context + ": writerGeneration", expectedWfs.writerGeneration(), actualWfs.writerGeneration());
                 assertEquals(context + ": files", expectedWfs.files(), actualWfs.files());
                 assertEquals(context + ": numRows", expectedWfs.numRows(), actualWfs.numRows());
-                assertEquals(context + ": perFileMetadata", expectedWfs.perFileMetadata(), actualWfs.perFileMetadata());
             }
         }
         assertEquals(context + ": lastWriterGeneration", expected.getLastWriterGeneration(), actual.getLastWriterGeneration());
