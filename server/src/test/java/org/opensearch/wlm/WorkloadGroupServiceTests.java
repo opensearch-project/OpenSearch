7d363acfba08b2b848ed34caf74a6d81f3a9e001
SaiManas2106 <saimanas2004@gmail.com>
2026-08-05 19:40:55 +0200

    Avoid rejections in monitor mode
    Signed-off-by: SaiManas2106 <saimanas2004@gmail.com>

--- server/src/test/java/org/opensearch/wlm/WorkloadGroupServiceTests.java
+++ server/src/test/java/org/opensearch/wlm/WorkloadGroupServiceTests.java
@@ -268,6 +268,39 @@ public class WorkloadGroupServiceTests extends OpenSearchTestCase {
         verify(spyState, never()).getResourceState();
     }
 
+    public void testRejectIfNeeded_whenWorkloadGroupIsMonitorMode() {
+        Set<WorkloadGroup> activeWorkloadGroups = getActiveWorkloadGroups(
+            "testWorkloadGroup",
+            WORKLOAD_GROUP_ID,
+            MutableWorkloadGroupFragment.ResiliencyMode.MONITOR,
+            Map.of(ResourceType.CPU, 0.10)
+        );
+        mockWorkloadGroupStateMap = new HashMap<>();
+        WorkloadGroupState workloadGroupState = new WorkloadGroupState();
+        workloadGroupState.getResourceState().get(ResourceType.CPU).setLastRecordedUsage(0.18);
+        mockWorkloadGroupStateMap.put(WORKLOAD_GROUP_ID, workloadGroupState);
+        mockWorkloadGroupsStateAccessor = new WorkloadGroupsStateAccessor(mockWorkloadGroupStateMap);
+
+        workloadGroupService = new WorkloadGroupService(
+            mockCancellationService,
+            mockClusterService,
+            mockThreadPool,
+            mockWorkloadManagementSettings,
+            mockNodeDuressTrackers,
+            mockWorkloadGroupsStateAccessor,
+            activeWorkloadGroups,
+            new HashSet<>()
+        );
+        when(mockWorkloadManagementSettings.getWlmMode()).thenReturn(WlmMode.ENABLED);
+        when(mockWorkloadManagementSettings.getNodeLevelCpuRejectionThreshold()).thenReturn(0.8);
+        when(mockNodeDuressTrackers.isNodeInDuress()).thenReturn(true);
+
+        workloadGroupService.rejectIfNeeded(WORKLOAD_GROUP_ID);
+
+        assertEquals(0, workloadGroupState.getResourceState().get(ResourceType.CPU).rejections.count());
+        assertEquals(0, workloadGroupState.totalRejections.count());
+    }
+
     public void testRejectIfNeeded_whenWorkloadGroupIsEnforcedMode_andNotBreaching() {
         WorkloadGroup testWorkloadGroup = getWorkloadGroup(
             "testWorkloadGroup",
