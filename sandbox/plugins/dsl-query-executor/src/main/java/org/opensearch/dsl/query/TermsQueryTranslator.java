1551f4c4cfa3d221cfaffb0e3cd6a436054c2b4d
Kamal <askkamal@amazon.com>
2026-08-10 05:35:48 +0000

    Remove _id rejection guards from term and terms translators
    Signed-off-by: Kamal <askkamal@amazon.com>

--- sandbox/plugins/dsl-query-executor/src/main/java/org/opensearch/dsl/query/TermsQueryTranslator.java
+++ sandbox/plugins/dsl-query-executor/src/main/java/org/opensearch/dsl/query/TermsQueryTranslator.java
@@ -13,7 +13,6 @@ import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.rex.RexNode;
 import org.opensearch.dsl.converter.ConversionContext;
 import org.opensearch.dsl.converter.ConversionException;
-import org.opensearch.index.mapper.IdFieldMapper;
 import org.opensearch.index.query.AbstractQueryBuilder;
 import org.opensearch.index.query.QueryBuilder;
 import org.opensearch.index.query.TermsQueryBuilder;
@@ -56,10 +55,6 @@ public class TermsQueryTranslator implements QueryTranslator {
         }
 
         String fieldName = termsQuery.fieldName();
-        // Replaces a generic field-not-found error with an actionable one naming the ids query.
-        if (IdFieldMapper.NAME.equals(fieldName)) {
-            throw new ConversionException("Terms query on _id is not supported; use the ids query instead");
-        }
         List<?> values = termsQuery.values();
 
         if (values == null || values.isEmpty()) {
