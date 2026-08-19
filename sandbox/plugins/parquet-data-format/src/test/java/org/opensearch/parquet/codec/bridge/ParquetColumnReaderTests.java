a78809f8de7c9c7c6180749a9312d2cb20bc38d2
Manas Lohani <manloh@amazon.com>
2026-08-19 10:46:41 +0530

    Move Parquet read bridge to the codec package
    The forward-only column cursor is not doc-values specific: later codec parts
    (binary/keyword columns, a doc-values skipper) read through the same native
    surface. Move it out of the doc-values namespace so those can share it.
    
      org.opensearch.parquet.docvaluescodec.bridge -> org.opensearch.parquet.codec.bridge
      ParquetDocValuesBridge                       -> ParquetCodecBridge
    
    No behaviour change.
    
    Signed-off-by: Manas Lohani <manloh@amazon.com>

--- sandbox/plugins/parquet-data-format/src/test/java/org/opensearch/parquet/codec/bridge/ParquetColumnReaderTests.java
+++ sandbox/plugins/parquet-data-format/src/test/java/org/opensearch/parquet/codec/bridge/ParquetColumnReaderTests.java
@@ -6,7 +6,7 @@
  * compatible open source license.
  */
 
-package org.opensearch.parquet.docvaluescodec.bridge;
+package org.opensearch.parquet.codec.bridge;
 
 import org.apache.arrow.c.ArrowArray;
 import org.apache.arrow.c.ArrowSchema;
